cd0842fa651bf42a9fe044cbb3e6aa69321c54f4
weqqr <slivnoychuvak@yandex.ru>
2016-04-09 21:38:09 +0300

    Refactor lord_names_rules

--- mods/lord_names_rules/init.lua
+++ mods/lord_names_rules/init.lua
@@ -1,56 +1,67 @@
 local SL = rawget(_G, "intllib") and intllib.Getter() or function(s) return s end
 
-local names_rules = { -- правила формирования имени
-	{	rule = function(S)
-			return #S > 2
+-- правила формирования имени
+local rules = {
+	{
+		match = function(name)
+			return #name > 2
 		end,
-		description = SL("your name must contain more than 2 symbols;"),
+		description = SL("Your name must contain more than 2 symbols;"),
 	},
-	{	rule = function(S)
-			return #S < 14
+	{
+		match = function(name)
+			return #name < 14
 		end,
-		description = SL("your name must be less than 14 symbols;"),
+		description = SL("Your name must be less than 14 symbols;"),
 	},
-	{	rule = function(S)
-			return not string.match(S, '[^%a_-]')
+	{
+		match = function(name)
+			return not string.match(name, '[^%a_-]')
 		end,
-		description = SL("your name must contain only letters, also allows '-' and '_';"),
+		description = SL("Your name must contain letters only; '-' and '_' are also allowed;"),
 	},
-	{	rule = function(S)
-			return string.match(S, "^[A-Z]")
+	{
+		match = function(name)
+			return string.match(name, "^[A-Z]")
 		end,
-		description = SL("your name must begin with capital letters;"),
+		description = SL("Your name must begin with capital letters;"),
 	},
 }
 
-local function check_name_player(name) -- возвращает true, если name удовлетворяет всем условиям, и false в противном случае
-	local S = name -- имя игрока..
-	for _, i in pairs(names_rules) do
-		if not i.rule(S) then return false end
+local function is_correct_name(name) -- возвращает true, если name удовлетворяет всем условиям, и false в противном случае
+	for _, rule in pairs(rules) do
+		if not rule.match(name) then
+			return false
+		end
 	end
+
 	return true
 end
 
 local function registered_player(name) -- если игрок уже зарегистрирован, возвращает true
-	local file = minetest.get_worldpath().."/players/"..name
-	if os.rename(file, file) then return true else return false end
+	local file = minetest.get_worldpath() .. "/players/" .. name
+	return os.rename(file, file)
 end
 
-minetest.register_on_prejoinplayer(function(name, ip) -- попытка клиента присоединиться к серверу
+minetest.register_on_prejoinplayer(function(name, ip)
 	local list_rules = ""
-	for _, i in pairs(names_rules) do
-		list_rules = list_rules.."\n- "..i.description
+	for _, rule in pairs(rules) do
+		list_rules = list_rules .. "\n- " .. rule.description
 	end
-	if (not check_name_player(name))and(not registered_player(name)) then -- ошибка в имени и игрок ещё не зарегистрирован
+	if not minetest.is_singleplayer() and    -- не синглплеер
+			not is_correct_name(name) and    -- имя некорректное
+			not registered_player(name) then -- игрок не зарегистрирован
 		return "\n"..SL("Invalid character name")..list_rules
 	end
 end)
 
-minetest.register_on_joinplayer(function(player) -- присоединение клиента к серверу
+minetest.register_on_joinplayer(function(player)
 	local name = player:get_player_name()
-	if not check_name_player(name) then -- ошибка в имени
+	if not minetest.is_singleplayer() and not is_correct_name(name) then -- ошибка в имени
 		minetest.chat_send_player(name, SL("Invalid character name. Contact to the administration."))
 	end
 end)
 
-if minetest.setting_getbool("msg_loading_mods") then minetest.log("action", minetest.get_current_modname().." mod LOADED") end
+if minetest.setting_getbool("msg_loading_mods") then
+	minetest.log("action", minetest.get_current_modname().." mod LOADED")
+end
