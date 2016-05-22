b383bdfdd92cc5b0f44e380e3135b4c28354749e
narrnika <narr13niki@gmail.com>
2016-05-22 18:04:04 +0400

    add listring

--- mods/castle/init.lua
+++ mods/castle/init.lua
@@ -85,6 +85,8 @@ function default.get_ironbound_chest_formspec(pos)
 		"size[8,9]"..
 		"list[nodemeta:".. spos .. ";main;0,0;8,4;]"..
 		"list[current_player;main;0,5;8,4;]"..
+		"listring[nodemeta:".. spos .. ";main]"..
+		"listring[current_player;main]"..
 		"background[-0.5,-0.65;9,10.35;gui_chestbg.png]"..
 		"listcolors[#606060AA;#888;#141318;#30434C;#FFF]"
 	return formspec
