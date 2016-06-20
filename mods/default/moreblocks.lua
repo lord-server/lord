ebaa3727dd05a856cb94914e3b14b85aa4380a5c
Badger <badger2house1@gmail.com>
2016-06-20 11:13:04 +0400

    Сюда будем добавлять новые узлы

---
+++ mods/default/moreblocks.lua
@@ -0,0 +1,15 @@
+local SL = rawget(_G, "intllib") and intllib.Getter() or function(s) return s end
+
+minetest.register_node("default:blackout", {
+	description = SL("Blackout"), 
+	tiles = {"default_blackout.png"},
+	drawtype = "glasslike",
+	paramtype = "light",
+	sunlight_propagates = true,
+	is_ground_content = false,
+    walkable = false,
+    buildable_to = true,
+    pointable = false,
+	groups = {not_in_creative_inventory=1,dig_immediate=3},
+	--post_effect_color = {a = 10, r = 0, g = 0, b = 0},
+})
