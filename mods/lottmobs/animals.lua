592c830ba95e6165636c68372d6f3f3b16d72d39
Doloment <sawerter@inbox.ru>
2017-06-22 19:25:45 +0700

    Добавил выпадение яйца. Переместил яйцо. Нужен тест.

--- mods/lottmobs/animals.lua
+++ mods/lottmobs/animals.lua
@@ -1,5 +1,139 @@
 local SL = lord.require_intllib()
 
+mobs:register_arrow("mobs_animal:egg_entity", {
+	visual = "sprite",
+	visual_size = {x=.5, y=.5},
+	textures = {"mobs_chicken_egg.png"},
+	velocity = 6,
+
+	hit_player = function(self, player)
+		player:punch(minetest.get_player_by_name(self.playername) or self.object, 1.0, {
+			full_punch_interval = 1.0,
+			damage_groups = {fleshy = 1},
+		}, nil)
+	end,
+
+	hit_mob = function(self, player)
+		player:punch(minetest.get_player_by_name(self.playername) or self.object, 1.0, {
+			full_punch_interval = 1.0,
+			damage_groups = {fleshy = 1},
+		}, nil)
+	end,
+
+	hit_node = function(self, pos, node)
+
+		if math.random(1, 10) > 1 then
+			return
+		end
+
+		pos.y = pos.y + 1
+
+		local nod = minetest.get_node_or_nil(pos)
+
+		if not nod
+		or not minetest.registered_nodes[nod.name]
+		or minetest.registered_nodes[nod.name].walkable == true then
+			return
+		end
+
+		local mob = minetest.add_entity(pos, "lottmobs:chicken")
+		local ent2 = mob:get_luaentity()
+
+		mob:set_properties({
+			textures = ent2.child_texture[1],
+			visual_size = {
+				x = ent2.base_size.x / 2,
+				y = ent2.base_size.y / 2
+			},
+			collisionbox = {
+				ent2.base_colbox[1] / 2,
+				ent2.base_colbox[2] / 2,
+				ent2.base_colbox[3] / 2,
+				ent2.base_colbox[4] / 2,
+				ent2.base_colbox[5] / 2,
+				ent2.base_colbox[6] / 2
+			},
+		})
+
+		ent2.child = true
+		ent2.tamed = true
+		ent2.owner = self.playername
+	end
+})
+
+
+-- egg throwing item
+
+local egg_GRAVITY = 9
+local egg_VELOCITY = 19
+
+local mobs_shoot_egg = function (item, player, pointed_thing)
+
+	local playerpos = player:getpos()
+
+	minetest.sound_play("default_place_node_hard", {
+		pos = playerpos,
+		gain = 1.0,
+		max_hear_distance = 5,
+	})
+
+	local obj = minetest.add_entity({
+		x = playerpos.x,
+		y = playerpos.y +1.5,
+		z = playerpos.z
+	}, "mobs_animal:egg_entity")
+
+	local ent = obj:get_luaentity()
+	local dir = player:get_look_dir()
+
+	ent.velocity = egg_VELOCITY -- needed for api internal timing
+	ent.switch = 1 -- needed so that egg doesn't despawn straight away
+
+	obj:setvelocity({
+		x = dir.x * egg_VELOCITY,
+		y = dir.y * egg_VELOCITY,
+		z = dir.z * egg_VELOCITY
+	})
+
+	obj:setacceleration({
+		x = dir.x * -3,
+		y = -egg_GRAVITY,
+		z = dir.z * -3
+	})
+
+	-- pass player name to egg for chick ownership
+	local ent2 = obj:get_luaentity()
+	ent2.playername = player:get_player_name()
+
+	item:take_item()
+
+	return item
+end
+
+minetest.register_node("lottmobs:egg", {
+	description = S("Chicken Egg"),
+	tiles = {"lottmobs_egg.png"},
+	inventory_image  = "lottmobs_egg.png",
+	visual_scale = 0.7,
+	drawtype = "plantlike",
+	wield_image = "lottmobs_egg.png",
+	paramtype = "light",
+	walkable = false,
+	is_ground_content = true,
+	sunlight_propagates = true,
+	selection_box = {
+		type = "fixed",
+		fixed = {-0.2, -0.5, -0.2, 0.2, 0, 0.2}
+	},
+	groups = {snappy = 2, dig_immediate = 3},
+	after_place_node = function(pos, placer, itemstack)
+		if placer:is_player() then
+			minetest.set_node(pos, {name = "lottmobs:egg", param2 = 1})
+		end
+	end,
+	on_use = mobs_shoot_egg
+})
+
 mobs:register_mob("lottmobs:chicken", {
 	type = "animal",
 	hp_min = 5,
