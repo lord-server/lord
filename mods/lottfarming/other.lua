b76101e92111270a31b2cff752b05f6818d19159
Doloment <sawerter@inbox.ru>
2017-08-29 20:58:13 +0700

    Update other.lua

--- mods/lottfarming/other.lua
+++ mods/lottfarming/other.lua
@@ -53,7 +53,7 @@ minetest.register_craftitem("lottfarming:salted_dough", {
 minetest.register_craft({
 	type = "shapeless",
 	output = "lottfarming:dough_with_egg",
-	recipe = {"lottmobs:chicken_egg", "lottfarming:dough"},
+	recipe = {"lottmobs:egg", "lottfarming:dough"},
 })
 
 minetest.register_craftitem("lottfarming:dough_with_egg", {
