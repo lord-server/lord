bcb5d6b21f73ae36af1787766b9632c271400d3d
Mikola314 <mikola.plotnik@bk.ru>
2017-09-14 21:45:18 +0300

    bucket:bucket_empty stack_max = 99

--- mods/bucket/init.lua
+++ mods/bucket/init.lua
@@ -130,7 +130,6 @@ end
 minetest.register_craftitem("bucket:bucket_empty", {
 	description = SL("Empty Bucket"),
 	inventory_image = "bucket.png",
-	stack_max = 1,
 	liquids_pointable = true,
 	on_use = function(itemstack, user, pointed_thing)
 		-- Must be pointing to node
