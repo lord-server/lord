42a0d7c262fe76b644793e34dd826faa02d5528f
Doloment <sawerter@inbox.ru>
2017-09-17 11:31:52 +0700

    Вспомнил про ,

--- mods/lottfarming/cake.lua
+++ mods/lottfarming/cake.lua
@@ -48,7 +48,7 @@ for i, size in ipairs(sizes) do
 		is_ground_content = false,
 		groups = {crumbly=3},
 		--sounds = sounds,
-		stack_max = 1
+		stack_max = 1,
 		node_box = {
 			type = "fixed",
 			fixed = {
@@ -75,7 +75,7 @@ for i, size in ipairs(sizes) do
 		is_ground_content = false,
 		groups = {crumbly=3},
 		--sounds = sounds,
-		stack_max = 1
+		stack_max = 1,
 		node_box = {
 			type = "fixed",
 			fixed = {
