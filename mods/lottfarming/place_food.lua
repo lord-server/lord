c9accc1e45caf6215e73447209bf0292ce9903be
Doloment <sawerter@inbox.ru>
2017-10-10 23:57:40 +0700

    Мелонтест дубль 2

--- mods/lottfarming/place_food.lua
+++ mods/lottfarming/place_food.lua
@@ -1,5 +1,7 @@
 local SL = lord.require_intllib()
 
+local cube_sizes = {-1, -0.5, -0, 0.5}
+
 local sizes = {-0.4375, -0.3125, -0.1875, -0.0625, 0.0625, 0.1875, 0.3125}
 
 for i, size in ipairs(sizes) do
@@ -118,3 +120,63 @@ for i, size in ipairs(sizes) do
 		}
 	})
 end
+
+for i, size in ipairs(cube_sizes) do
+	local slice_mel = i - 1
+	local slice_b = i - 1
+	local name_mel
+	local name_cabb
+	local description_mel
+	local description_cabb
+	local drop_mel
+	local drop_cabb
+	local tiles_mel
+	local tiles_cabb
+
+	if slice_mel == 0 then
+		name_mel = "lottfarming:melon_3"
+		description_mel = SL("Melon")
+		drop_mel = "lottfarming:melon"
+		tiles_mel = {"lottfarming_cake_top.png", "lottfarming_cake_bottom.png", "lottfarming_hcake_side.png"}
+	else
+		name_mel = "lottfarming:melon_3"..slice_mel
+		drop_mel = "lottfarming:melon_3"..slice_mel
+		tiles = {"lottfarming_melon_top.png", "lottfarming_melon_top.png", "lottfarming_melon_side.png", "lottfarming_melon_inner.png", "lottfarming_melon_side.png", "lottfarming_melon_side.png"}
+	end
+
+	if slice_cabb == 0 then
+		name_cabb = "lottfarming:cabbage"
+		description_cabb = SL("Cabbage")
+		drop_cabb = "lottfarming:cabbage"
+		tiles_cabb = {"lottfarming_cake_top.png", "lottfarming_cake_bottom.png", "lottfarming_bcake_side.png"}
+	else
+		name_cabb = "lottfarming:cabbage_"..slice_cabb
+		drop_cabb = "lottfarming:cabbage_"..slice_cabb
+		tiles_cabb = tiles = {"lottfarming_cabbage_top.png", "lottfarming_cabbage_top.png", "lottfarming_cabbage_side.png", "lottfarming_cabbage_inner.png", "lottfarming_cabbage_side.png", "lottfarming_cabbage_side.png"}
+
+	end
+	
+	minetest.register_node(name_mel, {
+		description = description_mel,
+		drop = drop_mel,
+		drawtype = "nodebox",
+		tiles = tiles_mel,
+		paramtype = "light",
+		is_ground_content = false,
+		groups = {crumbly=3},
+		--sounds = sounds,
+		stack_max = 64,
+		on_rightclick = function(pos, node, clicker, player)
+			if minetest.is_protected(pos, player:get_player_name())
+				else
+					clicker:set_hp(clicker:get_hp() + 1)
+			
+					if i < #cube_sizes then
+						minetest.swap_node(pos, {name="lottfarming:melon_3_"..i})
+					else
+						minetest.remove_node(pos)
+					end
+				end,
+			end,
+		end
+	})
