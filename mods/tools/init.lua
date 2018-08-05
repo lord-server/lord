237d0240cd7ed89a29a79f6b88adc14f3b8e6968
Doloment <sawerter@inbox.ru>
2018-08-05 20:06:34 +0700

    Update init.lua

--- mods/tools/init.lua
+++ mods/tools/init.lua
@@ -29,7 +29,7 @@ tools.sources = {
 minetest.register_item(":", {
 	type = "none",
 	wield_image = "wieldhand.png",
-	wield_scale = {x=1,y=1,z=2.5},
+	wield_scale = {x=1,y=1,z=3.5},
 	tool_capabilities = {
 		full_punch_interval = 0.9,
 		max_drop_level = 0,
