e0d542e1c5fc8b2b3ea8b2bba73be2eab06f2e79
Mikola314 <mikola.plotnik@bk.ru>
2018-11-18 16:20:39 +0300

    Fixed air fields of stalactites #84

--- mods/icicles/init.lua
+++ mods/icicles/init.lua
@@ -62,7 +62,7 @@ local function generate(minp, maxp, seed, chunks_per_volume, icicles_per_chunk, 
 	local inverse_chance = math.floor(chunk_size*chunk_size*chunk_size / icicles_per_chunk)
 	for i=1,num_chunks do
 		local y0 = pr:next(y_min, y_max-chunk_size+1)
-		if y0 >= height_min and y0 <= height_max then
+		if y0 >= height_min and y0 <= height_max and (y0+2)%16 ~= 0 then
 			local x0 = pr:next(minp.x, maxp.x-chunk_size+1)
 			local z0 = pr:next(minp.z, maxp.z-chunk_size+1)
 			local p0 = {x=x0, y=y0, z=z0}
