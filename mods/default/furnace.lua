c9f5de0a5dcefe4bf5760ecec102c09e03678dbd
Alexander Chibrikin <alek13.me@gmail.com>
2018-12-11 16:27:22 +0300

    just update default/furnace.lua from LOTT repo. Fixes #120

--- mods/default/furnace.lua
+++ mods/default/furnace.lua
@@ -5,46 +5,46 @@ local SL = lord.require_intllib()
 
 function default.get_furnace_active_formspec(fuel_percent, item_percent)
 	return "size[8,8.5]"..
-		default.gui_bg..
-		default.gui_bg_img..
-		default.gui_slots..
-		"list[context;src;2.75,0.5;1,1;]"..
-		"list[context;fuel;2.75,2.5;1,1;]"..
-		"image[2.75,1.5;1,1;default_furnace_fire_bg.png^[lowpart:"..
-		(100-fuel_percent)..":default_furnace_fire_fg.png]"..
-		"image[3.75,1.5;1,1;gui_furnace_arrow_bg.png^[lowpart:"..
-		(item_percent)..":gui_furnace_arrow_fg.png^[transformR270]"..
-		"list[context;dst;4.75,0.96;2,2;]"..
-		"list[current_player;main;0,4.25;8,1;]"..
-		"list[current_player;main;0,5.5;8,3;8]"..
-		"listring[context;dst]"..
-		"listring[current_player;main]"..
-		"listring[context;src]"..
-		"listring[current_player;main]"..
-		"listring[context;fuel]"..
-		"listring[current_player;main]"..
-		"background[-0.5,-0.65;9,10.35;gui_furnacebg.png]"
+			default.gui_bg..
+			default.gui_bg_img..
+			default.gui_slots..
+			"list[context;src;2.75,0.5;1,1;]"..
+			"list[context;fuel;2.75,2.5;1,1;]"..
+			"image[2.75,1.5;1,1;default_furnace_fire_bg.png^[lowpart:"..
+			(100-fuel_percent)..":default_furnace_fire_fg.png]"..
+			"image[3.75,1.5;1,1;gui_furnace_arrow_bg.png^[lowpart:"..
+			(item_percent)..":gui_furnace_arrow_fg.png^[transformR270]"..
+			"list[context;dst;4.75,0.96;2,2;]"..
+			"list[current_player;main;0,4.25;8,1;]"..
+			"list[current_player;main;0,5.5;8,3;8]"..
+			"listring[context;dst]"..
+			"listring[current_player;main]"..
+			"listring[context;src]"..
+			"listring[current_player;main]"..
+			"listring[context;fuel]"..
+			"listring[current_player;main]"..
+			"background[-0.5,-0.65;9,10.35;gui_furnacebg.png]"
 end
 
 function default.get_furnace_inactive_formspec()
 	return "size[8,8.5]"..
-		default.gui_bg..
-		default.gui_bg_img..
-		default.gui_slots..
-		"list[context;src;2.75,0.5;1,1;]"..
-		"list[context;fuel;2.75,2.5;1,1;]"..
-		"image[2.75,1.5;1,1;default_furnace_fire_bg.png]"..
-		"image[3.75,1.5;1,1;gui_furnace_arrow_bg.png^[transformR270]"..
-		"list[context;dst;4.75,0.96;2,2;]"..
-		"list[current_player;main;0,4.25;8,1;]"..
-		"list[current_player;main;0,5.5;8,3;8]"..
-		"listring[context;dst]"..
-		"listring[current_player;main]"..
-		"listring[context;src]"..
-		"listring[current_player;main]"..
-		"listring[context;fuel]"..
-		"listring[current_player;main]"..
-		"background[-0.5,-0.65;9,10.35;gui_furnacebg.png]"
+			default.gui_bg..
+			default.gui_bg_img..
+			default.gui_slots..
+			"list[context;src;2.75,0.5;1,1;]"..
+			"list[context;fuel;2.75,2.5;1,1;]"..
+			"image[2.75,1.5;1,1;default_furnace_fire_bg.png]"..
+			"image[3.75,1.5;1,1;gui_furnace_arrow_bg.png^[transformR270]"..
+			"list[context;dst;4.75,0.96;2,2;]"..
+			"list[current_player;main;0,4.25;8,1;]"..
+			"list[current_player;main;0,5.5;8,3;8]"..
+			"listring[context;dst]"..
+			"listring[current_player;main]"..
+			"listring[context;src]"..
+			"listring[current_player;main]"..
+			"listring[context;fuel]"..
+			"listring[current_player;main]"..
+			"background[-0.5,-0.65;9,10.35;gui_furnacebg.png]"
 end
 
 --
@@ -118,7 +118,7 @@ local function furnace_node_timer(pos, elapsed)
 	local fuel
 
 	local update = true
-	while update do
+	while elapsed > 0 and update do
 		update = false
 
 		srclist = inv:get_list("src")
@@ -133,13 +133,18 @@ local function furnace_node_timer(pos, elapsed)
 		cooked, aftercooked = minetest.get_craft_result({method = "cooking", width = 1, items = srclist})
 		cookable = cooked.time ~= 0
 
+		local el = math.min(elapsed, fuel_totaltime - fuel_time)
+		if cookable then -- fuel lasts long enough, adjust el to cooking duration
+			el = math.min(el, cooked.time - src_time)
+		end
+
 		-- Check if we have enough fuel to burn
 		if fuel_time < fuel_totaltime then
 			-- The furnace is currently active and has enough fuel
-			fuel_time = fuel_time + elapsed
+			fuel_time = fuel_time + el
 			-- If there is a cookable item then check if it is ready yet
 			if cookable then
-				src_time = src_time + elapsed
+				src_time = src_time + el
 				if src_time >= cooked.time then
 					-- Place result in dst list if possible
 					if inv:room_for_item("dst", cooked.item) then
@@ -148,6 +153,9 @@ local function furnace_node_timer(pos, elapsed)
 						src_time = src_time - cooked.time
 						update = true
 					end
+				else
+					-- Item could not be cooked: probably missing fuel
+					update = true
 				end
 			end
 		else
@@ -165,8 +173,7 @@ local function furnace_node_timer(pos, elapsed)
 					-- Take fuel from fuel list
 					inv:set_stack("fuel", 1, afterfuel.items[1])
 					update = true
-					fuel_totaltime = fuel.time + (fuel_time - fuel_totaltime)
-					src_time = src_time + elapsed
+					fuel_totaltime = fuel.time + (fuel_totaltime - fuel_time)
 				end
 			else
 				-- We don't need to get new fuel since there is no cookable item
@@ -176,7 +183,7 @@ local function furnace_node_timer(pos, elapsed)
 			fuel_time = 0
 		end
 
-		elapsed = 0
+		elapsed = elapsed - el
 	end
 
 	if fuel and fuel_totaltime > fuel.time then
