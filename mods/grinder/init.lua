788d83ecc352fad9aa7e56561b5d079f0e5caab0
Alexander Chibrikin <alek13.me@gmail.com>
2018-12-13 16:23:46 +0300

    bring out grinder processing (abm.action)

--- mods/grinder/init.lua
+++ mods/grinder/init.lua
@@ -410,98 +410,13 @@ function grinder.swap_node(pos,name)
 	return node.name
 end
 
+
+local Processor = dofile(minetest.get_modpath(minetest.get_current_modname()).."/processor.lua")
 minetest.register_abm({
 	nodenames = {"grinder:grinder", "grinder:grinder_active"},
 	interval = 1,
 	chance = 1,
-	action = function(pos, node, active_object_count, active_object_count_wider)
-		local meta = minetest.get_meta(pos)
-		local inv  = meta:get_inventory()
-
-		local recipe = nil
-		for i, name in pairs({
-				"fuel_totaltime",
-				"fuel_time",
-				"src_totaltime",
-				"src_time"}) do
-			if not meta:get_float(name) then
-				meta:set_float(name, 0.0)
-			else
-				--print(name.." не обнулено для ")
-			end
-		end
-		--print("fuel_time="..meta:get_float("fuel_time"))
-		--print("fuel_totaltime="..meta:get_float("fuel_totaltime"))
-		--print("-------------------------------------")
-		local result = grinder.get_grinding_recipe("grinding", inv:get_list("src"))
-		local was_active = false
-		--print("fuel_time="..meta:get_float("fuel_time"))
-		--print("fuel_totaltime="..meta:get_float("fuel_totaltime"))
-		if meta:get_float("fuel_time") < meta:get_float("fuel_totaltime") then
-			was_active = true
-			--print("Дробилка активна")
-			meta:set_int("fuel_time", meta:get_int("fuel_time") + 1)
-			if result then
-				meta:set_int("src_time", meta:get_int("src_time") + 1)
-				--print("Name="..result.new_input:get_name())
-				--print(dump(result))
-				if meta:get_int("src_time") >= result.time then
-					meta:set_int("src_time", 0)
-					local result_stack = ItemStack(result.output)
-					if inv:room_for_item("dst", result_stack) then
-						inv:set_stack("src",1, result.new_input)
-						inv:add_item("dst", result_stack)
-					end
-				end
-			else
-				meta:set_int("src_time", 0)
-			end
-		end
-
-		local cooked = 10
-		if result then
-			cooked = result.time or 10
-		end
-		if meta:get_float("fuel_time") < meta:get_float("fuel_totaltime") then
-			local percent = math.floor(meta:get_float("fuel_time") / meta:get_float("fuel_totaltime") * 100)
-			local item_percent = math.floor(meta:get_float("src_time") / cooked * 100)
-			meta:set_string("infotext", SL(("%s Grinding"):format(machine_name)).." ("..percent.."%)")
-			grinder.swap_node(pos, "grinder:grinder_active")
-			meta:set_string("formspec",grinder.get_grinder_active_formspec(pos, percent, item_percent))
-			return
-		end
-
-		recipe = grinder.get_grinding_recipe("grinding", inv:get_list("src"))
-		if not recipe then
-			if was_active then
-				meta:set_string("infotext", SL(("%s is empty"):format(machine_name)))
-				grinder.swap_node(pos, "grinder:grinder")
-				meta:set_string("formspec", grinder.grinder_inactive_formspec)
-			end
-			--print("Выход, поскольку нет рецепта")
-			return
-		end
-
-		local fuel = nil
-		local afterfuel
-		local fuellist = inv:get_list("fuel")
-
-		if fuellist then
-			fuel, afterfuel = minetest.get_craft_result({method = "fuel", width = 1, items = fuellist})
-		end
-
-		if fuel.time <= 0 then
-			meta:set_string("infotext", SL(("%s Out Of Heat"):format(machine_name)))
-			grinder.swap_node(pos, "grinder:grinder")
-			meta:set_string("formspec", grinder.grinder_inactive_formspec)
-			return
-		end
-
-		meta:set_string("fuel_totaltime", fuel.time)
-		meta:set_string("fuel_time", 0)
-
-		inv:set_stack("fuel", 1, afterfuel.items[1])
-	end,
+	action = Processor.act,
 })
 
 minetest.register_craftitem("grinder:coal_dust", {
