968250d0a225b7547232e288816bd26be5eca184
Alexander Chibrikin <alek13.me@gmail.com>
2018-12-17 01:49:38 +0300

    extract Grinder:activate() & Grinder:deactivate() methods; so move swapNode() into Grinder

--- mods/grinder/grinder.lua
+++ mods/grinder/grinder.lua
@@ -1,3 +1,7 @@
+local SL = lord.require_intllib()
+
+--- @type string
+local machine_name = "Grinder"
 
 ---
 --- @class Grinder
@@ -28,7 +32,7 @@ end
 --- @return NodeMetaRef
 local function getInitiatedMeta(pos)
     local meta = minetest.get_meta(pos)
-    for i, name in pairs({
+    for _, name in pairs({
         "fuel_totaltime",
         "fuel_time",
         "src_totaltime",
@@ -41,6 +45,22 @@ local function getInitiatedMeta(pos)
     return meta
 end
 
+--- Swaps node if node is not same and return old node name.
+--- @see minetest.swap_node (https://dev.minetest.net/minetest.swap_node)
+---
+--- @private
+--- @param pos table {x,y,z}
+--- @param name string
+--- @return string
+local function swapNode(pos, name)
+    local node = minetest.get_node(pos)
+    if node.name ~= name then
+        node.name = name
+        minetest.swap_node(pos, node)
+    end
+    return node.name
+end
+
 -- -----------------------------------------------------------------------------------------------
 -- Public functions:
 
@@ -50,6 +70,26 @@ function Grinder:getMeta()
     return self.meta or getInitiatedMeta(self.position)
 end
 
+--- Sets Node into active grinder with new hint.
+--- @public
+--- @param hint_en string A template for hinting in English. Use "%s" for machine name placeholder.
+function Grinder:activate(hint_en, cooked)
+    local meta = self:getMeta()
+    local percent = math.floor(meta:get_float("fuel_time") / meta:get_float("fuel_totaltime") * 100)
+    local item_percent = math.floor(meta:get_float("src_time") / cooked * 100)
+    self:getMeta():set_string("infotext", SL((hint_en):format(machine_name)).." ("..percent.."%)")
+    swapNode(self.position, "grinder:grinder_active")
+    self:getMeta():set_string("formspec",grinder.get_grinder_active_formspec(pos, percent, item_percent))
+end
+
+--- Sets Node into inactive grinder with new hint.
+--- @public
+--- @param hint_en string A template for hinting in English. Use "%s" for machine name placeholder.
+function Grinder:deactivate(hint_en)
+    self:getMeta():set_string("infotext", SL((hint_en):format(machine_name)))
+    swapNode(self.position, "grinder:grinder")
+    self:getMeta():set_string("formspec", grinder.grinder_inactive_formspec)
+end
 
 
 return Grinder
--- mods/grinder/processor.lua
+++ mods/grinder/processor.lua
@@ -1,32 +1,11 @@
-local SL = lord.require_intllib()
 --- @type Grinder
 local Grinder = dofile(minetest.get_modpath(minetest.get_current_modname()).."/grinder.lua")
 
---- @type string
-local machine_name = "Grinder"
 
 ---
 --- @class Processor
 ---
 local Processor = {}
-
--- -----------------------------------------------------------------------------------------------
--- Private functions:
-
---- Swaps node if node is not same and return old node name
---- @see minetest.swap_node (https://dev.minetest.net/minetest.swap_node)
----
---- @param pos table {x,y,z}
---- @param name string
---- @return string
-local function swapNode(pos, name)
-    local node = minetest.get_node(pos)
-    if node.name ~= name then
-        node.name = name
-        minetest.swap_node(pos, node)
-    end
-    return node.name
-end
 
 -- -----------------------------------------------------------------------------------------------
 -- Public functions:
@@ -74,20 +53,14 @@ function Processor.act (pos)
         cooked = result.time or 10
     end
     if meta:get_float("fuel_time") < meta:get_float("fuel_totaltime") then
-        local percent = math.floor(meta:get_float("fuel_time") / meta:get_float("fuel_totaltime") * 100)
-        local item_percent = math.floor(meta:get_float("src_time") / cooked * 100)
-        meta:set_string("infotext", SL(("%s Grinding"):format(machine_name)).." ("..percent.."%)")
-        swapNode(pos, "grinder:grinder_active")
-        meta:set_string("formspec",grinder.get_grinder_active_formspec(pos, percent, item_percent))
+        g:activate("%s Grinding", cooked)
         return
     end
 
     recipe = grinder.get_grinding_recipe("grinding", inv:get_list("src"))
     if not recipe then
         if was_active then
-            meta:set_string("infotext", SL(("%s is empty"):format(machine_name)))
-            swapNode(pos, "grinder:grinder")
-            meta:set_string("formspec", grinder.grinder_inactive_formspec)
+            g:deactivate("%s is empty")
         end
         --print("Выход, поскольку нет рецепта")
         return
@@ -102,9 +75,7 @@ function Processor.act (pos)
     end
 
     if fuel.time <= 0 then
-        meta:set_string("infotext", SL(("%s Out Of Heat"):format(machine_name)))
-        swap_node(pos, "grinder:grinder")
-        meta:set_string("formspec", grinder.grinder_inactive_formspec)
+        g:deactivate("%s Out Of Heat")
         return
     end
 
