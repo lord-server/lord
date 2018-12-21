bc3b9fdec865b31bce0ea382634e80cf02e4075e
Alexander Chibrikin <alek13.me@gmail.com>
2018-12-21 20:44:50 +0300

    remove unused global var; reformat code

--- mods/grinder/src/grinder.lua
+++ mods/grinder/src/grinder.lua
@@ -1,28 +1,19 @@
-
-
-grinder = {}
-
 require('grinder.Recipe')
 local craft = require('grinder.craft')
 local node = require('grinder.node')
+local Processor = require('grinder.Processor')
 
 
 for _, craftRecipe in pairs(craft.recipes) do
 	minetest.register_craft(craftRecipe)
 end
-
 for _, item in pairs(craft.items) do
 	minetest.register_craftitem(item.name, item.definition)
 end
 
 
 minetest.register_node("grinder:grinder", node.inactive)
-
 minetest.register_node("grinder:grinder_active", node.active)
-
-
---- @type Processor
-local Processor = require('grinder.Processor')
 minetest.register_abm({
 	nodenames = {"grinder:grinder", "grinder:grinder_active"},
 	interval = 1,
