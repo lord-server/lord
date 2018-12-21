30ed9df0610c3d772d25973afdd11eafe2de11bf
Alexander Chibrikin <alek13.me@gmail.com>
2018-12-21 22:03:34 +0300

    bring out grinding recipes definition into separate file; use `grinder.Recipe` as class/module

--- mods/grinder/src/grinder.lua
+++ mods/grinder/src/grinder.lua
@@ -1,8 +1,9 @@
-require('grinder.Recipe')
-local craft = require('grinder.craft')
-local node = require('grinder.node')
-local Processor = require('grinder.Processor')
+local craft     = require('grinder.craft')
+local node      = require('grinder.node')
+local recipes   = require('grinder.recipes')
 
+local Recipe    = require('grinder.Recipe')
+local Processor = require('grinder.Processor')
 
 for _, craftRecipe in pairs(craft.recipes) do
 	minetest.register_craft(craftRecipe)
@@ -11,12 +12,13 @@ for _, item in pairs(craft.items) do
 	minetest.register_craftitem(item.name, item.definition)
 end
 
+Recipe.registerRecipes(recipes)
 
 minetest.register_node("grinder:grinder", node.inactive)
 minetest.register_node("grinder:grinder_active", node.active)
 minetest.register_abm({
-	nodenames = {"grinder:grinder", "grinder:grinder_active"},
-	interval = 1,
-	chance = 1,
-	action = Processor.act,
+	nodenames = { "grinder:grinder", "grinder:grinder_active" },
+	interval  = 1,
+	chance    = 1,
+	action    = Processor.act,
 })
--- mods/grinder/src/grinder/Recipe.lua
+++ mods/grinder/src/grinder/Recipe.lua
@@ -76,28 +76,14 @@ function Recipe.get(items)
 	return {time = recipe.time,	new_input = new_input, output = recipe.output}
 end
 
--- -----------------------------------------------------------------------------------------------
--- Init recipes:
-
-
-local recipes = {
-	{"default:stone", "default:cobble", 5},
-	{"default:desert_stone", "default:desert_cobble", 5},
-	{"default:cobble", "default:gravel", 5},
-	{"default:desert_cobble", "default:gravel", 5},
-	{"default:mossycobble", "default:gravel", 5},
-	{"default:snowycobble", "default:gravel", 5},
-	{"default:gravel", "default:sand", 5},
-	{"default:sand", "default:clay", 5},
-	{"default:coal_lump", "grinder:coal_dust 2", 5},
-	{"default:glass", "default:sand", 5},
-	{"vessels:glass_bottle 5", "default:sand", 8},
-	{"vessels:drinking_glass 3", "default:sand", 6},
-	{"vessels:glass_fragments 3", "default:sand", 6},
-}
-
-for _, data in pairs(recipes) do
-	register_recipe({input = data[1], output = data[2], time = data[3]})
+--- Registers grinding recipes
+---
+--- @static
+--- @param recipes table<table>
+function Recipe.registerRecipes(recipes)
+	for _, data in pairs(recipes) do
+		register_recipe({input = data[1], output = data[2], time = data[3]})
+	end
 end
 
 return Recipe
