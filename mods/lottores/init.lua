c36bbcdc2b319e506b14d27e7f87b029d190e6e2
Doloment <sawerter@inbox.ru>
2019-01-16 21:23:54 +0700

    Update init.lua

--- mods/lottores/init.lua
+++ mods/lottores/init.lua
@@ -1177,5 +1177,4 @@ minetest.register_craft({
 	recipe = "group:galvorn_item",
 })
 
-if minetest.settings:get_bool("msg_loading_mods") then minetest.log("action", minetest.get_current_modname().." mod LOADED") end
-
+lord.mod_loaded()
