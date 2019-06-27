040dd19d35bb1c41fc8e69169710f4d8faa15ba9
Doloment <sawerter@inbox.ru>
2019-06-28 02:34:17 +0700

    Update init.lua

--- mods/hud_modpack/hudbars/init.lua
+++ mods/hud_modpack/hudbars/init.lua
@@ -190,8 +190,8 @@ function hb.register_hudbar(identifier, text_color, label, textures, default_sta
 		local ids = {}
 		local state = {}
 		local name = player:get_player_name()
-		bgscale = {}
-		iconscale = {}
+		local bgscale = {}
+		local iconscale = {}
 		local text
 		local barnumber
 		local bgiconnumber
