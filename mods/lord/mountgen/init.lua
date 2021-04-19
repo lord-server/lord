835dded4b8f11203cce5befb7129dd2ba3123278
Vladislav Tsendrovskii <vlad@vltc.net.eu.org>
2021-04-19 03:20:26 +0300

    Add snow and grass

--- mods/lord/mountgen/init.lua
+++ mods/lord/mountgen/init.lua
@@ -1,34 +1,63 @@
 local ANGLE=45*3.141/180
 local Y0 = 0
+local SNOW_LINE = 50
+local SNOW_LINE_RAND = 4
+
+local function is_inside(pos, top, angle)
+	local dx = pos.x - top.x
+	local dz = pos.z - top.z
+	local d = math.sqrt(dx*dx+dz*dz)
+	local h = top.y-pos.y
+	return d <= h*math.tan(ANGLE)
+end
+
+local function is_top(pos, top, angle)
+	return is_inside(pos, top, angle) and not is_inside({x=pos.x, y=pos.y+1, z=pos.z}, top, angle)
+end
 
 minetest.register_tool("mountgen:mount_tool", {
         description = "Горный посох",
         inventory_image = "ghost_tool.png",
         on_use = function(itemstack, user, pointed_thing)
-		local pos = user:get_pos()
-		minetest.log("use mount stick at "..pos.x.." "..pos.y.." "..pos.z)
+		local top = user:get_pos()
+		minetest.log("use mount stick at "..top.x.." "..top.y.." "..top.z)
 
-		local H = pos.y - Y0
+		local H = top.y - Y0
 		local W = H * math.tan(ANGLE)
 
-		local x1 = pos.x - W
-		local x2 = pos.x + W
+		local x1 = top.x - W
+		local x2 = top.x + W
 
-		local z1 = pos.z - W
-		local z2 = pos.z + W
+		local z1 = top.z - W
+		local z2 = top.z + W
 
 		local y1 = Y0
-		local y2 = pos.y
+		local y2 = top.y
 
 		for y = y1,y2+1 do
 		for x = x1,x2+1 do
 		for z = z1,z2+1 do
-			local dx = x - pos.x
-			local dz = z - pos.z
-			local d = math.sqrt(dx*dx+dz*dz)
-			local h = y2-y
-			if d <= h*math.tan(ANGLE) then
-				minetest.set_node({x=x,y=y,z=z}, {name="default:stone"})
+			local pos = {x=x,y=y,z=z}
+			local inside = is_inside(pos, top, ANGLE)
+			local top    = is_top(pos, top, ANGLE)
+
+			if inside then
+				if top then
+					local place_snow = false
+					if SNOW_LINE ~= nil then
+						local sl = SNOW_LINE - math.random(0, SNOW_LINE_RAND)
+						if y >= sl then
+							place_snow = true
+						end
+					end
+					if place_snow then
+						minetest.set_node(pos, {name="default:snowblock"})
+					else
+						minetest.set_node(pos, {name="lottmapgen:dunland_grass"})
+					end
+				else
+					minetest.set_node(pos, {name="default:stone"})
+				end
 			end
 		end
 		end
