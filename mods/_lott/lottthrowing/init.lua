be558f1c8bcdf02f0b075be3dd61b28a3c2da468
Vladislav Tsendrovskii <vlad@vltc.net.eu.org>
2021-04-24 19:03:45 +0300

    Move shooting node to embrasure mod

--- mods/_lott/lottthrowing/init.lua
+++ mods/_lott/lottthrowing/init.lua
@@ -116,23 +116,6 @@ minetest.register_tool("lottthrowing:bow_wood_mallorn", {
 	end,
 })
 
-minetest.register_node("lottthrowing:arrownode", {
-	description = SL("Automatic shooting node"),
-	tiles = {"lottblocks_snowycobble.png"},
-	on_construct = function(pos)
-		local timer = minetest.get_node_timer(pos)
-		timer:start(1)
-	end,
-	on_timer = function(pos, elapsed)
-		local timer = minetest.get_node_timer(pos)
-		timer:start(1)
-		local dir = {x = -1, y=0, z=0}
-		local arrow = "arrows:arrow_steel"
-		minetest.log("shoot at "..pos.x.." "..pos.y.." "..pos.z)
-		throwing:shoot({x=pos.x, y=pos.y, z=pos.z}, "node", arrow, pos, dir, 0.5)
-	end,
-})
-
 minetest.register_craft({
 	output = 'lottthrowing:bow_wood',
 	recipe = {
---
+++ mods/lord/embrasure/init.lua
@@ -0,0 +1,18 @@
+local SL = lord.require_intllib()
+
+minetest.register_node("embrasure:arrownode", {
+	description = SL("Automatic shooting node"),
+	tiles = {"lottblocks_snowycobble.png"},
+	on_construct = function(pos)
+		local timer = minetest.get_node_timer(pos)
+		timer:start(1)
+	end,
+	on_timer = function(pos, elapsed)
+		local timer = minetest.get_node_timer(pos)
+		timer:start(1)
+		local dir = {x = -1, y=0, z=0}
+		local arrow = "arrows:arrow_steel"
+		minetest.log("shoot at "..pos.x.." "..pos.y.." "..pos.z)
+		throwing:shoot({x=pos.x, y=pos.y, z=pos.z}, "node", arrow, pos, dir, 0.5)
+	end,
+})
