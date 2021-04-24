95492e494afd67110420b3f4de72ea450dfaa85f
Vladislav Tsendrovskii <vlad@vltc.net.eu.org>
2021-04-24 22:35:46 +0300

    Add displaying HP of players on arena (ID=2316)

---
+++ mods/lord/arena/init.lua
@@ -0,0 +1,39 @@
+
+local ARENA_AREA_ID = 2316
+
+local function player_display_hp(player)
+	local name = player:get_player_name()
+	local hp = player:get_hp()
+	local color
+	if hp > 10 then
+		color = "#FFFFFF"
+	elseif hp > 5 then
+		color = "#FFFF00"
+	else
+		color = "#FF0000"
+	end
+	player:set_properties({nametag = name.." HP: "..hp, nametag_color = color,})
+end
+
+local function player_undisplay_hp(player)
+	local name = player:get_player_name()
+	player:set_properties({nametag = name, nametag_color = "#FFFFFF",})
+end
+
+minetest.register_globalstep(function(dtime)
+	for _, player in pairs(minetest.get_connected_players()) do
+		local pos = vector.round(player:getpos())
+		local in_arena = false
+		for id, area in pairs(areas:getAreasAtPos(pos)) do
+			if id == ARENA_AREA_ID then
+				in_arena = true
+				break
+			end
+		end
+		if in_arena then
+			player_display_hp(player)
+		else
+			player_undisplay_hp(player)
+		end
+	end
+end)
