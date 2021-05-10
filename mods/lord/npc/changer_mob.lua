aeafc806880c80a3dc8de298d8b3d734ee7e2a57
Vladislav Tsendrovskii <vlad@vltc.net.eu.org>
2021-05-10 20:13:45 +0300

    Separate npc mob engine
    Goods change mob
    
    Edit mob tag color
    
    Edit mob texture

---
+++ mods/lord/npc/changer_mob.lua
@@ -0,0 +1,200 @@
+local esc = minetest.formspec_escape
+
+local function user_mob_content(self, width, pos)
+	local formspec = ""
+	formspec = formspec.."list[detached:"..self.id..";receive;0.25,"..pos..";1,1]"
+	formspec = formspec.."list[detached:"..self.id..";give;1.25,"..pos..";1,1]"
+	pos = pos + 1.5
+	formspec = formspec.."list[current_player;main;0.25,"..pos..";8,1;]"
+	pos = pos + 1
+	return formspec, pos
+end
+
+local function admin_mob_content(self, width, pos)
+	local formspec = ""
+	formspec = formspec.."list[detached:"..self.id..";admin_receive;0.25,"..pos..";1,1]"
+	formspec = formspec.."list[detached:"..self.id..";admin_give;1.25,"..pos..";1,1]"
+	pos = pos + 1.5
+	formspec = formspec.."list[current_player;main;0.25,"..pos..";8,1;]"
+	pos = pos + 1
+	return formspec, pos
+end
+
+local function main_form_handle(self, clicker, fields, can_edit)
+end
+
+local function form_handle(self, clicker, formname, fields, can_edit)
+end
+
+
+-- inventory functions
+local function allow_put(inv, listname, index, stack, player)
+	if listname ~= "give" then
+		return 99
+	end
+	return 0
+end
+
+local function allow_take(inv, listname, index, stack, player)
+	return 99
+end
+
+local function allow_move(inv, from_list, from_index, to_list, to_index, count, player)
+	if to_list == "give" then
+		return 0
+	end
+	return 99
+end
+
+local function update_changer(self, inv)
+	self.give = inv.get_stack(inv, "admin_give", 1):get_name()
+	self.give_qty = inv.get_stack(inv, "admin_give", 1):get_count()
+	self.receive = inv.get_stack(inv, "admin_receive", 1):get_name()
+	self.receive_qty = inv.get_stack(inv, "admin_receive", 1):get_count()
+	minetest.log("Changer receive: "..self.receive.." "..self.receive_qty)
+	minetest.log("Changer give: "..self.give.." "..self.give_qty)
+end
+
+local function update_takeout(self, inv)
+	local received     = inv.get_stack(inv, "receive", 1):get_name()
+	local received_qty = inv.get_stack(inv, "receive", 1):get_count()
+
+	if received == self.receive and received_qty >= self.receive_qty then
+		-- put good
+		inv.set_stack(inv, "give", 1, self.give.." "..self.give_qty)
+	else
+		inv.set_stack(inv, "give", 1, nil)
+	end
+end
+
+local function take_fee(self, inv)
+	local received     = inv.get_stack(inv, "receive", 1):get_name()
+	local received_qty = inv.get_stack(inv, "receive", 1):get_count()
+	if received ~= self.receive then
+		return
+	end
+
+	local newamount = received_qty-self.receive_qty
+	if newamount > 0 then
+		inv.set_stack(inv, "receive", 1, self.receive.." "..newamount)
+	else
+		inv.set_stack(inv, "receive", 1, nil)
+	end
+end
+
+local function on_put(inv, listname, index, stack, player)
+	local playername = player:get_player_name()
+	local self = npc.player_mobs[playername]
+	if self == nil then
+		return
+	end
+
+	if listname == "admin_receive" then
+		update_changer(self, inv)
+	elseif listname == "admin_give" then
+		update_changer(self, inv)
+	elseif listname == "receive" then
+		update_takeout(self, inv)
+	end
+end
+
+local function on_take(inv, listname, index, stack, player)
+	local playername = player:get_player_name()
+	local self = npc.player_mobs[playername]
+	if self == nil then
+		return
+	end
+
+	if listname == "admin_receive" then
+		update_changer(self, inv)
+	elseif listname == "admin_give" then
+		update_changer(self, inv)
+	elseif listname == "receive" then
+		update_takeout(self, inv)
+	elseif listname == "give" then
+		take_fee(self, inv)
+		update_takeout(self, inv)
+	end
+end
+
+local function create_inventory(self, id)
+	self.inventory = minetest.get_inventory({type="detached", name=id})
+	if self.inventory ~= nil then
+		return
+	end
+
+	local move_put_take = {
+		allow_put = allow_put,
+		allow_take = allow_take,
+		allow_move = allow_move,
+		on_put = on_put,
+		on_take = on_take
+	}
+	self.inventory = minetest.create_detached_inventory(id, move_put_take)
+	self.inventory:set_size("admin_receive", 1)
+	self.inventory:set_size("admin_give", 1)
+	self.inventory:set_size("receive", 1)
+	self.inventory:set_size("give", 1)
+
+	self.inventory:set_stack("admin_give", 1, self.give.." "..self.give_qty)
+	self.inventory:set_stack("admin_receive", 1, self.receive.." "..self.receive_qty)
+end
+
+local function init_from_staticdata(self, mobdata)
+	local data = minetest.deserialize(mobdata)
+	if data["receive"] ~= nil then
+		self.receive = data["receive"]
+		self.receive_qty = data["receive_qty"]
+	else
+		self.receive = "default:dirt"
+		self.receive_qty = 1
+	end
+
+	if data["give"] ~= nil then
+		self.give = data["give"]
+		self.give_qty = data["give_qty"]
+	else
+		self.give = "default:stone"
+		self.give_qty = 1
+	end
+
+	if data["id"] ~= nil then
+		self.id = data["id"]
+	else
+		self.id = esc(self.mobname..self.name..math.random(1, 1000))
+	end
+
+	create_inventory(self, self.id)
+end
+
+local function init_new(self)
+	self.id = esc(self.mobname..self.name..math.random(1, 1000))
+	self.receive = "default:dirt"
+	self.receive_qty = 1
+	self.give = "default:stone"
+	self.give_qty = 1
+	create_inventory(self, self.id)
+end
+
+local function get_mobdata(self)
+	local data = {
+		["id"] = self.id,
+		["receive"] = self.receive,
+		["receive_qty"] = self.receive_qty,
+		["give"] = self.give,
+		["give_qty"] = self.give_qty,
+	}
+	return minetest.serialize(data)
+end
+
+npc:register_mob("npc:changer_mob", {
+	user_mob_content = user_mob_content,
+	admin_mob_content = admin_mob_content,
+	main_form_handle = main_form_handle,
+	form_handle = form_handle,
+	init_from_staticdata = init_from_staticdata,
+	init_new = init_new,
+	get_mobdata = get_mobdata,
+	face_user = true,
+	width = 8.5,
+})
---
+++ mods/lord/npc/npc.lua
@@ -0,0 +1,310 @@
+local S = minetest.get_translator("npc")
+local esc = minetest.formspec_escape
+
+npc = {
+	["required_priv"] = "server",
+	["player_mobs"] = {},
+}
+
+local function build_main_form(self)
+	local width = self.width
+	local bw = width - 0.5
+	local pos = 0
+	local formspec = ""
+
+	-- show greeting
+	formspec = formspec.."textarea[0.5,"..pos..";"..bw..",1.5;;;"..esc(self.greeting).."]"
+	pos = pos + 1.5
+
+	-- show mob content
+	local content, newpos = self:user_mob_content(width, pos)
+	formspec = formspec..content
+	pos = newpos
+
+	-- show Close button
+	pos = pos + 0.2
+	formspec = formspec.."button_exit[0.25,"..pos..";"..bw..",1;close_form;"..esc(S("Close")).."]"
+	pos = pos + 1
+	formspec = "size["..width..","..pos.."]"..formspec
+	return formspec
+end
+
+local function build_main_form_editable(self)
+	local width = self.width
+	local bw = width - 0.5
+	local pos = 0.5
+	local formspec = ""
+
+	-- show mobname and greeting editable fields
+	formspec = formspec.."field[0.5,"..pos..";"..bw..",0.5;edit_name;;"..esc(self.mobname).."]"
+	pos = pos + 1
+	formspec = formspec.."field[0.5,"..pos..";"..bw..",0.5;edit_color;;"..esc(self.color).."]"
+	pos = pos + 1
+	formspec = formspec.."field[0.5,"..pos..";"..bw..",0.5;edit_texture;;"..esc(self.texture).."]"
+	pos = pos + 1
+	formspec = formspec.."textarea[0.5,"..pos..";"..bw..",1.5;edit_greeting;;"..esc(self.greeting).."]"
+	pos = pos + 1.5
+
+	-- show mob content (editable)
+	local content, newpos = self:admin_mob_content(width, pos)
+	formspec = formspec..content
+	pos = newpos
+
+	-- show Take and Close buttons
+	pos = pos + 0.5
+	formspec = formspec.."button[0.25,"..pos..";"..bw..",1;save_main;"..esc(S("Save")).."]"
+	pos = pos + 1
+	formspec = formspec.."button_exit[0.25,"..pos..";"..bw..",1;take_mob;"..esc(S("Take")).."]"
+	pos = pos + 1
+	formspec = formspec.."button_exit[0.25,"..pos..";"..bw..",1;close_form;"..esc(S("Close")).."]"
+	pos = pos + 1
+	formspec = "size["..width..","..pos.."]"..formspec
+	return formspec
+end
+
+local function show_main(self, clicker)
+	local player = clicker:get_player_name()
+	local can_edit = minetest.get_player_privs(player)[npc.required_priv]
+	if can_edit then
+		minetest.show_formspec(player, "npc:main_form", build_main_form_editable(self))
+	else
+		minetest.show_formspec(player, "npc:main_form", build_main_form(self))
+	end
+end
+
+minetest.register_on_player_receive_fields(function(clicker, formname, fields)
+	local player = clicker:get_player_name()
+	local can_edit = minetest.get_player_privs(player)[npc.required_priv]
+	local self = npc.player_mobs[player]
+	if self == nil then
+		return
+	end
+
+	if formname == "npc:main_form" then
+		-- handling main form
+		if fields["save_main"] ~= nil then
+			-- save mob name and greeting
+			self.mobname  = fields["edit_name"]
+			self.color    = fields["edit_color"]
+			self.greeting = fields["edit_greeting"]
+			self.texture  = fields["edit_texture"]
+			self.object:set_properties({
+				nametag       = self.mobname,
+				nametag_color = self.color,
+				textures      = {self.texture},
+			})
+			self:show_main(clicker)
+		elseif fields["take_mob"] ~= nil then
+			-- take mob to inventory
+			local inventory = clicker:get_inventory()
+			if inventory:room_for_item("main", self.name) then
+				local new_stack = ItemStack(self.name.."_egg")
+
+				local data_str = self:get_staticdata()
+				new_stack:set_metadata(data_str)
+
+				local inv = clicker:get_inventory()
+				if inv:room_for_item("main", new_stack) then
+					inv:add_item("main", new_stack)
+				else
+					minetest.add_item(clicker:getpos(), new_stack)
+				end
+
+				self.object:remove()
+				npc.player_mobs[player] = nil
+			end
+		elseif fields["close_form"] ~= nil then
+			-- form will close automatically
+			npc.player_mobs[player] = nil
+		else
+			-- call mob content
+			self:main_form_handle(clicker, fields, can_edit)
+		end
+	else
+		-- handle mob content
+		self:form_handle(clicker, formname, fields, can_edit)
+	end
+end)
+
+local function face_pos(self, pos)
+	local s = self.object:getpos()
+	local vec = {x=pos.x-s.x, y=pos.y-s.y, z=pos.z-s.z}
+	local yaw = math.atan2(vec.z,vec.x)-math.pi/2
+	if self.drawtype == "side" then
+		yaw = yaw+(math.pi/2)
+	end
+	self.object:setyaw(yaw)
+	return yaw
+end
+
+local function interact_infomob(self, clicker)
+	local player = clicker:get_player_name()
+	local can_edit = minetest.get_player_privs(player)[npc.required_priv]
+	if can_edit or self.face_user then
+		face_pos(self, clicker:getpos())
+	end
+	npc.player_mobs[player] = self
+	self:show_main(clicker)
+end
+
+local function default_user_mob_content(self, width, pos)
+	return "", pos
+end
+
+local function default_admin_mob_content(self, width, pos)
+	return "", pos
+end
+
+local function default_main_form_handle(self, clicker, fields, can_edit)
+end
+
+local function default_form_handle(self, clicker, formname, fields, can_edit)
+end
+
+local function default_init_from_staticdata(self, mobdata)
+end
+
+local function default_init_new(self)
+end
+
+local function default_get_mobdata(self)
+	return minetest.serialize({})
+end
+
+local function on_activate(self, staticdata)
+	local data = minetest.deserialize(staticdata)
+	if data ~= nil then
+		if data["mobname"] ~= nil then
+			self.mobname = data["mobname"]
+		else
+			self.mobname = self.definition.mobname
+		end
+
+		if data["color"] ~= nil then
+			self.color = data["color"]
+		else
+			self.color = self.definition.color
+		end
+
+		if data["greeting"] ~= nil then
+			self.greeting = data["greeting"]
+		else
+			self.greeting = self.definition.message
+		end
+
+		if data["texture"] ~= nil then
+			self.texture = data["texture"]
+		else
+			self.texture = self.definition.texture
+		end
+
+		if data["mobdata"] ~= nil then
+			self:init_from_staticdata(data["mobdata"])
+		else
+			self:init_new()
+		end
+	else
+		self.mobname  = self.definition.mobname
+		self.color    = self.definition.color
+		self.greeting = self.definition.message
+		self.texture  = self.definition.texture
+		self:init_new()
+	end
+
+	self.object:set_armor_groups({immortal = 1})
+	self.object:set_properties({
+		nametag       = self.mobname,
+		nametag_color = self.color,
+		textures      = {self.texture},
+	})
+end
+
+local function get_staticdata(self)
+	local data = {
+		["mobname"] = self.mobname,
+		["color"] = self.color,
+		["texture"] = self.texture,
+		["greeting"] = self.greeting,
+		["mobdata"] = self:get_mobdata(),
+	}
+	return minetest.serialize(data)
+end
+
+function npc:register_mob(name, definition)
+	minetest.register_entity(name, {
+		definition = definition,
+		physical = true,
+
+		collisionbox = {-0.3,-1.0,-0.3, 0.3,0.8,0.3},
+		visual = "mesh",
+		mesh = "human_model.x",
+
+		texture = definition.texture or "lottmobs_rohan_guard_2.png",
+		mobname = definition.mobname or "Меродок",
+		greeting = definition.greeting or "Привет! Как дела, что делаешь?",
+		color = definition.color or "#FFBB00",
+		face_user = definition.face_user or false,
+		width = definition.width or 8,
+
+		on_rightclick = interact_infomob,
+		on_punch = interact_infomob,
+		show_main = show_main,
+
+		-- mob content functions
+		user_mob_content = definition.user_mob_content or default_user_mob_content,
+		admin_mob_content = definition.admin_mob_content or default_admin_mob_content,
+		main_form_handle = definition.main_form_handle or default_main_form_handle,
+		form_handle = definition.form_handle or default_form_handle,
+		init_from_staticdata = definition.init_from_staticdata or default_init_from_staticdata,
+		init_new = definition.init_new or default_init_new,
+		get_mobdata = definition.get_mobdata or default_get_mobdata,
+
+		on_activate = on_activate,
+		get_staticdata = get_staticdata,
+	})
+
+	minetest.register_craftitem(name.."_egg", {
+
+		description = name.." egg",
+		inventory_image = "npc_info_mob.png",
+		groups = {not_in_creative_inventory = 1},
+		stack_max = 1,
+
+		on_place = function(itemstack, placer, pointed_thing)
+			local player = placer:get_player_name()
+			local can_edit = minetest.get_player_privs(player)[npc.required_priv]
+			if not can_edit then
+				return
+			end
+
+			local pos = pointed_thing.above
+
+			-- am I clicking on something with existing on_rightclick function?
+			local under = minetest.get_node(pointed_thing.under)
+			local def = minetest.registered_nodes[under.name]
+			if def and def.on_rightclick then
+				return def.on_rightclick(pointed_thing.under, under, placer, itemstack)
+			end
+
+			if pos
+			and within_limits(pos, 0)
+			and not minetest.is_protected(pos, player) then
+				pos.y            = pos.y + 0.5
+
+				local data       = itemstack:get_metadata()
+				local entity     = minetest.add_entity(pos, name, data)
+				local lua_entity = entity:get_luaentity()
+
+				if not lua_entity then
+					entity:remove()
+					return
+				end
+
+				-- since mob is unique we remove egg once spawned
+				itemstack:take_item()
+			end
+
+			return itemstack
+		end,
+	})
+end
