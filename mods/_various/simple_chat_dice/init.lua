f9c11290285a3d25c9eac721cd469a37b74c8e30
ptah-alexs <alexs6@mail.ru>
2021-06-10 00:00:57 +0400

    Update init.lua

--- mods/_various/simple_chat_dice/init.lua
+++ mods/_various/simple_chat_dice/init.lua
@@ -4,13 +4,12 @@ minetest.register_chatcommand("dice", {
     params = S("<MAX_NUM>"),
     description = S("print out random number"),
     func = function(name, param)
+		local t = 6
         if (param ~= "") then
-            t = tonumber(param)
-            if (t == nil) then
-                t = 6
+            local t1 = tonumber(param)
+            if (t1 ~= nil) then
+                t = t1
             end
-        else
-            t = 6
         end
         local num = math.random(1,t)
         return true, name .. " ".. S("dice") .. ": " .. num
