6b7f9df3993164a11a38b9e9b19b7700cea72620
Aleksandr Opachev <alexs6@mail.ru>
2021-06-10 21:51:54 +0400

    replce spaces to tabs

--- mods/lord/dice/init.lua
+++ mods/lord/dice/init.lua
@@ -1,19 +1,19 @@
 local S = minetest.get_translator("dice")
 
 minetest.register_chatcommand("dice", {
-    max_num = S("<MAX_NUM>"),
-    description = S("prints out random number"),
-    func = function(name, param_num)
-        local t = 6
-        if (param_num ~= "") then
-            local t1 = tonumber(param_num)
-            if (t1 ~= nil) then
-                t = t1
-            end
-        end
-        local num = math.random(1, t)
-        return true, name .. " ".. S("dice") .. ": " .. num
-    end,
+	max_num = "<MAX_NUM>",
+	description = S("prints out random number"),
+	func = function(name, param)
+		local max_limit = 6
+		if (param ~= "") then
+			local current_limit = tonumber(param)
+			if (current_limit ~= nil) then
+				max_limit = current_limit
+			end
+		end
+		local num = math.random(1, max_limit)
+		return true, name .. " ".. S("dice") .. ": " .. num
+	end,
 })
 
-lord.mod_loaded()
+--lord.mod_loaded()
