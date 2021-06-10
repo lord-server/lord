f85cafa306bf067a2efcf6f9ada6bce8b6b31058
Aleksandr Opachev <alexs6@mail.ru>
2021-06-10 06:42:43 +0400

    1. move mod into lord subdirectory 2. Update translation 3. Update description

--- mods/lord/dice/init.lua
+++ mods/lord/dice/init.lua
@@ -1,17 +1,17 @@
 local S = minetest.get_translator("dice")
 
 minetest.register_chatcommand("dice", {
-    params = S("<MAX_NUM>"),
-    description = S("print out random number"),
-    func = function(name, param)
-		local t = 6
-        if (param ~= "") then
-            local t1 = tonumber(param)
+    max_num = S("<MAX_NUM>"),
+    description = S("prints out random number"),
+    func = function(name, param_num)
+        local t = 6
+        if (param_num ~= "") then
+            local t1 = tonumber(param_num)
             if (t1 ~= nil) then
                 t = t1
             end
         end
-        local num = math.random(1,t)
+        local num = math.random(1, t)
         return true, name .. " ".. S("dice") .. ": " .. num
     end,
 })
