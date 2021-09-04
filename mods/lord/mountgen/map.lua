a9dee4fa1cc0e2576569cff9aae064e6c6e5605e
Tsendrovskii Vladislav <vlad@vltc.net.eu.org>
2021-09-05 02:53:07 +0300

    Ускорение работы горного посоха (#424)
    * Accelerate mountgen with voxelmanip

--- mods/lord/mountgen/map.lua
+++ mods/lord/mountgen/map.lua
@@ -1,10 +1,10 @@
-local function place_grass(pos)
+local function get_grass(pos)
 	local id = math.random(2,5)
 	local name = "default:grass_"..tostring(id)
-	minetest.set_node(pos, {name=name})
+	return name
 end
 
-local function place_flower(pos)
+local function get_flower(pos)
 	local names = {	"flowers:dandelion_white",
 			"flowers:dandelion_yellow",
 			"flowers:geranium",
@@ -14,50 +14,45 @@ local function place_flower(pos)
 			}
 	local id = math.random(1,#names)
 	local name = names[id]
-	minetest.set_node(pos, {name=name})
+	return name
 end
 
-local function place_sapling(pos)
+local function get_sapling(pos)
 	local name = "default:sapling"
-	minetest.set_node(pos, {name=name})
-	--default.grow_tree({x=pos.x, y=pos.y-1,z=pos.z}, math.random(1, 4) == 1)
+	return name
 end
 
 
-mountgen.place_top = function(x, y, z, config)
-	local pos = {x=x,y=y,z=z}
-
+mountgen.top_node = function(pos, config)
 	local place_snow = false
 	local sl = config.SNOW_LINE - math.random(0, config.SNOW_LINE_RAND)
-	if y >= sl then
+	if pos.y >= sl then
 		place_snow = true
 	end
 
 	if place_snow then
-		minetest.set_node(pos, {name="default:snowblock"})
+		return "default:snowblock"
 	else
-		local upper = {x=x,y=y+1,z=z}
-		minetest.set_node(pos, {name=config.top_cover})
+		return config.top_cover
+	end
+end
 
-		local node = minetest.get_node(upper)
-		if node.name == "air" then
-			if math.random(0,100) < config.GRASS_PERCENT then
-				place_grass(upper)
-			end
-
-			if y <= config.FLOWERS_LINE then
-				if math.random(0,100) < config.FLOWERS_PERCENT then
-					place_flower(upper)
-				end
-			end
-
-			if y <= config.TREE_LINE then
-				if math.random(0,1000) < config.TREE_PROMILLE then
-					place_sapling(upper)
-				end
-			end
-		end
+mountgen.upper_node = function(pos, config)
+	if pos.y >= config.SNOW_LINE - config.SNOW_LINE_RAND then
+		return nil
+	end
+	if math.random(0,100) < config.GRASS_PERCENT then
+		return get_grass()
 	end
 
+	if pos.y <= config.FLOWERS_LINE and math.random(0,100) < config.FLOWERS_PERCENT then
+		return get_flower()
+	end
+
+	if pos.y <= config.TREE_LINE and math.random(0,1000) < config.TREE_PROMILLE then
+		return get_sapling()
+	end
+
+	return nil
 end
 
