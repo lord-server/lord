52f8d71eccbdf91bccccf7b11b373656946526c3
Vladislav Tsendrovskii <vlad@vltc.net.eu.org>
2021-10-25 22:16:40 +0300

    Move campfire to separate mod

--- mods/_minetest_game/fire/init.lua
+++ mods/_minetest_game/fire/init.lua
@@ -1,7 +1,6 @@
 local SL = lord.require_intllib()
 
 -- minetest/fire/init.lua
-dofile(minetest.get_modpath("fire").."/campfire.lua")
 
 minetest.register_node("fire:basic_flame", {
 	description = SL("Fire"),
---
+++ mods/_various/campfire/init.lua
@@ -0,0 +1,1 @@
+dofile(minetest.get_modpath("campfire").."/campfire.lua")
