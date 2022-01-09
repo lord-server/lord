c9d2956da95e162a5288f23ec09f8773c8190645
Alexander Chibrikin <alek13.me@gmail.com>
2022-01-09 23:17:21 +0300

    #267 Update `MTG/default`: upd `crafting.lua`: remove `pine_wood` stairs&slabs (already exists in lottplants)
    # Conflicts:
    #	mods/lord/_overwrites/MTG/stairs/init.lua

--- mods/lord/_overwrites/MTG/stairs/init.lua
+++ mods/lord/_overwrites/MTG/stairs/init.lua
@@ -1,1 +1,9 @@
+
+
 -- stairs/init.lua
+-- т.к. в LOTT (`lottplants/nodes.lua) была изначально своя сосна (`lottplants:pinewood`),
+-- то и её ступеньки нам не нужны
+minetest.unregister_item("stairs:slab_pine_wood")
+minetest.unregister_item("stairs:stair_inner_pine_wood")
+minetest.unregister_item("stairs:stair_outer_pine_wood")
+minetest.unregister_item("stairs:stair_pine_wood")
