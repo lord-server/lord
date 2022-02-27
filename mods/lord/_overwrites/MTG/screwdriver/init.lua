5aa5d444a1d331cfaa94f0baf291d9832d747dbe
weqqr <weqqr@yandex.ru>
2022-02-27 23:20:54 +0300

    Fix screwdriver

--- mods/lord/_overwrites/MTG/screwdriver/init.lua
+++ mods/lord/_overwrites/MTG/screwdriver/init.lua
@@ -2,7 +2,11 @@
 
 -- у нас свои отвертки в `lord/lord_screwdriver`
 minetest.clear_craft({output = "screwdriver:screwdriver"})
-minetest.unregister_item("screwdriver:screwdriver")
 
--- для совместимости с другими модами
-minetest.register_alias("screwdriver:screwdriver", "lord_screwdriver:screwdriver")
+minetest.register_craft({
+	output = "screwdriver:screwdriver",
+	recipe = {
+		{"default:steel_ingot", ""},
+		{"", "group:stick"}
+	}
+})
