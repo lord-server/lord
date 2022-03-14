35ed936e3e1b13480abeba1b0ab2f50959f0c00d
Vladislav Tsendrovskii <vlad@vltc.net.eu.org>
2022-03-14 19:50:36 +0300

    Messaging fix

--- mods/lord/player_guide/init.lua
+++ mods/lord/player_guide/init.lua
@@ -2,9 +2,10 @@ local S = minetest.get_translator("player_guide")
 local esc = minetest.formspec_escape
 
 -- Display guide message when violating area protection
+local violation_message = "Hello! Unfortunately, the area, where you are trying to dig or build, is used by other players. So, building/digging resources here is not allowed. But you can search other place, which is not used and build or dig resources there. Best regards, L.O.R.D. server!"
 local function violation_formspec(player_name)
 	formspec = "size[6,5]"
-	formspec = formspec.."textarea[0.5,1;5,3;;;"..esc(S("area_violation")).."]"
+	formspec = formspec.."textarea[0.5,1;5,3;;;"..esc(S(violation_message)).."]"
 	formspec = formspec.."button_exit[0.25,3.5;5,1;close_form;"..esc(S("ok")).."]"
 	return formspec
 end
