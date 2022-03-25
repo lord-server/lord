1b62f5240af0d1540c8695e5666df7e51273be12
Vladislav Tsendrovskii <vlad@vltc.net.eu.org>
2022-03-25 21:49:36 +0500

    Continue building 'skeleton' of AI

--- mods/lord/right_mobs/right_mobs_ai/init.lua
+++ mods/lord/right_mobs/right_mobs_ai/init.lua
@@ -1,6 +1,5 @@
 right_mobs_ai = {
 	states = {
-		death = 0,
 		stroll = 1,
 		rest = 2,
 		runaway = 3,
@@ -13,40 +12,205 @@ right_mobs_ai = {
 	},
 }
 
-local function default_attack(context, attack)
+local function serialize_state(state)
+	if state == right_mobs_ai.states.stroll then
+		return "stroll"
+	elseif state == right_mobs_ai.states.rest then
+		return "rest"
+	elseif state == right_mobs_ai.states.runaway then
+		return "runaway"
+	elseif state == right_mobs_ai.states.goto_target then
+		return "goto_target"
+	elseif state == right_mobs_ai.states.attack then
+		return "attack"
+	end
 end
 
-local function default_death(context)
+local function deserialize_state(state)
+	if state == "stroll" then
+		return right_mobs_ai.states.stroll
+	elseif state == "rest" then
+		return right_mobs_ai.states.rest
+	elseif state == "runaway" then
+		return right_mobs_ai.states.runaway
+	elseif state == "goto_target" then
+		return right_mobs_ai.states.goto_target
+	elseif state == "attack" then
+		return right_mobs_ai.states.attack
+	end
 end
 
-local function default_walk(context, target)
+-- processing actions
+local function process_attack(context, position, velocity)
+	local attack = nil
+	if context.definition.select_attack then
+		attack = context.definition.select_attack(context, position, velocity)	
+	end
+
+	if attack and context.target then
+		if context.definition.attack then
+			context.definition.attack(context, context.target, attack, context.userdata)
+		end
+	end
 end
 
-local function default_stay(context)
+local function process_walk(context, current_position, current_velocity, speed)
+	-- TODO: use random delta
+	local rx = 0
+	local ry = 0
+	local rz = 0
+ 	local target_position = {x = current_position.x + rx, y = current_position.y + ry, z = current_position.z + rz}
+	 if context.definition.walk then
+		context.definition.walk(context, target_position, speed, context.userdata)
+	end
 end
 
+local function process_targeting(context, current_position, current_velocity)
+	update_target_position(context)
+	if context.definition.walk then
+		context.definition.walk(context, context.target_position, context.definition.targeting_speed, context.userdata)
+	end
+end
+
+local function process_stay(context, position, velocity)
+	context.target = nil
+	context.target_position = nil
+	if context.definition.stay then
+		context.definition.stay(context, context.userdata)
+	end
+end
+
+local update_target_position = function(context)
+	if context.target then
+		context.target_position = context.target:get_pos()
+	end
+end
+
+-- handlers
 local function default_on_punched(context, puncher, attributes)
+	context.state = right_mobs_ai.states.goto_target
+	context.target = puncher
+	update_target_position(context)
 end
 
-local function default_think(context, dtime)
+local function default_on_target_lost(context)
+	context.state = right_mobs_ai.states.rest
 end
 
-right_mobs_ai.init_new_mob = function(self, def)
-	local context = {
+local function process_target_lost(context)
+	context.target = nil
+	context.target_position = nil
+	if context.definition.on_target_lost then
+		context.definition.on_target_lost(context)
+	end
+end
+
+local function default_think(context, position, velocity, dtime)
+end
+
+local function default_select_attack(context, position, velocity)
+	return nil
+end
+
+right_mobs_ai.process = function(self, context, position, velocity, dtime)
+	if context.state == right_mobs_ai.states.stroll then
+		process_walk(context, position, velocity, context.stroll_speed)
+	elseif context.state == right_mobs_ai.states.rest then
+		process_stay(context, position, velocity)
+	elseif context.state == right_mobs_ai.states.runaway then
+		process_walk(context, position, velocity, context.runaway_speed)
+	elseif context.state == right_mobs_ai.states.goto_target then
+		process_targeting(context, position, velocity)
+	elseif context.state == right_mobs_ai.states.attack then
+		process_attack(context, position, velocity)
+	end
+
+	if context.definition.think then
+		context.definition.think(context, position, velocity, dtime)
+	end
+end
+
+right_mobs_ai.punch = function(self, context, puncher, attributes)
+	if context.definition.on_punched then
+		context.definition.on_punched(context, puncher, attributes)
+	end
+end
+
+right_mobs_ai.get_targets = function(self, context)
+	if context.target then
+		return {context.target}
+	else
+		return {}
+	end
+end
+
+right_mobs_ai.target_lost = function(self, context, target)
+	if context.target == target then
+		process_target_lost(context)
+	end
+end
+
+right_mobs_ai.register_mob = function(self, name, def)
+	local definition = {
 		aux = def.aux,
-		state = self.states.rest,
 		available_attacks = def.available_attacks or {},
 
-		-- implementations of actions
-		attack = def.attack or default_attack
-		death = def.death or default_death,
-		walk = def.walk or default_walk,
-		stay = def.stay or default_stay,
+		-- actions
+		attack = def.attack,
+		walk = def.walk,
+		stay = def.stay,
 
 		-- reactions on external actions
-		punched = def.on_punched or default_on_punched,
+		on_punched = def.on_punched or default_on_punched,
+		on_target_lost = def.on_target_lost or default_on_target_lost,
 
 		-- decision function
 		think = def.think or default_think,
+		select_attack = def.select_attack or default_select_attack,
+
+		-- parameters
+		stroll_speed = def.stroll_speed or right_mobs_ai.defaults.speed,
+		runaway_speed = def.runaway_speed or right_mobs_ai.defaults.speed,
+		targeting_speed = def.targeting_speed or right_mobs_ai.defaults.speed,
 	}
+
+	self.mob_definitions[name] = config
+end
+
+right_mobs_ai.init_new_mob = function(self, name, userdata)
+	local context = {
+		definition = self.mob_definitions[name],
+		state = self.states.rest,
+		name = name,
+		userdata = userdata,
+	}
+	return context
+end
+
+right_mobs_ai.serialize = function(self, context)
+	-- сохранение целей сложно реализовать, поэтому при засыпании моб забывает цели
+	if context.state == self.states.attack or context.state == self.states.goto_target then
+		context.state = self.states.rest
+		context.target = nil
+		context.target_position = nil
+	end
+
+	local data = {
+		name = context.name,
+		state = serialize_state(context.state),
+	}
+	return minetest.serialize(data)
+end
+
+right_mobs_ai.init_from_serialized = function(self, serialized, userdata)
+	local deserialized = minetest.deserialize(data)
+	local name = deserialized.name
+	local state = deserialize_state(deserialized.state)
+	local context = {
+		name = name,
+		state = state,
+		definition = self.mob_definitions[name],
+		userdata = userdata,
+	}
+	return context
 end
