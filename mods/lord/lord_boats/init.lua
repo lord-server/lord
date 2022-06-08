48ef93059fd44e387c954caeaa4f4289c4a2a6b9
ssdaniel24 <bo7oaonteg2m@mail.ru>
2022-06-08 15:00:35 +0300

    [lord_boats] Refactoring boats, made API

--- mods/lord/lord_boats/init.lua
+++ mods/lord/lord_boats/init.lua
@@ -1,9 +1,70 @@
--- lord_boats/init.lua
+local S = minetest.get_translator("lord_boats")
 
 -- Load files
 local default_path = minetest.get_modpath("lord_boats")
-dofile(default_path.."/rowboat.lua")
-dofile(default_path.."/sailboat.lua")
+dofile(default_path.."/functions.lua")
+
+
+-- Registering boats
+lord_boats.register_boat("lord_boats:row_boat", {
+	initial_properties = {
+		physical = true,
+		-- Warning: Do not change the position of the collisionbox top surface,
+		-- lowering it causes the boat to fall through the world if underwater
+		collisionbox = { -0.6, -0.25, -0.6, 0.6, 0.45, 0.6, },
+		visual = "mesh",
+		mesh = "rowboat.obj",
+		textures = {"default_wood.png"},
+	},
+	driver_shift = { x = 0, y = 0.4, z = 0, },
+	driver_bone_position = { x = 0, y = 1, z = -4, },
+	control_modifier = { up = 1.0, down = 1.0, left_right = 0.5, },
+	vertical_acceleration = {
+		fast_condition = 3,
+		fast_up = 3,
+		up = 3,
+		down = 10,
+	},
+	description = S("Row Boat"),
+	inventory_image = "rowboat_inventory.png",
+	wield_image = "rowboat_wield.png",
+	recipe = {
+		{"",           "",           ""          },
+		{"group:wood", "",           "group:wood"},
+		{"group:wood", "group:wood", "group:wood"},
+	},
+	fuel_burntime = 20,
+})
+
+lord_boats.register_boat("lord_boats:sail_boat", {
+	initial_properties = {
+		physical = true,
+		-- Warning: Do not change the position of the collisionbox top surface,
+		-- lowering it causes the boat to fall through the world if underwater
+		collisionbox = { -1, -0.25, -1, 1, 0.65, 1, },
+		visual = "mesh",
+		mesh = "sailboat.obj",
+		textures = {"sailboat.png"},
+	},
+	driver_shift = { x = 0, y = 0.6, z = 0, },
+	driver_bone_position = { x = 0, y = 1, z = -1, },
+	control_modifier = { up = 2.0, down = 0.5, left_right = 0.9, },
+	vertical_acceleration = {
+		fast_condition = 1,
+		fast_up = 5,
+		up = 5,
+		down = 20,
+	},
+	description = S("Sail Boat"),
+	inventory_image = "sailboat_inventory.png",
+	wield_image = "sailboat_wield.png",
+	recipe = {
+		{"",           "wool:white",     ""          },
+		{"group:wood", "wool:white",     "group:wood"},
+		{"group:tree", "boats:row_boat", "group:tree"},
+	},
+	fuel_burntime = 30,
+})
 
 
 --Aliases for lord_boats
