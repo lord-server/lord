5bf7eb553eda93c6d3ea7354c6b0fc829b409d46
ssdaniel24 <107036969+ssdaniel24@users.noreply.github.com>
2022-07-20 21:09:45 +0300

    Небольшая правка lord_money (#652)

--- mods/lord/lord_money/money.lua
+++ mods/lord/lord_money/money.lua
@@ -4,28 +4,28 @@ local SL = minetest.get_translator("lord_money")
 
 minetest.register_craftitem("lord_money:diamond_coin", {
 	description = SL("Diamond Coin"),
-	inventory_image = "money_diamond_coin.png",
+	inventory_image = "lord_money_diamond_coin.png",
 	groups = {not_in_creative_inventory=1},
 	stack_max = 999,
 })
 
 minetest.register_craftitem("lord_money:gold_coin", {
 	description = SL("Gold Coin"),
-	inventory_image = "money_gold_coin.png",
+	inventory_image = "lord_money_gold_coin.png",
 	groups = {not_in_creative_inventory=1},
 	stack_max = 999,
 })
 
 minetest.register_craftitem("lord_money:silver_coin", {
 	description = SL("Silver Coin"),
-	inventory_image = "money_silver_coin.png",
+	inventory_image = "lord_money_silver_coin.png",
 	groups = {not_in_creative_inventory=1},
 	stack_max = 999,
 })
 
 minetest.register_craftitem("lord_money:copper_coin", {
 	description = SL("Copper Coin"),
-	inventory_image = "money_copper_coin.png",
+	inventory_image = "lord_money_copper_coin.png",
 	groups = {not_in_creative_inventory=1},
 	stack_max = 999,
 })
