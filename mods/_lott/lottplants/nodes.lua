8c8c768c2d860645257ac8dbdc47cd61e6a617e3
ssdaniel24 <107036969+ssdaniel24@users.noreply.github.com>
2022-07-24 20:28:42 +0300

    Fix leaf decay with using default.register_leafdecay (fixes #662) (#677)

--- mods/_lott/lottplants/nodes.lua
+++ mods/_lott/lottplants/nodes.lua
@@ -1044,6 +1044,73 @@ minetest.register_node("lottplants:yavannamirefruit", {
 	end,
 })
 
+-- LEAFDECAY
+-- Регистрация опадающей листвы и др.
+
+-- Alders / Ольха
+default.register_leafdecay({
+	trunks = {"lottplants:aldertree"},
+	leaves = {"lottplants:alderleaf"},
+	radius = 2,
+})
+
+default.register_leafdecay({
+	trunks = {"default:tree"},
+	leaves = {
+		"lottplants:appleleaf", "default:leaves", "default:apple", -- Apple Tree / Яблоня
+		"lottplants:beechleaf", -- Beeches / Бук
+		"lottplants:culumaldaleaf", "lottplants:yellowflowers", -- Culumalda / Кулумальда
+		"lottplants:elmleaf", -- Elms / Вяз
+		"lottplants:plumleaf", "lottplants:plum", -- Plum Tree / Слива
+		"lottplants:rowanleaf", "lottplants:rowanberry", -- Rowans / Рябина
+		"lottplants:whiteleaf", -- White Tree / Белое дерево
+		"lottplants:yavannamireleaf", "lottplants:yavannamirefruit", -- Yavannamire / Йаванамирэ
+	},
+	radius = 6, -- такая большая цифра только из-за бука
+})
+
+-- Lebethron / Лебетрон
+default.register_leafdecay({
+	trunks = {"lottplants:lebethrontree"},
+	leaves = {"lottplants:lebethronleaf"},
+	radius = 2,
+})
+
+-- Birches / Береза
+default.register_leafdecay({
+	trunks = {"lottplants:birchtree"},
+	leaves = {"lottplants:birchleaf"},
+	radius = 3,
+})
+
+-- Firs / Ель
+default.register_leafdecay({
+	trunks = {"lottplants:firtree"},
+	leaves = {"lottplants:firleaf"},
+	radius = 4,
+})
+
+-- (Young) Mallorn / (Молодой) маллорн
+default.register_leafdecay({
+	trunks = {"lottplants:mallorntree", "lottplants:mallorntree_young"},
+	leaves = {"lottplants:mallornleaf"},
+	radius = 2,
+})
+
+-- Pines / Сосна
+default.register_leafdecay({
+	trunks = {"lottplants:pinetree"},
+	leaves = {"lottplants:pineleaf"},
+	radius = 2,
+})
+
+-- Mirk Large/Small / Большое/Малое дерево Лихолесья
+default.register_leafdecay({
+	trunks = {"default:jungletree"},
+	leaves = {"lottplants:mirkleaf"},
+	radius = 2,
+})
+
 --Wood
 
 minetest.register_node("lottplants:pinewood", {
