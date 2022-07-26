c17ba2a532c1d4845a3d4db3e35435c30c0003c5
ssdaniel24 <bo7oaonteg2m@mail.ru>
2022-07-26 13:51:36 +0300

    Combining visual negative effects into one function

--- mods/_lott/lottfarming/orc_food.lua
+++ mods/_lott/lottfarming/orc_food.lua
@@ -1,5 +1,28 @@
 local SL = lord.require_intllib()
 
+local function make_negative_visual_effect(user)
+	local first_screen = user:hud_add({
+		hud_elem_type = "image",
+		position = {x=0, y=0},
+		scale = {x=100, y=100},
+		text = "orc_negative_effect.png",
+		offset = {x=0, y=0},
+	})
+	minetest.after(10, function()
+		user:hud_remove(first_screen)
+		local second_screen = user:hud_add({
+			hud_elem_type = "image",
+			position = {x=0.5, y=0.5},
+			scale = {x=-100, y=-100},
+			text = "orc_negative_effect1.png",
+			offset = {x=0, y=0},
+		})
+		minetest.after(10, function()
+			user:hud_remove(second_screen)
+		end)
+	end)
+end
+
 minetest.register_craftitem("lottfarming:orc_food", {
 	description = SL("Orc Food"),
 	inventory_image = "lottfarming_orc_food.png",
@@ -7,26 +30,7 @@ minetest.register_craftitem("lottfarming:orc_food", {
 		local name = user:get_player_name()
 		hbhunger.hunger[name] = 20
 		hbhunger.set_hunger_raw(user)
-		local first_screen = user:hud_add({
-			hud_elem_type = "image",
-			position = {x=0, y=0},
-			scale = {x=100, y=100},
-			text = "orc_food.png",
-			offset = {x=0, y=0},
-		})
-		minetest.after(10, function()
-			user:hud_remove(first_screen)
-			local second_screen = user:hud_add({
-				hud_elem_type = "image",
-				position = {x=0.5, y=0.5},
-				scale = {x=-100, y=-100},
-				text = "orc_food1.png",
-				offset = {x=0, y=0},
-			})
-			minetest.after(10, function()
-				user:hud_remove(second_screen)
-			end)
-		end)
+		make_negative_visual_effect(user)
 		itemstack:take_item(1)
 		local inv = user:get_inventory()
 		if inv:room_for_item("main", "lottfarming:bowl") then
@@ -52,26 +56,7 @@ minetest.register_craftitem("lottfarming:orc_medicine", {
 	inventory_image = "lottfarming_orc_medicine.png",
 	on_use = function(itemstack, user, pointed_thing)
 		user:set_hp(20)
-		local first_screen = user:hud_add({
-			hud_elem_type = "image",
-			position = {x=0, y=0},
-			scale = {x=100, y=100},
-			text = "orc_medicine.png",
-			offset = {x=0, y=0},
-		})
-		minetest.after(10, function()
-			user:hud_remove(first_screen)
-			local second_screen = user:hud_add({
-				hud_elem_type = "image",
-				position = {x=0.5, y=0.5},
-				scale = {x=-100, y=-100},
-				text = "orc_medicine1.png",
-				offset = {x=0, y=0},
-			})
-			minetest.after(10, function()
-				user:hud_remove(second_screen)
-			end)
-		end)
+		make_negative_visual_effect(user)
 		itemstack:take_item(1)
 		local inv = user:get_inventory()
 		if inv:room_for_item("main", "vessels:drinking_glass") then
