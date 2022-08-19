c728f0faca19b0e83ef641c95489167661aedb82
Aleksey <weqqr@yandex.ru>
2022-08-19 20:20:34 +0300

    Revert "Update lottblocks/wooden_stuff.lua - new fences (#602)" (#704)
    This reverts commit cd2c47f5965b8b68264c158897136ddf616a1769.

--- mods/lord/_overwrites/MTG/default/init.lua
+++ mods/lord/_overwrites/MTG/default/init.lua
@@ -1,5 +1,4 @@
 local S = default.get_translator
-local S2 = minetest.get_translator(minetest.get_current_modname()) -- для фикса локализации
 
 -- default/chests.lua
 
@@ -504,7 +503,7 @@ minetest.register_node(":default:tree_trunk", {
 })
 
 minetest.register_node(":default:jungletree_trunk", {
-	description = S2("Jungle Tree Trunk"),
+	description = S("Jungle Tree Trunk"),
 	tiles = {"default_jungletree_top.png", "default_jungletree_top.png", "default_jungletree.png"},
 	paramtype2 = "facedir",
 	is_ground_content = false,
@@ -635,12 +634,4 @@ minetest.register_abm({
 	action = function(...)
 		grow_papyrus_but_on_soils(...)
 	end
-})
-
--- Фикс локализации эвкалипта
-minetest.override_item("default:jungletree", {
-	description = S2("Jungle Tree"),
-})
-minetest.override_item("default:junglewood", {
-	description = S2("Jungle Wood"),
 })
