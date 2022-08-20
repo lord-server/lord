0807f008a53313910f4dfaa2dbdb733413a0fa1c
ssdaniel24 <bo7oaonteg2m@mail.ru>
2022-08-20 22:21:22 +0300

    Refactored and update protector doors with new registration

--- mods/_various/protector_lott/doors.lua
+++ mods/_various/protector_lott/doors.lua
@@ -2,36 +2,7 @@ local SL = lord.require_intllib()
 
 -- Register Protected Doors
 
-local function on_rightclick(pos, dir, check_name, replace, replace_dir, params)
-	pos.y = pos.y+dir
-	if minetest.get_node(pos).name ~= check_name then
-		return
-	end
-	local p2 = minetest.get_node(pos).param2
-	p2 = params[p2 + 1]
-
-	minetest.swap_node(pos, {name = replace_dir, param2 = p2})
-
-	pos.y = pos.y-dir
-	minetest.swap_node(pos, {name = replace, param2 = p2})
-
-	local snd_1 = "doors_door_close"
-	local snd_2 = "doors_door_open"
-	if params[1] == 3 then
-		snd_1 = "doors_door_open"
-		snd_2 = "doors_door_close"
-	end
-
-	if minetest.get_meta(pos):get_int("right") ~= 0 then
-		minetest.sound_play(snd_1, {
-			pos = pos, gain = 0.3, max_hear_distance = 10})
-	else
-		minetest.sound_play(snd_2, {
-			pos = pos, gain = 0.3, max_hear_distance = 10})
-	end
-end
-
-local function reg_prot_door(desc, name, door, mat, texture_i, texture_a, texture_b, texture_c)
+local function reg_prot_door(desc, name, door, mat, texture_i, texture_uv)
 	local gd
 	if mat == "wood" then
 		gd = {snappy = 1, choppy = 2, oddly_breakable_by_hand = 2, door = 1, unbreakable = 1, wooden = 1}
@@ -41,56 +12,19 @@ local function reg_prot_door(desc, name, door, mat, texture_i, texture_a, textur
 		gd = {snappy = 1, choppy = 2, oddly_breakable_by_hand = 2, door = 1, unbreakable = 1}
 	end
 	doors.register_door(name, {
+		tiles = {{ name = texture_uv.."^protector_logo_door_uv.png", backface_culling = true }},
 		description = SL(desc),
 		inventory_image = texture_i.."^protector_logo_i.png",
 		groups = gd,
-		tiles_bottom = {texture_b.."^protector_logo.png", texture_c},
-		tiles_top = {texture_a, texture_c},
-		sounds = default.node_sound_wood_defaults(),
 		sunlight = true,
-	})
-
-	minetest.override_item(name .. "_b_1", {
-		on_rightclick = function(pos, node, clicker)
-			if not minetest.is_protected(pos, clicker:get_player_name()) then
-				on_rightclick(pos, 1,
-				name .. "_t_1", name .. "_b_2", name .. "_t_2", {1, 2, 3, 0})
-			end
-		end,
-	})
-
-	minetest.override_item(name.."_t_1", {
-		on_rightclick = function(pos, node, clicker)
-			if not minetest.is_protected(pos, clicker:get_player_name()) then
-				on_rightclick(pos, -1,
-				name .. "_b_1", name .. "_t_2", name .. "_b_2", {1, 2, 3, 0})
-			end
-		end,
-	})
-
-	minetest.override_item(name.."_b_2", {
-		on_rightclick = function(pos, node, clicker)
-			if not minetest.is_protected(pos, clicker:get_player_name()) then
-				on_rightclick(pos, 1,
-				name .. "_t_2", name .. "_b_1", name .. "_t_1", {3, 0, 1, 2})
-			end
-		end,
-	})
-
-	minetest.override_item(name.."_t_2", {
-		on_rightclick = function(pos, node, clicker)
-			if not minetest.is_protected(pos, clicker:get_player_name()) then
-				on_rightclick(pos, -1,
-				name .. "_b_2", name .. "_t_1", name .. "_b_1", {3, 0, 1, 2})
-			end
-		end,
-	})
-
-	minetest.register_craft({
-		output = name,
 		recipe = {
 			{door, "protector_lott:protect2"}
-		}
+		},
+		on_rightclick = function(pos, node, clicker)
+			if not minetest.is_protected(pos, clicker:get_player_name()) then
+				doors.door_toggle(pos, node, clicker)
+			end
+		end,
 	})
 end
 
@@ -99,31 +33,25 @@ end
 
 reg_prot_door(
 	"Protected Wooden Door", "protector_lott:door_wood", "doors:door_wood", "wood",
-	"doors_wood.png",
-	"doors_wood_a.png",
-	"doors_wood_b.png",
-	"doors_brown.png"
+	"doors_item_wood.png",
+	"doors_door_wood.png"
 )
 
 reg_prot_door(
 	"Protected Steel Door", "protector_lott:door_steel", "doors:door_steel", "steel",
-	"doors_steel.png", "doors_steel_a.png", "doors_steel_b.png", "doors_grey.png"
+	"doors_item_steel.png", "doors_door_steel.png"
 )
 
 reg_prot_door(
 	"Protected Glass Door", "protector_lott:door_glass", "doors:door_glass", "glass",
-	"doors_glass.png",
-	"doors_glass_a.png",
-	"doors_glass_b.png",
-	"doors_glass_side.png"
+	"doors_item_glass.png",
+	"doors_door_glass.png"
 )
 
 reg_prot_door(
 	"Protected Obsidian Glass Door", "protector_lott:door_obsidian_glass", "doors:door_obsidian_glass", "glass",
-	"doors_obsidian_glass.png",
-	"doors_obsidian_glass_a.png",
-	"doors_obsidian_glass_b.png",
-	"doors_obsidian_glass_side.png"
+	"doors_item_obsidian_glass.png",
+	"doors_door_obsidian_glass.png"
 )
 
 -- from lottblocks mod
@@ -131,61 +59,47 @@ reg_prot_door(
 reg_prot_door(
 	"Protected Junglewood Door", "protector_lott:door_junglewood", "lottblocks:door_junglewood", "wood",
 	"lottblocks_door_junglewood.png",
-	"lottblocks_door_junglewood_a.png",
-	"lottblocks_door_junglewood_b.png",
-	"lottblocks_edge_junglewood.png"
+	"lottblocks_door_junglewood_uv.png"
 )
 
 reg_prot_door(
 	"Protected Alder Door", "protector_lott:door_alder", "lottblocks:door_alder", "wood",
 	"lottblocks_door_alder.png",
-	"lottblocks_door_alder_a.png",
-	"lottblocks_door_alder_b.png",
-	"lottblocks_edge_alder.png"
+	"lottblocks_door_alder_uv.png"
 )
 
 reg_prot_door(
 	"Protected Birch Door", "protector_lott:door_birch", "lottblocks:door_birch", "wood",
 	"lottblocks_door_birch.png",
-	"lottblocks_door_birch_a.png",
-	"lottblocks_door_birch_b.png",
-	"lottblocks_edge_birch.png"
+	"lottblocks_door_birch_uv.png"
 )
 
 reg_prot_door(
 	"Protected Pine Door", "protector_lott:door_pine", "lottblocks:door_pine", "wood",
-	"lottblocks_door_pine.png", "lottblocks_door_pine_a.png", "lottblocks_door_pine_b.png", "lottblocks_edge_pine.png"
+	"lottblocks_door_pine.png", "lottblocks_door_pine_uv.png"
 )
 
 reg_prot_door(
 	"Protected Lebethron Door", "protector_lott:door_lebethron", "lottblocks:door_lebethron", "wood",
 	"lottblocks_door_lebethron.png",
-	"lottblocks_door_lebethron_a.png",
-	"lottblocks_door_lebethron_b.png",
-	"lottblocks_edge_lebethron.png"
+	"lottblocks_door_lebethron_uv.png"
 )
 
 reg_prot_door(
 	"Protected Mallorn Door", "protector_lott:door_mallorn", "lottblocks:door_mallorn", "wood",
 	"lottblocks_door_mallorn.png",
-	"lottblocks_door_mallorn_a.png",
-	"lottblocks_door_mallorn_b.png",
-	"lottblocks_edge_mallorn.png"
+	"lottblocks_door_mallorn_uv.png"
 )
 
 -- from castle mod
 reg_prot_door(
 	"Protected Oak Door", "protector_lott:oak_door", "castle:oak_door", "wood",
-	"castle_oak_door_inv.png",
-	"castle_oak_door_top.png",
-	"castle_oak_door_bottom.png",
-	"door_oak.png"
+	"castle_oak_door.png",
+	"castle_oak_door_uv.png"
 )
 
 reg_prot_door(
 	"Protected Jail Door", "protector_lott:jail_door", "castle:jail_door", "steel",
-	"castle_jail_door_inv.png",
-	"castle_jail_door_top.png",
-	"castle_jail_door_bottom.png",
-	"door_jail.png"
+	"castle_jail_door.png",
+	"castle_jail_door_uv.png"
 )
