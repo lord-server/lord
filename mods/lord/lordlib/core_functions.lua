6e91bf4a657979c33999f6ca00a1a2703bcf1fbd
Van <vanicgame@yandex.ru>
2022-08-22 18:41:04 +0300

    Review fix 4

--- mods/lord/lordlib/core_functions.lua
+++ mods/lord/lordlib/core_functions.lua
@@ -7,10 +7,13 @@ local MICROSECONDS = 1000000
 -- Нажатие
 lord.registered_on_press = {}
 
---- register_on_press(func(player, control_name))
---- Регистрирует обратный вызов на нажатие кнопки из списка controls
---- * player - игрок
---- * control_name - кнопка из списка controls
+---
+--- Регистрирует обратный вызов на нажатие кнопки из списка controls.
+---
+--- @param func fun(player:Player, control_name:string)
+--- Принимает функцию колбека со след параметрами:
+---  * `player` - игрок
+---  * `control_name` - кнопка из списка controls
 lord.register_on_press = function(func)
 	table.insert(lord.registered_on_press, func)
 end
@@ -18,11 +21,14 @@ end
 -- Отпуск
 lord.registered_on_release = {}
 
---- register_on_release(func(player, control_name, release_time))
+---
 --- Регистрирует обратный вызов на отпуск кнопки из списка controls
---- * player - игрок
---- * control_name - кнопка из списка controls
---- * release_time - время удержания кнопки до того, как её отпустили
+---
+--- @param func fun(player, control_name, release_time)
+--- Принимает функцию колбека со след параметрами:
+---  * `player` - игрок
+---  * `control_name` - кнопка из списка controls
+---  * `release_time` - время удержания кнопки до того, как её отпустили
 lord.register_on_release = function(func)
 	table.insert(lord.registered_on_release, func)
 end
@@ -30,12 +36,15 @@ end
 -- Удержание
 lord.registered_on_hold = {}
 
---- register_on_hold(func(player, control_name, hold_time, dtime))
+---
 --- Регистрирует обратный вызов на удержании кнопки из списка controls
---- * player - игрок
---- * control_name - кнопка из списка controls
---- * hold_time - время удержания кнопки
---- * dtime - время между прошлым и нынешним вызовом
+---
+--- @param func fun(player, control_name, hold_time, dtime)
+--- Принимает функцию колбека со след параметрами:
+---  * `player` - игрок
+---  * `control_name` - кнопка из списка controls
+---  * `hold_time` - время удержания кнопки
+---  * `dtime` - время между прошлым и нынешним вызовом
 lord.register_on_hold = function(func)
 	table.insert(lord.registered_on_hold, func)
 end
@@ -43,19 +52,22 @@ end
 -- Смена индекса предмета в руке
 lord.registered_on_wield_index_change = {}
 
---- register_on_wield_index_change(func(player, player_wield_index, player_last_wield_index))
+---
 --- Регистрирует обратный вызов на смену индекса предмета в руке
---- * player - игрок
---- * player_wield_index - текущий индекс предмета в руке
---- * player_last_wield_index - прошлый индекс предмета в руке
+---
+--- @param func fun(player, player_wield_index, player_last_wield_index)
+--- Принимает функцию колбека со след параметрами:
+---  * `player` - игрок
+---  * `player_wield_index` - текущий индекс предмета в руке
+---  * `player_last_wield_index` - прошлый индекс предмета в руке
 lord.register_on_wield_index_change = function(func)
 	table.insert(lord.registered_on_wield_index_change, func)
 end
 
---- reset_hold_time(player, control_name)
+---
 --- Сбрасывает время удержания кнопки из списка controls
---- * player - игрок
---- * control_name - кнопка из списка controls
+--- @param player ObjRef - игрок
+--- @param control_name string - кнопка из списка controls
 lord.reset_hold_time = function(player, control_name)
 	if not player then return end
 
