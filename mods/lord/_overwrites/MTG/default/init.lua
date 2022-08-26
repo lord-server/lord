cf53edb2aa90589d7321408456c84504e9c40bde
ssdaniel24 <bo7oaonteg2m@mail.ru>
2022-08-26 11:20:15 +0300

    Fixed default:ladder_wood to be simular with other ladders ✍️

--- mods/lord/_overwrites/MTG/default/init.lua
+++ mods/lord/_overwrites/MTG/default/init.lua
@@ -525,6 +525,75 @@ minetest.override_item("default:jungletree", {
 	end,
 })
 
+-- смена вида лестницы (ladder) на nodebox из lottblocks/wooden_stuff.lua
+minetest.override_item("default:ladder_wood", {
+	drawtype = "nodebox",
+	tiles = {"default_wood.png"},
+	particle_image = {"default_wood.png"},
+	paramtype2 = "facedir",
+	node_box = {
+		type = "fixed",
+		fixed = {
+			{ -0.5, -0.5, 0.5 - 1 / 7, -0.5 + 1 / 7, 0.5, 0.5 },
+			{ 0.5 - 1 / 7, -0.5, 0.5 - 1 / 7, 0.5, 0.5, 0.5 },
+			{ -0.5 + 1 / 7, 0.5 - 1 / 6 - 1 / 12, 0.5 - 1 / 16, 0.5 - 1 / 7, 0.5 - 1 / 12, 0.5 },
+			{ -0.5 + 1 / 7, 0.5 - 1 / 12 - 1 / 6 * 3, 0.5 - 1 / 16, 0.5 - 1 / 7, 0.5 - 1 / 12 - 1 / 6 * 2, 0.5 },
+			{ -0.5 + 1 / 7, 0.5 - 1 / 12 - 1 / 6 * 5, 0.5 - 1 / 16, 0.5 - 1 / 7, 0.5 - 1 / 12 - 1 / 6 * 4, 0.5 },
+		},
+	},
+	selection_box = {
+		type = "fixed",
+		fixed = {
+			{ -0.5, -0.5, 0.5 - 1 / 7, -0.5 + 1 / 7, 0.5, 0.5 },
+			{ 0.5 - 1 / 7, -0.5, 0.5 - 1 / 7, 0.5, 0.5, 0.5 },
+			{ -0.5 + 1 / 7, 0.5 - 1 / 6 - 1 / 12, 0.5 - 1 / 16, 0.5 - 1 / 7, 0.5 - 1 / 12, 0.5 },
+			{ -0.5 + 1 / 7, 0.5 - 1 / 12 - 1 / 6 * 3, 0.5 - 1 / 16, 0.5 - 1 / 7, 0.5 - 1 / 12 - 1 / 6 * 2, 0.5 },
+			{ -0.5 + 1 / 7, 0.5 - 1 / 12 - 1 / 6 * 5, 0.5 - 1 / 16, 0.5 - 1 / 7, 0.5 - 1 / 12 - 1 / 6 * 4, 0.5 },
+		},
+	},
+	on_place = function(itemstack, placer, pointed_thing)
+		if pointed_thing.type == "node" and
+			minetest.registered_nodes[minetest.get_node(pointed_thing.above).name].buildable_to == true then
+			local under = pointed_thing.under
+			local node_under = minetest.get_node(under)
+			local param2 = nil
+			local above = pointed_thing.above
+			local above_2 = { x = above.x, y = above.y, z = above.z }
+			above_2.y = above_2.y + 1
+
+			if minetest.registered_nodes[node_under.name].on_rightclick then
+				return minetest.registered_nodes[node_under.name].on_rightclick(under, node_under, placer, itemstack)
+			end
+
+			if minetest.is_protected(above, placer:get_player_name()) or
+				minetest.is_protected(above_2, placer:get_player_name()) then
+				minetest.record_protection_violation(above, placer:get_player_name())
+				return itemstack
+			end
+
+			if pointed_thing.above.x < pointed_thing.under.x then
+				param2 = 1
+			elseif pointed_thing.above.x > pointed_thing.under.x then
+				param2 = 3
+			elseif pointed_thing.above.z < pointed_thing.under.z then
+				param2 = 0
+			elseif pointed_thing.above.z > pointed_thing.under.z then
+				param2 = 2
+			end
+
+			if param2 then
+				minetest.set_node(pointed_thing.above, { name = "default:ladder_wood", param2 = param2 })
+				if not default.creative then
+					itemstack:take_item()
+				end
+			end
+			return itemstack
+		end
+	end,
+	node_placement_prediction = "",
+})
+
+
 
 -- default/functions.lua
 
