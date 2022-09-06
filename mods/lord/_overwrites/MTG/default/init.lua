2c338955ce248130f292c140c92477c9da3dedef
ssdaniel24 <bo7oaonteg2m@mail.ru>
2022-09-06 21:13:07 +0300

    Made falling_trees param always true

--- mods/lord/_overwrites/MTG/default/init.lua
+++ mods/lord/_overwrites/MTG/default/init.lua
@@ -531,54 +531,37 @@ minetest.override_item("default:jungletree", {
 
 -- Падение ствола деревьев вниз при ломании
 
-local falling_trees = minetest.settings:get_bool("falling_trees")
+function default.dig_tree(pos, node, name, digger, height, radius, drop)
+	minetest.node_dig(pos, node, digger)
 
-if not falling_trees then
-	if minetest.is_singleplayer() then
-		falling_trees = false
-	else
-		falling_trees = true
+	if minetest.is_protected(pos, digger:get_player_name()) then
+		return
 	end
-end
 
-if falling_trees == true then
-	function default.dig_tree(pos, node, name, digger, height, radius, drop)
-		minetest.node_dig(pos, node, digger)
-
-		if minetest.is_protected(pos, digger:get_player_name()) then
-			return
-		end
-
-		local base_y = pos.y
-		for i = 1, (height + 5) do
-			pos.y        = base_y + i
-			local node_i = minetest.get_node(pos)
-			if node_i.name ~= name or i == (height + 5) then
-				minetest.remove_node({ x = pos.x, y = pos.y - 1, z = pos.z })
-				for k = -radius, radius do
-					for l = -radius, radius do
-						for j = 0, 1 do
-							local tree_bellow = minetest.get_node({ x = pos.x + k, y = pos.y - 1, z = pos.z + l })
-							if tree_bellow.name ~= name then
-								local pos1 = { x = pos.x + k, y = pos.y + j, z = pos.z + l }
-								if minetest.get_node(pos1).name == name then
-									minetest.spawn_item(pos1, drop)
-									minetest.remove_node(pos1)
-								end
+	local base_y = pos.y
+	for i = 1, (height + 5) do
+		pos.y        = base_y + i
+		local node_i = minetest.get_node(pos)
+		if node_i.name ~= name or i == (height + 5) then
+			minetest.remove_node({ x = pos.x, y = pos.y - 1, z = pos.z })
+			for k = -radius, radius do
+				for l = -radius, radius do
+					for j = 0, 1 do
+						local tree_bellow = minetest.get_node({ x = pos.x + k, y = pos.y - 1, z = pos.z + l })
+						if tree_bellow.name ~= name then
+							local pos1 = { x = pos.x + k, y = pos.y + j, z = pos.z + l }
+							if minetest.get_node(pos1).name == name then
+								minetest.spawn_item(pos1, drop)
+								minetest.remove_node(pos1)
 							end
 						end
 					end
 				end
-				return
-			elseif node_i.name == name then
-				minetest.set_node({ x = pos.x, y = pos.y - 1, z = pos.z }, { name = name })
 			end
+			return
+		elseif node_i.name == name then
+			minetest.set_node({ x = pos.x, y = pos.y - 1, z = pos.z }, { name = name })
 		end
-	end
-else
-	function default.dig_tree(pos, node, name, digger, height, radius, drop)
-		minetest.node_dig(pos, node, digger)
-		return nil
 	end
 end
 
