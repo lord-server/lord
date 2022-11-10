d7df30b0abff55bb210f369a52bde7fee9f66deb
Alexander Chibrikin <alek13.me@gmail.com>
2022-11-11 02:44:57 +0300

    Fix books capabilities for correct tooltips. fixes #800 (#820)

--- mods/_lott/lottinventory/forbidden.lua
+++ mods/_lott/lottinventory/forbidden.lua
@@ -223,15 +223,6 @@ minetest.register_tool("lottinventory:forbidden_crafts_book",{
     wield_image = "",
     wield_scale = {x=1,y=1,z=1},
     stack_max = 1,
-    tool_capabilities = {
-        full_punch_interval = 1.0,
-        max_drop_level=0,
-        groupcaps={
-            fleshy={times={[2]=0.80, [3]=0.40}, uses=20, maxlevel=1},
-            snappy={times={[2]=0.80, [3]=0.40}, uses=20, maxlevel=1},
-            choppy={times={[3]=0.90}, uses=20, maxlevel=0}
-        }
-    },
     groups = {armor_crafts=1, book=1, paper=1},
     on_use = function(itemstack, player, pointed_thing)
 		local pn = player:get_player_name();
