a8e34dcdd8025e2ef7fc5b169d415e4f67621c78
Alexander Chibrikin <alek13.me@gmail.com>
2022-12-02 03:41:56 +0300

    #267 Update MTG/default: upd mapgen from MTG 5.4.1

--- mods/_minetest_game/default/init.lua
+++ mods/_minetest_game/default/init.lua
@@ -55,7 +55,8 @@ dofile(default_path.."/tools.lua")
 dofile(default_path.."/item_entity.lua")
 dofile(default_path.."/craftitems.lua")
 dofile(default_path.."/crafting.lua")
--- не обновлён, т.к. генерация своя(убогая, но какая досталась), пока нет возможности обновить её:
+-- обновлён, но не подключаем, т.к. генерация своя(убогая, но какая досталась), пока нет возможности обновить её.
+-- генерация перенесена в `mods/lord/_overwrites/MTG/default/mapgen.lua`
 --dofile(default_path.."/mapgen.lua")
 -- добавились, но пока что не подключаем, т.к. есть аналогичные наши
 --dofile(default_path.."/aliases.lua")
