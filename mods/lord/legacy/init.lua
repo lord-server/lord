d98674e82bcc45dcf7ea09597a00db17ca7784e1
Doloment <doloment@internet.ru>
2022-12-29 07:17:55 +0700

    Closes #894. Remove 'default:junglesapling', use 'lottplants:mirksapling' instead

--- mods/lord/legacy/init.lua
+++ mods/lord/legacy/init.lua
@@ -102,6 +102,8 @@ minetest.register_alias("default:sign_wall_wood", "default:sign_wall")
 
 --- Далее идут наши алиасы
 
+-- Remove junglesapling
+minetest.register_alias("default:junglesapling", "lottplants:mirksapling")
 
 minetest.register_alias("scorched_stuff", "default:scorched_stuff")
 minetest.register_alias("rat", "default:dirt")
