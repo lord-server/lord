178eec5d72f14f546d1f1674c19073940b14ba48
Van <82447582+VanicGame@users.noreply.github.com>
2023-01-11 23:39:08 +0300

    Lord bows _experimental. Relates to #76 (#700)
     * Добавлены луки с механикой натягивания тетивы и стрелы с поворотом в сторону вектора движения
     * Мод экспериментальный, нужно тестить, в т.ч. на арене в режиме PvP (см. #700)
     * Без крафтов
    
    Co-authored-by: Alexander Chibrikin <alek13.me@gmail.com>

---
+++ mods/lord/_experimental/lord_bows/entities_projectiles.lua
@@ -0,0 +1,116 @@
+-- entities projectiles
+
+projectiles  = {}
+local pi     = math.pi
+local arctan = math.atan2
+local sqr    = math.sqrt
+
+-- Обновление таймера жизни стрелы
+local function update_life_timer(entity, dtime)
+	if not entity.timer_is_start then
+		return
+	end
+
+	entity.life_timer = entity.life_timer - dtime
+
+	if entity.life_timer <= 0 then
+		entity.object:remove()
+	end
+end
+
+-- Нанесение урона цели
+local function punch_target(entity, target, damage)
+	target:punch(entity.object, 1.0, {
+		full_punch_interval = 1.0,
+		damage_groups       = {fleshy = damage},
+	})
+	entity.object:remove()
+end
+
+-- Обработка попадания взависимости от цели
+local function hit_handling(entity, target, name, def)
+	-- Попадание по игроку
+	if target:is_player() then
+		punch_target(entity, target, def.damage)
+	else
+		--Столкновение двух стрел
+		if target:get_luaentity().name == name then
+			entity.object:set_acceleration({x = 0, y = GRAVITY * -1, z = 0})
+			target:set_acceleration({x = 0, y = GRAVITY * -1, z = 0})
+			-- Попадание по сущности
+		else
+			punch_target(entity, target, def.damage)
+		end
+	end
+end
+
+-- Обработка столкновения
+local function collision_handling(entity, moveresult, name, def)
+	entity.object:set_velocity({x = 0, y = 0, z = 0})
+	entity.object:set_acceleration({x = 0, y = 0, z = 0})
+
+	entity.timer_is_start = true
+
+	if not moveresult.collisions[1] then
+		return
+	end
+
+	if moveresult.collisions[1].type == "node" then
+		return
+	end
+
+	local target = moveresult.collisions[1].object
+
+	hit_handling(entity, target, name, def)
+end
+
+local function flight_processing(entity)
+	local vel = entity.object:get_velocity()
+	if vel.y ~= 0 then
+		local rot = {
+			x = 0,
+			y = pi + arctan(vel.z, vel.x),
+			z = arctan(vel.y, sqr(vel.z * vel.z + vel.x * vel.x))}
+		entity.object:set_rotation(rot)
+	end
+end
+
+projectiles.register_projectile_arrow_type = function(name, item, def)
+	-- Независящие от def параметры
+	minetest.register_entity(name, {
+		max_hp               = 1,
+		physical             = true,
+		collide_with_objects = true,
+		pointable            = true, -- Потом изменить на false
+		use_texture_alpha    = true,
+		visual_size          = {x = 1.5, y = 1.5, z = 1.5},
+		visual               = "mesh",
+		mesh                 = "projectile_arrow_type.obj",
+		collisionbox         = {-0.15, -0.15, -0.15, 0.15, 0.15, 0.15},
+
+		-- Таймер жизни:
+		life_timer           = 20,
+		timer_is_start       = false,
+
+		-- Зависящие от def параметры
+		textures             = def.textures,
+
+		-- Функции
+		on_step              = function(self, dtime, moveresult)
+
+			if moveresult.collides or moveresult.standing_on_object then
+				collision_handling(self, moveresult, name, def)
+			else
+				flight_processing(self)
+			end
+
+			update_life_timer(self, dtime)
+		end,
+
+		on_rightclick        = function(self, clicker)
+			local pos = self.object:get_pos()
+			self.object:remove()
+			minetest.add_item(pos, item)
+		end,
+	})
+end
---
+++ mods/lord/_experimental/lord_bows/init.lua
@@ -0,0 +1,67 @@
+-- lord bows
+GRAVITY = minetest.settings:get("movement_gravity") or 9.81
+
+dofile(minetest.get_modpath(minetest.get_current_modname()) .. "/entities_projectiles.lua")
+dofile(minetest.get_modpath(minetest.get_current_modname()) .. "/mechanics_throwing.lua")
+
+local function table_merge(t1, t2)
+	for k, v in pairs(t2) do
+		t1[k] = v
+	end
+	return t1
+end
+
+local stage_groups = {not_in_creative_inventory = 1}
+
+local function register_bow(name, def)
+	local wield_scale      = {x = 2, y = 2, z = 0.75}
+
+	local all_stage_groups = table_merge(stage_groups, def.groups)
+
+	minetest.register_tool(name, {
+		range             = 1,
+		description       = def.desc,
+		wield_scale       = wield_scale,
+		inventory_image   = def.inventory_image .. ".png",
+		wield_image       = def.inventory_image .. ".png",
+		tool_capabilities = def.tool_capabilities,
+		groups            = def.groups,
+	})
+
+	for i = 2, 4 do
+		minetest.register_tool(name .. "_" .. i, {
+			description       = def.desc,
+			range             = 0,
+			wield_scale       = wield_scale,
+			inventory_image   = def.inventory_image .. "_" .. i .. ".png",
+			wield_image       = def.inventory_image .. "_" .. i .. ".png",
+			tool_capabilities = def.tool_capabilities,
+			groups            = all_stage_groups,
+		})
+	end
+end
+
+register_bow("lord_bows:bow_wooden", {
+	desc              = "Wooden bow",
+	inventory_image   = "items_tools_bow_wooden", -- указывается без .png
+	tool_capabilities = {
+		full_punch_interval = 1.2,
+		max_drop_level      = 0,
+		groupcaps           = {
+			crumbly = {
+				times    = {[1] = 3.00, [2] = 1.60, [3] = 0.60},
+				uses     = 10,
+				maxlevel = 1
+			},
+		},
+		damage_groups       = {fleshy = 2},
+	},
+	groups            = {wooden = 1, bow = 1},
+})
+
+minetest.register_craftitem("lord_bows:arrow", {
+	description     = "Arrow",
+	inventory_image = "items_tools_arrow.png",
+	groups          = {projectiles = 1, arrow = 1},
+	stack_max       = 99
+})
