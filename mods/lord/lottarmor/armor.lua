cd80c5ebe7fb2e884a8c9dd73cf1c487509c9a90
Alexander Chibrikin <alek13.me@gmail.com>
2023-01-25 16:32:54 +0300

    перенос заблудившегося кусочка кода в нужный обработчик формы сумок

--- mods/lord/lottarmor/armor.lua
+++ mods/lord/lottarmor/armor.lua
@@ -147,6 +147,13 @@ end
 
 --- Bags
 minetest.register_on_player_receive_fields(function(player, formname, fields)
+	if fields.main then
+		local name           = player:get_player_name()
+		local formspec_armor = armor:get_armor_formspec(name)
+		if formspec_armor ~= nil then
+			minetest.show_formspec(player:get_player_name(), "armor", formspec_armor)
+		end
+	end
 	if fields.bags then
 		minetest.show_formspec(player:get_player_name(), "custom", get_formspec(player,"bags"))
 		return
--- mods/lord/lottinventory/init.lua
+++ mods/lord/lottinventory/init.lua
@@ -7,16 +7,6 @@ dofile(minetest.get_modpath(minetest.get_current_modname()) .. "/brewing.lua")
 dofile(minetest.get_modpath(minetest.get_current_modname()) .. "/master.lua")
 
 
-minetest.register_on_player_receive_fields(function(player, formname, fields)
-	if fields.main then
-		local name           = player:get_player_name()
-		local formspec_armor = armor:get_armor_formspec(name)
-		if formspec_armor ~= nil then
-			minetest.show_formspec(player:get_player_name(), "armor", formspec_armor)
-		end
-	end
-end)
-
 minetest.register_craft({
 	output = 'lottinventory:crafts_book',
 	recipe = {
