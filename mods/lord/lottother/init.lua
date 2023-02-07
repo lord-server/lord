bf7273d35d5c278e2186feb528d9071eba988176
Van <vanicgame@yandex.ru>
2023-02-07 15:58:56 +0300

    Переход на встроенные переводы

--- mods/lord/lottother/init.lua
+++ mods/lord/lottother/init.lua
@@ -1,4 +1,4 @@
-local SL = lord.require_intllib()
+local SL = minetest.get_translator("lottother")
 
 dofile(minetest.get_modpath("lottother") .. "/rings.lua")
 dofile(minetest.get_modpath("lottother") .. "/ms.lua")
