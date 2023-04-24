1d2536790067f761a36e3de19ac49aaa16f4e3b1
Van <vanicgame@yandex.ru>
2023-04-24 14:52:11 +0300

    Исправление механизма приручения

--- mods/lord/lottmobs/horse.lua
+++ mods/lord/lottmobs/horse.lua
@@ -511,11 +511,11 @@ mobs:register_mob("lottmobs:horse", {
 		{ "farming:wheat_5", "air", 0 }, { "farming:wheat_4", "air", 0 }, { "farming:wheat_3", "air", 0 },
 		{ "lottfarming:barley_3", "air", 0 }, { "lottfarming:barley_wild", "air", 0 }
 	},
-	follow               = { "farming:sheaf_wheat", "lottother:beast_ring", "lottfarming:sheaf_barley", "default:apple" },
+	follow               = { "farming:wheat", "lottother:beast_ring", "lottfarming:sheaf_barley", "default:apple" },
 	view_range           = 9,
 	on_rightclick        = function(self, clicker)
 		local item = clicker:get_wielded_item()
-		if item:get_name() == "farming:sheaf_wheat" or item:get_name() == "lottother:beast_ring" then
+		if item:get_name() == "farming:wheat" or item:get_name() == "lottother:beast_ring" then
 			if math.random(1, 3) ~= 1 then
 				minetest.chat_send_player(
 					clicker:get_player_name(),
@@ -575,11 +575,11 @@ mobs:register_mob("lottmobs:horsepeg", {
 		{ "farming:wheat_5", "air", 0 }, { "farming:wheat_4", "air", 0 }, { "farming:wheat_3", "air", 0 },
 		{ "lottfarming:barley_3", "air", 0 }, { "lottfarming:barley_wild", "air", 0 }
 	},
-	follow               = { "farming:sheaf_wheat", "lottother:beast_ring", "lottfarming:sheaf_barley", "default:apple" },
+	follow               = { "farming:wheat", "lottother:beast_ring", "lottfarming:sheaf_barley", "default:apple" },
 	view_range           = 9,
 	on_rightclick        = function(self, clicker)
 		local item = clicker:get_wielded_item()
-		if item:get_name() == "farming:sheaf_wheat" or item:get_name() == "lottother:beast_ring" then
+		if item:get_name() == "farming:wheat" or item:get_name() == "lottother:beast_ring" then
 			if math.random(1, 3) ~= 1 then
 				minetest.chat_send_player(
 					clicker:get_player_name(),
@@ -639,11 +639,11 @@ mobs:register_mob("lottmobs:horseara", {
 		{ "farming:wheat_5", "air", 0 }, { "farming:wheat_4", "air", 0 }, { "farming:wheat_3", "air", 0 },
 		{ "lottfarming:barley_3", "air", 0 }, { "lottfarming:barley_wild", "air", 0 }
 	},
-	follow               = { "farming:sheaf_wheat", "lottother:beast_ring", "lottfarming:sheaf_barley", "default:apple" },
+	follow               = { "farming:wheat", "lottother:beast_ring", "lottfarming:sheaf_barley", "default:apple" },
 	view_range           = 9,
 	on_rightclick        = function(self, clicker)
 		local item = clicker:get_wielded_item()
-		if item:get_name() == "farming:sheaf_wheat" or item:get_name() == "lottother:beast_ring" then
+		if item:get_name() == "farming:wheat" or item:get_name() == "lottother:beast_ring" then
 			if math.random(1, 3) ~= 1 then
 				minetest.chat_send_player(
 					clicker:get_player_name(),
@@ -705,12 +705,12 @@ mobs:register_mob("lottmobs:shirepony", {
 		{ "lottfarming:barley_3", "air", 0 }, { "lottfarming:barley_wild", "air", 0 }
 	},
 	follow               = {
-		"farming:sheaf_wheat", "lottother:beast_ring", "lottfarming:sheaf_barley", "lottfarming:carrot_item"
+		"farming:wheat", "lottother:beast_ring", "lottfarming:sheaf_barley", "lottfarming:carrot_item"
 	},
 	view_range           = 5,
 	on_rightclick        = function(self, clicker)
 		local item = clicker:get_wielded_item()
-		if item:get_name() == "farming:sheaf_barley" or item:get_name() == "lottother:beast_ring" then
+		if item:get_name() == "lottfarming:sheaf_barley" or item:get_name() == "lottother:beast_ring" then
 			if math.random(1, 3) ~= 1 then
 				minetest.chat_send_player(
 					clicker:get_player_name(),
@@ -772,12 +772,12 @@ mobs:register_mob("lottmobs:shireponyblack", {
 		{ "lottfarming:barley_3", "air", 0 }, { "lottfarming:barley_wild", "air", 0 }
 	},
 	follow               = {
-		"farming:sheaf_wheat", "lottother:beast_ring", "lottfarming:sheaf_barley", "lottfarming:carrot_item"
+		"farming:wheat", "lottother:beast_ring", "lottfarming:sheaf_barley", "lottfarming:carrot_item"
 	},
 	view_range           = 5,
 	on_rightclick        = function(self, clicker)
 		local item = clicker:get_wielded_item()
-		if item:get_name() == "farming:sheaf_barley" or item:get_name() == "lottother:beast_ring" then
+		if item:get_name() == "lottfarming:sheaf_barley" or item:get_name() == "lottother:beast_ring" then
 			if math.random(1, 3) ~= 1 then
 				minetest.chat_send_player(
 					clicker:get_player_name(),
