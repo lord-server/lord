8bcb1e1a6a1c2a9d8fe335dbc0658f0b24b11220
alek13 <alek13.me@gmail.com>
2023-05-05 16:58:28 +0300

    `equipment.ForPlayer`: add `:clear(kind)`, `:items(kind)`, fix `:get()`. Closes #1021. Relates to #1012, #1017, #339

--- mods/lord/Player/equipment/src/equipment/ForPlayer.lua
+++ mods/lord/Player/equipment/src/equipment/ForPlayer.lua
@@ -1,5 +1,5 @@
-local setmetatable
-	= setmetatable
+local setmetatable, ipairs
+	= setmetatable, ipairs
 
 
 --- @class equipment.ForPlayer
@@ -28,11 +28,26 @@ end
 
 --- @param kind string
 --- @param slot number
+--- @return ItemStack
 function ForPlayer:get(kind, slot)
-	self.player:get_inventory():get_stack(kind, slot)
+	return self.player:get_inventory():get_stack(kind, slot)
 end
 
---- @param kind string    kind(type) of equipment. For ex. "armor"|"clothing"|<your one>
+--- Returns numeric iterator (`ipairs()`) for items of `kind`.
+---
+--- Use:
+---	```lua
+---	    for slot, item in equipment.for_player(player):items(kind) do
+---	        <statements>
+---	    end
+--- ```
+--- @param kind string    kind(type) of equipment. For ex. "armor"|"clothing"|<your_one>
+--- @return fun(tbl:table<number,ItemStack>):number,ItemStack
+function ForPlayer:items(kind)
+	return ipairs(self.player:get_inventory():get_list(kind))
+end
+
+--- @param kind string    kind(type) of equipment. For ex. "armor"|"clothing"|<your_one>
 --- @param slot number    slot number
 --- @param item ItemStack item to set into slot
 function ForPlayer:set(kind, slot, item)
@@ -40,12 +55,23 @@ function ForPlayer:set(kind, slot, item)
 	self.event.trigger(self.player, kind, "set", slot, item) -- TODO use Event.SET constant
 end
 
---- @param kind string    kind(type) of equipment. For ex. "armor"|"clothing"|<your one>
+--- @param kind string    kind(type) of equipment. For ex. "armor"|"clothing"|<your_one>
 --- @param slot number    slot number
 function ForPlayer:delete(kind, slot)
 	local item = self.player:get_inventory():get_stack(kind, slot)
 	self.player:get_inventory():set_stack(kind, slot, nil)
 	self.event.trigger(self.player, kind, "delete", slot, item) -- TODO use Event.DELETE constant
+end
+
+--- Deletes all existing slots, also triggers `delete` event & so,
+---   calls all subscribed to `on_delete`&`on_change` callbacks.
+--- @param kind string    kind(type) of equipment. For ex. "armor"|"clothing"|<your_one>
+function ForPlayer:clear(kind)
+	for slot, item in self:items(kind) do
+		if not item:is_empty() then
+			self:delete(kind, slot)
+		end
+	end
 end
 
 
