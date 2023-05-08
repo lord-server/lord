ce755d6dafb46caf23fc4658d147595a2dece5a2
Van <vanicgame@yandex.ru>
2023-05-08 20:26:12 +0300

    Исправление форм слов

--- mods/_lott/lottachievements/triggers.lua
+++ mods/_lott/lottachievements/triggers.lua
@@ -35,7 +35,7 @@ lottachievements.register_trigger("dig", function(def)
 		end
 		return {
 			perc = itemcount / tmp.target,
-			label = S("Mine @1/@2", itemcount, tmp.target),
+			label = S("Mined @1/@2", itemcount, tmp.target),
 		}
 	end
 	def.getDefaultDescription = function(self)
@@ -72,7 +72,7 @@ lottachievements.register_trigger("place", function(def)
 		end
 		return {
 			perc = itemcount / tmp.target,
-			label = S("Place @1/@2", itemcount, tmp.target),
+			label = S("Placed @1/@2", itemcount, tmp.target),
 		}
 	end
 	def.getDefaultDescription = function(self)
@@ -109,7 +109,7 @@ lottachievements.register_trigger("eat", function(def)
 		end
 		return {
 			perc = itemcount / tmp.target,
-			label = S("Eat @1/@2", itemcount, tmp.target),
+			label = S("Eaten @1/@2", itemcount, tmp.target),
 		}
 	end
 	def.getDefaultDescription = function(self)
@@ -162,7 +162,7 @@ lottachievements.register_trigger("chat", function(def)
 		end
 		return {
 			perc = itemcount / tmp.target,
-			label = S("Write chat messages @1/@2", itemcount, tmp.target),
+			label = S("Chat messages written @1/@2", itemcount, tmp.target),
 		}
 	end
 	def.getDefaultDescription = function(self)
@@ -184,7 +184,7 @@ lottachievements.register_trigger("join", function(def)
 		end
 		return {
 			perc = itemcount / tmp.target,
-			label = S("Join the game @1/@2", itemcount, tmp.target),
+			label = S("Game joins @1/@2", itemcount, tmp.target),
 		}
 	end
 	def.getDefaultDescription = function(self)
@@ -215,7 +215,7 @@ lottachievements.register_trigger("craft", function(def)
 		end
 		return {
 			perc = itemcount / tmp.target,
-			label = S("Craft @1/@2", itemcount, tmp.target),
+			label = S("Crafted @1/@2", itemcount, tmp.target),
 		}
 	end
 	def.getDefaultDescription = function(self)
@@ -272,7 +272,7 @@ lottachievements.register_trigger("kill", function(def)
 		end
 		return {
 			perc = itemcount / tmp.target,
-			label = S("Kill @1/@2", itemcount, tmp.target),
+			label = S("Killed @1/@2", itemcount, tmp.target),
 		}
 	end
 	def.getDefaultDescription = function(self)
