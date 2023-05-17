b0bdbe23a10b80e46435467e25d02b6a76721321
Doloment <doloment@internet.ru>
2023-05-18 06:08:05 +0700

    Closes #999. Add Mordor Brick, Orc Stone, change crafts and textures (#1045)
    * Closes #999. Add Mordor Brick, Orc Stone, change crafts and textures
    * Minor translation fixes for hardwood

--- mods/lord/grinder/src/grinder/definition/recipes.lua
+++ mods/lord/grinder/src/grinder/definition/recipes.lua
@@ -13,4 +13,8 @@ return {
 	{"vessels:glass_bottle 5", "default:sand", 8},
 	{"vessels:drinking_glass 3", "default:sand", 6},
 	{"vessels:glass_fragments 3", "default:sand", 6},
+
+	{"lottmapgen:mordor_stone", "lottmapgen:mordor_cobble", 5},
+	{"lottmapgen:mordor_cobble", "lottmapgen:mordor_gravel", 5},
+	{"lottmapgen:mordor_gravel", "lottmapgen:mordor_sand", 5},
 }
--- mods/lord/lottblocks/init.lua
+++ mods/lord/lottblocks/init.lua
@@ -11,6 +11,7 @@ dofile(minetest.get_modpath("lottblocks").."/lamps.lua")
 dofile(minetest.get_modpath("lottblocks").."/palantiri.lua")
 dofile(minetest.get_modpath("lottblocks").."/guides.lua")
 
+-- Snowy Cobble
 minetest.register_node("lottblocks:snowycobble", {
 	description = S("Snowy Cobblestone"),
 	tiles = {"lottblocks_snowycobble.png"},
@@ -28,6 +29,79 @@ stairs.register_stair_and_slab(
 	false,
 	S("Inner Snowy Cobble Stair"),
 	S("Outer Snowy Cobble Stair")
+)
+
+-- Mordor Stone Bricks
+minetest.register_node("lottblocks:mordor_stone_brick", {
+	description = S("Mordor Stone Brick"),
+	tiles = {"lottblocks_mordor_stone_brick.png"},
+	is_ground_content = false,
+	groups = {cracky=2},
+})
+stairs.register_stair_and_slab(
+		"mordor_stone_brick",
+		"lottblocks:mordor_stone_brick",
+		{cracky=2},
+		{"lottblocks_mordor_stone_brick.png"},
+		S("Mordor Stone Brick Stair"),
+		S("Mordor Stone Brick Slab"),
+		default.node_sound_stone_defaults(),
+		false,
+		S("Inner Mordor Stone Brick Stair"),
+		S("Outer Mordor Stone Brick Stair")
+)
+minetest.register_node("lottblocks:mordor_stone_block", {
+	description = S("Mordor Stone Block"),
+	tiles = {"lottblocks_mordor_stone_block.png"},
+	is_ground_content = false,
+	groups = {cracky=2},
+})
+stairs.register_stair_and_slab(
+		"mordor_stone_block",
+		"lottblocks:mordor_stone_block",
+		{cracky=2},
+		{"lottblocks_mordor_stone_block.png"},
+		S("Mordor Stone Block Stair"),
+		S("Mordor Stone Block Slab"),
+		default.node_sound_stone_defaults(),
+		false,
+		S("Inner Mordor Stone Block Stair"),
+		S("Outer Mordor Stone Block Stair")
+)
+minetest.register_craft({
+	output = 'lottblocks:mordor_stone_brick 4',
+	recipe = {
+		{'lottmapgen:mordor_stone', 'lottmapgen:mordor_stone'},
+		{'lottmapgen:mordor_stone', 'lottmapgen:mordor_stone'},
+	}
+})
+minetest.register_craft({
+	output = 'lottblocks:mordor_stone_block 9',
+	recipe = {
+		{'lottmapgen:mordor_stone', 'lottmapgen:mordor_stone', 'lottmapgen:mordor_stone'},
+		{'lottmapgen:mordor_stone', 'lottmapgen:mordor_stone', 'lottmapgen:mordor_stone'},
+		{'lottmapgen:mordor_stone', 'lottmapgen:mordor_stone', 'lottmapgen:mordor_stone'},
+	}
+})
+
+-- Orc Stone
+minetest.register_node("lottblocks:orc_stone", {
+	description = S("Orc Stone"),
+	tiles = {"lottblocks_orc_stone.png"},
+	is_ground_content = false,
+	groups = {cracky=2, stone=1},
+})
+stairs.register_stair_and_slab(
+		"orc_stone",
+		"lottblocks:orc_stone",
+		{cracky=2},
+		{"lottblocks_orc_stone.png"},
+		S("Orc Stone Stair"),
+		S("Orc Stone Slab"),
+		default.node_sound_stone_defaults(),
+		false,
+		S("Inner Orc Stone Stair"),
+		S("Outer Orc Stone Stair")
 )
 minetest.register_node("lottblocks:orc_brick", {
 	description = S("Orc Brick"),
@@ -47,6 +121,58 @@ stairs.register_stair_and_slab(
 		S("Inner Orc Brick Stair"),
 		S("Outer Orc Brick Stair")
 )
+minetest.register_node("lottblocks:orc_block", {
+	description = S("Orc Block"),
+	tiles = {"lottblocks_orc_block.png"},
+	is_ground_content = false,
+	groups = {cracky=2},
+})
+stairs.register_stair_and_slab(
+		"orc_block",
+		"lottblocks:orc_block",
+		{cracky=2},
+		{"lottblocks_orc_block.png"},
+		S("Orc Block Stair"),
+		S("Orc Block Slab"),
+		default.node_sound_stone_defaults(),
+		false,
+		S("Inner Orc Block Stair"),
+		S("Outer Orc Block Stair")
+)
+
+minetest.register_craft({
+	type = "cooking",
+	output = "lottblocks:orc_stone",
+	recipe = "lottmapgen:mordor_stone",
+})
+minetest.register_craft({
+	type = "cooking",
+	output = "lottblocks:orc_brick",
+	recipe = "lottblocks:mordor_stone_brick",
+})
+minetest.register_craft({
+	type = "cooking",
+	output = "lottblocks:orc_block",
+	recipe = "lottblocks:mordor_stone_block",
+})
+minetest.register_craft({
+	output = 'lottblocks:orc_brick 4',
+	recipe = {
+		{'lottblocks:orc_stone', 'lottblocks:orc_stone'},
+		{'lottblocks:orc_stone', 'lottblocks:orc_stone'},
+	}
+})
+minetest.register_craft({
+	output = 'lottblocks:orc_block 9',
+	recipe = {
+		{'lottblocks:orc_stone', 'lottblocks:orc_stone', 'lottblocks:orc_stone'},
+		{'lottblocks:orc_stone', 'lottblocks:orc_stone', 'lottblocks:orc_stone'},
+		{'lottblocks:orc_stone', 'lottblocks:orc_stone', 'lottblocks:orc_stone'},
+	}
+})
+
+
+-- Marble
 minetest.register_node("lottblocks:marble_brick", {
 	description = S("Marble Brick"),
 	tiles = {"lottblocks_marble_brick.png"},
@@ -70,12 +196,5 @@ minetest.register_craft({
 	recipe = {
 		{'lottores:marble', 'lottores:marble'},
 		{'lottores:marble', 'lottores:marble'},
-	}
-})
-minetest.register_craft({
-	output = 'lottblocks:orc_brick 4',
-	recipe = {
-		{'lottmapgen:mordor_stone', 'lottmapgen:mordor_stone'},
-		{'lottmapgen:mordor_stone', 'lottmapgen:mordor_stone'},
 	}
 })
