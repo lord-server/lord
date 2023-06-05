3f6c086f582bb1041a5c1ba031bdd043160b10c6
Doloment <doloment@internet.ru>
2023-06-05 22:41:09 +0700

    Closes #1052. Fix 'Raw Mordor Clay Block' grinder craft. Relates to #1002. (#1055)

--- mods/lord/Blocks/grinder/src/grinder/definition/recipes.lua
+++ mods/lord/Blocks/grinder/src/grinder/definition/recipes.lua
@@ -17,5 +17,5 @@ return {
 	{"lottmapgen:mordor_stone", "lottmapgen:mordor_cobble", 5},
 	{"lottmapgen:mordor_cobble", "lottmapgen:mordor_gravel", 5},
 	{"lottmapgen:mordor_gravel", "lottmapgen:mordor_sand", 5},
-	{"lottmapgen:mordor_sand", "lord_bricks:mordor_clay", 5},
+	{"lottmapgen:mordor_sand", "clay_types:mordor_clay_block_raw", 5},
 }
