03187ef41a87de8c88fa6fe65b75f8f41fc121e5
alek13 <alek13.me@gmail.com>
2023-06-06 02:42:57 +0300

    Change hourly server message: add links

--- mods/lord/server_message/init.lua
+++ mods/lord/server_message/init.lua
@@ -1,17 +1,19 @@
-minetest.log("action", minetest.get_current_modname().." mod STARTED")
-
 -- периодичность в секундах
-local Message_period = 60*60
+local MESSAGE_PERIOD = 60 * 60
 -- текст сообщения
-local Message_text = "# Server: Хотите увидеть свой домик с высоты? "..
-	"Узнать последние новости и посмотреть лучшие постройки сервера?\n"..
-	"Тогда бегом сюда -> vk.com/minetest_lord. "..
-	"Это и многое другое в группе ВК «Сервер Minetest L.O.R.D»"
+local Message_text   = minetest.colorize("#0f0", "# Server: ") .. "Хотите увидеть свой домик с высоты? " ..
+	"Узнать последние новости и посмотреть лучшие постройки сервера?\n" ..
+	"Группа ВК: " .. minetest.colorize("#00f", "https://vk.com/minetest_lord") .. " (новости, фоточки и пр.)\n" ..
+	"Discord: " .. minetest.colorize("#00f", "https://discord.gg/uTX3mbb") ..
+	" (общаемся, задаём вопросы, делимся настроением)\n" ..
+	"YouTube: " .. minetest.colorize("#00f", "https://youtube.com/@lord-server") .. " (стримы и небольшие видео)\n" ..
+	"Сайт: " .. minetest.colorize("#00f", "https://lord-server.ru/") .. " (есть карта мира)\n" ..
+	"GitHub: " .. minetest.colorize("#00f", "https://github.com/lord-server/lord") .. "\n"
 
-local Timer = 0
+local Timer          = 0
 minetest.register_globalstep(function(DTime)
 	Timer = Timer + DTime;
-	if (Timer >= Message_period)and(minetest.get_connected_players()[1] ~= nil) then
+	if (Timer >= MESSAGE_PERIOD) and (minetest.get_connected_players()[1] ~= nil) then
 		minetest.chat_send_all(Message_text)
 		Timer = 0
 	end
