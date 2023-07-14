cae8e446459bd3a62189b4c251867b3d41740e82
Van <82447582+VanicGame@users.noreply.github.com>
2023-07-15 00:31:37 +0300

    Защищённая рамка. Closes #1050 (#1078)
    * Добавление защищенной рамки
    * Change craft for protected frame. 
    ---------
    Co-authored-by: alek13 <alek13.me@gmail.com>

--- mods/_various/itemframes/init.lua
+++ mods/_various/itemframes/init.lua
@@ -30,7 +30,7 @@ minetest.register_entity("itemframes:item",{
 		if self.texture ~= nil then
 			self.object:set_properties({textures={self.texture}})
 		end
-		if self.nodename ~= "itemframes:frame" then
+		if self.nodename ~= "itemframes:frame" and self.nodename ~= "itemframes:protected_frame" then
 			self.object:set_properties({automatic_rotate=1})
 		end
 	end,
@@ -50,14 +50,15 @@ facedir[3] = {x=-1,y=0,z=0}
 
 local remove_item = function(pos, node)
 	local objs = nil
-	if node.name == "itemframes:frame" then
+	if node.name == "itemframes:frame" or node.name == "itemframes:protected_frame" then
 		objs = minetest.get_objects_inside_radius(pos, .5)
 	elseif minetest.get_item_group(node.name, "group:pedestal") then
 		objs = minetest.get_objects_inside_radius({x=pos.x,y=pos.y+1,z=pos.z}, .5)
 	end
 	if objs then
 		for _, obj in ipairs(objs) do
-			if obj and obj:get_luaentity() and obj:get_luaentity().name == "itemframes:item" then
+			if obj and obj:get_luaentity() and (obj:get_luaentity().name == "itemframes:item" or
+			obj:get_luaentity().name == "itemframes:protected_frame") then
 				obj:remove()
 			end
 		end
@@ -68,7 +69,7 @@ local update_item = function(pos, node)
 	remove_item(pos, node)
 	local meta = minetest.get_meta(pos)
 	if meta:get_string("item") ~= "" then
-		if node.name == "itemframes:frame" then
+		if node.name == "itemframes:frame" or node.name == "itemframes:protected_frame" then
 			local posad = facedir[node.param2]
 			if not posad then return end
 			pos.x = pos.x + posad.x*6.5/16
@@ -80,7 +81,7 @@ local update_item = function(pos, node)
 		tmp.nodename = node.name
 		tmp.texture = ItemStack(meta:get_string("item")):get_name()
 		local e = minetest.add_entity(pos,"itemframes:item")
-		if node.name == "itemframes:frame" then
+		if node.name == "itemframes:frame" or node.name == "itemframes:protected_frame" then
 			local yaw = math.pi*2 - node.param2 * math.pi/2
 			e:set_yaw(yaw)
 		end
@@ -90,7 +91,7 @@ end
 local drop_item = function(pos, node)
 	local meta = minetest.get_meta(pos)
 	if meta:get_string("item") ~= "" then
-		if node.name == "itemframes:frame" then
+		if node.name == "itemframes:frame" or node.name == "itemframes:protected_frame" then
 			minetest.add_item(pos, meta:get_string("item"))
 		elseif minetest.get_item_group(node.name, "group:pedestal") then
 			minetest.add_item({x=pos.x,y=pos.y+1,z=pos.z}, meta:get_string("item"))
@@ -153,6 +154,58 @@ minetest.register_node("itemframes:frame",{
 	end,
 })
 
+minetest.register_node("itemframes:protected_frame",{
+	description = SL("Protected item frame"),
+	drawtype = "nodebox",
+	node_box = { type = "fixed", fixed = {-0.5, -0.5, 7/16, 0.5, 0.5, 0.5} },
+	selection_box = { type = "fixed", fixed = {-0.5, -0.5, 7/16, 0.5, 0.5, 0.5} },
+	tiles = {"itemframes_protected_frame.png"},
+	inventory_image = "itemframes_protected_frame.png",
+	wield_image = "itemframes_protected_frame.png",
+	paramtype = "light",
+	paramtype2 = "facedir",
+	sunlight_propagates = true,
+	groups = { choppy=2,dig_immediate=2, paper = 1 },
+	legacy_wallmounted = true,
+	sounds = default.node_sound_defaults(),
+	after_place_node = function(pos, placer, itemstack)
+		local meta = minetest.get_meta(pos)
+		meta:set_string("infotext", SL("Protected item frame"))
+	end,
+	on_rightclick = function(pos, node, clicker, itemstack)
+		if not itemstack then return end
+		local item_name
+		if not minetest.registered_items[itemstack:get_name()] then
+			return
+		end
+		if itemstack:is_empty() then
+			item_name = " "
+		else
+			item_name = ": "..minetest.registered_items[itemstack:get_name()].description
+		end
+		local meta = minetest.get_meta(pos)
+		if not minetest.is_protected(pos, clicker:get_player_name()) then
+			drop_item(pos,node)
+			local s = itemstack:take_item()
+			meta:set_string("infotext", SL("Protected item frame")..item_name)
+			meta:set_string("item",s:to_string())
+			update_item(pos,node)
+		end
+		return itemstack
+	end,
+	on_punch = function(pos,node,puncher)
+		local meta = minetest.get_meta(pos)
+		if not minetest.is_protected(pos, puncher:get_player_name()) then
+			meta:set_string("infotext", SL("Protected item frame"))
+			drop_item(pos, node)
+		end
+	end,
+	can_dig = function(pos,player)
+		local meta = minetest.get_meta(pos)
+		return not minetest.is_protected(pos, player:get_player_name())
+	end,
+})
+
 minetest.register_craft({
 	output = 'itemframes:frame',
 	recipe = {
@@ -160,6 +213,12 @@ minetest.register_craft({
 		{'group:stick', 'default:paper', 'group:stick'},
 		{'group:stick', 'group:stick', 'group:stick'},
 	}
+})
+
+minetest.register_craft({
+	output = 'itemframes:protected_frame',
+	type = "shapeless",
+	recipe = {'itemframes:frame', 'protector_lott:protect2'}
 })
 
 function itemframes.register_pedestal(subname, recipeitem, groups, images, description, sounds)
