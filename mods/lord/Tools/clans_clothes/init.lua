40bc25b725093e4da8d0edd894cd6cf18c34be27
alek13 <alek13.me@gmail.com>
2023-08-23 16:38:09 +0300

    Add Vassala clan cloak.

--- mods/lord/Tools/clans_clothes/init.lua
+++ mods/lord/Tools/clans_clothes/init.lua
@@ -6,3 +6,10 @@ minetest.register_tool("clans_clothes:mason_cloak", {
 	groups = {armor_heal=0, clothes=1, no_preview = 1, clothes_cloak=1},
 	wear = 0
 })
+
+minetest.register_tool("clans_clothes:vassal_cloak", {
+	description = S("Vassal Clan Cloak"),
+	inventory_image = "clans_clothes_inv_vassal_cloak.png",
+	groups = {armor_heal=0, clothes=1, no_preview = 1, clothes_cloak=1},
+	wear = 0
+})
