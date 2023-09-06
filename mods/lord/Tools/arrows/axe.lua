16752659e1a5df25a9155808990311d9130b9f03
alek13 <alek13.me@gmail.com>
2023-09-07 01:56:55 +0300

    Change throwing axes damsge by mass. Fixes #1128

--- mods/lord/Tools/arrows/axe.lua
+++ mods/lord/Tools/arrows/axe.lua
@@ -1,9 +1,9 @@
 local SL = minetest.get_translator("arrows")
 
-AXE_MASS = 3.0    -- MASS - Масса топора
-AXE_VEL = 20      -- VEL  - Velocity - начальная скорость
-AXE_BASE_DC = 0.1 -- DC   - Damage Coeficient
-AXE_KFR = 0.05    -- KFR  - коэффициент трения о воздух
+local AXE_MASS = 1.5    -- MASS - Масса топора
+local AXE_VEL = 20      -- VEL  - Velocity - начальная скорость
+local AXE_BASE_DC = 0.1 -- DC   - Damage Coeficient
+local AXE_KFR = 0.05    -- KFR  - коэффициент трения о воздух
 
 local axe_node_box = {
 	type = "fixed",
