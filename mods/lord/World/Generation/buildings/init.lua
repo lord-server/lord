bfeedc37a0f1d051530fc4aa8c784b8c114ec9d8
alek13 <alek13.me@gmail.com>
2023-12-10 21:50:18 +0300

    MapGen: extract Dwarf Tamb generation into separate file. Relates to #1140, #1141

--- mods/lord/World/Generation/buildings/init.lua
+++ mods/lord/World/Generation/buildings/init.lua
@@ -1,5 +1,7 @@
-
-local mod_path = minetest.get_modpath(minetest.get_current_modname())
-local require = function(name) return dofile(mod_path .. "/src/" .. name:gsub("%.", "/") .. ".lua") end
+local mod_path    = minetest.get_modpath(minetest.get_current_modname())
+local old_require = require
+require           = function(name) return dofile(mod_path .. "/src/" .. name:gsub("%.", "/") .. ".lua") end
 
 require("dungeons")
+
+require = old_require
