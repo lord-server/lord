e06e2ddfae6a85386cddd57d88f3636f18afafaf
alek13 <alek13.me@gmail.com>
2023-12-11 03:58:12 +0300

    MapGen: Dungeons: add some bricks, stone & mossy cooble into walls. Closes #1140

--- mods/lord/World/Generation/buildings/src/dungeons.lua
+++ mods/lord/World/Generation/buildings/src/dungeons.lua
@@ -1,7 +1,9 @@
 
 local dwarf_tomb = require('dungeons.dwarf_tomb')
+local walls = require('dungeons.walls')
 
 
-minetest.register_on_dungeon_generated(function(minp, maxp, data, area, room_centers)
+minetest.register_on_dungeon_generated(function(minp, maxp, data, area, room_centers, rooms_walls)
 	dwarf_tomb.on_dungeon_generated(minp, maxp, data, area, room_centers)
+	walls.on_dungeon_generated(minp, maxp, data, area, room_centers, rooms_walls)
 end)
