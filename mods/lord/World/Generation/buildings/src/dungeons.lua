eb3baa07dea39bdea58f8085352ca46e9782b60c
alek13 <alek13.me@gmail.com>
2023-12-14 07:03:07 +0300

    MapGen: Dungeons: add minimal interior. Closes #1141

--- mods/lord/World/Generation/buildings/src/dungeons.lua
+++ mods/lord/World/Generation/buildings/src/dungeons.lua
@@ -1,8 +1,10 @@
 
 local dwarf_tomb = require('dungeons.dwarf_tomb')
 local walls      = require('dungeons.walls')
+local interior   = require('dungeons.interior')
 
 minetest.register_on_dungeon_generated(function(minp, maxp, data, param2_data, area, rooms_centers, rooms_walls)
 	dwarf_tomb.on_dungeon_generated(minp, maxp, data, area, rooms_centers)
 	walls.on_dungeon_generated(minp, maxp, data, area, rooms_centers, rooms_walls)
+	interior.on_dungeon_generated(minp, maxp, data, param2_data, area, rooms_centers, rooms_walls)
 end)
