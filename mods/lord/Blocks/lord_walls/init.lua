4f6f25986ace396c0c775e6ada03cc0b2e2a9699
alek13 <alek13.me@gmail.com>
2024-03-19 18:14:29 +0300

    Clay nodes: add walls for mordor clay & chamotte block/brick/masonries. Closes #1250

--- mods/lord/Blocks/lord_walls/init.lua
+++ mods/lord/Blocks/lord_walls/init.lua
@@ -22,3 +22,22 @@ walls.register(":walls:green_marble", S("Green Marble Wall"), "lord_blocks_green
 	"lord_blocks:green_marble", default.node_sound_stone_defaults())
 walls.register(":walls:dungeon_stone", S("Dungeon Stone Wall"), "castle_dungeon_stone.png",
 	"castle:dungeon_stone", default.node_sound_stone_defaults())
+
+-- Clay/chamotte:
+walls.register(":walls:mordor_clay_block", S("Mordor Clay Block Wall"), "lord_bricks_mordor_clay_block.png",
+	"lord_bricks:mordor_clay_block", default.node_sound_stone_defaults())
+walls.register(":walls:mordor_clay_brick", S("Mordor Clay Brick Wall"), "lord_bricks_mordor_clay_brick.png",
+	"lord_bricks:mordor_clay_brick", default.node_sound_stone_defaults())
+walls.register(":walls:mordor_clay_masonry", S("Mordor Clay Masonry Wall"), "lord_bricks_mordor_clay_masonry.png",
+	"lord_bricks:mordor_clay_masonry", default.node_sound_stone_defaults())
+walls.register(":walls:mordor_clay_masonry_large", S("Large Mordor Clay Masonry Wall"),
+	"lord_bricks_mordor_clay_masonry_large.png", "lord_bricks:mordor_clay_masonry", default.node_sound_stone_defaults())
+
+walls.register(":walls:chamotte_block", S("Chamotte Block Wall"), "lord_bricks_chamotte_block.png",
+	"lord_bricks:chamotte_block", default.node_sound_stone_defaults())
+walls.register(":walls:chamotte_brick", S("Chamotte Brick Wall"), "lord_bricks_chamotte_brick.png",
+	"lord_bricks:chamotte_brick", default.node_sound_stone_defaults())
+walls.register(":walls:chamotte_masonry", S("Chamotte Masonry Wall"), "lord_bricks_chamotte_masonry.png",
+	"lord_bricks:chamotte_masonry", default.node_sound_stone_defaults())
+walls.register(":walls:chamotte_masonry_large", S("Large Chamotte Masonry Wall"),
+	"lord_bricks_chamotte_masonry_large.png", "lord_bricks:chamotte_masonry", default.node_sound_stone_defaults())
