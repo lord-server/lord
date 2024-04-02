6cbf38c52233ccbe0327d6640dfbfb4bfa303e61
alek13 <alek13.me@gmail.com>
2024-04-02 03:03:05 +0300

    Remove our builtin translations. Closes #1187

--- mods/lord/Core/builtin/init.lua
+++ mods/lord/Core/builtin/init.lua
@@ -3,5 +3,3 @@ local mod_path = minetest.get_modpath(minetest.get_current_modname())
 local require = function(name) return dofile(mod_path .. "/src/" .. name:gsub("%.", "/") .. ".lua") end
 
 require("mapgen")
-
--- Translations (`./locale/__builtin.ru.tr`) need to be removed after it appears in MT (#1187).
