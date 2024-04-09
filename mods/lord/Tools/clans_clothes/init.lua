07d54ccb0a6739154464f0c16892deca1be5febc
alek13 <alek13.me@gmail.com>
2024-04-09 22:41:22 +0300

    Fix Hansa clan cloak

--- mods/lord/Tools/clans_clothes/init.lua
+++ mods/lord/Tools/clans_clothes/init.lua
@@ -16,7 +16,7 @@ minetest.register_tool("clans_clothes:vassal_cloak", {
 
 minetest.register_tool("clans_clothes:hansa_cloak", {
 	description     = S("Hansa Clan Cloak"),
-	inventory_image = "clans_clothes_inv_vassal_cloak.png",
+	inventory_image = "clans_clothes_inv_hansa_cloak.png",
 	groups          = { armor_heal = 0, clothes = 1, no_preview = 1, clothes_cloak = 1 },
 	wear            = 0
 })
