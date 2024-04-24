57ae916f23e43545d266d75cdb3aeb5475b9663f
Doloment <doloment@internet.ru>
2024-04-24 21:46:21 +0700

    Add Shale and Pyroxenite rocks; format config.lua. Relates to #1270 #1283

--- mods/lord/Blocks/lord_rocks/src/rocks/config.lua
+++ mods/lord/Blocks/lord_rocks/src/rocks/config.lua
@@ -1,14 +1,16 @@
 
 return {
 	-- rocks registrations
-	-- name = definition,
-	["lord_rocks:diorite"] = { softness = 1, definition = {} },
-	["lord_rocks:granite"] = { softness = 1, definition = {} },
-	["lord_rocks:basalt"] = { softness = 2, definition = {} },
-	["lord_rocks:andesite"] = { softness = 2, definition = {} },
-	["lord_rocks:peridotite"] = { softness = 1, definition = {} },
-	["lord_rocks:grey_tuff"] = { softness = 3, definition = {} },
-	["lord_rocks:red_tuff"] = { softness = 3, definition = {} },
+	-- name = {softness, definition},
+	["lord_rocks:diorite"]        = { softness = 1, definition = {} },
+	["lord_rocks:granite"]        = { softness = 1, definition = {} },
+	["lord_rocks:basalt"]         = { softness = 2, definition = {} },
+	["lord_rocks:andesite"]       = { softness = 2, definition = {} },
+	["lord_rocks:peridotite"]     = { softness = 1, definition = {} },
+	["lord_rocks:grey_tuff"]      = { softness = 3, definition = {} },
+	["lord_rocks:red_tuff"]       = { softness = 3, definition = {} },
 	["lord_rocks:grey_quartzite"] = { softness = 2, definition = {} },
 	["lord_rocks:pink_quartzite"] = { softness = 2, definition = {} },
+	["lord_rocks:shale"]          = { softness = 3, definition = {} },
+	["lord_rocks:pyroxenite"]     = { softness = 2, definition = {} },
 }
