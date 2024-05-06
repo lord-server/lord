a7df5ce3294e7ca6efc11baef23087ea658688b6
alek13 <alek13.me@gmail.com>
2024-05-06 20:38:46 +0300

    Extract textures folder detection in `Core`. Relates to #1233

--- mods/lord/Blocks/lottblocks/wooden_stuff.lua
+++ mods/lord/Blocks/lottblocks/wooden_stuff.lua
@@ -6,9 +6,7 @@ local DS = os.DIRECTORY_SEPARATOR
 --- @param sub_folder string
 --- @return string
 local function textures_folder(sub_folder)
-	sub_folder = sub_folder and (sub_folder .. DS) or ""
-	local mod_path = minetest.get_modpath(minetest.get_current_modname())
-	return mod_path .. DS .. "textures" .. DS .. "wooden_stuff" .. DS .. sub_folder
+	return minetest.get_mod_textures_folder("wooden_stuff" .. DS .. sub_folder)
 end
 
 --- @param name string
