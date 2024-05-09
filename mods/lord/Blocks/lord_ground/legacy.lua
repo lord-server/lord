069070f55240d6c6038eca99fdfe9260802f75bd
alek13 <alek13.me@gmail.com>
2024-05-09 22:29:46 +0300

    Ground: Dirt: for biomes: change grass colors: each biome have own. Closes #1320

--- mods/lord/Blocks/lord_ground/legacy.lua
+++ mods/lord/Blocks/lord_ground/legacy.lua
@@ -4,7 +4,7 @@
 minetest.register_alias("lottmapgen:dunland_grass", "lord_ground:dirt_dunland")
 minetest.register_alias("lottmapgen:fangorn_grass", "lord_ground:dirt_fangorn")
 minetest.register_alias("lottmapgen:gondor_grass", "lord_ground:dirt_gondor")
-minetest.register_alias("lottmapgen:ironhill_grass", "lord_ground:dirt_iron_hill")
+minetest.register_alias("lottmapgen:ironhill_grass", "lord_ground:dirt_iron_hills")
 minetest.register_alias("lottmapgen:ithilien_grass", "lord_ground:dirt_ithilien")
 minetest.register_alias("lottmapgen:lorien_grass", "lord_ground:dirt_lorien")
 minetest.register_alias("lottmapgen:mirkwood_grass", "lord_ground:dirt_mirkwood")
--- mods/lord/Blocks/lord_ground/src/ground/config.lua
+++ mods/lord/Blocks/lord_ground/src/ground/config.lua
@@ -2,16 +2,16 @@
 return {
 	dirts = {
 		biome = {
-			-- name                        = {softness [= 3], title, definition [= {}] },
-			["lord_ground:dirt_dunland"]   = { softness = 3, },
-			["lord_ground:dirt_fangorn"]   = { softness = 3, },
-			["lord_ground:dirt_gondor"]    = { softness = 3, },
-			["lord_ground:dirt_iron_hill"] = { softness = 3, },
-			["lord_ground:dirt_ithilien"]  = { softness = 3, },
-			["lord_ground:dirt_lorien"]    = { softness = 3, },
-			["lord_ground:dirt_mirkwood"]  = { softness = 3, },
-			["lord_ground:dirt_rohan"]     = { softness = 3, },
-			["lord_ground:dirt_shire"]     = { softness = 3, },
+			-- name                         = {softness [= 3], title, definition [= {}] },
+			["lord_ground:dirt_dunland"]    = { softness = 3, },
+			["lord_ground:dirt_fangorn"]    = { softness = 3, },
+			["lord_ground:dirt_gondor"]     = { softness = 3, },
+			["lord_ground:dirt_iron_hills"] = { softness = 3, },
+			["lord_ground:dirt_ithilien"]   = { softness = 3, },
+			["lord_ground:dirt_lorien"]     = { softness = 3, },
+			["lord_ground:dirt_mirkwood"]   = { softness = 3, },
+			["lord_ground:dirt_rohan"]      = { softness = 3, },
+			["lord_ground:dirt_shire"]      = { softness = 3, },
 		},
 		mixed = {
 			-- name                     = { craft_from = "<mod>:<node>",   softness = n, definition [= {}] },
