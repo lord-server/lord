66b34d187144a2f67440e5d3ca4ebc58998eb31a
Doloment <doloment@internet.ru>
2024-05-10 03:00:54 +0700

    Fix small texture construction mistake. Relates to #1296 #1273

--- mods/lord/Blocks/lord_ores/rock_ores.lua
+++ mods/lord/Blocks/lord_ores/rock_ores.lua
@@ -2,7 +2,7 @@ local S = minetest.get_translator("lord_ores")
 
 for name, registration in pairs(rocks.get_lord_nodes()) do
 	local stripped_name = name:replace("lord_rocks:", "")
-	local rock_texture       = minetest.registered_nodes[name].tiles[1]:replace(".png", "")
+	local rock_texture       = minetest.registered_nodes[name].tiles[1]
 
 	-- MTG ores
 
