03f40ae50b16a78c2bf2fd871d2bc85ae80d4c59
alek13 <alek13.me@gmail.com>
2024-05-10 13:49:13 +0300

    Add members into International clan.

--- mods/lord/Player/clans/init.lua
+++ mods/lord/Player/clans/init.lua
@@ -29,7 +29,7 @@ local clans_storage = {
 	international = {
 		name = "international",
 		title = "International",
-		players = { "Alges", "Shishka_Intern", "Dasada", "Sakamoto", "Stesrr" },
+		players = { "Alges", "Shishka_Intern", "Dasada", "Sakamoto", "Stesrr", "RPNK", "Cheika", "Vvb" },
 	},
 }
 --- @type table<string,boolean> local cache for clan is online
