4ed854aa816e6b0521f21820e5a736d3dc3b5bbf
ssdaniel24 <bo7oaonteg2m@mail.ru>
2024-05-13 00:22:18 +0300

    Clans refactoring
    - Added abstraction with storage.lua
    - Added normal API commands
    - Added some chat commands
    
    P.S. *NEEDS TESTING AAAAAAAA*

--- mods/lord/Player/clans/init.lua
+++ mods/lord/Player/clans/init.lua
@@ -1,81 +1,112 @@
---- @class clans.Clan
---- @field public title string
---- @field public players table<number,string>
+local mod_path    = minetest.get_modpath(minetest.get_current_modname())
+local old_require = require
+require           = function(name) return dofile(mod_path .. "/src/" .. name:gsub("%.", "/") .. ".lua") end
 
---- @type StorageRef
-local clan_storage = minetest.get_mod_storage()
+clans = {} -- namespace
 
--- HACK: this is a quick hardcoded local storage for clans data
--- TODO: commands for manage
---- @type table<string,clans.Clan>|clans.Clan[]
---[[local clans_storage = {
-	masons = {
-		title   = "Masons",
-		players = {
-			"Petus_mason", "Swed_mason", "Dormi_mason", "JikiSo_mason", "Alek_mason", "Zhekil_mason",
-		},
-	},
-	vassals = {
-		title   = "Vassals",
-		players = { "Pilsner_vassal", "PePe_vassal", "JVD_vassal", "Semi_vassal" },
-	},
-	hansa = {
-		title   = "Hansa",
-		players = { "Qundark", "Kema" },
-	},
-	international = {
-		title = "International",
-		players = { "Alges", "Shishka_Intern", "Dasada", "Sakamoto", "Stesrr" },
-	},
+--[[clans = {
+	get_by_player  = get_clan_by_player,
+	get_by_name    = get_clan_from_storage,
+	clan_is_online = clan_is_online,
 }]]
 
---- @param name string
---- @return clans.Clan
-local function get_clan_from_storage(name)
-	local data = clan_storage:get_string(name)
-	return minetest.parse_json(data)
-end
-
---- @param clan_name string
---- @param clan clans.Clan
-local function set_clan_in_storage(clan_name, clan)
-	local data = minetest.write_json(clan)
-	clan_storage:set_string(clan_name, data)
-end
-
---- @return table
-local function list_clans_from_storage()
-	return clan_storage:get_keys()
-end
-
---- @type table<string,boolean> local cache for clan is online
-local clan_is_online_cache = {}
+local clan_storage = require("storage")
 
 --- @param player_name string
---- @return clans.Clan|nil returns clan data ( `{ title = "...", players {...}, ... }` )
-local function get_clan_by_player_name(player_name)
-	for _, clan_name in ipairs(list_clans_from_storage()) do
-		local clan = get_clan_from_storage(clan_name)
-		if clan.players == nil then return nil end
+--- @return clans.Clan|nil
+function clans.get_by_player_name(player_name)
+	for _, clan in pairs(clan_storage.list()) do
+		print("get_by_player_name: " .. dump(clan))
 		if table.contains(clan.players, player_name) then
 			return clan
 		end
 	end
-
 	return nil
 end
 
 --- @param player Player
---- @return clans.Clan|nil returns clan data ( `{ title = "...", players {...}, ... }` )
-local function get_clan_by_player(player)
+--- @return clans.Clan|nil
+function clans.get_by_player(player)
 	local player_name = player:get_player_name()
-	return get_clan_by_player_name(player_name)
+	return clans.get_by_player_name(player_name)
+end
+
+--- @param name string
+--- @return clans.Clan|nil
+function clans.get_by_name(name)
+	return clan_storage.get(name)
+end
+
+clans.err = {
+	[1] = "clan with this name is already created",
+	[2] = "given player(s) is(are) already assigned to some other clan",
+	[3] = "there is no clan with given name",
+	[4] = "there is no such player in this clan",
+}
+
+--- @param name string
+--- @param title string
+--- @param members string[]
+--- @return boolean,string|nil
+function clans.create_clan(name, title, members)
+	if clans.get_by_name(name) ~= nil then return false, clans.err[1] end
+	for _, m in ipairs(members) do
+		if clans.get_by_player_name(m) ~= nil then return false, clans.err[2] end
+	end
+
+	clan_storage.set({ name = name, title = title, players = members or {} })
+	return true, nil
+end
+
+--- @param name string
+--- @return boolean,string|nil
+function clans.remove_clan(name)
+	if clans.get_by_name(name) == nil then return false, clans.err[3] end
+
+	clan_storage.delete(name)
+	return true, nil
+end
+
+--- @param clan_name string
+--- @param player_name string
+--- @return boolean,string|nil
+function clans.add_player_to_clan(clan_name, player_name)
+	if clans.get_by_player_name(player_name) ~= nil then return false, clans.err[2] end
+	local clan = clans.get_by_name(clan_name)
+	if clan == nil then return false, clans.err[3] end
+
+	table.insert(clan.players, player_name)
+	clan_storage.set(clan)
+	return true, nil
+end
+
+--- @param clan_name string
+--- @param player_name string
+--- @return boolean,string|nil
+function clans.remove_player_from_clan(clan_name, player_name)
+	if clans.get_by_player_name(player_name) == nil or clans.get_by_player_name(player_name).name ~= clan_name then return false, clans.err[4] end
+	local clan = clans.get_by_name(clan_name)
+	if clan == nil then return false, clans.err[3] end
+
+	local updated_members = {}
+	for _, p in ipairs(clan.players) do
+		if p ~= player_name then table.insert(updated_members, p) end
+	end
+	clan.players = updated_members
+	clan_storage.set(clan)
+
+	return true, nil
+end
+
+--- @return table
+function clans.list()
+	return clan_storage.list()
 end
 
 --- @param name string
 --- @return boolean|nil
 local function check_clan_is_online(name)
-	local clan = get_clan_from_storage(name)
+	local clan = clans.get_by_name(name)
 	if not clan then return nil end
 	for _, player in pairs(minetest.get_connected_players()) do
 		if table.contains(clan.players, player:get_player_name()) then
@@ -85,10 +116,13 @@ local function check_clan_is_online(name)
 	return false
 end
 
+--- @type table<string,boolean> local cache for clan is online
+local clan_is_online_cache = {}
+
 --- @param name string
 --- @return boolean|nil
-local function clan_is_online(name)
-	if not get_clan_from_storage(name) then return nil end
+function clans.clan_is_online(name)
+	if not clans.get_by_name(name) then return nil end
 	if clan_is_online_cache[name] == nil then
 		clan_is_online_cache[name] = check_clan_is_online(name)
 	end
@@ -97,14 +131,11 @@ local function clan_is_online(name)
 end
 
 minetest.register_on_joinplayer(function(player, last_login)
-	if not player or not player:is_player() then
-		return
-	end
+	if not player or not player:is_player() then return end
 
-	local clan = get_clan_by_player_name(player)
-	if not clan then
-		return
-	end
+	local clan = clans.get_by_player(player)
+
+	if not clan then return end
 
 	clan_is_online_cache[clan.name] = true
 
@@ -117,94 +148,15 @@ minetest.register_on_leaveplayer(function(player, timed_out)
 	if not player or not player:is_player() then
 		return
 	end
-	local clan = get_clan_by_player_name(player)
-	if not clan then
-		return
-	end
+	--- @type Player
+	player = player
+	local clan = clans.get_by_player(player)
+	if not clan then return end
 
 	clan_is_online_cache[clan.name] = nil -- reset cache (this will force recalc cache)
 end)
 
---- API ---
 
-clans = {
-	get_by_player  = get_clan_by_player,
-	get_by_name    = get_clan_from_storage,
-	clan_is_online = clan_is_online,
-}
+require("commands")
 
-clans.err = {
-	[1] = "clan with this name is already created",
-	[2] = "given player(s) is(are) already assigned to some other clan",
-	[3] = "there is no clan with given name",
-	[4] = "there is no such player in this clan",
-}
-
---- @param name string
---- @param members table<string>
---- @return table
-function clans.create_clan(name, title, members)
-	if clans.get_by_name(name) ~= nil then return { false, clans.err[1] } end
-
-	for _, m in ipairs(members) do
-		if get_clan_by_player_name(m) ~= nil then return { false, clans.err[2] } end
-	end
-
-	set_clan_in_storage(name, { title = title, players = members })
-	return { true, nil }
-end
-
---- @param name string
---- @return table
-function clans.remove_clan(name)
-	if clans.get_by_name(name) == nil then return { false, clans.err[3] } end
-
-	set_clan_in_storage(name, nil)
-	if clan_is_online_cache[name] ~= nil then clan_is_online_cache[name] = nil end
-	return { true, nil }
-end
-
---- @param clan_name string
---- @param player string
---- @return table
-function clans.add_player_to_clan(clan_name, player)
-	if clans.get_by_name(clan_name) == nil then return { false, clans.err[3] } end
-	if get_clan_by_player_name(player) ~= nil then return { false, clans.err[2] } end
-
-	local clan = get_clan_from_storage(clan_name)
-	table.insert(clan.players, player)
-	set_clan_in_storage(clan_name, clan)
-	return { true, nil }
-end
-
---- @param clan_name string
---- @param player string
---- @return table
-function clans.remove_player_from_clan(clan_name, player)
-	if clans.get_by_name(clan_name) == nil then return { false, clans.err[3] } end
-	if get_clan_by_player_name(player) == nil then return { false, clans.err[4] } end
-
-	local clan = get_clan_from_storage(clan_name)
-	local updated_members = {}
-	for _, p in ipairs(clan.players) do
-		if p ~= player then table.insert(updated_members, p) end
-	end
-	clan.players = updated_members
-	set_clan_in_storage(clan_name, clan)
-
-	return { true, nil }
-end
-
---- @return table
-function clans.list_clans()
-	return list_clans_from_storage()
-end
-
---- @param name string
---- @return table
-function clans.get_clan(name)
-	if clans.get_by_name(name) == nil then return { false, clans.err[3] } end
-
-	return get_clan_from_storage(name)
-end
-
+require = old_require
