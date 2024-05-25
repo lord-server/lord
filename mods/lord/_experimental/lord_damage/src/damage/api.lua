381f14c96302a9857357ce58d6b688d31e1f1a2e
Doloment <doloment@internet.ru>
2024-05-26 01:59:52 +0700

    Add testing features; fix some issues with api

--- mods/lord/_experimental/lord_damage/src/damage/api.lua
+++ mods/lord/_experimental/lord_damage/src/damage/api.lua
@@ -1,7 +1,7 @@
 -- local S = minetest.get_translator("lord_damage")
 
 local function calculate_damage_absorption(player, amount, damage_type)
-	local protection = player:get_armor_groups()[type]
+	local protection = player:get_armor_groups()[damage_type]
 	if not protection or type(protection) ~= "number" or protection <= 0 then
 		return amount
 	end
@@ -19,15 +19,15 @@ local damage = {
 	damage_types = {},
 }
 
---- @param damage_type     string    damage type name
---- @param behavior function  function which is called on deal_damage()
+--- @param damage_type string    damage type name
+--- @param behavior    function  function which is called on deal_damage()
 local function register_damage_type(damage_type, behavior)
 	damage.damage_types[damage_type] = behavior
 	return true
 end
 
 register_damage_type("direct", function(player, amount, reason)
-	return base_behavior(player, amount, "direct", reason or { type = "direct" })
+	return base_behavior(player, amount, "direct", reason or { type = "set_hp", damage_type = "direct" })
 end)
 
 local function deal_damage(player, amount, damage_type, reason)
---
+++ mods/lord/_experimental/lord_damage/src/for_testing.lua
@@ -0,0 +1,23 @@
+minetest.register_chatcommand("set_armor", {
+    params = "<type> [value]",
+    description = "",
+    privs = {},
+    func = function(name, param)
+		if not param or param == "" then
+			return
+		end
+		local player = minetest.get_player_by_name(name)
+		local protection_type = param:split(" ")[1]
+		local value = tonumber(param:split(" ")[2])
+		local armor = player:get_armor_groups()
+		armor[protection_type] = value
+		player:set_armor_groups(armor)
+	end,
+})
+
+minetest.register_craftitem("lord_damage:direct_dealer",{
+	description = "Direct dealer",
+	on_use = function(itemstack, player, pointed_thing)
+		lord_damage.deal_damage(player, 5, "direct")
+	end
+})
