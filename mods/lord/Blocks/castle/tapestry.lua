a3b6963c996fed3b24ffa649d335bc0094333a38
ssdaniel24 <bo7oaonteg2m@mail.ru>
2024-05-28 13:39:13 +0300

    Added tapestry.register_long API func

--- mods/lord/Blocks/castle/tapestry.lua
+++ mods/lord/Blocks/castle/tapestry.lua
@@ -90,23 +90,10 @@ function tapestry.register(name, desc_prefix, tile)
 	}))
 end
 
-
-
-
-
-for _, row in ipairs(dye.dyes) do
-	local name = row[1]
-	local desc = row[2]
-	local craft_color_group = row[3]
-	-- Node Definition
-	minetest.register_node("castle:long_tapestry_"..name, {
-	         drawtype = "nodebox",
-		description = SL(desc.." Tapestry (Long)"),
-		tiles = {"wool_"..name..".png"},
-		groups = {oddly_breakable_by_hand=3,flammable=3,not_in_creative_inventory=1},
-		sounds = default.node_sound_defaults(),
-	         paramtype = "light",
-	         paramtype2 = "facedir",
+function tapestry.register_long(name, desc_prefix, tile)
+	minetest.register_node(name, table.merge(tapestry_node_def_template, {
+		description = SL(desc_prefix.." Tapestry (Long)"),
+		tiles = { tile, },
 		node_box = {
 		    type = "fixed",
 		    fixed = {
@@ -127,16 +114,10 @@ for _, row in ipairs(dye.dyes) do
 			    {-0.500000,-0.500000,0.437500,0.500000,2.500000,0.500000},
 		    },
 	    },
-	})
-	if craft_color_group then
-		-- Crafting from normal tapestry and wool
-		minetest.register_craft({
-			type = "shapeless",
-			output = 'castle:long_tapestry_'..name,
-			recipe = {'wool:'..craft_color_group, 'castle:tapestry_'..name},
-		})
-	end
+	}))
 end
+
+
 
 for _, row in ipairs(dye.dyes) do
 	local name = row[1]
@@ -184,14 +165,23 @@ end
 
 -- registering colored tapestries
 for _, dye in ipairs(dye.dyes) do
-	local name = "castle:tapestry_"..dye[1]
 	local desc_prefix = dye[2]
 	local tile = "wool_"..dye[1]..".png"
-	tapestry.register(name, desc_prefix, tile)
 	local material = "wool:"..dye[1]
+
+	local name = "castle:tapestry_"..dye[1]
+	tapestry.register(name, desc_prefix, tile)
 	minetest.register_craft({
 		type = "shapeless",
 		output = name,
 		recipe = { material, 'default:stick', },
+	})
+
+	local name_long = "castle:long_tapestry_"..dye[1]
+	tapestry.register_long(name_long, desc_prefix, tile)
+	minetest.register_craft({
+		type = "shapeless",
+		output = name_long,
+		recipe = {'wool:'..material, 'castle:tapestry_'..name},
 	})
 end
