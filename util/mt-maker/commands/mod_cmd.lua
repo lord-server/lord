38161a38405fed750d79ef8bb74889c5763fd347
alek13 <alek13.me@gmail.com>
2024-06-01 00:15:29 +0300

    Genenerated mods: fix tabs in `init.kua`; add `mod.conf`; add cmd & arg description.

--- util/mt-maker/commands/mod_cmd.lua
+++ util/mt-maker/commands/mod_cmd.lua
@@ -24,23 +24,29 @@ local function create_files(mod_name)
 	create_file_with(mod_name .. DS .. "locale" .. DS .. mod_name .. ".en.tr", "# textdomain: " .. mod_name)
 	create_file_with(mod_name .. DS .. "locale" .. DS .. mod_name .. ".ru.tr", "# textdomain: " .. mod_name)
 
+	create_file_with(mod_name .. DS .. "mod.conf", "name = " .. mod_name)
 
-	create_file_with(mod_name .. DS .. "init.lua",
-	"local DS          = os.DIRECTORY_SEPARATOR\
-	local mod_path    = minetest.get_modpath(minetest.get_current_modname())\
-	local old_require = require\
-	require           = function(name) return dofile(mod_path .. DS .. \"src\" .. DS .. name:gsub(\"%.\", DS) .. \".lua\") end\
-	\
-	\
-	require(\"" .. mod_name .. "\").init()\
+	create_file_with(
+		mod_name .. DS .. "init.lua",
+
+		"local DS          = os.DIRECTORY_SEPARATOR\
+local mod_path    = minetest.get_modpath(minetest.get_current_modname())\
+local old_require = require\
+require           = function(name) return dofile(mod_path .. DS .. \"src\" .. DS .. name:gsub(\"%.\", DS) .. \".lua\") end\
+\
+\
+require(\"" .. mod_name .. "\").init()\
 \
 \
 require = old_require\
-")
+"
+	)
 
 
-	create_file_with(mod_name .. DS .. "src" .. DS .. mod_name..".lua",
-	"local api    = require(\""..mod_name..".api\")\
+	create_file_with(
+		mod_name .. DS .. "src" .. DS .. mod_name..".lua",
+
+		"local api    = require(\""..mod_name..".api\")\
 local config = require(\""..mod_name..".config\")\
 \
 local function register_api()\
@@ -54,20 +60,20 @@ return {\
 	end,\
 }\
 "
-)
+	)
 end
 
 
 return {
 	schema = "mod <mod_name>",
-	--description = "Creates mod backbone.",
-	--positional_args = { -- Set description or {description, default} for positional arguments
-	--	mod_name = "Name of creating mod",
-	--},
+	description = "Creates mod backbone.",
+	positional_args = { -- Set description or {description, default} for positional arguments
+		mod_name = "Name of creating mod",
+	},
 	action = function(parsed, command, app)
 		create_dirs(parsed.mod_name, app)
 		create_files(parsed.mod_name)
 
-		app.theme.success("Backbone for MT Mod `" .. parsed.mod_name .. "` created")
+		app.theme.success("Backbone for MT Mod `" .. parsed.mod_name .. "` created!")
 	end,
 }
