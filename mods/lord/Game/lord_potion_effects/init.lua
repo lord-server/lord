1c9fb7e173458d88d4d1eff2de551eeda3a063ff
Vladislav Tsendrovskii <vlad@vltc.net.eu.org>
2024-06-01 21:30:14 +0500

    Potion manager

---
+++ mods/lord/Game/lord_potion_effects/init.lua
@@ -0,0 +1,4 @@
+lord_potion_effects = {}
+
+dofile(minetest.get_modpath("lord_potion_effects").."/src/effects_manager.lua")
+dofile(minetest.get_modpath("lord_potion_effects").."/src/player_effects.lua")
