e30e13c67073d47ab5fe5e53f9f31617c57bc75b
Doloment <doloment@internet.ru>
2024-06-01 19:17:39 +0700

    Add new physical damage types; create damage types via base behaviors; toxic -> toxical; various fixes

--- mods/lord/_experimental/lord_damage/src/damage/api.lua
+++ mods/lord/_experimental/lord_damage/src/damage/api.lua
@@ -1,5 +1,3 @@
--- local S = minetest.get_translator("lord_damage")
-
 local function calculate_damage_absorption(player, amount, damage_type)
 	local protection = player:get_armor_groups()[damage_type]
 	if not protection or type(protection) ~= "number" or protection <= 0 then
@@ -7,11 +5,6 @@ local function calculate_damage_absorption(player, amount, damage_type)
 	end
 
 	return amount - protection
-end
-
-local function base_behavior(player, amount, damage_type, reason)
-	amount = calculate_damage_absorption(player, amount, damage_type)
-	return player:set_hp(player:get_hp() - amount, reason)
 end
 
 local damage = {
@@ -51,12 +44,89 @@ local function deal_damage(player, amount, damage_type, reason, source, chunks)
 	return damage.damage_types[damage_type](player, amount, reason, source, chunks)
 end
 
+local function base_behavior(player, amount, damage_type, reason, source)
+	amount = calculate_damage_absorption(player, amount, damage_type)
+	reason = reason or { type = "set_hp", damage_type = damage_type, source = source, }
+	return player:set_hp(player:get_hp() - amount, reason)
+end
+
+local function periodic_base_behavior(player, amount, damage_type, reason, source, chunks, do_in_cycle)
+	amount = calculate_damage_absorption(player, amount, damage_type)
+	reason = reason or { type = "set_hp", damage_type = damage_type, source = source, }
+	do_in_cycle = do_in_cycle or function() end
+
+	local max_cycle       = math.floor(amount/chunks)
+	local leftover_damage = amount%chunks
+	local player_has_died = false
+	local player_has_left = false
+
+	local cycle_number = 0
+	for i = 1, max_cycle do
+		minetest.after(cycle_number, function()
+			if not player then
+				return
+			end
+
+			if player:is_player() then
+				minetest.register_on_dieplayer(function(dieplayer)
+					if dieplayer:get_player_name() == player:get_player_name() then
+						player_has_died = true
+					end
+				end)
+				minetest.register_on_leaveplayer(function(leaveplayer)
+					if leaveplayer:get_player_name() == player:get_player_name() then
+						player_has_left = true
+					end
+				end)
+			end
+
+			local reset_damage = player_has_died or player_has_left or (source and not get_source_status(player, source))
+			if reset_damage then
+				leftover_damage = 0
+				return
+			end
+
+			do_in_cycle()
+
+			player:set_hp(player:get_hp() - chunks, reason)
+		end)
+		cycle_number = cycle_number + 1
+	end
+	minetest.after(max_cycle, function()
+		if not player or leftover_damage == 0 then
+			return
+		end
+
+		if player:is_player() then
+			minetest.register_on_dieplayer(function(dieplayer)
+				if dieplayer:get_player_name() == player:get_player_name() then
+					player_has_died = true
+				end
+			end)
+			minetest.register_on_leaveplayer(function(leaveplayer)
+				if leaveplayer:get_player_name() == player:get_player_name() then
+					player_has_left = true
+				end
+			end)
+		end
+
+		local reset_damage = player_has_died or player_has_left or (source and not get_source_status(player, source))
+		if reset_damage then
+			return
+		end
+
+		do_in_cycle()
+		player:set_hp(player:get_hp() - leftover_damage, reason)
+	end)
+end
+
 
 return {
 	calculate_damage_absorption = calculate_damage_absorption,
 	register_damage_type        = register_damage_type,
 	get_source_status           = get_source_status,
 	base_behavior               = base_behavior,
+	periodic_base_behavior      = periodic_base_behavior,
 	deal_damage                 = deal_damage,
 	set_source                  = set_source,
 	get_nodes                   = function() return damage.damage_types end,
