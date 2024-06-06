571277e8c55bb8bed1ab156da16cabd8407772c5
alek13 <alek13.me@gmail.com>
2024-06-06 13:20:58 +0300

    Fix crash on clan player auto-kick. Closes #1430

--- mods/lord/Player/clans/src/clans/players/auto_kick.lua
+++ mods/lord/Player/clans/src/clans/players/auto_kick.lua
@@ -11,18 +11,21 @@ local function kick_inactive_players_from_clan()
 	local time_now = os.time()
 	for clan_name, clan in pairs(clans.list()) do
 		for _, player in ipairs(clan.players) do
-			local last_login = auth_handler.get_auth(player).last_login
-			if last_login ~= nil and time_now - last_login > MAX_INACTIVE_PERIOD then
-				clans.clan_players_remove(clan_name, player)
-				minetest.log(
-					"info",
-					string.format(
-						"[clans] removed %s player from %s clan for being offline since %d",
-						player,
-						clan_name,
-						last_login
+			local auth = auth_handler.get_auth(player)
+			if auth then
+				local last_login = auth.last_login
+				if last_login ~= nil and time_now - last_login > MAX_INACTIVE_PERIOD then
+					clans.clan_players_remove(clan_name, player)
+					minetest.log(
+						"info",
+						string.format(
+							"[clans] removed %s player from %s clan for being offline since %d",
+							player,
+							clan_name,
+							last_login
+						)
 					)
-				)
+				end
 			end
 		end
 	end
@@ -40,7 +43,7 @@ return {
 	register = function()
 		minetest.register_on_mods_loaded(function()
 			-- HACK: waiting for auth system loading:
-			minetest.after(30, call_every_day, kick_inactive_players_from_clan)
+			minetest.after(60, call_every_day, kick_inactive_players_from_clan)
 		end)
 	end
 }
