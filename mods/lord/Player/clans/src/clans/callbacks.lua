1a268bc3e4fb3d7ac2893fe353881f71140a73af
alek13 <alek13.me@gmail.com>
2024-06-06 21:25:09 +0300

    Clans v2: rename & collect all subscribers callbacks in one array. Relates to #1424

--- mods/lord/Player/clans/src/clans/callbacks.lua
+++ mods/lord/Player/clans/src/clans/callbacks.lua
@@ -6,33 +6,35 @@ local callbacks = {} -- namespace
 ---@alias OnClanPlayerAddingCallback fun(clan:clans.Clan, player_name:string)
 ---@alias OnClanPlayerRemovingCallback fun(clan:clans.Clan, player_name:string)
 
+
+local subscribers = {}
 ---@type OnClanCreationCallback[]
-local on_clan_create_callbacks = {}
+subscribers.on_clan_created = {}
 ---@type OnClanDeletionCallback[]
-local on_clan_deletion_callbacks = {}
+subscribers.on_clan_deleted = {}
 ---@type OnClanPlayerAddingCallback[]
-local on_clan_player_adding_callbacks = {}
+subscribers.on_clan_player_added = {}
 ---@type OnClanPlayerRemovingCallback[]
-local on_clan_player_removing_callbacks = {}
+subscribers.on_clan_player_removed = {}
 
 
 ---- Subscribing: ----
 
 ---@param func OnClanCreationCallback
 function callbacks.on_clan_created(func)
-	table.insert(on_clan_create_callbacks, func)
+	table.insert(subscribers.on_clan_created, func)
 end
 ---@param func OnClanDeletionCallback
 function callbacks.on_clan_deleted(func)
-	table.insert(on_clan_deletion_callbacks, func)
+	table.insert(subscribers.on_clan_deleted, func)
 end
 ---@param func OnClanPlayerAddingCallback
 function callbacks.on_clan_player_added(func)
-	table.insert(on_clan_player_adding_callbacks, func)
+	table.insert(subscribers.on_clan_player_added, func)
 end
 ---@param func OnClanPlayerRemovingCallback
 function callbacks.on_clan_player_removed(func)
-	table.insert(on_clan_player_removing_callbacks, func)
+	table.insert(subscribers.on_clan_player_removed, func)
 end
 
 
@@ -40,27 +42,27 @@ end
 
 ---@param clan clans.Clan
 function callbacks.run_on_clan_creation_callbacks(clan)
-	for _, func in ipairs(on_clan_create_callbacks) do
+	for _, func in ipairs(subscribers.on_clan_created) do
 		func(clan)
 	end
 end
 ---@param clan clans.Clan
 function callbacks.run_on_clan_deletion_callbacks(clan)
-	for _, func in ipairs(on_clan_deletion_callbacks) do
+	for _, func in ipairs(subscribers.on_clan_deleted) do
 		func(clan)
 	end
 end
 ---@param clan        clans.Clan
 ---@param player_name string
 function callbacks.run_on_clan_player_adding_callbacks(clan, player_name)
-	for _, func in ipairs(on_clan_player_adding_callbacks) do
+	for _, func in ipairs(subscribers.on_clan_player_added) do
 		func(clan, player_name)
 	end
 end
 ---@param clan        clans.Clan
 ---@param player_name string
 function callbacks.run_on_clan_player_removing_callbacks(clan, player_name)
-	for _, func in ipairs(on_clan_player_removing_callbacks) do
+	for _, func in ipairs(subscribers.on_clan_player_removed) do
 		func(clan, player_name)
 	end
 end
