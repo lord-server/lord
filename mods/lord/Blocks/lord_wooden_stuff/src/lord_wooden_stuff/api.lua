fb4517e14c5133713fe8b5d571d46056e976a653
ssdaniel24 <bo7oaonteg2m@mail.ru>
2024-06-16 07:53:35 +0300

    Removed unknown namespace from api. Relates to #1233

--- mods/lord/Blocks/lord_wooden_stuff/src/lord_wooden_stuff/api.lua
+++ mods/lord/Blocks/lord_wooden_stuff/src/lord_wooden_stuff/api.lua
@@ -379,7 +379,7 @@ end
 ---@param description string
 ---@param texture string
 ---@param wood_name string
-function lord_wooden_stuff.register_wooden_stuff(name, description, texture, wood_name)
+local function register_wooden_stuff(name, description, texture, wood_name)
 	local node_groups     = table.copy(minetest.registered_nodes[wood_name].groups)
 	node_groups["wood"]   = nil
 	node_groups["wooden"] = 1
@@ -404,20 +404,22 @@ function lord_wooden_stuff.register_wooden_stuff(name, description, texture, woo
 	register_chair(name, description, texture, wood_name, node_groups)
 end
 
-lord_wooden_stuff.register_wooden_stuff("wood", "Wooden", "default_wood.png", "default:wood")
-lord_wooden_stuff.register_wooden_stuff("junglewood", "Junglewood", "default_junglewood.png", "default:junglewood")
-lord_wooden_stuff.register_wooden_stuff("alder", "Alder", "lord_planks_alder.png", "lord_planks:alder")
-lord_wooden_stuff.register_wooden_stuff("beech", "Beech", "lord_planks_beech.png", "lord_planks:beech")
-lord_wooden_stuff.register_wooden_stuff("birch", "Birch", "lord_planks_birch.png", "lord_planks:birch")
-lord_wooden_stuff.register_wooden_stuff("cherry", "Cherry", "lord_planks_cherry.png", "lord_planks:cherry")
-lord_wooden_stuff.register_wooden_stuff("culumalda", "Culumalda", "lord_planks_culumalda.png", "lord_planks:culumalda")
-lord_wooden_stuff.register_wooden_stuff("elm", "Elm", "lord_planks_elm.png", "lord_planks:elm")
-lord_wooden_stuff.register_wooden_stuff("fir", "Fir", "lord_planks_fir.png", "lord_planks:fir")
-lord_wooden_stuff.register_wooden_stuff("hardwood", "Hardwood", "lord_planks_hardwood.png", "lord_planks:hardwood")
-lord_wooden_stuff.register_wooden_stuff("lebethron", "Lebethron", "lord_planks_lebethron.png", "lord_planks:lebethron")
-lord_wooden_stuff.register_wooden_stuff("mallorn", "Mallorn", "lord_planks_mallorn.png", "lord_planks:mallorn")
-lord_wooden_stuff.register_wooden_stuff("pine", "Pine", "lord_planks_pine.png", "lord_planks:pine")
-lord_wooden_stuff.register_wooden_stuff("plum", "Plum", "lord_planks_plum.png", "lord_planks:plum")
+register_wooden_stuff("wood", "Wooden", "default_wood.png", "default:wood")
+register_wooden_stuff("junglewood", "Junglewood", "default_junglewood.png", "default:junglewood")
+register_wooden_stuff("alder", "Alder", "lord_planks_alder.png", "lord_planks:alder")
+register_wooden_stuff("beech", "Beech", "lord_planks_beech.png", "lord_planks:beech")
+register_wooden_stuff("birch", "Birch", "lord_planks_birch.png", "lord_planks:birch")
+register_wooden_stuff("cherry", "Cherry", "lord_planks_cherry.png", "lord_planks:cherry")
+register_wooden_stuff("culumalda", "Culumalda", "lord_planks_culumalda.png", "lord_planks:culumalda")
+register_wooden_stuff("elm", "Elm", "lord_planks_elm.png", "lord_planks:elm")
+register_wooden_stuff("fir", "Fir", "lord_planks_fir.png", "lord_planks:fir")
+register_wooden_stuff("hardwood", "Hardwood", "lord_planks_hardwood.png", "lord_planks:hardwood")
+register_wooden_stuff("lebethron", "Lebethron", "lord_planks_lebethron.png", "lord_planks:lebethron")
+register_wooden_stuff("mallorn", "Mallorn", "lord_planks_mallorn.png", "lord_planks:mallorn")
+register_wooden_stuff("pine", "Pine", "lord_planks_pine.png", "lord_planks:pine")
+register_wooden_stuff("plum", "Plum", "lord_planks_plum.png", "lord_planks:plum")
 
 minetest.register_alias("lord_wooden_stuff:wooden_stanchion", "lord_wooden_stuff:wood_stanchion")
 minetest.register_alias("lord_wooden_stuff:fence_junglewood", "default:fence_junglewood")
+
+return { register_wooden_stuff = register_wooden_stuff }
