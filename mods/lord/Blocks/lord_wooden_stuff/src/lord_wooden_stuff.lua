46e93ac63324e3dae29f0d99f8c3738aaa6d1ba5
ssdaniel24 <bo7oaonteg2m@mail.ru>
2024-06-16 08:53:54 +0300

    Moved wood types to config. Related to #1233

--- mods/lord/Blocks/lord_wooden_stuff/src/lord_wooden_stuff.lua
+++ mods/lord/Blocks/lord_wooden_stuff/src/lord_wooden_stuff.lua
@@ -1,13 +1,23 @@
 local api    = require("lord_wooden_stuff.api")
---local config = require("lord_wooden_stuff.config")
+local config = require("lord_wooden_stuff.config")
 
 local function register_api()
 	_G.lord_wooden_stuff = api
 end
 
+local function register_lord_wooden_stuff()
+	for _, wood_def in ipairs(config.wood_defs) do
+		local name_postfix = wood_def[1]
+		local desc_prefix = wood_def[2]
+		local planks_texture = wood_def[3]
+		local planks_name = wood_def[4]
+		api.register_wooden_stuff(name_postfix, desc_prefix, planks_texture, planks_name)
+	end
+end
 
 return {
 	init = function()
 		register_api()
+		register_lord_wooden_stuff()
 	end,
 }
---
+++ mods/lord/Blocks/lord_wooden_stuff/src/lord_wooden_stuff/config.lua
@@ -0,0 +1,20 @@
+---@type string[][]
+local wood_defs = {
+--    name postfix, desc prefix,  planks texture,              planks nodename
+	{ "alder",      "Alder",      "lord_planks_alder.png",     "lord_planks:alder", },
+	{ "beech",      "Beech",      "lord_planks_beech.png",     "lord_planks:beech", },
+	{ "birch",      "Birch",      "lord_planks_birch.png",     "lord_planks:birch", },
+	{ "cherry",     "Cherry",     "lord_planks_cherry.png",    "lord_planks:cherry", },
+	{ "culumalda",  "Culumalda",  "lord_planks_culumalda.png", "lord_planks:culumalda", },
+	{ "elm",        "Elm",        "lord_planks_elm.png",       "lord_planks:elm", },
+	{ "fir",        "Fir",        "lord_planks_fir.png",       "lord_planks:fir", },
+	{ "hardwood",   "Hardwood",   "lord_planks_hardwood.png",  "lord_planks:hardwood", },
+	{ "junglewood", "Junglewood", "default_junglewood.png",    "default:junglewood", },
+	{ "lebethron",  "Lebethron",  "lord_planks_lebethron.png", "lord_planks:lebethron", },
+	{ "mallorn",    "Mallorn",    "lord_planks_mallorn.png",   "lord_planks:mallorn", },
+	{ "pine",       "Pine",       "lord_planks_pine.png",      "lord_planks:pine", },
+	{ "plum",       "Plum",       "lord_planks_plum.png",      "lord_planks:plum", },
+	{ "wood",       "Wooden",     "default_wood.png",          "default:wood", },
+}
+
+return { wood_defs = wood_defs, }
