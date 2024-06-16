b94f2d3b7caeeae1906aa0f69c42fad859cdf102
ssdaniel24 <bo7oaonteg2m@mail.ru>
2024-06-16 10:35:40 +0300

    Handling legacy stuff. Relates to #1233

--- mods/lord/Blocks/lord_wooden_stuff/src/lord_wooden_stuff.lua
+++ mods/lord/Blocks/lord_wooden_stuff/src/lord_wooden_stuff.lua
@@ -1,5 +1,6 @@
 local api    = require("lord_wooden_stuff.api")
 local config = require("lord_wooden_stuff.config")
+local legacy = require("lord_wooden_stuff.legacy")
 
 local function register_api()
 	_G.lord_wooden_stuff = api
@@ -15,9 +16,17 @@ local function register_lord_wooden_stuff()
 	end
 end
 
+local function handle_legacy()
+	for _, wood_def in ipairs(config.wood_defs) do
+		local wood = wood_def[1]
+		legacy.register_aliases_by_wood(wood)
+	end
+end
+
 return {
 	init = function()
 		register_api()
 		register_lord_wooden_stuff()
+		handle_legacy()
 	end,
 }
