73e320d244dad46ea2bcbb2c27eb91e2931e22c0
ssdaniel24 <bo7oaonteg2m@mail.ru>
2024-06-20 12:39:20 +0300

    Added painting overwrites
    With adding crafts for legacy brushes. Relates to #1197.

---
+++ mods/lord/_overwrites/various/painting/init.lua
@@ -0,0 +1,14 @@
+for _, dye in ipairs(dye.dyes) do
+	local color = dye[1]
+	minetest.register_craft({
+		output = "painting:brush_"..color,
+		recipe = {
+			{"dye:"..color},
+			{"group:stick"},
+			{"group:stick"}
+		}
+	})
+end
+
+minetest.register_alias("painting:brush_darkgrey", "painting:brush_dark_grey")
+minetest.register_alias("painting:brush_darkgreen", "painting:brush_dark_green")
