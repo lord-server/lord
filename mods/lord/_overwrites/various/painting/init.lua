5e1c82a9e93ca928d42d915dd9c6f0a4a3d06de7
ssdaniel24 <bo7oaonteg2m@mail.ru>
2024-06-20 23:30:17 +0300

    Unregistered painting canvas 64x64 (it causes freezes)

--- mods/lord/_overwrites/various/painting/init.lua
+++ mods/lord/_overwrites/various/painting/init.lua
@@ -12,3 +12,5 @@ end
 
 minetest.register_alias("painting:brush_darkgrey", "painting:brush_dark_grey")
 minetest.register_alias("painting:brush_darkgreen", "painting:brush_dark_green")
+
+minetest.unregister_item("painting:canvas_64")
