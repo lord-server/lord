31c7f9dee1729023c291edd6980e1e34da92636a
alek13 <alek13.me@gmail.com>
2024-06-29 02:42:16 +0300

    Dirt blocks: add aliases for ghost bloscks. Relates to #1348. Closes #1428

--- mods/lord/Blocks/lord_ground/legacy.lua
+++ mods/lord/Blocks/lord_ground/legacy.lua
@@ -12,3 +12,17 @@ minetest.register_alias("lottmapgen:rohan_grass", "lord_ground:dirt_rohan")
 minetest.register_alias("lottmapgen:shire_grass", "lord_ground:dirt_shire")
 
 minetest.register_alias("lottmapgen:mordor_sand", "lord_ground:mordor_sand")
+
+
+-- DEFAULTS (Ghost blocks)
+minetest.register_alias("defaults:lottmapgen_dunland_grass", "defaults:lord_ground_dirt_dunland")
+minetest.register_alias("defaults:lottmapgen_fangorn_grass", "defaults:lord_ground_dirt_fangorn")
+minetest.register_alias("defaults:lottmapgen_gondor_grass", "defaults:lord_ground_dirt_gondor")
+minetest.register_alias("defaults:lottmapgen_ironhill_grass", "defaults:lord_ground_dirt_iron_hills")
+minetest.register_alias("defaults:lottmapgen_ithilien_grass", "defaults:lord_ground_dirt_ithilien")
+minetest.register_alias("defaults:lottmapgen_lorien_grass", "defaults:lord_ground_dirt_lorien")
+minetest.register_alias("defaults:lottmapgen_mirkwood_grass", "defaults:lord_ground_dirt_mirkwood")
+minetest.register_alias("defaults:lottmapgen_rohan_grass", "defaults:lord_ground_dirt_rohan")
+minetest.register_alias("defaults:lottmapgen_shire_grass", "defaults:lord_ground_dirt_shire")
+
+minetest.register_alias("lottmapgen:mordor_sand", "lord_ground:mordor_sand")
