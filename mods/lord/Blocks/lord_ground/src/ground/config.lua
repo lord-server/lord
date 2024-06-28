bd8540ef6d2a5f6723d67705e698eab52bb8438a
alek13 <alek13.me@gmail.com>
2024-06-28 20:02:16 +0300

    Dirt bloscks: add coarse & stony dirts for desert & mordor. Closes #1321

--- mods/lord/Blocks/lord_ground/src/ground/config.lua
+++ mods/lord/Blocks/lord_ground/src/ground/config.lua
@@ -23,6 +23,24 @@ local config = {
 				groups = { soil = 1 },
 				soil  = { wet = "<fake>", dry = "lord_ground:coarse_dirt" }
 			} },
+
+			["lord_ground:coarse_dirt_desert"] = { craft_from = "default:desert_gravel", softness = 2, definition = {
+				groups = { soil = 1 },
+				soil  = { wet = "<fake>", dry = "default:dirt" }
+			} },
+			["lord_ground:stony_dirt_desert"]  = { craft_from = "default:desert_cobble", softness = 1, definition = {
+				groups = { soil = 1 },
+				soil  = { wet = "<fake>", dry = "lord_ground:coarse_dirt_desert" }
+			} },
+
+			["lord_ground:coarse_dirt_mordor"] = { craft_from = "lottmapgen:mordor_gravel", softness = 2, definition = {
+				groups = { soil = 1 },
+				soil  = { wet = "<fake>", dry = "default:dirt" }
+			} },
+			["lord_ground:stony_dirt_mordor"]  = { craft_from = "lottmapgen:mordor_cobble", softness = 1, definition = {
+				groups = { soil = 1 },
+				soil  = { wet = "<fake>", dry = "lord_ground:coarse_dirt_mordor" }
+			} },
 		},
 	},
 	--sand = {
@@ -40,7 +58,14 @@ if mordor_from_setting and mordor_to_setting then
 		from = { x = tonumber(mordor_from[1]), z = tonumber(mordor_from[2]) },
 		to   = { x = tonumber(mordor_to[1]), z = tonumber(mordor_to[2]) },
 		exclude_dirts = { "lord_ground:dirt_dunland", "lord_ground:dirt_mirkwood" },
-		covers_with   = { "lord_rocks:mordor_stone", "lord_ground:coarse_dirt", "lord_ground:stony_dirt" },
+		covers_with   = {
+			"lord_rocks:mordor_stone",
+			"lord_rocks:mordor_stone",
+			"lord_ground:coarse_dirt",
+			"lord_ground:coarse_dirt_mordor",
+			"lord_ground:stony_dirt",
+			"lord_ground:stony_dirt_mordor",
+		},
 	}
 else
 	minetest.log("warning", "lord_ground.mordor_lands.from/to aren't set, disabling the functionality...")
