f48a13463d1482934ef1d73bd2f6c7d3c82d73db
Alexander Chibrikin <alek13.me@gmail.com>
2024-06-30 23:01:30 +0300

    Revert "lottblocks: added sound cooldowns to music stuff (#1461). Closes #1405."
    This reverts commit 624ec4021eb3b08e835985f5697a4fc1670d2189.

--- mods/lord/Blocks/lottblocks/music.lua
+++ mods/lord/Blocks/lottblocks/music.lua
@@ -1,27 +1,5 @@
 local S = minetest.get_translator("lottblocks")
 
----@readonly
----@type number
-local SOUND_COOLDOWN = 5
---- Contains sounds cache. Key is sum of coords (literally) or player name.
----@type table<number|string,boolean>
-local sound_cooldown_cache = {}
-
----@param name string
----@param def table
----@param player_name? string
-local function play_sound(name, def, player_name)
-	local p = def.pos
-	local hash = player_name or p.x + p.y + p.z -- HACK: rough "unique" hash
-	if not sound_cooldown_cache[hash] then
-		minetest.sound_play(name, def)
-		sound_cooldown_cache[hash] = true
-		minetest.after(SOUND_COOLDOWN, function()
-			sound_cooldown_cache[hash] = nil
-		end)
-	end
-end
-
 minetest.register_node("lottblocks:dwarf_harp", {
 	description   = S("Dwarvern Harp"),
 	tiles         = {
@@ -36,7 +14,7 @@ minetest.register_node("lottblocks:dwarf_harp", {
 	paramtype     = "light",
 	paramtype2    = "facedir",
 	on_punch      = function(pos)
-		play_sound("lottblocks_harp", {
+		minetest.sound_play("lottblocks_harp", {
 			pos               = pos,
 			max_hear_distance = 12,
 			gain              = 1,
@@ -117,11 +95,11 @@ for _, row in ipairs(whistle) do
 		description     = S(wood:gsub("^%l", string.upper) .. " (Note " .. note .. ") Whistle"),
 		inventory_image = "lottblocks_" .. wood .. "_whistle.png",
 		on_use          = function(itemstack, user)
-			play_sound(note, {
+			minetest.sound_play(note, {
 				pos               = user:get_pos(),
 				max_hear_distance = 7,
 				gain              = 1,
-			}, user:get_player_name())
+			})
 		end
 	})
 	minetest.register_craft({
@@ -142,7 +120,7 @@ minetest.register_node("lottblocks:gong", {
 	paramtype = "light",
 	paramtype2 = "facedir",
 	on_punch = function(pos)
-		play_sound("gong", {
+		minetest.sound_play("gong", {
 			pos = pos,
 			max_hear_distance = 48,
 			gain = 1,
