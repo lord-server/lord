1ae87e29ab85400e91e059a05b2840c1248b034b
alek13 <alek13.me@gmail.com>
2024-07-02 19:27:27 +0300

    Rebalance: Food: take ru titles for analytics document. Closes #1502

--- util/tmp-mods/food_points_stat/init.lua
+++ util/tmp-mods/food_points_stat/init.lua
@@ -1,13 +1,20 @@
 
-local function find_foods_after_mods_loaded()
-	minetest.register_on_mods_loaded(function()
-		for key, value in pairs(minetest.registered_items) do
-			if value._tt_food then
-				print(key .. "\t" .. value._tt_food_hp )
-			end
+local function find_foods_points()
+	local foods_points_lines = ""
+
+	for key, value in pairs(minetest.registered_items) do
+		if value._tt_food then
+			local description = minetest.get_translated_string('ru', value.description)
+			description = description:split("\n")[1]
+			foods_points_lines = foods_points_lines .. key .. "\t" .. description .. "\t" .. value._tt_food_hp .. "\n"
 		end
-	end)
+	end
+
+	return foods_points_lines
 end
 
-
-find_foods_after_mods_loaded()
+minetest.after(5, function()
+	local filepath = minetest.get_worldpath() .. "/foods_points.txt"
+	io.write_to_file(filepath, find_foods_points())
+	print("Food points saved to: " .. filepath)
+end)
