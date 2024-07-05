97f33bc0eda9cf9d6ca8b5494b74defe64312fb3
alek13 <alek13.me@gmail.com>
2024-07-05 14:23:22 +0300

    Add/Move `/armor.{get|set}` command(s) into `dev` from `lord_damage` branch. Relates to # 1509

--- mods/lord/Game/debugtools/src/commands.lua
+++ mods/lord/Game/debugtools/src/commands.lua
@@ -1,7 +1,5 @@
 
-
-print(__FILE_LINE__())
-
+------------------------   S A T I E T Y   ------------------------
 minetest.register_chatcommand("satiety.get", {
 	func = function(name, _)
 		local player    = minetest.get_player_by_name(name)
@@ -17,8 +15,7 @@ minetest.register_chatcommand("satiety.get", {
 
 minetest.register_chatcommand("satiety.set", {
 	params = "<value>",
-	func = function(name, param)
-		print(dump(param))
+	func   = function(name, param)
 		if not param or param == "" then
 			return false, "<value> not specified"
 		end
@@ -38,4 +35,34 @@ minetest.register_chatcommand("satiety.set", {
 			"From `hbhunger.get_hunger_raw()`: " .. hbhunger.get_hunger_raw(player) .. "\n" ..
 			"From `hbhunger.hunger[name]`: " .. hbhunger.hunger[name]
 	end
+})
+
+------------------------   A R M O R   ------------------------
+minetest.register_chatcommand("armor.get", {
+	func = function(name, _)
+		return true, dump(minetest.get_player_by_name(name):get_armor_groups())
+	end
+})
+
+minetest.register_chatcommand("armor.set", {
+	params = "<type> [value]",
+	func   = function(name, param)
+		if not param or param == "" then
+			return false, "params: <type> [<value>]"
+		end
+		local params = param:split(" ")
+		if #params < 1 or #params > 2 then
+			return false, "params: <type> [<value>]"
+		end
+
+		local type         = params[1]
+		local value        = tonumber(params[2])
+
+		local player       = minetest.get_player_by_name(name)
+		local armor_groups = player:get_armor_groups()
+		armor_groups[type] = value
+		player:set_armor_groups(armor_groups)
+
+		return true, dump(minetest.get_player_by_name(name):get_armor_groups())
+	end,
 })
