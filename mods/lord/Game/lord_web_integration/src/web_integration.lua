c8a97c4f83347da47a17e0f2b2daa5d99e052662
alek13 <alek13.me@gmail.com>
2024-07-10 18:14:41 +0300

    Game-Web: add tmp command for register clan players on webside. Closes #1532

--- mods/lord/Game/lord_web_integration/src/web_integration.lua
+++ mods/lord/Game/lord_web_integration/src/web_integration.lua
@@ -3,6 +3,50 @@ local Logger      = require("web_integration.Logger")
 local for_players = require("web_integration.for_players")
 local for_clans   = require("web_integration.for_clans")
 
+local function create_player_on_web(player_name, notify_player_name)
+	minetest.chat_send_player(notify_player_name, player_name .. ": send `::create()`")
+	web_api.players:create(
+		{
+			name       = player_name,
+			race       = races.get_race(player_name),
+			gender     = races.get_gender(player_name),
+			last_login = minetest.get_auth_handler().get_auth(player_name).last_login
+		},
+
+		function(result)
+			local created_player = minetest.parse_json(result.data)
+			if created_player == nil then
+				Logger.error("Can't store player web id: unable to parse response json: " .. result.data)
+				return
+			end
+
+			Storage.set_player_web_id(player_name, created_player.id)
+			minetest.chat_send_player(notify_player_name, player_name .. ":web_id: " .. created_player.id)
+		end,
+
+		Logger.log_api_error
+	)
+end
+
+local function register_tmp_command_for_integrate_clan_players()
+	minetest.register_chatcommand('web_send_clan_players', {
+		privs = { server = true },
+		func  = function(executor_name)
+			local seconds = 0
+			for _, clan in pairs(clans.list()) do
+				for _, player_name in pairs(clan.players) do
+					local player_web_id = Storage.get_player_web_id(player_name)
+					if not player_web_id then
+						seconds = seconds + 1
+						minetest.after(seconds, function()
+							create_player_on_web(player_name, executor_name)
+						end)
+					end
+				end
+			end
+		end
+	})
+end
 
 
 return {
@@ -17,5 +61,7 @@ return {
 
 		for_players.register(Storage, Logger)
 		for_clans.register(Storage, Logger)
+
+		register_tmp_command_for_integrate_clan_players()
 	end,
 }
