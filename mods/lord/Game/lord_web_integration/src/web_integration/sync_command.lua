951ae4becc413ce9a4224143aa5e112182d4a96b
alek13 <alek13.me@gmail.com>
2024-07-11 17:28:15 +0300

    Game-Web: add clan id sync. Closes #1538

--- mods/lord/Game/lord_web_integration/src/web_integration/sync_command.lua
+++ mods/lord/Game/lord_web_integration/src/web_integration/sync_command.lua
@@ -3,7 +3,7 @@ local Storage
 --- @type web_integration.Player
 local WebPlayer
 --- @type web_integration.Clan
---local WebClan
+local WebClan
 
 local function create_player_on_web(player_name, notify_player_name)
 	minetest.chat_send_player(notify_player_name, player_name .. ": send `::create()`")
@@ -23,27 +23,72 @@ local function create_player_on_web(player_name, notify_player_name)
 	)
 end
 
-local function sync_clans_handler(executor_name)
-	local seconds = 0
-	minetest.chat_send_player(executor_name, "Web Sync Clans Start")
-	for _, clan in pairs(clans.list()) do
-		minetest.chat_send_player(executor_name, "Web Sync Clan: " .. clan.name)
-		for _, player_name in pairs(clan.players) do
-			local player_web_id = Storage.get_player_web_id(player_name)
-			if not player_web_id then
-				minetest.chat_send_player(executor_name, "Web Sync Player: " .. player_name)
-				seconds = seconds + 1
-				minetest.after(seconds, function()
-					create_player_on_web(player_name, executor_name)
-				end)
-			else
-				minetest.chat_send_player(
-					executor_name,
-					"Web Sync Player: " .. player_name .. " already synced: " .. player_web_id
-				)
-			end
+---@param clan clans.Clan
+---@param notify_player_name string
+local function create_clan_on_web(clan, notify_player_name)
+	minetest.chat_send_player(notify_player_name, "Clan " .. clan.name .. ": send `::create()`")
+
+	WebClan.create(
+		clan,
+		function(created_clan)
+			minetest.chat_send_player(notify_player_name, clan.name .. ":web_id: " .. created_clan.id)
+		end,
+		function(linked_clan)
+			minetest.chat_send_player(notify_player_name, clan.name .. ":linked web_id: " .. linked_clan.id)
+		end,
+		function(result)
+			minetest.chat_send_player(notify_player_name, clan.name .. ":failed: " .. dump(result))
+		end
+	)
+end
+
+---@param players_names string[]
+---@param executor_name string
+---@param iteration number
+local function sync_players(players_names, executor_name, iteration)
+	for _, player_name in pairs(players_names) do
+		local player_web_id = Storage.get_player_web_id(player_name)
+
+		if not player_web_id then
+			minetest.chat_send_player(executor_name, "Web Sync Player: " .. player_name)
+			iteration = iteration + 1
+			minetest.after(iteration, function()
+				create_player_on_web(player_name, executor_name)
+			end)
+		else
+			minetest.chat_send_player(
+				executor_name,
+				"Web Sync Player: " .. player_name .. " already synced: " .. player_web_id
+			)
 		end
 	end
+
+	return iteration
+end
+
+--- @param clan clans.Clan
+--- @param executor_name string
+--- @param iteration number
+local function sync_clan(clan, executor_name, iteration)
+	local clan_web_id = Storage.get_clan_web_id(clan.name)
+	if not clan_web_id then
+		iteration = iteration + 1
+		minetest.after(iteration, function()
+			create_clan_on_web(clan, executor_name)
+		end)
+	end
+end
+
+
+local function sync_clans_handler(executor_name)
+	minetest.chat_send_player(executor_name, "Web Sync Clans Start")
+	local iteration = 0
+	for _, clan in pairs(clans.list()) do
+		minetest.chat_send_player(executor_name, "Web Sync Clan: " .. clan.name)
+		iteration = sync_players(clan.players, executor_name, iteration)
+		iteration = sync_clan(clan, executor_name, iteration)
+	end
+
 	minetest.chat_send_player(executor_name, "Web Sync Clans Finish")
 end
 
@@ -52,8 +97,9 @@ end
 return {
 	--- @param storage    web_integration.Storage
 	--- @param web_player web_integration.Player
-	register = function(storage, web_player)
-		Storage, WebPlayer = storage, web_player
+	--- @param web_clan   web_integration.Clan
+	register = function(storage, web_player, web_clan)
+		Storage, WebPlayer, WebClan = storage, web_player, web_clan
 		minetest.register_chatcommand('web_sync_clans', {
 			privs = { server = true },
 			func  = sync_clans_handler
