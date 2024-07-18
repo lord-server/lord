b8cdcd63cb1824b7a4b2672b43bc1ba7aa5a76d2
alek13 <alek13.me@gmail.com>
2024-07-18 20:43:01 +0300

    Add `minetest.mod()` helper. Relates to #913

--- mods/lord/Core/builtin/src/mod.lua
+++ mods/lord/Core/builtin/src/mod.lua
@@ -1,5 +1,6 @@
 --- @type string
 local DS = os.DIRECTORY_SEPARATOR
+local debug_mode = minetest.settings:get_bool("debug", false)
 
 --- @param sub_folder string
 --- @return string
@@ -9,3 +10,21 @@ function minetest.get_mod_textures_folder(sub_folder)
 	return mod_path .. DS .. "textures" .. DS .. sub_folder
 end
 
+--- @class minetest.Mod
+--- @field name string name of mod
+--- @field path string path of mad
+
+--- @param mod_init_function fun(mod:minetest.Mod)
+function minetest.mod(mod_init_function)
+	local mod_name = minetest.get_current_modname()
+	local mod_path = minetest.get_modpath(mod_name)
+
+	mod_init_function({
+		name = mod_name,
+		path = mod_path,
+	})
+
+	if debug_mode then
+		minetest.log("info", string.format("Mod loaded: [%s]", mod_name))
+	end
+end
