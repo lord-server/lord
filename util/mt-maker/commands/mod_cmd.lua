5291e63f856fd7c15a6676cee699ecac01f2dc83
alek13 <alek13.me@gmail.com>
2024-07-19 03:06:00 +0300

    Util: `./lord mod` cli-cmd: Change `init.lua` generation for use new `minetest.mod()`. Relates to #913

--- util/mt-maker/commands/mod_cmd.lua
+++ util/mt-maker/commands/mod_cmd.lua
@@ -28,17 +28,11 @@ local function create_files(mod_name)
 
 	create_file_with(
 		mod_name .. DS .. "init.lua",
-
-		"local DS          = os.DIRECTORY_SEPARATOR\
-local mod_path    = minetest.get_modpath(minetest.get_current_modname())\
-local old_require = require\
-require           = function(name) return dofile(mod_path .. DS .. \"src\" .. DS .. name:gsub(\"%.\", DS) .. \".lua\") end\
+		"\
 \
-\
-require(\"" .. mod_name .. "\").init()\
-\
-\
-require = old_require\
+minetest.mod(function(mod)\
+	require(\"" .. mod_name .. "\").init()\
+end)\
 "
 	)
 
