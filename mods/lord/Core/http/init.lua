75c3437dfaf87136e8bd343396b4142df15f0b67
alek13 <alek13.me@gmail.com>
2024-07-20 18:43:19 +0300

    Fix impossibility of obtaining http API. Broken while #913

--- mods/lord/Core/http/init.lua
+++ mods/lord/Core/http/init.lua
@@ -1,10 +1,12 @@
 
+local http_api = minetest.request_http_api()
+if not http_api then
+	minetest.log(
+		"error",
+		"[http] You should add mod `http` into `secure.http_mods` setting in your `minetest.conf`.")
+	return
+end
 
 minetest.mod(function(mod)
-	local http_api = minetest.request_http_api()
-	if not http_api then
-		minetest.log("error", "You should add mod `http` into `secure.http_mods` setting in your `minetest.conf`.")
-		return
-	end
 	require("http").init(http_api)
 end)
