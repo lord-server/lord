4444bfc36e68959712676b258fb4f5c077954ee2
alek13 <alek13.me@gmail.com>
2024-07-27 17:34:19 +0300

    Refactoring: `lottarmor`: move armor wear into `equipment_armor`. Relates to #339, #1012

--- mods/lord/Player/equipment_armor/src/equipment_armor.lua
+++ mods/lord/Player/equipment_armor/src/equipment_armor.lua
@@ -1,8 +1,48 @@
+local math_random
+    = math.random
+
+local SL = minetest.get_translator("lottarmor")
+
+
 local ARMOR_EQUIPMENT_SIZE = 5
 equipment.Kind.ARMOR       = "armor"
+
+--- @param stack     ItemStack
+--- @param player    Player
+local function item_wear(stack, slot, player)
+	local use = stack:get_definition().groups["armor_use"] or 0
+	stack:add_wear(use)
+
+	local armor_inv = minetest.get_inventory({type="detached", name = player:get_player_name().."_armor"})
+	if stack:get_count() == 0 then
+		local desc = minetest.registered_items[stack:get_name()].description
+		if desc then
+			minetest.chat_send_player(player:get_player_name(), desc.." "..SL("got destroyed!"))
+		end
+		armor_inv:set_stack("armor", slot, nil)
+		equipment.for_player(player):delete(equipment.Kind.ARMOR, slot)
+	else
+		armor_inv:set_stack("armor", slot, stack)
+		equipment.for_player(player):set(equipment.Kind.ARMOR, slot, stack)
+	end
+end
+
+local handle_armor_wear = function(player)
+	for slot, stack in equipment.for_player(player):items(equipment.Kind.ARMOR) do
+		if stack:get_count() > 0 then
+			-- chance that the hit landed on this slot (on this element of the armor)
+			if math_random(2) == 1 then
+				item_wear(stack, slot, player)
+				break -- the hit could get only one element of the armor
+			end
+		end
+	end
+end
+
 
 return {
 	init = function()
 		equipment.Kind.register(equipment.Kind.ARMOR, ARMOR_EQUIPMENT_SIZE)
+		minetest.register_on_punchplayer(handle_armor_wear)
 	end
 }
