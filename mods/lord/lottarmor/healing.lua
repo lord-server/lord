620c93489a365314872858fb1eaffd0ca7bf2819
alek13 <alek13.me@gmail.com>
2024-07-29 02:01:25 +0300

    Refactoring: `lottarmor`: fix healing (damage avoid). Relates to #339, #1012.

--- mods/lord/lottarmor/healing.lua
+++ mods/lord/lottarmor/healing.lua
@@ -3,6 +3,7 @@ local math_random
 
 
 --- @param player Player
+--- @return number
 local function get_armor_healing_chance(player)
 	local armor_healing_chance = 0
 	for _, stack in equipment.for_player(player):items(equipment.Kind.ARMOR) do
@@ -10,16 +11,19 @@ local function get_armor_healing_chance(player)
 			armor_healing_chance = armor_healing_chance + (stack:get_definition().groups["armor_heal"] or 0)
 		end
 	end
+
+	return armor_healing_chance
 end
 
+--- @param player Player
+--- @param
 local handle_armor_healing = function(player)
 	local name = player:get_player_name()
 	if not name then return end
 
 	local armor_healing_chance = get_armor_healing_chance(player)
 	if math_random(100) < armor_healing_chance then
-		player:set_hp(armor.player_hp[name])
-		return
+		return true -- this will prevent the default damage mechanism, so no damage will be applied
 	end
 end
 
