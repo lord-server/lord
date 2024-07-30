e51419740c04c563a14d41a98a32ccbd4847f4a9
alek13 <alek13.me@gmail.com>
2024-07-31 00:55:21 +0300

    Add `international` cloak (+ some refactoring `clans_clothes`). Closes #1378

--- mods/lord/Tools/clans_clothes/init.lua
+++ mods/lord/Tools/clans_clothes/init.lua
@@ -1,22 +1,26 @@
-local S = minetest.get_translator("clans_clothes")
+minetest.mod(function(mod)
+	local S = minetest.get_translator("clans_clothes")
 
-minetest.register_tool("clans_clothes:mason_cloak", {
-	description     = S("Masons Clan Cloak"),
-	inventory_image = "clans_clothes_inv_mason_cloak.png",
-	groups          = { clothes = 1, no_preview = 1, clothes_cloak = 1 },
-	wear            = 0
-})
 
-minetest.register_tool("clans_clothes:vassal_cloak", {
-	description     = S("Vassals Clan Cloak"),
-	inventory_image = "clans_clothes_inv_vassal_cloak.png",
-	groups          = { clothes = 1, no_preview = 1, clothes_cloak = 1 },
-	wear            = 0
-})
+	-- Кланы, которые запросили плащ, прислали текстуры на GH
+	local clan_titles = {
+		['masons']        = 'Masons',
+		['vassals']       = 'Vassals',
+		['hansa']         = 'Hansa',
+		['international'] = 'International',
+	}
 
-minetest.register_tool("clans_clothes:hansa_cloak", {
-	description     = S("Hansa Clan Cloak"),
-	inventory_image = "clans_clothes_inv_hansa_cloak.png",
-	groups          = { clothes = 1, no_preview = 1, clothes_cloak = 1 },
-	wear            = 0
-})
+	for id, title in pairs(clan_titles) do
+		minetest.register_tool("clans_clothes:cloak_" .. id, {
+			description     = S(title .. " Clan Cloak"),
+			inventory_image = "clans_clothes_cloak_" .. id .. "_inv.png",
+			groups          = { clothes = 1, no_preview = 1, clothes_cloak = 1 },
+			wear            = 0
+		})
+	end
+
+	-- TODO: удалить в следующем релизе (сами апдейтятся через сундук движком при load map block)
+	minetest.register_alias('clans_clothes:mason_cloak',  'clans_clothes:cloak_masons')
+	minetest.register_alias('clans_clothes:vassal_cloak', 'clans_clothes:cloak_vassals')
+	minetest.register_alias('clans_clothes:hansa_cloak',  'clans_clothes:cloak_hansa')
+end)
