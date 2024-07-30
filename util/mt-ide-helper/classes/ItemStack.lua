589f369ce4621581a902e5311c9f93555218f5c3
alek13 <alek13.me@gmail.com>
2024-07-30 22:20:32 +0300

    IDE-helper: full docs for `ItemStack`

--- util/mt-ide-helper/classes/ItemStack.lua
+++ util/mt-ide-helper/classes/ItemStack.lua
@@ -1,80 +1,110 @@
 ---@class ItemStack
 ItemStack = {}
 
---- put some item or stack onto this stack, returns leftover ItemStack
+--- @class ItemStackTable: table
+--- @field name     string
+--- @field count    number
+--- @field wear     number
+--- @field metadata string|
+
+--- @alias ItemStackString string
+
+--- Put some item or stack onto this stack, returns leftover ItemStack
+--- @param item ItemStack|ItemStackString|ItemStackTable
+--- @return self
 function ItemStack:add_item(item) end
 
---- increases wear by amount if the item is a tool
----@param amount number
+--- Increases wear by amount if the item is a tool, otherwise does nothing.
+--- Wear is only added if the item is a tool. Adding wear might destroy the item.
+--- @param amount number Valid amount range is [0,65536]
+--- @return boolean Returns true if the item is (or was) a tool.
 function ItemStack:add_wear(amount) end
 
---- removes all items from the stack, making it empty
+--- Removes all items from the stack, making it empty
+--- @return boolean it seams that it always return `true`
 function ItemStack:clear() end
 
---- returns number of items on the stack
----@return number
+--- Returns number of items on the stack
+--- @return number
 function ItemStack:get_count() end
 
---- returns true/false (success), clears item on failure
----@param count number
+--- Returns true/false (success), clears item on failure
+--- @param count number count > 0 && count <= 65535  -- or clears
+--- @return boolean
 function ItemStack:set_count(count) end
 
---- returns the item definition table
+--- @return ItemDefinition returns the item definition table
 function ItemStack:get_definition() end
 
---- returns get_stack_max() - get_count()
+--- Returns get_stack_max() - get_count()
+--- @return number
 function ItemStack:get_free_space() end
 
 --- @return MetaDataRef
 function ItemStack:get_meta() end
 
---- returns a string, for compatibility reasons this is equal to get_meta():get_string("")
----@deprecated
+--- Returns a string, for compatibility reasons this is equal to get_meta():get_string("")
+--- @deprecated
 function ItemStack:get_metadata() end
 
---- returns item name (e.g. "default:stone")
----@return string
+--- Returns item name (e.g. "default:stone")
+--- @return string
 function ItemStack:get_name() end
 
---- returns true/false (success), clears item on failure
----@param item_name string
+--- Returns true/false (success), clears item on failure
+--- @param item_name string
+--- @return boolean
 function ItemStack:set_name(item_name) end
 
---- returns the maximum size of the stack (depends on the item)
+--- Returns the maximum size of the stack (depends on the item)
+--- @return number
 function ItemStack:get_stack_max() end
 
---- returns the digging properties of the item, or those of the hand if none are defined for this item type
+--- Returns the digging properties of the item, or those of the hand if none are defined for this item type
+--- @return table
 function ItemStack:get_tool_capabilities() end
 
---- returns tool wear (0-65535), 0 for non-tools
+--- Returns tool wear (0-65535), 0 for non-tools
+--- @return number
 function ItemStack:get_wear() end
 
---- returns true/false (success), clears item on failure
+--- Returns true/false (success), clears item on failure
+--- @param wear number wear <= 65535, otherwise clears
+--- @return boolean
 function ItemStack:set_wear(wear) end
 
---- return true if stack is empty
----@return boolean
+--- Return true if stack is empty
+--- @return boolean
 function ItemStack:is_empty() end
 
---- returns true if the item name refers to a defined item type
----@return boolean
+--- Returns true if the item name refers to a defined item type
+--- @return boolean
 function ItemStack:is_known() end
 
---- returns true if item or stack can be fully added to this one
+--- Returns true if item or stack can be fully added to this one
+--- @param item ItemStack|ItemStackString|ItemStackTable
+--- @return boolean
 function ItemStack:item_fits(item) end
 
---- copy (don't remove) up to n items from this stack; returns copied ItemStack; if n is omitted, n=1 is used
-function ItemStack:peek_item(n) end
+--- Copy (don't remove) up to n items from this stack; returns copied ItemStack; if n is omitted, n=1 is used
+--- @param count number
+--- @return ItemStack
+function ItemStack:peek_item(count) end
 
---- replace the contents of this stack (item can also be an itemstring or table)
+--- Replace the contents of this stack (item can also be an itemstring or table)
+--- @param item ItemStack|ItemStackString|ItemStackTable
+--- @return boolean seams always `true`
 function ItemStack:replace(item) end
 
---- take (and remove) up to n items from this stack; returns taken ItemStack; if n is omitted, n=1 is used
-function ItemStack:take_item(n) end
+--- Take (and remove) up to n items from this stack; returns taken ItemStack; if n is omitted, n=1 is used
+--- @param count number
+--- @return ItemStack
+function ItemStack:take_item(count) end
 
---- returns the stack in itemstring form
+--- Returns the stack in itemstring form
+--- @return string
 function ItemStack:to_string() end
 
---- returns the stack in Lua table form
----@return table
+--- Returns the stack in Lua table form or `nil` if empty.
+--- @return table|nil
 function ItemStack:to_table() end
