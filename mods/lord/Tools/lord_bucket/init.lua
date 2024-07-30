44cb87fb511f8aa2e35ed8431b3dcadca0874660
alek13 <alek13.me@gmail.com>
2024-07-31 01:02:18 +0300

    Balance: Food: add `bucket:bucket_salt` and recipes. Relates to #1573

--- mods/lord/Tools/lord_bucket/init.lua
+++ mods/lord/Tools/lord_bucket/init.lua
@@ -1,31 +1,59 @@
-local S = minetest.get_translator("lord_bucket")
+minetest.mod(function(mod)
+	local S = minetest.get_translator("lord_bucket")
 
-minetest.register_craftitem(":bucket:bucket_ice", {
-	description = S("Ice Bucket"),
-	inventory_image = "bucket_ice.png",
-	stack_max = 1,
-})
 
-minetest.register_craft({output = 'bucket:bucket_ice 1',
-	recipe = {{'bucket:bucket_empty', 'default:ice'}}
-})
+	minetest.register_craftitem(":bucket:bucket_ice", {
+		description     = S("Ice Bucket"),
+		inventory_image = "bucket_ice.png",
+		stack_max       = 1,
+	})
 
-minetest.register_craft({output = "bucket:bucket_water",
-	type = "cooking",
-	recipe = "bucket:bucket_ice",
-})
+	minetest.register_craft({
+		output = 'bucket:bucket_ice 1',
+		recipe = { { 'bucket:bucket_empty', 'default:ice' } }
+	})
 
-minetest.register_craftitem(":bucket:bucket_snow", {
-	description = S("Snow Bucket"),
-	inventory_image = "bucket_snow.png",
-	stack_max = 1,
-})
+	minetest.register_craft({
+		output = "bucket:bucket_water",
+		type   = "cooking",
+		recipe = "bucket:bucket_ice",
+	})
 
-minetest.register_craft({output = 'bucket:bucket_snow 1',
-	recipe = {{'bucket:bucket_empty', 'default:snowblock'}}
-})
+	minetest.register_craftitem(":bucket:bucket_snow", {
+		description     = S("Snow Bucket"),
+		inventory_image = "bucket_snow.png",
+		stack_max       = 1,
+	})
 
-minetest.register_craft({output = "bucket:bucket_water",
-	type = "cooking",
-	recipe = "bucket:bucket_snow",
-})
+	minetest.register_craft({
+		output = 'bucket:bucket_snow 1',
+		recipe = { { 'bucket:bucket_empty', 'default:snowblock' } }
+	})
+
+	minetest.register_craft({
+		output = "bucket:bucket_water",
+		type   = "cooking",
+		recipe = "bucket:bucket_snow",
+	})
+
+	minetest.register_craftitem(":bucket:bucket_salt", {
+		description     = S("Salt Bucket"),
+		inventory_image = "bucket_salt.png",
+	})
+
+	minetest.register_craft({
+		type     = "cooking",
+		output   = "bucket:bucket_salt",
+		recipe   = "bucket:bucket_water",
+		cooktime = 10,
+	})
+
+	minetest.register_craft({
+		output       = "lottores:salt",
+		recipe       = { { "bucket:bucket_salt" } },
+		replacements = {
+			{ "bucket:bucket_salt", "bucket:bucket_empty" },
+		},
+	})
+end)
+
