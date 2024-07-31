f74f03c13824b59351e1884dc51217d28f61f068
alek13 <alek13.me@gmail.com>
2024-08-01 02:02:41 +0300

    Tooltips: decrease cyclomatic complexity for `snippet.digging`. Relates to #1587

--- mods/lord/Player/Help/lord_tooltips/src/snippet/digging.lua
+++ mods/lord/Player/Help/lord_tooltips/src/snippet/digging.lua
@@ -4,22 +4,35 @@ local items,                     colorize
 local S = minetest.get_translator(minetest.get_current_modname())
 
 
+--- @param caps_times table<number,number>
+--- @param level      number
+--- @param maxlevel   number
+--- @return number|nil
+local function get_dig_time(caps_times, level, maxlevel)
+	local time = caps_times[level]
+
+	return time
+		and (maxlevel > 1 and time / maxlevel or time)
+		or  nil
+end
+
 --- @param caps table
---- @return number,boolean
+--- @return number|nil,boolean
 local function get_min_digtime(caps)
 	local maxlevel = caps.maxlevel or 1
 	local unique = maxlevel > 1 and false or true
 	--- @type number
-	local mintime
+	local mintime = nil
 
-	if caps.times then
-		for r=1,3 do
-			local time = caps.times[r]
-			if time and maxlevel > 1 then
-				time = time / maxlevel
-			end
-			if time and ((not mintime) or (time < mintime)) then
-				if mintime and (time < mintime) then
+	if not caps.times then
+		return mintime, unique
+	end
+
+	for r = 1, 3 do
+		local time = get_dig_time(caps.times, r, maxlevel)
+		if time then
+			if not mintime or  time < mintime then
+				if mintime then
 					unique = false
 				end
 				mintime = time
@@ -30,6 +43,23 @@ local function get_min_digtime(caps)
 	return mintime, unique
 end
 
+--- @param caps  table
+--- @param group string
+--- @return string
+local function get_digging_list_item(caps, group)
+	local mintime, unique_mintime
+	mintime, unique_mintime = get_min_digtime(caps)
+	if mintime and (mintime > 0 or (not unique_mintime)) then
+		return S(
+			'@1 blocks in @2s (minimal)',
+			colorize('#bbb', S(group..'_dig')), string.format('%.2f', mintime)
+		)
+	elseif mintime and mintime == 0 then
+		return S('@1 blocks instantly', colorize('#bbb', S(group..'_dig')))
+	end
+
+	return nil
+end
 
 --- @param item_string string
 --- @return string|nil
@@ -39,18 +69,11 @@ return function(item_string)
 
 	local digging_strings = {}
 	for group, caps in pairs(tool_capabilities.groupcaps) do
-		local mintime, unique_mintime
-		local list_item
 		if caps.times then
-			mintime, unique_mintime = get_min_digtime(caps)
-			if mintime and (mintime > 0 or (not unique_mintime)) then
-				list_item =  S('@1 blocks in @2s (minimal)', colorize('#bbb', S(group..'_dig')), string.format('%.2f', mintime))
-				digging_strings[#digging_strings+1] = '  • ' .. list_item
-			elseif mintime and mintime == 0 then
-				list_item = S('@1 blocks instantly', colorize('#bbb', S(group..'_dig')))
-				digging_strings[#digging_strings+1] = '  • ' .. list_item
+			local list_item = get_digging_list_item(caps, group)
+			if list_item then
+				digging_strings[#digging_strings + 1] = '  • ' .. list_item
 			end
-
 		end
 	end
 
