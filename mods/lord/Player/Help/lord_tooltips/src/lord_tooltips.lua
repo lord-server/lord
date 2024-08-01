e9e451958dbd6f358e6d969c51dce3629ad143d3
alek13 <alek13.me@gmail.com>
2024-08-01 03:25:34 +0300

    Tooltips: copy remaining snippet `node_info` ad is. Relates to #1587

--- mods/lord/Player/Help/lord_tooltips/src/lord_tooltips.lua
+++ mods/lord/Player/Help/lord_tooltips/src/lord_tooltips.lua
@@ -4,6 +4,7 @@ local damage_snippet     = require('snippet.damage')
 local armor_snippet      = require('snippet.armor')
 local food_snippet       = require('snippet.food')
 local buffs_snippet      = require('snippet.buffs')
+local node_info_snippet  = require('snippet.node_info')
 
 
 return {
@@ -14,6 +15,7 @@ return {
 		tt.register_snippet(armor_snippet)
 		tt.register_snippet(food_snippet)
 		tt.register_snippet(buffs_snippet)
+		tt.register_snippet(node_info_snippet)
 		-- Прочность: armor_use/wear
 	end,
 }
