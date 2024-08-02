e68bec6a163155193a910c0e5ca63080ad206d88
alek13 <alek13.me@gmail.com>
2024-08-02 06:58:59 +0300

    Fix hardcoded `ingredient` in `/crafting_from` command.

--- mods/lord/Game/debugtools/src/commands/recipes.lua
+++ mods/lord/Game/debugtools/src/commands/recipes.lua
@@ -9,10 +9,11 @@ minetest.register_chatcommand('crafting_from', {
 		if not minetest.registered_items[param[1]] then
 			return false, 'Unknown/unregistered `ingredient`: ' .. param[1]
 		end
+		local ingredient  = param[1]
 		local recursively = param[2] and param[2] == 'true' and true or false
 
 		local output = ''
-		for i, recipe in pairs(minetest.get_all_craft_recipes_from('lottores:salt', recursively)) do
+		for i, recipe in pairs(minetest.get_all_craft_recipes_from(ingredient, recursively)) do
 			local out_item_name = recipe.output:split(' ')[1]
 			output = output .. out_item_name .. '   (' .. minetest.colorize('grey', recipe.method) .. ')\n'
 		end
