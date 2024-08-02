4394a95bc4448ec387169a5fad5a9ea1d6561bbb
alek13 <alek13.me@gmail.com>
2024-08-02 06:49:13 +0300

    Balance: melee weapon: racial. Closes #1599

--- mods/lord/Tools/tools/src/special.lua
+++ mods/lord/Tools/tools/src/special.lua
@@ -1,47 +1,68 @@
-local SL = minetest.get_translator("tools")
+local S = minetest.get_translator("tools")
+
 
 minetest.register_tool("tools:sword_elven", {
-	description = SL("Elven Sword"),
-	inventory_image = "tools_sword_elven.png",
+	description       = S("Elven Sword"),
+	inventory_image   = "tools_sword_elven.png",
 	tool_capabilities = {
-		full_punch_interval = 0.25,
-		max_drop_level=2,
-		groupcaps={
-			snappy={times={[1]=1.60, [2]=1.30, [3]=0.90}, uses=50, maxlevel=3},
-		},
-		damage_groups = {fleshy=7.5, soulful = 3},
+		max_drop_level      = 2,
+		groupcaps           = { snappy = { times = { [1] = 1.60, [2] = 1.30, [3] = 0.90 }, uses = 50, maxlevel = 3 }, },
+		damage_groups       = { fleshy = 6.2, soulful = 8 },
+		full_punch_interval = 1,
 	},
-	groups = {bronze_item = 1},
+	groups            = { bronze_item = 1 },
 })
-
 minetest.register_tool("tools:sword_orc", {
-	description = SL("Orcish Sword"),
-	inventory_image = "tools_sword_orc.png",
+	description       = S("Orcish Sword"),
+	inventory_image   = "tools_sword_orc.png",
 	tool_capabilities = {
-		full_punch_interval = 0.75,
-		max_drop_level=2,
-		groupcaps={
-			snappy={times={[1]=2.25, [2]=1.80, [3]=1.30}, uses=17, maxlevel=3},
-		},
-		damage_groups = {fleshy=6.5},
+		max_drop_level      = 2,
+		groupcaps           = { snappy = { times = { [1] = 2.25, [2] = 1.80, [3] = 1.30 }, uses = 17, maxlevel = 3 }, },
+		damage_groups       = { fleshy = 7.8 },
+		full_punch_interval = 1.2,
 	},
-	groups = {steel_item = 1},
+	groups            = { steel_item = 1 },
+})
+minetest.register_tool("tools:battleaxe_dwarven", {
+	description       = S("Dwarven Battleaxe"),
+	inventory_image   = "castle_battleaxe.png",
+	tool_capabilities = {
+		max_drop_level      = 1,
+		groupcaps           = {
+			choppy = { times = { [1] = 2.10, [2] = 0.90, [3] = 0.50 }, uses = 20, maxlevel = 3 },
+			snappy = { times = { [1] = 1.90, [2] = 0.90, [3] = 0.30 }, uses = 20, maxlevel = 3 },
+		},
+		damage_groups       = { fleshy = 25 },
+		full_punch_interval = 2.0,
+	},
+	groups            = { steel_item = 1 },
 })
 
 minetest.register_craft({
 	output = 'tools:sword_elven',
 	recipe = {
-		{'', 'default:steel_ingot', ''},
-		{'default:bronze_ingot', 'default:steel_ingot', 'default:bronze_ingot'},
-		{'default:mese_crystal', 'group:stick', 'default:mese_crystal'},
+		{ '', 'default:steel_ingot', '' },
+		{ 'default:bronze_ingot', 'default:steel_ingot', 'default:bronze_ingot' },
+		{ 'default:mese_crystal', 'group:stick', 'default:mese_crystal' },
 	}
 })
-
 minetest.register_craft({
 	output = 'tools:sword_orc',
 	recipe = {
-		{'', 'default:steel_ingot', 'default:steel_ingot'},
-		{'', 'default:steel_ingot', ''},
-		{'', 'bones:bone', ''},
+		{ '', 'default:steel_ingot', 'default:steel_ingot' },
+		{ '', 'lottores:mithril_lump', '' },
+		{ '', 'default:mese_crystal', '' },
 	}
 })
+minetest.register_craft({
+	output = "tools:battleaxe_dwarven",
+	recipe = {
+		{ "lottores:mithril_ingot", "default:mese_crystal", "lottores:mithril_ingot" },
+		{ "lottores:mithril_ingot", "group:stick",          "lottores:mithril_ingot" },
+		{ "",                       "group:stick",          "" }
+	}
+})
+
+
+-- @tags: legacy
+minetest.register_alias("castle:battleaxe", "tools:battleaxe_dwarven")
