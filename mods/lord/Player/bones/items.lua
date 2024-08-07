5b0013caca94e783a18118b07fa839089aed1801
Doloment <doloment@internet.ru>
2024-08-07 21:36:06 +0700

    Fix overpowered Bone Scythe; make it reach farther

--- mods/lord/Player/bones/items.lua
+++ mods/lord/Player/bones/items.lua
@@ -15,12 +15,13 @@ minetest.register_craftitem("bones:bone", {
 minetest.register_tool("bones:bone_scythe", {
 	description = SL("Bone Scythe"),
 	inventory_image = "bones_scythe.png",
+	range = 7,
 	tool_capabilities = {
-		full_punch_interval = 0.7,
+		full_punch_interval = 1.5,
 		max_drop_level=1,
 		groupcaps={
 			snappy={times={[1]=2.0, [2]=1.00, [3]=0.35}, uses=30, maxlevel=3},
 		},
-		damage_groups = {fleshy=7},
+		damage_groups = {fleshy=4},
 	}
 })
