c63dc0f3bf57e709bd028e9951d8d7d6bc369d2b
alek13 <alek13.me@gmail.com>
2024-08-23 17:09:57 +0300

    Refactoring `lottarmor`: extract `defense` mod into `_experimental`. Relates to #1012, #1610

---
+++ mods/lord/_experimental/tmp_defense/init.lua
@@ -0,0 +1,84 @@
+local math_limit, math_floor, string_match, ipairs
+	= math.limit, math.floor, string.match, ipairs
+
+--- @type table<number,string>|string[]
+local ARMOR_PURPOSE = { "head", "torso", "legs", "feet", "shield" }
+
+
+
+minetest.mod(function(mod)
+
+
+	local function collect_armor_data(player)
+		local armor_level     = 0
+		local armor_dmg_avoid = 0
+		local armor_fire      = 0
+		local armor_wear      = 0
+		local armor_count     = 0
+
+		local material = { type = nil, count = 1 } -- detection of same material armor-set
+
+		for slot, stack in equipment.for_player(player):items(equipment.Kind.ARMOR) do
+			if stack:get_count() == 1 then
+				local item_groups = stack:get_definition().groups
+				armor_level     = armor_level + (item_groups["armor_" .. ARMOR_PURPOSE[slot]] or 0)
+				armor_wear      = armor_wear + stack:get_wear()
+				armor_count     = armor_count + 1
+				armor_dmg_avoid = armor_dmg_avoid + (item_groups["damage_avoid_chance"] or 0)
+				armor_fire      = armor_fire + (item_groups["armor_fire"] or 0)
+				local mat = string_match(stack:get_name(), "%:.+_(.+)$")
+				if material.type then
+					if material.type == mat then
+						material.count = material.count + 1
+					end
+				else
+					material.type = mat
+				end
+			end
+		end
+
+		return armor_level, armor_dmg_avoid, armor_fire, armor_wear, armor_count,
+			(material.type and material.count == #ARMOR_PURPOSE)
+	end
+
+	local function rebuild_armor_groups(player, armor_level)
+		local immortal = player:get_armor_groups().immortal
+		if player:get_meta():get("lott:immunity") ~= nil and (not immortal or immortal == 0) then
+			return { fleshy = 1 }
+		end
+
+		local armor_groups = {}
+		if immortal and immortal ~= 0 then
+			armor_groups.immortal = 1
+		end
+
+		armor_level = math_limit(armor_level, 0, 100)
+		armor_groups.level = math_floor(armor_level / 20)
+		armor_groups.fleshy = 100 - armor_level
+
+		return armor_groups
+	end
+
+	local function set_player_defense(player)
+		local name = player:get_player_name()
+		if not name then
+			return
+		end
+
+		local armor_level, armor_dmg_avoid, armor_fire, armor_wear, armor_count, same_material_set = collect_armor_data(player)
+
+		if same_material_set then
+			armor_level = armor_level * 1.1
+		end
+
+		player:set_armor_groups(rebuild_armor_groups(player, armor_level))
+	end
+
+	equipment.on_change(equipment.Kind.ARMOR, function(player, kind, event, slot, item)
+		set_player_defense(player)
+	end)
+
+
+	dofile(mod.path .. "/damage_avoid.lua")
+
+end)
--- mods/lord/lottarmor/armor.lua
+++ mods/lord/lottarmor/armor.lua
@@ -4,10 +4,7 @@ local math_floor, string_match, ipairs
 
 --- @type table<number,string>|string[]
 local PHYSICS_TYPES = { "jump", "speed", "gravity" }
---- @type table<number,string>|string[]
-local ARMOR_PURPOSE = { "head", "torso", "legs", "feet", "shield" }
 
--- Armor API
 
 armor = {
 	def       = {},
@@ -15,7 +12,7 @@ armor = {
 
 
 equipment.on_change(equipment.Kind.ARMOR, function(player, kind, event, slot, item)
-	armor:set_player_armor(player)
+	armor:set_player_physics(player)
 end)
 
 --- @param player        Player
@@ -36,89 +33,14 @@ local function collect_physics(player)
 	return physics
 end
 
-local function collect_armor_data(player)
-	local armor_level     = 0
-	local armor_dmg_avoid = 0
-	local armor_fire      = 0
-	local armor_wear      = 0
-	local armor_count     = 0
 
-	local material = { type = nil, count = 1 } -- detection of same material armor-set
-
-	for slot, stack in equipment.for_player(player):items(equipment.Kind.ARMOR) do
-		if stack:get_count() == 1 then
-			local item_groups = stack:get_definition().groups
-			armor_level     = armor_level + (item_groups["armor_" .. ARMOR_PURPOSE[slot]] or 0)
-			armor_wear      = armor_wear + stack:get_wear()
-			armor_count     = armor_count + 1
-			armor_dmg_avoid = armor_dmg_avoid + (item_groups["damage_avoid_chance"] or 0)
-			armor_fire      = armor_fire + (item_groups["armor_fire"] or 0)
-			local mat = string_match(stack:get_name(), "%:.+_(.+)$")
-			if material.type then
-				if material.type == mat then
-					material.count = material.count + 1
-				end
-			else
-				material.type = mat
-			end
-		end
-	end
-
-	return armor_level, armor_dmg_avoid, armor_fire, armor_wear, armor_count,
-		(material.type and material.count == #ARMOR_PURPOSE)
-end
-
---- Ensure that `value` is between `min` and `max` and correct it by `min`/`max` limits
---- @param value number
---- @param min   number
---- @param max   number
----
---- @return number returns corrected value if it goes out of range
-local function limit(value, min, max)
-	value = value <   0 and   0 or value
-	value = value > 100 and 100 or value
-
-	return value
-end
-
-local function rebuild_armor_groups(player, armor_level)
-	local immortal = player:get_armor_groups().immortal
-	if player:get_meta():get("lott:immunity") ~= nil and (not immortal or immortal == 0) then
-		return { fleshy = 1 }
-	end
-
-	local armor_groups = {}
-	if immortal and immortal ~= 0 then
-		armor_groups.immortal = 1
-	end
-
-	armor_level = limit(armor_level, 0, 100)
-	armor_groups.level = math_floor(armor_level / 20)
-	armor_groups.fleshy = 100 - armor_level
-
-	return armor_groups
-end
 
 --- @param player Player
-function armor:set_player_armor(player)
+function armor:set_player_physics(player)
 	local name = player:get_player_name()
 	if not name then
 		return
 	end
-
-	local armor_level, armor_dmg_avoid, armor_fire, armor_wear, armor_count, same_material_set = collect_armor_data(player)
-
-	if same_material_set then
-		armor_level = armor_level * 1.1
-	end
-
-	player:set_armor_groups(rebuild_armor_groups(player, armor_level))
-
-	self.def[name].state     = armor_wear
-	self.def[name].count     = armor_count
-	self.def[name].level     = armor_level
-	self.def[name].dmg_avoid = armor_dmg_avoid
-	self.def[name].fire      = armor_fire
 
 	local physics_o = collect_physics(player)
 	player:set_physics_override(physics_o)
@@ -142,6 +64,6 @@ equipment.on_load(equipment.Kind.ARMOR, function(player, kind, event, slot, item
 		fire      = 0,
 	}
 
-	armor:set_player_armor(player)
+	armor:set_player_physics(player)
 end)
 
