b814a1b5910057b7c23dbcd9f799c230a503c549
ssdaniel24 <bo7oaonteg2m@mail.ru>
2024-08-27 18:05:50 +0300

    Fixed oil colors crafting with new oil

--- mods/lord/_overwrites/various/painting/init.lua
+++ mods/lord/_overwrites/various/painting/init.lua
@@ -1,13 +1,28 @@
 for _, dye in ipairs(dye.dyes) do
 	local color = dye[1]
+	local dye_item = "dye:"..color
+
 	minetest.register_craft({
 		output = "painting:brush_"..color,
 		recipe = {
-			{"dye:"..color},
-			{"group:stick"},
-			{"group:stick"}
+			{ dye_item, },
+			{ "group:stick", },
+			{ "group:stick, "}
 		}
 	})
+
+	-- TODO: remove after fixing sfence/painting#15 issue
+	minetest.clear_craft({output = "painting"})
+
+	minetest.register_craft{
+		output = "painting:oil_color_"..color,
+		recipe = {
+			{ dye_item, },
+			{ "lottfarming:vegetable_oil", },
+			{ "vessels:drinking_glass", },
+		},
+		replacements = {{ "lottfarming:vegetable_oil", "vessels:glass_bottle", }},
+	}
 end
 
 minetest.register_alias("painting:brush_darkgrey", "painting:brush_dark_grey")
