3f605ddee5828445878a5b0fa946e536034052bf
alek13 <alek13.me@gmail.com>
2024-09-07 21:28:09 +0300

    Damage Systesm: add usege example into `lord_damage`. Relates to #1623, #1626

--- mods/lord/Game/lord_damage/src/lord_damage.lua
+++ mods/lord/Game/lord_damage/src/lord_damage.lua
@@ -4,15 +4,30 @@ return {
 	--- @param mod minetest.Mod
 	init = function(mod)
 		-- TODO: register damage Types #1623
+		-- TODO: use config ?
+		damage.Type.FLESHY = 'fleshy'
+		damage.Type.FIRE   = 'fire'
+		damage.Type.SOUL   = 'soul'
+		damage.Type.POISON = 'poison'
 
-		-- TODO: register particles on damage of Type #1624
+		damage.Type.register(damage.Type.FLESHY)
+		damage.Type.register(damage.Type.FIRE)
+		damage.Type.register(damage.Type.SOUL)
+		damage.Type.register(damage.Type.POISON)
 
-		-- temporary test TODO: remove
+		-- temporary test TODO: remove/rewrite
 		if mod.debug then
+
+			-- TODO: register particles on damage of Type #1624
+			damage.on_damage_of(damage.Type.FIRE, function(...)
+				mod.logger.action('----------- damage.Type.FIRE: -----------')
+				mod.logger.action(dump({...}))
+			end)
+
 			damage.on_damage(function(player, amount, reason)
 				mod.logger.action('Damaged: ' .. dump({player, amount, reason}))
 
-				-- TODO: remove or move as damage Type detection into `Core/damage` mode
+				-- TODO: remove or move as damage Type detection into `Core/damage` mode #1626
 				if reason.node then
 					local node_definition = minetest.registered_nodes[reason.node]
 					print(dump(node_definition.damage_groups or (
