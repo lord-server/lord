654f9324d6b17f8cfca28f637176496af2318a2e
alek13 <alek13.me@gmail.com>
2024-09-09 00:33:15 +0300

    Damage System: add `damage.Type` detection for damaging nodes. Relates to #1626

--- mods/lord/Core/damage/src/damage/Type.lua
+++ mods/lord/Core/damage/src/damage/Type.lua
@@ -1,9 +1,16 @@
+local assert, table_contains, pairs, typeof
+    = assert, table.contains, pairs, type
+
 local TypeEvent = require('damage.Type.Event')
 
 
 --- @static
 --- @class damage.Type
 local Type = {
+	--- @private
+	--- @static
+	--- @type string
+	default = nil,
 	--- @private
 	--- @static
 	--- @type string[]
@@ -13,11 +20,61 @@ local Type = {
 }
 
 --- @param name string
+--- @return damage.Type
 function Type.register(name)
 	Type.registered[#Type.registered + 1] = name
 
 	TypeEvent.Type[name]        = name
 	TypeEvent.subscribers[name] = {}
+
+	return Type
+end
+
+--- @return string|nil
+function Type.get_default()
+	return Type.default or Type.registered[1]
+end
+
+--- @param type string
+--- @return damage.Type
+function Type.set_default(type)
+	assert(table_contains(Type.registered, type))
+	Type.default = type
+
+	return Type
+end
+
+--- @param damage_groups table<string,number>
+--- @return string|nil
+function Type.get_from_groups(damage_groups)
+	for type, damage in pairs(damage_groups) do
+		if table_contains(Type.registered, type) then
+			return type
+		end
+	end
+
+	return nil
+end
+
+--- @param reason PlayerHPChangeReason
+--- @return string
+function Type.detect(reason)
+	local type = Type.get_default()
+	assert(typeof(type) == 'string')
+
+	if reason.node then
+		local node_definition = minetest.registered_nodes[reason.node]
+
+		local damage_groups = node_definition.damage_groups or (
+			node_definition.tool_capabilities
+				and (node_definition.tool_capabilities.damage_groups or {})
+				or {}
+		)
+
+		type = Type.get_from_groups(damage_groups) or type
+	end
+
+	return type
 end
 
 --- @return string[]
--- mods/lord/Core/damage/src/damage/api.lua
+++ mods/lord/Core/damage/src/damage/api.lua
@@ -2,27 +2,29 @@ local Event     = require('damage.Event')
 local Type      = require('damage.Type')
 local TypeEvent = require('damage.Type.Event')
 
+
 --- @param player    Player
 --- @param hp_change number
 --- @param reason    PlayerHPChangeReason
 minetest.register_on_player_hpchange(function(player, hp_change, reason)
 	if hp_change < 0 then
 		Event:trigger(Event.Type.on_damage, player, -hp_change, reason)
+		TypeEvent:trigger(Type.detect(reason), player, -hp_change, reason)
 	end
 end)
 
 
 return {
-	Type = Type,
+	Type          = Type,
 
 	register_type = Type.register,
 
 	--- @type fun(callback:damage.callbacks.OnDamage)
-	on_damage = Event:on(Event.Type.on_damage),
+	on_damage     = Event:on(Event.Type.on_damage),
 
 	--- @param type     string|damage.Type name of damage type (for ex.: `"fleshy"`, `"fire"`, ...)
 	--- @param callback damage.callbacks.OnDamageOf
-	on_damage_of = function(type, callback)
+	on_damage_of  = function(type, callback)
 		TypeEvent:subscribe(type, callback)
 	end
 }
