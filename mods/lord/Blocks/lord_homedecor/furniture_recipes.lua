e592cd084ae2765590710f47618d622a54ab901d
alek13 <alek13.me@gmail.com>
2024-09-17 14:06:27 +0300

    Fix steel torch craft.

--- mods/lord/Blocks/lord_homedecor/furniture_recipes.lua
+++ mods/lord/Blocks/lord_homedecor/furniture_recipes.lua
@@ -23,9 +23,17 @@ minetest.register_craft({
 })
 
 minetest.register_craft({
-	output = "lord_homedecor:torch_wall 10",
+	output = "lord_homedecor:torch_wall 4",
 	recipe = {
 		{ "default:coal_lump" },
+		{ "default:steel_ingot" },
+	},
+})
+
+minetest.register_craft({
+	output = "lord_homedecor:torch_wall 2",
+	recipe = {
+		{ "default:charcoal_lump" },
 		{ "default:steel_ingot" },
 	},
 })
