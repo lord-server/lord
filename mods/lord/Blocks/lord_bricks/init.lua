2c1617e6522fbb7e68612ba537d2e4ae82141457
alek13 <alek13.me@gmail.com>
2024-10-04 03:36:59 +0300

    Stony Blocks: restructure code in `lord_bricks`. Relates to #1304

--- mods/lord/Blocks/lord_bricks/init.lua
+++ mods/lord/Blocks/lord_bricks/init.lua
@@ -1,15 +1,5 @@
-local S = minetest.get_translator(minetest.get_current_modname())
-
-dofile(minetest.get_modpath(minetest.get_current_modname()).."/crafts.lua")
-dofile(minetest.get_modpath(minetest.get_current_modname()).."/chamotte_bricks.lua")
-dofile(minetest.get_modpath(minetest.get_current_modname()).."/mordor_bricks.lua")
-dofile(minetest.get_modpath(minetest.get_current_modname()).."/rock_bricks.lua")
 
 
--- Raw Brick for default clay
-
-minetest.register_craftitem("lord_bricks:clay_brick_raw", {
-	description = S("Raw Clay Brick"),
-	inventory_image = "lord_bricks_clay_brick_raw.png"
-})
-
+minetest.mod(function(mod)
+	require('bricks').init(mod)
+end)
---
+++ mods/lord/Blocks/lord_bricks/src/bricks.lua
@@ -0,0 +1,20 @@
+local S = minetest.get_translator(minetest.get_current_modname())
+
+require('bricks.rock')
+
+-- Raw Brick for default clay
+minetest.register_craftitem("lord_bricks:clay_brick_raw", {
+	description = S("Raw Clay Brick"),
+	inventory_image = "lord_bricks_clay_brick_raw.png"
+})
+
+
+return {
+	--- @param mod minetest.Mod
+	init = function(mod)
+		require('bricks.crafts')
+		require('bricks.chamotte')
+		require('bricks.mordor')
+
+	end
+}
