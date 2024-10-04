5049d5cf0de2bf30fbe81596e0cda1470a41099e
alek13 <alek13.me@gmail.com>
2024-10-04 21:20:24 +0300

    Stones: add stairs & slabs for bricks & blocks from rocks. Relates to #1304

--- mods/lord/Blocks/lord_bricks/src/bricks/rock.lua
+++ mods/lord/Blocks/lord_bricks/src/bricks/rock.lua
@@ -1,5 +1,10 @@
 local S = minetest.get_translator(minetest.get_current_modname())
 
+
+-- TODO: deduplicate `mordor_stone_{brick|block}` & stairs|slab
+-- now them duplicates in `lottblocks`
+
+
 for node_name, registration in pairs(rocks.get_lord_nodes()) do
 	local rock = minetest.registered_nodes[node_name]
 	local name = node_name:split(':')[2]
@@ -8,20 +13,22 @@ for node_name, registration in pairs(rocks.get_lord_nodes()) do
 	minetest.log('info', 'use texture: ' .. texture .. ' at ' .. __FILE_LINE__())
 
 	local brick_name = 'lord_bricks:' .. name .. '_brick'
+	local brick_tile = texture .. '^(lord_bricks_overlay_brick.png^[opacity:70)'
 	minetest.register_node(brick_name, {
 		description       = S(base_description .. ' Brick'),
 		paramtype2        = 'facedir',
 		place_param2      = 0,
-		tiles             = { texture .. '^(lord_bricks_overlay_brick.png^[opacity:70)' },
+		tiles             = { brick_tile },
 		is_ground_content = false,
 		groups            = { cracky = rock.groups.cracky, stone = 1, brick = 1 },
 		sounds            = default.node_sound_stone_defaults(),
 	})
 
-	local block_name       = 'lord_bricks:' .. name .. '_block'
+	local block_name = 'lord_bricks:' .. name .. '_block'
+	local block_tile = texture .. '^(lord_bricks_overlay_block.png^[opacity:70)'
 	minetest.register_node(block_name, {
 		description       = S(base_description .. ' Block'),
-		tiles             = { texture .. '^(lord_bricks_overlay_block.png^[opacity:70)' },
+		tiles             = { block_tile },
 		is_ground_content = false,
 		groups            = { cracky = rock.groups.cracky, stone = 1, brick = 1 },
 		sounds            = default.node_sound_stone_defaults(),
@@ -45,4 +52,30 @@ for node_name, registration in pairs(rocks.get_lord_nodes()) do
 			{ node_name, node_name, node_name, },
 		},
 	})
+
+	stairs.register_stair_and_slab(
+		brick_name:split(':')[2],
+		brick_name,
+		{ cracky = rock.groups.cracky, stone = 1 },
+		{ brick_tile },
+		S(base_description .. ' Brick Stair'),
+		S(base_description .. ' Brick Slab'),
+		default.node_sound_stone_defaults(),
+		false,
+		S('Inner' .. base_description .. ' Brick Stair'),
+		S('Outer' .. base_description .. ' Brick Stair')
+	)
+
+	stairs.register_stair_and_slab(
+		block_name:split(':')[2],
+		block_name,
+		{ cracky = rock.groups.cracky, stone = 1 },
+		{ block_tile },
+		S(base_description .. ' Block Stair'),
+		S(base_description .. ' Block Slab'),
+		default.node_sound_stone_defaults(),
+		false,
+		S('Inner' .. base_description .. ' Block Stair'),
+		S('Outer' .. base_description .. ' Block Stair')
+	)
 end
