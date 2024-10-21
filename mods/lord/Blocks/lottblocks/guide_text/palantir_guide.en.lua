2f2612de97dda34cf3aab429e327bd486e06c136
alek13 <alek13.me@gmail.com>
2024-10-21 03:41:51 +0300

    Forms: Palantiri Guide: channge form. Closes #1727

---
+++ mods/lord/Blocks/lottblocks/guide_text/palantir_guide.en.lua
@@ -0,0 +1,61 @@
+
+
+return {
+	about   =
+		'It reveals the ancient secrets of the creation of Palantiri - stones of insight, ' ..
+		'which allow one to see and travel through time and space.',
+	content =
+		'    The world is vast, and even the keen eye of an Elven being cannot comprehend all its vastness. ' ..
+		'But there is an art that opens the way to see beyond the visible and know the hidden. ' ..
+		'This art is based on the deep secrets passed down to us by the Valar and the higher powers of Arda, ' ..
+		'but at the same time requires an inquisitive spirit and a thirst for knowledge to create an Eye, ' ..
+		'through which one can know both the past and the future. ' ..
+		'Let these pages serve as a guide to understanding how to forge light and darkness in stone, ' ..
+		'which will open to you worlds beyond the tangible.\\\n' ..
+		'\\\n' ..
+		'    The palantiri are the greatest of Middle-earth\'s devices, ' ..
+		'allowing one to travel vast distances ' ..
+		'in the blink of an eye. Travel occurs between ' ..
+		'altars with installed Palantiri.\\\n' ..
+		'\\\n' ..
+		'    To create an altar with a Palantir, you need to prepare the following materials:\\\n' ..
+		'        - Palantir\\\n' ..
+		'        - At least 8 Tilkal blocks\\\n' ..
+		'        - At least 16 Mithril blocks\\\n' ..
+		'\\\n' ..
+		'    Before creating the altar, decide on a location. ' ..
+		'Once installed, the Tilkal blocks cannot be ' ..
+		'destroyed or moved.\\\n' ..
+		'\\\n' ..
+		'    Place at least 8 Tilkal blocks and at least ' ..
+		'16 Mithril blocks so that they are no further than ' ..
+		'two meters from the Palantir\'s location ' ..
+		'(a 5x5x5 meter zone with the Palantir in the center). ' ..
+		'In addition to these materials, you can use other materials. ' ..
+		'The Palantir must be freely accessible from different sides for movement to it. ' ..
+		'If there is not enough space, moving to this Palantir will be impossible.\\\n' ..
+		'\\\n' ..
+		'    After creating the altar and installing the Palantir, you need to ' ..
+		'contact the Palantir (RMB) and determine the ' ..
+		'name of the clan to which this ' ..
+		'Palantir will belong. You need to specify the name of this Palantir in this ' ..
+		'clan. You cannot attach your Palantir to ' ..
+		'another clan or name two Palantiras in one clan the same.\\\n' ..
+		'\\\n' ..
+		'    Next, you need to select the races that will be allowed to ' ..
+		'move in this Palantir clan. Those who ' ..
+		'try to break the ban will be very unlucky. ' ..
+		'If you prohibit movement for all races, only the owner of this clan will be able to move between ' ..
+		'Palantirs.\\\n' ..
+		'\\\n' ..
+		'    After installing the second and subsequent Palantirs, ' ..
+		'you need to visit all the remaining Palantirs in the clan and ' ..
+		'tap them to update them (LMB).\\\n' ..
+		'    To reconfigure the Palantir, you need to remove some of the ' ..
+		'mithril blocks until their number becomes ' ..
+		'less than 16 pcs.\\\n' ..
+		'    Next, you need to try to move to another ' ..
+		'Palantir or select the first line in the movement list. ' ..
+		'The Palantir will disconnect from the clan and the altar - you can ' ..
+		'restore the altar and install the Palantir again.'
+}
--- mods/lord/Blocks/lottblocks/guides.lua
+++ mods/lord/Blocks/lottblocks/guides.lua
@@ -1,94 +1,60 @@
-local S = minetest.get_translator("lottblocks")
+local S         = minetest.get_translator('lottblocks')
+local spec      = minetest.formspec
+local lord_spec = forms.Spec
+local mod_path  = minetest.get_modpath(minetest.get_current_modname())
 
-local lpp = 13
-local function guide_on_use(itemstack, user)
-	local player_name = user:get_player_name()
-	local data = minetest.registered_items[itemstack:get_name()].text
-	local page_info = minetest.deserialize(itemstack:get_metadata())
-	local page, lines, string = 1, {}, ""
 
-	local title = data.title
-	local text = data.text
-	local owner = data.owner
-	local page_max = data.page_max
-	local background = data.background or "gui_elfbg.png"
+local supported_lang_codes = { 'en', 'ru' }
+local DEFAULT_LANG = 'en'
 
-	for str in (text .. "\n"):gmatch("([^\n]*)[\n]") do
-		lines[#lines+1] = str
-	end
+--- @type {about:string,content:string}[]
+local book_text_in_lang = {}
+--- @param lang_code string
+--- @return {about:string,content:string}
+local function get_book_in_lang(lang_code)
+	if book_text_in_lang[lang_code] then return book_text_in_lang[lang_code] end
 
-	if page_info then
-		if page_info.page then
-			page = page_info.page
-		end
-	end
+	lang_code = table.contains(supported_lang_codes, lang_code) and lang_code or DEFAULT_LANG
+	book_text_in_lang[lang_code] = dofile(mod_path .. '/guide_text/palantir_guide.'.. lang_code ..'.lua')
 
-	for i = ((lpp * page) - lpp) + 1, lpp * page do
-		if not lines[i] then break end
-		string = string .. lines[i] .. "\n"
-	end
-
-	local formspec = "size[8,8]" ..
-		"background[8,8;1,1;" .. background .. ";true]"..
-		"label[0.5,0.5;" .. minetest.colorize("black"," от автора   ") .. minetest.colorize("purple",owner) .. "]" ..
-		"label[0.4,0;" .. minetest.colorize("purple", minetest.formspec_escape(title)) .. "]" ..
-		"textarea[0.5,1.5;7.5,7;;" ..
-		minetest.colorize("black", minetest.formspec_escape(string ~= "" and string or text)) .. ";]" ..
-		"button[2.4,7.6;0.8,0.8;book_prev;<]" ..
-		"label[3.1,7.7;" .. minetest.colorize("black","Страница "..page .. " из " .. page_max) .. "]" ..
-		"button[5.2,7.6;0.8,0.8;book_next;>]"
-
-	minetest.show_formspec(player_name, "lottother:guide", formspec)
+	return book_text_in_lang[lang_code]
 end
 
-minetest.register_on_player_receive_fields(function(player, formname, fields)
-	if formname ~= "lottother:guide" then return end
-	local stack = player:get_wielded_item()
+--- @param itemstack ItemStack
+--- @param user      Player|ObjectRef
+local function guide_on_use(itemstack, user)
+	local player_name = user:get_player_name()
 
-	if fields.book_next or fields.book_prev then
-		local data = minetest.registered_items[stack:get_name()].text
-		local page_info = minetest.deserialize(stack:get_metadata())
-		if not page_info then
-			page_info = {page = 1}
-		end
+	local lang_code = minetest.get_player_information(player_name).lang_code
+	local book = get_book_in_lang(lang_code)
 
-		if fields.book_next then
-			page_info.page = page_info.page + 1
-			if page_info.page > data.page_max then
-				page_info.page = 1
-			end
-		else
-			page_info.page = page_info.page - 1
-			if page_info.page == 0 then
-				page_info.page = data.page_max
-			end
-		end
+	local formspec = ''
+		.. spec.size(9, 9)
+		.. spec.image(-.2, -.2, 11.5, 10.95, '(lottblocks_palantir_guide_bg.png^[opacity:70)')
+		.. lord_spec.header2(0.9, 0, S('The Eye of the Seer: On Secrets of Palantiri Creation'))
+		.. lord_spec.italic(1.2, 0.4, S('The Book of Feanor, greatest of the Eldar artisans.'))
+		.. spec.box(0.625, 0.85, 7.45, 1.155, '#000')
+		.. lord_spec.italic_area_ro(1, 0.9, 8, 1.3, book.about)
+		.. spec.box(0.125, 2.2, 8.5, 6.4, '#000')
+		.. spec.textarea(0.5, 2.3, 8.55, 7.3, spec.read_only, '', book.content)
+		.. lord_spec.bold(4, 8.7, S('Feanor, son of Finwë, king of the Noldor'))
 
-		local data_str = minetest.serialize(page_info)
-		stack:set_metadata(data_str)
-		guide_on_use(stack, player)
-	end
+	minetest.show_formspec(player_name, 'lottother:guide', formspec)
+end
 
-	player:set_wielded_item(stack)
-end)
-
-local palantir_guide = dofile(minetest.get_modpath("lottblocks")
-	.. "/guide_text/palantir_guide.lua")
-
-minetest.register_craftitem("lottblocks:palantir_guide", {
-	description = S("Palantir Guidebook"),
-	inventory_image = "lottblocks_palantir_guide.png",
+minetest.register_craftitem('lottblocks:palantir_guide', {
+	description = S('Palantir Guidebook'),
+	inventory_image = 'lottblocks_palantir_guide.png',
 	groups = {book = 1, forbidden = 1},
 	stack_max = 1,
-	text = minetest.deserialize(palantir_guide),
-	on_use = function(itemstack, user)
+	on_use = function(itemstack, user, pointed_thing)
 		guide_on_use(itemstack, user)
-	end,
+	end
 })
 
 minetest.register_craft({
-	type = "shapeless",
-	output = "lottblocks:palantir_guide",
-	recipe = {"default:book", "lottblocks:palantir"},
-	replacements = {{"lottblocks:palantir", "lottblocks:palantir"}}
+	type = 'shapeless',
+	output = 'lottblocks:palantir_guide',
+	recipe = {'default:book', 'lottblocks:palantir'},
+	replacements = {{'lottblocks:palantir', 'lottblocks:palantir'}}
 })
