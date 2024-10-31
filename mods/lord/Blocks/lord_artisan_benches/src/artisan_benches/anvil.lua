1e5dbde94dc8528c80ea0610f3b86b4e7ee682cc
alek13 <alek13.me@gmail.com>
2024-10-31 05:39:30 +0300

    Artisan Benches: Workbench: remake model w texture. Closes #1740

--- mods/lord/Blocks/lord_artisan_benches/src/artisan_benches/anvil.lua
+++ mods/lord/Blocks/lord_artisan_benches/src/artisan_benches/anvil.lua
@@ -8,39 +8,46 @@ minetest.register_craft_method(minetest.CraftMethod.ANVIL)
 
 Form:register()
 
-minetest.register_node('lord_artisan_benches:anvil', {
-	drawtype    = 'nodebox',
-	description = S('Anvil'),
-	tiles       = { 'benches_anvil.png' },
-	groups      = { cracky = 2, falling_node = 1 },
-	paramtype   = 'light',
-	paramtype2  = 'facedir',
-	node_box    = {
-		type  = 'fixed',
-		fixed = {
-			{ -0.5, -0.5, -0.3, 0.5, -0.3, 0.3 },
-			{ -0.4, -0.5, -0.4, 0.4, -0.3, 0.4 },
-			{ -0.3, -0.5, -0.5, 0.3, -0.3, 0.5 },
-			{ -0.4, -0.3, -0.4, -0.1, -0.2, -0.1 },
-			{ -0.4, -0.3, 0.4, -0.1, -0.2, 0.1 },
-			{ 0.4, -0.3, 0.4, 0.1, -0.2, 0.1 },
-			{ 0.4, -0.3, -0.4, 0.1, -0.2, -0.1 },
-			{ -0.3, -0.2, -0.3, 0.3, -0.1, 0.3 },
-			{ -0.2, -0.1, -0.2, 0.2, 0.1, 0.2 },
-			{ -0.3, 0.1, -0.25, 0.3, 0.5, 0.25 },
-			{ -0.4, 0.1, -0.15, -0.3, 0.5, 0.15 },
-			{ -0.4, 0.15, -0.2, -0.3, 0.45, 0.2 },
-			{ -0.5, 0.15, -0.1, -0.4, 0.45, 0.1 },
-			{ -0.5, 0.2, -0.15, -0.4, 0.4, 0.15 },
-			{ -0.6, 0.25, -0.05, -0.5, 0.45, 0.05 },
-			{ -0.6, 0.3, -0.1, -0.5, 0.4, 0.1 },
-			{ -0.7, 0.35, -0.05, -0.6, 0.45, 0.05 },
-			{ 0.3, 0.1, -0.2, 0.4, 0.5, 0.2 },
-			{ 0.4, 0.2, -0.15, 0.5, 0.5, 0.15 },
-			{ 0.5, 0.3, -0.1, 0.6, 0.5, 0.1 },
-			{ 0.6, 0.4, -0.05, 0.7, 0.5, 0.05 },
-		},
+local px = 1/16
+local node_box = {
+	type  = 'fixed',
+	fixed = {
+		-- Подошва
+		{ -8*px, -8*px, -8*px,  -4*px, -5*px, -4*px }, -- ножки подошвы
+		{ -8*px, -8*px,  8*px,  -4*px, -5*px,  4*px },
+		{  8*px, -8*px,  8*px,   4*px, -5*px,  4*px },
+		{  8*px, -8*px, -8*px,   4*px, -5*px, -4*px },
+
+		{ -7*px, -8*px, -6*px,   7*px, -5*px,  6*px }, -- сердцевины подошвы
+		{ -5*px, -5*px, -4*px,   5*px, -2*px,  4*px },
+
+		-- Ножка / шея
+		{ -3*px, -2*px, -2*px,   3*px,  3*px,  2*px },
+		-- Рабочая область (наличник)
+		{ -5*px,  3*px, -4*px,   5*px,  8*px,  4*px },
+
+		-- Рог
+		{ -5*px,  5*px, -2*px,  -8*px,  8*px,  2*px },
+
+		-- Обух с отверстием Харди
+		{ 7*px, 5*px, -4*px,   8*px, 8*px,  4*px  },
+		{ 5*px, 5*px, -2*px,   7*px, 6*px,  2*px  },
+		{ 5*px, 5*px, -4*px,   7*px, 8*px, -2*px  },
+		{ 5*px, 5*px,  2*px,   7*px, 8*px,  4*px  },
 	},
+}
+
+
+minetest.register_node('lord_artisan_benches:anvil', {
+	description   = S('Anvil'),
+	drawtype      = 'mesh',
+	mesh          = 'anvil.obj',
+	tiles         = { 'benches_anvil.png' },
+	groups        = { cracky = 1, falling_node = 1 },
+	selection_box = node_box,
+	collision_box = node_box,
+	paramtype     = 'light',
+	paramtype2    = 'facedir',
 	on_rightclick = function(pos, node, clicker, itemstack, pointed_thing)
 		Form:new(clicker, pos):open()
 	end
