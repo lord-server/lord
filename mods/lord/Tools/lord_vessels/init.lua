fdde3c11aba47891a96d9503dbb9f6f476f6b3a9
alek13 <alek13.me@gmail.com>
2024-11-02 01:33:26 +0300

    Exterior: `steel_bottle`: use model, change texturese; rename to Steel Can. Closes #1755

--- mods/lord/Tools/lord_vessels/init.lua
+++ mods/lord/Tools/lord_vessels/init.lua
@@ -4,6 +4,7 @@ minetest.mod(function(mod)
 	require('glasses')
 	require('bottles')
 	require('pints')
+	require('can') -- steel bottle
 
 	dofile(mod.path .. '/legacy.lua')
 end)
---
+++ mods/lord/Tools/lord_vessels/src/can.lua
@@ -0,0 +1,20 @@
+local S = minetest.get_mod_translator()
+
+
+minetest.override_item('vessels:steel_bottle', {
+	description       = S('Steel Can'),
+	drawtype          = 'mesh',
+	mesh              = 'lord_vessels_steel_can.obj',
+	tiles             = { 'lord_vessels_steel_can.png' },
+	inventory_image   = '',
+	wield_image       = 'lord_vessels_steel_can_wield.png',
+	selection_box     = {
+		type  = 'fixed',
+		fixed = { -0.25, -0.5, -0.25, 0.25, 7/16, 0.25 }
+	},
+	paramtype         = 'light',
+	paramtype2        = 'facedir',
+	walkable          = false,
+	groups            = { vessel = 1, cracky = 1, attached_node = 1 },
+	is_ground_content = false,
+})
