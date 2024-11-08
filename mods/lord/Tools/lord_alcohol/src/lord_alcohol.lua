674062546957552edad4b34400dd9211b73f7a27
alek13 <alek13.me@gmail.com>
2024-11-08 03:48:48 +0300

    Refactoring: `lottpotion`: move alcohol recipes into config. Relates to #1751

--- mods/lord/Tools/lord_alcohol/src/lord_alcohol.lua
+++ mods/lord/Tools/lord_alcohol/src/lord_alcohol.lua
@@ -1,5 +1,5 @@
-local api    = require("lord_alcohol.api")
-local config = require("lord_alcohol.config")
+local api    = require('lord_alcohol.api')
+local config = require('lord_alcohol.config')
 
 
 lord_alcohol = {} -- luacheck: ignore unused global variable lord_alcohol
@@ -9,34 +9,20 @@ local function register_api()
 end
 
 local function register_lord_alcohol()
-	for _, alcohol in pairs(config) do
+	for _, alcohol in pairs(config.potions) do
 		api.register(alcohol.item_name, alcohol.satiety)
 	end
 end
 
 local function register_recipes()
-	-- just moved AS IS from `lottpotion`. TODO: move to config & API
-	local recipes = {
-		--Base Potion
-		{ "lottfarming:berries 5", "lord_vessels:drinking_glass_water", "lord_alcohol:wine" },
-		{ "farming:wheat0 3", "lord_vessels:drinking_glass_water", "lord_alcohol:beer" },
-		{ "bees:bottle_honey 6", "lord_vessels:drinking_glass_water", "lord_alcohol:mead", "vessels:glass_bottle 6" },
-		{ "default:apple 5", "lord_vessels:drinking_glass_water", "lord_alcohol:cider" },
-		{ "lottfarming:barley_seed 6", "lord_vessels:drinking_glass_water", "lord_alcohol:ale" },
-	}
-
-	for _, data in pairs(recipes) do
-		lottpotion_recipe.register("brew", {
-			input = { data[1], data[2] },
-			output = data[3],
-			time = data[5],
-		})
+	for _, recipe in pairs(config.recipes) do
+		lottpotion_recipe.register('brew', recipe)
 	end
 end
 
 
 return {
---- @param mod minetest.Mod
+	--- @param mod minetest.Mod
 	init = function(mod)
 		register_api()
 		register_lord_alcohol()
--- mods/lord/Tools/lord_alcohol/src/lord_alcohol/config.lua
+++ mods/lord/Tools/lord_alcohol/src/lord_alcohol/config.lua
@@ -1,15 +1,48 @@
 
---- @class lord_alcohol.config
+--- @class lord_alcohol.config.potion
 --- @field item_name string
 --- @field satiety   number
 
---- @type lord_alcohol.config[]
+--- @class lord_alcohol.config.recipe
+--- @field item_name string
+--- @field satiety   number
+
+--- @class lord_alcohol.config
+--- @field potions lord_alcohol.config.potion[]
+--- @field recipes number
+
+
+--- @type lord_alcohol.config
 local config = {
-	{ item_name = 'lord_alcohol:wine',  satiety =  9 },
-	{ item_name = 'lord_alcohol:beer',  satiety =  8 },
-	{ item_name = 'lord_alcohol:ale',   satiety =  9 },
-	{ item_name = 'lord_alcohol:mead',  satiety = 15 },
-	{ item_name = 'lord_alcohol:cider', satiety =  9 },
+	potions = {
+		{ item_name = 'lord_alcohol:wine',  satiety =  9 },
+		{ item_name = 'lord_alcohol:beer',  satiety =  8 },
+		{ item_name = 'lord_alcohol:ale',   satiety =  9 },
+		{ item_name = 'lord_alcohol:mead',  satiety = 15 },
+		{ item_name = 'lord_alcohol:cider', satiety =  9 },
+	},
+	recipes = {
+		{
+			input = { 'lottfarming:berries 5', 'lord_vessels:drinking_glass_water' },
+			output = 'lord_alcohol:wine'
+		},
+		{
+			input = { 'farming:wheat0 3', 'lord_vessels:drinking_glass_water' },
+			output = 'lord_alcohol:beer'
+		},
+		{
+			input = { 'bees:bottle_honey 6', 'lord_vessels:drinking_glass_water' },
+			output = 'lord_alcohol:mead'--, 'vessels:glass_bottle 6'
+		},
+		{
+			input = { 'default:apple 5', 'lord_vessels:drinking_glass_water' },
+			output = 'lord_alcohol:cider'
+		},
+		{
+			input = { 'lottfarming:barley_seed 6', 'lord_vessels:drinking_glass_water' },
+			output = 'lord_alcohol:ale'
+		},
+	},
 }
 
 
