70fe578d84e4a0545c2de574755a5a6d287dbc62
alek13 <alek13.me@gmail.com>
2024-11-15 01:58:47 +0300

    Artisan Benches: Grinder: rework `grinding_possible()`, fix using new craft system. Relates to #1791

--- mods/lord/Blocks/grinder/src/grinder/Processor.lua
+++ mods/lord/Blocks/grinder/src/grinder/Processor.lua
@@ -2,16 +2,20 @@
 local Grinder   = require("grinder.Grinder")
 
 
+--- - Returns whether grinding possible &
+--- - if possible, also returns (RecipeOutput - result, RecipeInput - remaining) for source & fuel.
+--- - So, returns `possible, result_source, remaining_source, result_fuel, remaining_fuel`
 --- @param inv InvRef
+--- @return boolean, RecipeOutput|nil, RecipeInput|nil, RecipeOutput|nil, RecipeInput|nil
 local function grinding_possible(inv)
-	local result, remaining = minetest.get_craft_result({
+	local result_source, remaining_source = minetest.get_craft_result({
 		method = 'grinder',
 		type   = 'cooking',
 		width  = 1,
 		items  = inv:get_list('src'),
 	})
-	if not result then
-		return false
+	if not result_source or result_source.time == 0 then
+		return false, nil, nil, nil, nil
 	end
 
 	local result_fuel, remaining_fuel = minetest.get_craft_result({
@@ -20,22 +24,17 @@ local function grinding_possible(inv)
 		items = inv:get_list("fuel")
 	})
 
-	local time              = result and result.time
-	local remaining_source  = result and remaining.items[1]
-	local result_source     = result and ItemStack(result.item)
+	local possible = result_source.time > 0 and result_fuel.time > 0 and inv:room_for_item("dst", result_source.item)
+	if not possible then
+		return false, nil, nil, nil, nil
+	end
 
-	return
-		result ~= nil and result_fuel.time > 0 and inv:room_for_item("dst", result_source),
-		time,
-		remaining_fuel.items[1],
-		remaining_source,
-		result_fuel,
-		result_source
+	return possible, result_source, remaining_source, result_fuel, remaining_fuel
 end
 
---- @param meta NodeMetaRef
---- @param remaining_fuel ItemStack
---- @param result_fuel table
+--- @param meta           NodeMetaRef
+--- @param remaining_fuel RecipeInput
+--- @param result_fuel    RecipeOutput
 local function burn_fuel(meta, remaining_fuel, result_fuel)
 	local fuel_time = meta:get_int("fuel_time")
 	local fuel_totaltime = meta:get_int("fuel_totaltime")
@@ -44,7 +43,7 @@ local function burn_fuel(meta, remaining_fuel, result_fuel)
 		meta:set_int("fuel_totaltime", result_fuel.time)
 	end
 	if fuel_time == 0 then
-		meta:get_inventory():set_stack("fuel", 1, remaining_fuel)
+		meta:get_inventory():set_list("fuel", remaining_fuel.items)
 	end
 
 	fuel_time = fuel_time + 1
@@ -56,24 +55,24 @@ local function burn_fuel(meta, remaining_fuel, result_fuel)
 	meta:set_int("fuel_time", fuel_time)
 end
 
---- @param meta NodeMetaRef
---- @param remaining_source ItemStack
---- @param result_source ItemStack
+--- @param meta             NodeMetaRef
+--- @param remaining_source RecipeInput
+--- @param result_source    RecipeOutput
 --- @param time number
-local function grind_source(meta, remaining_source, result_source, time)
+local function grind_source(meta, remaining_source, result_source)
 	local src_time = meta:get_int("src_time")
 	local src_totaltime = meta:get_int("src_totaltime")
 
 	if src_time == 0 or src_totaltime == 0 then
-		meta:set_int("src_totaltime", time)
+		meta:set_int("src_totaltime", result_source.time)
 	end
 
 	src_time = src_time + 1
 
 	if src_time >= src_totaltime then
 		local inv = meta:get_inventory()
-		inv:set_stack("src", 1, remaining_source)
-		inv:add_item("dst", result_source)
+		inv:set_list("src", remaining_source.items)
+		inv:add_item("dst", result_source.item)
 		src_time = 0
 	end
 
@@ -97,12 +96,12 @@ function Processor.act(pos)
 	local meta = g:get_meta()
 	local inv  = meta:get_inventory()
 
-	local possible, time, remaining_fuel, remaining_source, result_fuel, result_source = grinding_possible(inv)
+	local possible, result_source, remaining_source, result_fuel, remaining_fuel = grinding_possible(inv)
 	if possible then
 		g:activate("%s Grinding")
 
 		burn_fuel(meta, remaining_fuel, result_fuel)
-		grind_source(meta, remaining_source, result_source, time)
+		grind_source(meta, remaining_source, result_source)
 	else
 		g:deactivate("%s Out Of Heat")
 	end
