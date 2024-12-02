6608cbf492c79c5152c724af991f2b04f26e10ac
alek13 <alek13.me@gmail.com>
2024-12-02 03:17:07 +0300

    Effects/Potions: REMOVE `lottpotion`! Closes #1736. Closes #943

--- mods/lord/Tools/lottpotion/init.lua
+++
@@ -1,9 +0,0 @@
-
-lottpotion = {}
-
-dofile(minetest.get_modpath("lottpotion").."/potions/aka_api.lua")
-
-dofile(minetest.get_modpath("lottpotion").."/potions/arrows_aliases.lua")
-dofile(minetest.get_modpath("lottpotion").."/potions/ingredients.lua")
-dofile(minetest.get_modpath("lottpotion").."/potions/potions.lua")
-dofile(minetest.get_modpath("lottpotion").."/potions/recipes.lua")
