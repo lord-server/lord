94cb2208365de2e58d7a78a241c83e65d9d8a7b1
alek13 <alek13.me@gmail.com>
2024-12-05 14:37:46 +0300

    Artisan Benches modpack: fix `cauldron` aliases. Relates to #1798

--- mods/lord/Blocks/ArtisanBenches/cauldron/legacy.lua
+++ mods/lord/Blocks/ArtisanBenches/cauldron/legacy.lua
@@ -1,23 +1,13 @@
 
 -- @tags: legacy
 
-minetest.register_alias('lottpotion:cauldron_full',           'lord_artisan_benches:cauldron_3_3')
-minetest.register_alias('lottpotion:cauldron_two_third_full', 'lord_artisan_benches:cauldron_2_3')
-minetest.register_alias('lottpotion:cauldron_one_third_full', 'lord_artisan_benches:cauldron_1_3')
-minetest.register_alias('lottpotion:cauldron_empty',          'lord_artisan_benches:cauldron_0_3')
-
-minetest.register_alias('lord_artisan_benches:cauldron_3_3',  'cauldron:cauldron_3_3')
-minetest.register_alias('lord_artisan_benches:cauldron_2_3',  'cauldron:cauldron_2_3')
-minetest.register_alias('lord_artisan_benches:cauldron_1_3',  'cauldron:cauldron_1_3')
-minetest.register_alias('lord_artisan_benches:cauldron_0_3',  'cauldron:cauldron_0_3')
+minetest.register_alias('lottpotion:cauldron_full',           'cauldron:cauldron_3_3')
+minetest.register_alias('lottpotion:cauldron_two_third_full', 'cauldron:cauldron_2_3')
+minetest.register_alias('lottpotion:cauldron_one_third_full', 'cauldron:cauldron_1_3')
+minetest.register_alias('lottpotion:cauldron_empty',          'cauldron:cauldron_0_3')
 
 -- DEFAULTS (Ghost blocks)
-minetest.register_alias('defaults:lottpotion_cauldron_full',           'defaults:lord_artisan_benches_cauldron_3_3')
-minetest.register_alias('defaults:lottpotion_cauldron_two_third_full', 'defaults:lord_artisan_benches_cauldron_2_3')
-minetest.register_alias('defaults:lottpotion_cauldron_one_third_full', 'defaults:lord_artisan_benches_cauldron_1_3')
-minetest.register_alias('defaults:lottpotion_cauldron_empty',          'defaults:lord_artisan_benches_cauldron_0_3')
-
-minetest.register_alias('defaults:lord_artisan_benches_cauldron_3_3',  'defaults:cauldron_cauldron_3_3')
-minetest.register_alias('defaults:lord_artisan_benches_cauldron_2_3',  'defaults:cauldron_cauldron_2_3')
-minetest.register_alias('defaults:lord_artisan_benches_cauldron_1_3',  'defaults:cauldron_cauldron_1_3')
-minetest.register_alias('defaults:lord_artisan_benches_cauldron_0_3',  'defaults:cauldron_cauldron_0_3')
+minetest.register_alias('defaults:lottpotion_cauldron_full',           'defaults:cauldron_cauldron_3_3')
+minetest.register_alias('defaults:lottpotion_cauldron_two_third_full', 'defaults:cauldron_cauldron_2_3')
+minetest.register_alias('defaults:lottpotion_cauldron_one_third_full', 'defaults:cauldron_cauldron_1_3')
+minetest.register_alias('defaults:lottpotion_cauldron_empty',          'defaults:cauldron_cauldron_0_3')
