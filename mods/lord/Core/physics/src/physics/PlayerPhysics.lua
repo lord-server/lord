dd3acd7151dbbc7277ff9423998c927284f1a70a
alek13 <alek13.me@gmail.com>
2024-12-07 04:48:49 +0300

    `physics`: ability to add/sub/mul/div. Closes #1819

--- mods/lord/Core/physics/src/physics/PlayerPhysics.lua
+++ mods/lord/Core/physics/src/physics/PlayerPhysics.lua
@@ -1,5 +1,5 @@
-local table_merge, setmetatable
-	= table.merge, setmetatable
+local table_merge, table_add_values, table_sub_values, table_mul_values, table_div_values, setmetatable
+	= table.merge, table.add_values, table.sub_values, table.mul_values, table.div_values, setmetatable
 
 local Event = require('physics.Event')
 
@@ -37,17 +37,6 @@ function PlayerPhysics:refresh_player(player)
 	return self
 end
 
---- @param physics table
-function PlayerPhysics:set(physics)
-	self.physics = table_merge(self.physics, physics)
-
-	self.player:set_physics_override(table_merge(
-		self.player:get_physics_override(), self.physics
-	))
-
-	Event:trigger(Event.Type.on_change, self.player, self)
-end
-
 --- @overload fun()
 --- @param name string
 --- @return number|table<string,number>
@@ -55,6 +44,48 @@ function PlayerPhysics:get(name)
 	return name
 		and (self.physics[name] or nil)
 		or  self.physics
+end
+
+--- @private
+function PlayerPhysics:set_override()
+	self.player:set_physics_override(table_merge(
+		self.player:get_physics_override(), self.physics
+	))
+end
+
+--- @param physics physics_override_table
+function PlayerPhysics:set(physics)
+	self.physics = table_merge(self.physics, physics)
+	self:set_override()
+	Event:trigger(Event.Type.on_change, self.player, self)
+end
+
+--- @param physics physics_override_table
+function PlayerPhysics:add(physics)
+	self.physics = table_add_values(self.physics, physics)
+	self:set_override()
+	Event:trigger(Event.Type.on_change, self.player, self)
+end
+
+--- @param physics physics_override_table
+function PlayerPhysics:sub(physics)
+	self.physics = table_sub_values(self.physics, physics)
+	self:set_override()
+	Event:trigger(Event.Type.on_change, self.player, self)
+end
+
+--- @param physics physics_override_table
+function PlayerPhysics:mul(physics)
+	self.physics = table_mul_values(self.physics, physics)
+	self:set_override()
+	Event:trigger(Event.Type.on_change, self.player, self)
+end
+
+--- @param physics physics_override_table
+function PlayerPhysics:div(physics)
+	self.physics = table_div_values(self.physics, physics)
+	self:set_override()
+	Event:trigger(Event.Type.on_change, self.player, self)
 end
 
 
