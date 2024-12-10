276cb286ce81fb5b3ab3ba7394f229fec308fb15
alek13 <alek13.me@gmail.com>
2024-12-11 02:07:42 +0300

    Effects System: apply physics from armor with reason. Closes #1829

--- mods/lord/Game/lord_physics/src/lord_physics.lua
+++ mods/lord/Game/lord_physics/src/lord_physics.lua
@@ -6,25 +6,27 @@ local PHYSICS_TYPES = { "jump", "speed", "gravity" }
 
 --- @param player        Player
 local function collect_physics_from_armor(player)
-	local physics = { speed = 1, gravity = 1, jump = 1 }
+	local physics_delta = {}
 	for _, stack in equipment.for_player(player):not_empty(equipment.Kind.ARMOR) do
 		local item_groups = stack:get_definition().groups
 		for _, type in ipairs(PHYSICS_TYPES) do
 			local value = item_groups["physics_" .. type]
 			if value then
-				physics[type] = (physics[type] or 1) + value
+				physics_delta[type] = (physics_delta[type] or 0) + value
 			end
 		end
 	end
 
-	return physics
+	return physics_delta
 end
 
 --- @param player Player
 local function set_player_physics(player)
-	local physics_o = collect_physics_from_armor(player)
+	local physics_delta = collect_physics_from_armor(player)
 
-	physics.for_player(player):set(physics_o)
+	physics.for_player(player):set(physics_delta, {
+		name = 'armor', description = ''
+	})
 end
 
 
