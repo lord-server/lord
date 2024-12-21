ec04f4152c25e9cf416e0e94c214d84dff0cb16a
Doloment <doloment@internet.ru>
2024-12-22 03:11:23 +0700

    Lord Archery: move api for (cross-)bow registration to archery

--- mods/lord/_experimental/archery/src/archery.lua
+++ mods/lord/_experimental/archery/src/archery.lua
@@ -1,7 +1,15 @@
-local  processor = require("archery.processor")
+local public_api = require("archery.public_api")
+local processor = require("archery.processor")
+
+archery = {}
+
+local function register_api()
+	_G.archery = public_api
+end
 
 return {
 	init = function()
+		register_api()
 		return processor
 	end
 }
--- mods/lord/_experimental/lord_archery/src/lord_archery.lua
+++ mods/lord/_experimental/lord_archery/src/lord_archery.lua
@@ -1,24 +1,16 @@
-local api    = require("lord_archery.api")
 local config = require("lord_archery.config")
-
-lord_archery = {}
-
-local function register_api()
-	_G.lord_archery = api
-end
 
 local function register_bows()
 	for name, registration in pairs(config.bows) do
-		api.register_bow(name, registration)
+		archery.register_bow(name, registration)
 	end
 	for name, registration in pairs(config.crossbows) do
-		api.register_crossbow(name, registration)
+		archery.register_crossbow(name, registration)
 	end
 end
 
 return {
 	init = function()
-		register_api()
 		register_bows()
 	end,
 }
