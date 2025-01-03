8dfa8d0716a60488e108576d25aba43296356a7f
alek13 <alek13.me@gmail.com>
2025-01-03 07:24:26 +0300

    Mount Gen: Cone algo: initial refactoring. Relates to #1893

--- mods/lord/World/mountgen/algorithms/cone.lua
+++ mods/lord/World/mountgen/algorithms/cone.lua
@@ -1,30 +1,38 @@
----Generate mountain as cone
----@param map_w integer desired width of the mountain
----@param mountain_h integer mountain height
----@return table, integer, integer "height map, map size, center_coordinate"
-mountgen.cone = function(map_w, mountain_h)
+local math_max, math_sqrt, math_floor
+    = math.max, math.sqrt, math.floor
+
+
+local function fill_with_zeroes(diameter)
 	local map = {}
-
-	local W = math.max(map_w, 1)
-	local H = math.max(mountain_h, 1)
-
-	for i = 1, W do
+	for i = 1, diameter do
 		map[i] = {}
-		for j = 1, W do
-			map[i][j] = 0
+		for j = 1, diameter do  map[i][j] = 0  end
+	end
+
+	return map
+end
+
+---Generate mountain as cone
+---@param diameter number desired width of the mountain
+---@param height   number mountain height
+---@return table, number, number "height map, map size, center_coordinate"
+mountgen.cone = function(diameter, height)
+	diameter = math_max(diameter, 1)
+	height   = math_max(height, 1)
+
+	local height_map = fill_with_zeroes(diameter)
+
+	local radius = diameter / 2
+	local l0     = math_sqrt(radius^2 + radius^2)
+
+	for z = 0, diameter - 1 do
+		for x = 0, diameter - 1 do
+			local px = x - radius
+			local pz = z - radius
+			local l  = math_sqrt(px^2 + pz^2)
+			local h  = height * (1 - 2 * l / l0)
+			mountgen.set_value(height_map, z, x, h)
 		end
 	end
-
-	local l0 = math.sqrt(W/2*W/2 + W/2*W/2)
-
-	for z = 0,W-1 do
-	for x = 0,W-1 do
-		local px = x - W/2
-		local pz = z - W/2
-		local l = math.sqrt(px*px+pz*pz)
-		local h = H*(1-l/l0*2)
-		mountgen.set_value(map, z, x, h)
-	end
-	end
-	return map, W, math.floor(W/2)+1
+	return height_map, diameter, math_floor(radius) + 1
 end
