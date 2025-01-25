eead11ac480e348795a7c98a138c879865bc7199
alek13 <alek13.me@gmail.com>
2025-01-25 04:48:50 +0300

    NameTags: use `Core/player_namtag` in `Player/show_health`. Closes #1429. Fixes #1232

--- mods/lord/Player/show_health/init.lua
+++ mods/lord/Player/show_health/init.lua
@@ -3,49 +3,42 @@ local table_keys_has_one_of_values
 
 local ARENA_AREA_IDS = {}
 
-local arena_ids      = minetest.settings:get("arenas") or ""
-arena_ids            = string.split(arena_ids, ",")
+local arena_ids = minetest.settings:get('arenas') or ''
+arena_ids       = string.split(arena_ids, ',')
 for _, v in ipairs(arena_ids) do
 	table.insert(ARENA_AREA_IDS, tonumber(v))
 end
 
---- This table shows for whom of players hp is now displaying. And stores previous nametag.
+nametag.segments.add('health', '#0f0', ' ♥ %s')
+
+--- This table shows for whom of players hp is now displaying.
 --- @type table<string,boolean>
-local displayed_after_nametag = {}
+local health_displayed_for = {}
 
 --- @param player Player
 local function player_display_hp(player)
 	local name = player:get_player_name()
-	if displayed_after_nametag[name] then -- if health already displaying
+	local tag_segment = nametag.for_player(player):segment('health')
+	local hp = player:get_hp()
+
+	if health_displayed_for[name] and tag_segment:get_value() == hp then
 		return
 	end
 
-	local previous_nametag = player:get_nametag_attributes().text
-	if not previous_nametag or previous_nametag == "" then
-		previous_nametag = name
-	end
-	local hp = player:get_hp()
-	local color
-	if hp > 13 then
-		color = "#00FF00"
-	elseif hp > 6 then
-		color = "#FFFF00"
-	else
-		color = "#FF0000"
-	end
-	player:set_properties({nametag = previous_nametag .. minetest.colorize(color, " ♥ "..hp)})
-	displayed_after_nametag[name] = previous_nametag
+	local color = hp > 13 and '#00FF00'	or (hp > 6 and '#FFFF00' or '#FF0000')
+	tag_segment:set_value(hp, color):update()
+	health_displayed_for[name] = true
 end
 
 --- @param player Player
 local function player_undisplay_hp(player)
 	local name = player:get_player_name()
-	if not displayed_after_nametag[name] then
+	if not health_displayed_for[name] then
 		return
 	end
-	local previous_nametag = displayed_after_nametag[name]
-	player:set_properties({nametag = previous_nametag})
-	displayed_after_nametag[name] = nil
+
+	nametag.for_player(player):segment('health'):set_value(nil):update()
+	health_displayed_for[name] = nil
 end
 
 minetest.foreach_player_every(0.2, function(player)
