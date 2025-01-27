058780d8fb7ed9fa38eb0ab81da4432ce56f10dc
alek13 <alek13.me@gmail.com>
2025-01-27 03:07:36 +0300

    `Core`: `helpers`: add `math.quadratic_equation_roots()`.

--- mods/lord/Core/helpers/src/lua_ext/math.lua
+++ mods/lord/Core/helpers/src/lua_ext/math.lua
@@ -1,5 +1,5 @@
-local math_min, math_max
-    = math.min, math.max
+local math_min, math_max, math_sqrt
+    = math.min, math.max, math.sqrt
 
 
 --- `math.limit`/`math.clamp` ensures a given number stays within a specified range.
@@ -14,3 +14,24 @@ function math.limit(value, min, max)
 end
 
 math.clamp = math.limit
+
+--- Solves the quadratic equation `ax^2 + bx + c = 0`.
+--- Returns two roots or `nil` if no real roots.
+--- The first root is larger, the second is smaller.
+--- Roots can be equal.
+--- @param a number
+--- @param b number
+--- @param c number
+--- @return number|nil,number|nil
+function math.quadratic_equation_roots(a, b, c)
+	local discriminant = b^2 - 4 * a * c
+	if discriminant < 0 then
+		return nil, nil
+	end
+
+	local sqrt_discriminant = math_sqrt(discriminant)
+
+	return
+		(-b + sqrt_discriminant) / (2 * a),
+		(-b - sqrt_discriminant) / (2 * a)
+end
