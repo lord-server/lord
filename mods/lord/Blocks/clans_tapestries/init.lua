6a2a1bf3f0e1c4b65b6769677a3feacc6306a88c
alek13 <alek13.me@gmail.com>
2025-02-07 06:55:30 +0300

    Clans: Tapestries: add for `masons`, `vassals`, `hansa` & `international`. Closes #1956

--- mods/lord/Blocks/clans_tapestries/init.lua
+++ mods/lord/Blocks/clans_tapestries/init.lua
@@ -1,7 +1,28 @@
-local S = minetest.get_mod_translator()
 
-castle.tapestry.register(
-	"clans_tapestries:example",
-	S("Example Clan Tapestry"),
-	"clans_tapestries_example.png"
-)
+minetest.mod(function(mod)
+	local S        = mod.translator
+	local logger   = mod.logger
+	local colorize = minetest.colorize
+
+	castle.tapestry.register(
+		'clans_tapestries:example',
+		S('@1 Clan Tapestry', S('"Example"')),
+		'clans_tapestries_example.png'
+	)
+	-- --------------------------------
+	local clans_with_tapestries = {
+		'masons', 'vassals', 'hansa', 'international',
+	}
+	for _, clan_name in pairs(clans_with_tapestries) do
+		local clan = clans.clan_get_by_name(clan_name)
+		if not clan then
+			logger.warning('Clan `%s` not found', clan_name)
+		end
+		castle.tapestry.register(
+			'clans_tapestries:' .. clan_name,
+			S('@1 Clan Tapestry', colorize(clans.COLOR, clan.title)),
+			'clans_tapestries_' .. clan_name .. '.png'
+		)
+	end
+
+end)
