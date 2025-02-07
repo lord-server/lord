64182c9c92dbcfc7f4cc32e419a2dab661f9675d
alek13 <alek13.me@gmail.com>
2025-02-07 21:12:22 +0300

    Add translations for bricks/blocks & its stairs from `lord_rocks`. Relates to #2003

--- mods/lord/Blocks/lord_bricks/src/bricks/rock.lua
+++ mods/lord/Blocks/lord_bricks/src/bricks/rock.lua
@@ -11,7 +11,7 @@ for node_name, registration in pairs(rocks.get_lord_nodes()) do
 	local brick_name = 'lord_bricks:' .. name .. '_brick'
 	local brick_tile = texture .. '^(lord_bricks_overlay_brick.png^[opacity:70)'
 	minetest.register_node(brick_name, {
-		description       = S(base_description .. ' Brick'),
+		description       = S('@1 @2', S('Brick of'), S(base_description)),
 		paramtype2        = 'facedir',
 		place_param2      = 0,
 		tiles             = { brick_tile },
@@ -23,7 +23,7 @@ for node_name, registration in pairs(rocks.get_lord_nodes()) do
 	local block_name = 'lord_bricks:' .. name .. '_block'
 	local block_tile = texture .. '^(lord_bricks_overlay_block.png^[opacity:70)'
 	minetest.register_node(block_name, {
-		description       = S(base_description .. ' Block'),
+		description       = S('@1 @2', S('Block of'), S(base_description)),
 		tiles             = { block_tile },
 		is_ground_content = false,
 		groups            = { cracky = rock.groups.cracky, stone = 1, brick = 1 },
@@ -54,12 +54,12 @@ for node_name, registration in pairs(rocks.get_lord_nodes()) do
 		brick_name,
 		{ cracky = rock.groups.cracky, stone = 1 },
 		{ brick_tile },
-		S(base_description .. ' Brick Stair'),
-		S(base_description .. ' Brick Slab'),
+		S('@1 @2 Stair', S(base_description), S('Brick')),
+		S('@1 @2 Slab', S(base_description), S('Brick')),
 		default.node_sound_stone_defaults(),
 		false,
-		S('Inner' .. base_description .. ' Brick Stair'),
-		S('Outer' .. base_description .. ' Brick Stair')
+		S('Inner @1 @2 Stair', S(base_description), S('Brick')),
+		S('Outer @1 @2 Stair', S(base_description), S('Brick'))
 	)
 
 	stairs.register_stair_and_slab(
@@ -67,11 +67,11 @@ for node_name, registration in pairs(rocks.get_lord_nodes()) do
 		block_name,
 		{ cracky = rock.groups.cracky, stone = 1 },
 		{ block_tile },
-		S(base_description .. ' Block Stair'),
-		S(base_description .. ' Block Slab'),
+		S('@1 @2 Stair', S(base_description), S('Block')),
+		S('@1 @2 Slab', S(base_description), S('Block')),
 		default.node_sound_stone_defaults(),
 		false,
-		S('Inner' .. base_description .. ' Block Stair'),
-		S('Outer' .. base_description .. ' Block Stair')
+		S('Inner @1 @2 Stair', S(base_description), S('Block')),
+		S('Outer @1 @2 Stair', S(base_description), S('Block'))
 	)
 end
