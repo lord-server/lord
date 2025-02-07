0ff9e18ed94590cde7ad47791e4db217c1ac4ae3
alek13 <alek13.me@gmail.com>
2025-02-08 01:00:29 +0300

    Items: Food: Add pancakes. Closes #1995

--- mods/lord/Tools/lord_food/init.lua
+++ mods/lord/Tools/lord_food/init.lua
@@ -4,11 +4,30 @@
 minetest.mod(function(mod)
 	local S = mod.translator
 
-	minetest.register_craftitem("lord_food:lembas", {
-		description     = S("Lembas"),
-		inventory_image = "lord_food_lembas.png",
+	-- Lembas (no craft)
+	minetest.register_craftitem('lord_food:lembas', {
+		description     = S('Lembas'),
+		inventory_image = 'lord_food_lembas.png',
 		groups          = { food_bread = 1 },
 		on_use          = minetest.item_eat(30),
 		_tt_food_hp     = 30,
+	})
+
+	-- Pancakes
+	minetest.register_craftitem('lord_food:pancakes', {
+		description     = S('Pancakes'),
+		inventory_image = 'lord_food_pancakes.png',
+		on_use          = minetest.item_eat(24),
+		_tt_food_hp     = 24,
+	})
+	minetest.register_craft({
+		type   = 'shapeless',
+		recipe = {
+			'lord_vessels:glass_bottle_water',
+			'farming:flour',
+			'lottmobs:egg',
+			'bees:bottle_honey',
+		},
+		output = 'lord_food:pancakes',
 	})
 end)
