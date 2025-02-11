863b126752778f04b7cd988682716d50a6224cc0
blanka kato <112077355+blanka-kato@users.noreply.github.com>
2025-02-12 02:04:56 +0500

    Pancake in furnance (#2016). Relates to 1995. Closes #2014.
    * Pancake in furnance
    
    Changed the way `lord_food:pancakes` are cooked
    Now you need `lottfarming:dough_with_egg` and `furnance` for them
    
    Uncomment and edit `lottfarming:dough_with_egg`
    Edit `lord_food:pancakes`
    Add 'lord_food:pancakes_with_honey'
    
    Need testing
    Need texture review or redesign

--- mods/lord/Tools/lord_food/init.lua
+++ mods/lord/Tools/lord_food/init.lua
@@ -13,21 +13,36 @@ minetest.mod(function(mod)
 		_tt_food_hp     = 30,
 	})
 
-	-- Pancakes
+	-- Furnance-backed pancakes
 	minetest.register_craftitem('lord_food:pancakes', {
 		description     = S('Pancakes'),
 		inventory_image = 'lord_food_pancakes.png',
-		on_use          = minetest.item_eat(24),
-		_tt_food_hp     = 24,
+		on_use          = minetest.item_eat(14),
+		_tt_food_hp     = 14,
+	})
+
+	minetest.register_craft({
+		type = 'cooking',
+		cooktime = 15,
+		recipe = 'lottfarming:dough_with_egg',
+		output = 'lord_food:pancakes',
+	})
+
+	-- Pancakes with honey
+	minetest.register_craftitem('lord_food:pancakes_with_honey', {
+		description     = S('Pancakes with honey'),
+		inventory_image = 'lord_food_pancakes_with_honey.png',
+		on_use          = minetest.item_eat(25),
+		_tt_food_hp     = 25,
 	})
 	minetest.register_craft({
 		type   = 'shapeless',
+		output = 'lord_food:pancakes_with_honey 2',
 		recipe = {
-			'lord_vessels:glass_bottle_water',
-			'farming:flour',
-			'lottmobs:egg',
+			'lord_food:pancakes',
+			'lord_food:pancakes',
 			'bees:bottle_honey',
 		},
-		output = 'lord_food:pancakes',
+
 	})
 end)
