654dbc399a6d01fd3d478c4714208c51da9ed73b
blanka kato <112077355+blanka-kato@users.noreply.github.com>
2025-02-12 14:03:32 +0500

    Правка вывода сообщений в чат
    Fix #1831
    
    Ошибка могла возникать из-за того, что при генерации двух случайных `resources` случайно выбирается одно и то же значение дважды, т.к. не запоминался предыдущий выбор.
    
    Сейчас добавил таблицу индексов и цикл для выбора `resources` без повтора.

--- mods/lord/Game/server_message/init.lua
+++ mods/lord/Game/server_message/init.lua
@@ -15,17 +15,25 @@ local timer = 0
 minetest.register_globalstep(function(delta_time)
 	timer = timer + delta_time;
 	if (timer >= MESSAGE_PERIOD) and (minetest.get_connected_players()[1] ~= nil) then
+		local used_indices = {} --используется для хранения индексов уже выбранных ресурсов
 		local message = colorize('#0f0', '\n# Server: Оставайтесь на связи!')
 			.. ' (кликните по ссылке с Ctrl)\n'
 		message = message .. '+---------------------------------------------------------------------+\n'
 		message = message .. '|  ' ..
 			colorize('#ff0', 'Telegram:') ..' https://t.me/lord_server_ru                              |\n'
 		message = message .. '+---------------------------------------------------------------------+\n'
-		local random_resource = resources[math.random(#resources)]
-		message = message .. random_resource.title .. ': ' .. random_resource.url .. '\n'
-		random_resource = resources[math.random(#resources)]
-		message = message .. random_resource.title .. ': ' .. random_resource.url .. '\n'
+
+		for i = 1, 2 do
+			local index
+			-- Цикл ниже гарантирует, что индекс будет выбран заново, пока он не окажется уникальным среди уже использованных
+			repeat
+			index = math.random(#resources)
+			until not used_indices[index]
+			used_indices[index] = true
+			message = message .. resources[index].title .. ': ' .. resources[index].url .. '\n'
+		end
 		minetest.chat_send_all(message .. '\n')
 		timer = 0
+		used_indices = {}  -- Явная очистка таблицы после использования
 	end
 end)
