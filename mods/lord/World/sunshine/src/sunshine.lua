7cf16aa54611accda426a41759ff98dcf3a166a3
blanka kato <112077355+blanka-kato@users.noreply.github.com>
2025-02-21 00:42:54 +0500

    add comment ' -- aka "god rays" '

--- mods/lord/World/sunshine/src/sunshine.lua
+++ mods/lord/World/sunshine/src/sunshine.lua
@@ -5,7 +5,7 @@ require('commands')
 local default_lighting = {
 	shadows = { intensity = 0.5},
 	bloom = { intensity = 0.05 },
-	volumetric_light = { strength = 0.2 }
+	volumetric_light = { strength = 0.2 }, -- aka "god rays"
 }
 
 minetest.register_on_joinplayer(function(player)
