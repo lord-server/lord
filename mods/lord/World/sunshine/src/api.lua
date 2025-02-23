dabc264d132f991bdca121bb5d7f92fdc649f300
blanka kato <112077355+blanka-kato@users.noreply.github.com>
2025-02-23 16:34:53 +0500

    Add bloom settings

--- mods/lord/World/sunshine/src/api.lua
+++ mods/lord/World/sunshine/src/api.lua
@@ -3,6 +3,8 @@
 --- @class sunshine.Api
 local Api = {
 	light = {},
+	bloom ={},
+	reset = {},
 }
 function Api.light.set_for(player, value)
 	player:set_lighting({
@@ -12,5 +14,18 @@ function Api.light.set_for(player, value)
 	})
 end
 
+function Api.bloom.set_for(player, i, s, r)
+	player:set_lighting({
+		bloom = {
+			intensity = i,
+			strength_factor = s,
+			radius = r,
+		},
+	})
+end
+
+function Api.reset.set_for(player)
+	player:set_lighting()
+end
 
 return Api
--- mods/lord/World/sunshine/src/commands.lua
+++ mods/lord/World/sunshine/src/commands.lua
@@ -29,3 +29,52 @@ minetest.register_chatcommand('sunshine.set_light', {
         return true
     end,
 })
+
+minetest.register_chatcommand('sunshine.set_bloom', {
+    params = '<num1> <num2> <num3>',
+    description = 'Ввод трех числовых переменных для эффекта bloom',
+    func = function(name, param)
+        local num1, num2, num3 = param:match('^(%d+%.?%d*) (%d+%.?%d*) (%d+%.?%d*)$')
+        if num1 and num2 and num3 then
+            local i, s, r = tonumber(num1), tonumber(num2), tonumber(num3)
+
+            -- Проверка значений переменных
+            if i < 0.0 or i > 1.0 then
+                return false, 'Ошибка: i должно быть в диапазоне от 0.0 до 1.0.'
+            end
+            if s < 0.1 or s > 10.0 then
+                return false, 'Ошибка: s должно быть в диапазоне от 0.1 до 10.0.'
+            end
+            if r < 0.1 or r > 8.0 then
+                return false, 'Ошибка: r должно быть в диапазоне от 0.1 до 8.0.'
+            end
+
+            local player = minetest.get_player_by_name(name)
+            if player then
+                api.bloom.set_for(player, i, s, r)  -- Вызов функции с параметрами
+
+                return true, 'Вы ввели: ' .. i .. ', ' .. s .. ', ' .. r  -- Используйте i, s, r для вывода
+            else
+                return false, 'Ошибка: игрок не найден.'
+            end
+        else
+            return false, 'Ошибка: введите три числа.'
+        end
+    end
+})
+
+minetest.register_chatcommand('sunshine.reset', {
+    params = '<value>',
+    description = S('Set the volumetric light strength (0.0 to 1.0)'),
+    func = function(name, param)
+
+        local player = minetest.get_player_by_name(name)
+        if player then
+			api.reset.set_for(player)
+        else
+			return false, S('Player not found.')
+		end
+
+        return true
+    end,
+})
