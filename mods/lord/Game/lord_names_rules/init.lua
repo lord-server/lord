2327d0f9990ced2d99ddfd451c3ba9e8072154a1
lord-papirus <iotest@onenavigation.net>
2025-03-12 22:40:25 +0300

    Added server-side translation(names rules of user) when an error occurs on the register form of new user. Closes #2051

--- mods/lord/Game/lord_names_rules/init.lua
+++ mods/lord/Game/lord_names_rules/init.lua
@@ -1,59 +1,72 @@
-local SL = minetest.get_mod_translator()
+minetest.mod(function(mod)
+	local S = minetest.get_mod_translator()
 
--- правила формирования имени
-local rules = {
-	{
-		match = function(name)
-			return #name > 2
-		end,
-		description = SL("Your name must contain more than 2 symbols;"),
-	},
-	{
-		match = function(name)
-			return #name <= 24
-		end,
-		description = SL("Your name must be less than 24 symbols;"),
-	},
-	{
-		match = function(name)
-			return not string.match(name, '[^%a_-]')
-		end,
-		description = SL("Your name must contain letters only; '-' and '_' are also allowed;"),
-	},
-	{
-		match = function(name)
-			return string.match(name, "^[A-Z]")
-		end,
-		description = SL("Your name must begin with capital letters;"),
-	},
-}
+	minetest.register_on_prejoinplayer(function(usrname, ip)
 
--- возвращает true, если name удовлетворяет всем условиям, и false в противном случае
-local function is_correct_name(name)
-	for _, rule in pairs(rules) do
-		if not rule.match(name) then
-			return false
+		-- правила формирования имени
+		local rules = {
+			{
+				match = function(name)
+					return #name > 2
+				end,
+				check = "(v)",
+				description = S("Your name must contain more than 2 symbols;"),
+			},
+			{
+				match = function(name)
+					return #name <= 19
+				end,
+				check = "(v)",
+				description = S("Your name must be less than 19 symbols;"),
+			},
+			{
+				match = function(name)
+					return not string.match(name, '[^%a_-]')
+				end,
+				check = "(v)",
+				description = S("Your name must contain letters only, symbol '-' and '_' are also allowed;"),
+			},
+			{
+				match = function(name)
+					return string.match(name, "^[A-Z]")
+				end,
+				check = "(v)",
+				description = S("Your name must begin with capital letters;"),
+			},
+		}
+
+		-- возвращает true, если name удовлетворяет всем условиям, и false в противном случае
+		local function is_correct_name(name)
+			local check_flag = true
+			for _, rule in pairs(rules) do
+				if not rule.match(name) then
+					rule.check = "(x)"
+					check_flag = false
+				else
+					rule.check = "(v)"
+				end
+			end
+
+			return check_flag
 		end
-	end
+		local name = usrname
+		if minetest.is_singleplayer() or minetest.player_exists(name) then
+			return
+		end
+		if not is_correct_name(name) then
+			local list_rules = ""
+			--выводим правила сервера на русском языке
+			for _, rule in pairs(rules) do
+				list_rules = list_rules .. "\n" .. rule.check .. " " .. minetest.get_translated_string("ru", rule.description)
+			end
+			-- выводим правила сервера на английском языке
+			list_rules = list_rules .. "\n\n" .. "Your name does not accepted with the server rules:"
+			for _, rule in pairs(rules) do
+				list_rules = list_rules .. "\n" .. rule.check .. " " .. rule.description
+			end
 
-	return true
-end
-
-minetest.register_on_prejoinplayer(function(name, ip)
-	local list_rules = ""
-	for _, rule in pairs(rules) do
-		list_rules = list_rules .. "\n- " .. rule.description
-	end
-	if not minetest.is_singleplayer() and
-			not is_correct_name(name) and
-			not minetest.player_exists(name) then
-		return "\n"..SL("Invalid character name")..list_rules
-	end
-end)
-
-minetest.register_on_joinplayer(function(player)
-	local name = player:get_player_name()
-	if not minetest.is_singleplayer() and not is_correct_name(name) then -- ошибка в имени
-		minetest.chat_send_player(name, SL("Invalid character name. Contact to the administration."))
-	end
+			return "\n" .. minetest.get_translated_string("ru", S("Your name does not accepted with the server rules:"))
+				.. list_rules
+		end
+	end)
 end)
