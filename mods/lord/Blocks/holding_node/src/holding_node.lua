66c88951304d30d7704890f2c0cc436e6cd4fca9
blanka kato <112077355+blanka-kato@users.noreply.github.com>
2025-04-07 02:13:52 +0500

    Set node description to local file

--- mods/lord/Blocks/holding_node/src/holding_node.lua
+++ mods/lord/Blocks/holding_node/src/holding_node.lua
@@ -1,25 +1,13 @@
 local S = minetest.get_mod_translator()
+local node = require('holding_node.node')
 
 
 holding_node = {} -- luacheck: ignore unused global variable holding_node
 
 local function register_node()
-	minetest.register_node('holding_node:holding_node', {
-		description = S('Holding block'),
-
-		--- @param pos Position
-		on_construct = function(pos)
-			local meta = minetest.get_meta(pos)
-			meta:set_string ('name', S('-'))
-			meta:set_int    ('in_event_list', 1)
-			meta:set_int    ('active', 0)
-			meta:set_int    ('last_activate_at', 0)
-			meta:set_string ('captured_by_clan', S('Nobody'))
-			meta:set_int    ('captured_at', 0)
-			meta:set_int    ('reward_gived_at', 0)
-		end,
-	})
+	minetest.register_node('holding_node:holding_node', node.definition)
 end
+
 
 return {
 	--- @param mod minetest.Mod
