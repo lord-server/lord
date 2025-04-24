90ff7029818541c23255600385af9d567e581176
alek13 <alek13.me@gmail.com>
2025-04-24 12:52:57 +0300

    Wrap `lord_music_box/init.lua` content into `minetest.mod()`.

--- mods/lord/Blocks/lord_music_box/init.lua
+++ mods/lord/Blocks/lord_music_box/init.lua
@@ -1,15 +1,18 @@
-minetest.register_node("lord_music_box:music_box", {
-	description = "Music box",
-	tiles = {'default_stone.png'},
-	groups = {not_in_creative_inventory=1},
-})
 
-minetest.register_abm({
-    nodenames = {"lord_music_box:music_box"},
-    interval = 12,
-    chance = 1,
-    action = function(pos, node)
-        minetest.sound_play("markiza",
-						{pos = pos, max_hear_distance = 10, gain = 0.05})
-    end,
-})
+minetest.mod(function(mod)
+	minetest.register_node("lord_music_box:music_box", {
+		description = "Music box",
+		tiles = {'default_stone.png'},
+		groups = {not_in_creative_inventory=1},
+	})
+
+	minetest.register_abm({
+		nodenames = {"lord_music_box:music_box"},
+		interval = 12,
+		chance = 1,
+		action = function(pos, node)
+			minetest.sound_play("markiza",
+				{pos = pos, max_hear_distance = 10, gain = 0.05})
+		end,
+	})
+end)
