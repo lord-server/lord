bab7a8bd162575773c5b3d1a44d518caee21d9f1
blanka kato <112077355+blanka-kato@users.noreply.github.com>
2025-05-06 18:11:06 +0500

    Clans: Hold points: autoopen confuguration Form (#2146). Closes #2125. Closes #2126

---
+++ mods/lord/Blocks/holding_node/src/holding_node/Form.lua
@@ -0,0 +1,34 @@
+local S = minetest.get_mod_translator()
+local spec = minetest.formspec
+
+
+--- @class holding_node.node.Form: base_classes.Form.Base
+--- @field node_position Position
+--- @field node_meta NodeMetaRef
+local Form = base_classes.Form:personal():for_node():extended({
+	NAME = 'holding_node:holding_node_config',
+})
+
+--- @private
+--- @return string
+function Form:get_spec()
+	local str_pos = self.node_position.x .. ',' .. self.node_position.y .. ',' .. self.node_position.z
+
+	return ''
+		.. spec.size(8,9)
+		.. spec.field(1, 1, 3, 1, 'input_name',S('Name'), '')
+		.. spec.button_exit(1, 2, 3, 1, 'save', S('Save'))
+		.. spec.list('nodemeta:' .. str_pos, 'reward', 1, 3, 4, 1)
+end
+
+--- @param fields table
+function Form:handle(fields)
+	if fields.input_name and fields.save then
+		if self.node_meta then
+			self.node_meta:set_string('name', fields.input_name)
+		end
+	end
+end
+
+
+return Form:register()
--- mods/lord/Blocks/holding_node/src/holding_node/node.lua
+++ mods/lord/Blocks/holding_node/src/holding_node/node.lua
@@ -1,4 +1,8 @@
+local Form = require('holding_node.Form')
+
+
 local S = minetest.get_mod_translator()
+
 
 local definition = {
 	description = S('Holding block'),
@@ -6,6 +10,7 @@ local definition = {
 	--- @param pos Position
 	on_construct = function(pos)
 		local meta = minetest.get_meta(pos)
+
 		meta:set_string ('name', S('-'))
 		meta:set_int    ('in_event_list', 1)
 		meta:set_int    ('active', 0)
@@ -13,7 +18,75 @@ local definition = {
 		meta:set_string ('captured_by_clan', S('Nobody'))
 		meta:set_int    ('captured_at', 0)
 		meta:set_int    ('reward_gived_at', 0)
+		meta:set_string ('battle_stat', minetest.serialize({}))
+		local inv = meta:get_inventory()
+		inv:set_size('reward', 8)
 	end,
+
+	--- @param placer Player
+	--- @param itemstack ItemStack
+	--- @param pointed_thing pointed_thing
+	on_place = function(itemstack, placer, pointed_thing)
+		if not minetest.check_player_privs(placer, 'server') then
+			minetest.chat_send_player(placer:get_player_name(), S('It\'s for admins only! How did you get this thing?!'))
+
+			return itemstack, nil
+		end
+		-- Если проверка пройдена, возвращаем стандартное поведение
+		return minetest.item_place(itemstack, placer, pointed_thing)
+	end,
+
+	--- @param pos Position
+	--- @param placer Player
+	after_place_node = function(pos, placer)
+		if not minetest.check_player_privs(placer, 'server') then
+				minetest.chat_send_player(placer:get_player_name(), S('It\'s for admins only! How did you get this thing?!'))
+
+				return nil
+		end
+		Form:new(placer, pos):open()
+	end,
+
+	--- @param pos Position
+	--- @param player Player
+	--- @param pointed_thing pointed_thing
+	--- @param node Node
+	on_punch = function(pos, node, player, pointed_thing)
+
+		-- чтоб код не падал если ноду ударит не игрок
+		if not player or not minetest.is_player(player) then
+			return
+		end
+
+		-- чтоб код не падал если ноду ударит игрок без клана
+		local clan = clans.clan_get_by_player(player)
+
+		if not clan then
+			minetest.chat_send_player(player:get_player_name(), S('For clan players only'))
+			return
+		end
+
+		-- нужно проверить наличие клана игрока в таблице статистики, обновить время захвата при наличии
+		local meta = minetest.get_meta(pos)
+		local battle_stat = minetest.deserialize(meta:get_string('battle_stat')) or {}
+		local current_time = os.time()
+
+		battle_stat[clan.name] = 0
+
+		-- обновить метаданные ноды
+		meta:set_string('battle_stat', minetest.serialize(battle_stat))
+		meta:set_string('captured_by_clan', clan.name)
+		meta:set_int('captured_at', current_time)
+	end,
+
+	--- @param pos Position
+	--- @param node Node
+	--- @param clicker Player
+	--- @param itemstack ItemStack
+	--- @param pointed_thing pointed_thing
+	on_rightclick = function(pos, node, clicker, itemstack, pointed_thing)
+
+	end
 }
 
 
