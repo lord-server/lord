597e43da2073199c8da60ef1d02ec629013f6a30
blanka kato <112077355+blanka-kato@users.noreply.github.com>
2025-05-15 19:21:09 +0500

    Cleanup code. Update structure. Close #2204
    Раскидал куски мода по файлам
    Статику кинул в конфиг: ноты разделил для каждого инструмента.
    Функции вынес в отдельный файл
    
    Добавил:
    - металлофон
    - модельки для обеих нод
    - поджал звуки

--- mods/lord/Blocks/Music/music_instruments/src/music_instruments.lua
+++ mods/lord/Blocks/Music/music_instruments/src/music_instruments.lua
@@ -1,6 +1,4 @@
-local api    = require('music_instruments.api')
-local config = require('music_instruments.config') -- luacheck: ignore unused local variable config
-local adjuster = require('music_instruments.adjuster') -- luacheck: ignore unused local variable adjuster
+local api = require('music_instruments.music_node.api')
 
 
 music_instruments = {} -- luacheck: ignore unused global variable music_instruments
@@ -15,4 +13,6 @@ return {
 	init = function(mod)
 		register_api()
 	end,
+	require('music_instruments.adjuster'),
+	require('music_instruments.music_node.config'),
 }
--- mods/lord/Blocks/Music/music_instruments/src/music_instruments/adjuster.lua
+++ mods/lord/Blocks/Music/music_instruments/src/music_instruments/adjuster.lua
@@ -1,77 +1,47 @@
-local note_names = require('music_instruments.config').note_names
+local music_node = require('music_instruments.music_node')
+local config = require('music_instruments.music_node.config')
 
 
---- @param offset number Смещение в полутоновом выражении.
---- @return {note: string, pitch: number} -- Данные о ноте: название и значение pitch.
-local function get_tone(offset)
-	-- При отсутствии значения возвращаем ноту для 0
-	return note_names[offset] or note_names[0]
-end
-
---- Проигрывает звук для заданной позиции и смещения тона.
---- Дубль
+--- Изменяет тон ноды
 --- @param pos Position
---- @param offset number Смещение в полутоновом выражении.
-local function play_tone(pos, offset)
-	local t = get_tone(offset)
-	minetest.sound_play("c5_note", {
-		pos = pos,
-		gain = 1.0,
-		pitch = t.pitch,
-	})
-end
-
---- Обновляет метаданные ноды и проигрывает звук.
---- Дубль
---- @param pos Position
---- @param offset number Смещение в полутоновом выражении.
-local function update_node(pos, offset)
-	local meta = minetest.get_meta(pos)
-	meta:set_int("semitones", offset)
-	local t = get_tone(offset)
-	meta:set_string("infotext", "Semitones: " .. offset .. ", Note: " .. t.note)
-	play_tone(pos, offset)
-end
-
---- Проверяет, является ли нода по заданной позиции нашей нодой.
---- @param pos Position
---- @return boolean -- true, если нода имеет имя music_box:pitch_node, иначе false.
-local function is_valid_music_box(pos)
-	return minetest.get_node(pos).name == 'music_instruments:calimba'
-end
-
---- Изменяет смещение тональности ноды и обновляет её состояние.
---- @param pos Position
---- @param delta number Изменение смещения (например, 1 или -1).
+--- @param delta number Шаг изменения (1 или -1)
 local function adjust_semitones(pos, delta)
 	local meta = minetest.get_meta(pos)
-	local offset = meta:get_int("semitones") + delta
-	if offset > 12 then offset = 12 end
-	if offset < -12 then offset = -12 end
-	update_node(pos, offset)
+	local instrument_id = meta:get_string('instrument')
+	local current = meta:get_int('semitones')
+	local new_offset = math.clamp(current + delta, config.min_offset, config.max_offset)
+
+	music_node.update_node(pos, instrument_id, new_offset)
+	music_node.play_sound(pos, instrument_id, new_offset)
 end
 
 minetest.register_tool('music_instruments:pitch_adjuster', {
-	description = "Pitch Adjuster (ЛКМ – повысить тон, ПКМ – понизить тон)",
-	inventory_image = "default_tool_steelaxe.png",
+	description = 'Регулятор тона',
+	inventory_image = 'default_tool_steelaxe.png',
 
 	--- @param itemstack ItemStack
 	--- @param user Player
+	---- @param pointed_thing PointedThing
 	on_use = function(itemstack, user, pointed_thing)
-		if pointed_thing.type == "node" and is_valid_music_box(pointed_thing.under) then
-			adjust_semitones(pointed_thing.under, 1)
+		if pointed_thing.type == 'node' then
+			local node_name = minetest.get_node(pointed_thing.under).name
+			if string.find(node_name, 'music_instruments') then
+				adjust_semitones(pointed_thing.under, 1)
+			end
 		end
-
-		return itemstack
 	end,
 
-	--- @param itemstack ItemStack
-	--- @param user Player
+    --- @param itemstack ItemStack
+    --- @param user Player
+    ---- @param pointed_thing PointedThing
 	on_place = function(itemstack, user, pointed_thing)
-		if pointed_thing.type == "node" and is_valid_music_box(pointed_thing.under) then
-			adjust_semitones(pointed_thing.under, -1)
+		if pointed_thing.type == 'node' then
+			local node_name = minetest.get_node(pointed_thing.under).name
+			if string.find(node_name, 'music_instruments') then
+				adjust_semitones(pointed_thing.under, -1)
+			end
 		end
 
 		return itemstack
-	end,
+	end
 })
