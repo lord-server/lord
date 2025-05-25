33a8f2fac235acb13ac14cd70b579ac0c5cbd225
blanka kato <112077355+blanka-kato@users.noreply.github.com>
2025-05-26 01:50:35 +0500

    Clans: Hold Points: Player inventory added. Closes #2135
    В форму добавил:
    - отображение имени ноды в поле ввода
    - отображение инвентаря игрока
    - подпись к инвентарю самой ноды
    
    В ноду добавил открытие формы для админов при 'rightclick'

--- mods/lord/Blocks/holding_node/src/holding_node/Form.lua
+++ mods/lord/Blocks/holding_node/src/holding_node/Form.lua
@@ -15,10 +15,13 @@ function Form:get_spec()
 	local str_pos = self.node_position.x .. ',' .. self.node_position.y .. ',' .. self.node_position.z
 
 	return ''
-		.. spec.size(8,9)
-		.. spec.field(1, 1, 3, 1, 'input_name',S('Name'), '')
-		.. spec.button_exit(1, 2, 3, 1, 'save', S('Save'))
-		.. spec.list('nodemeta:' .. str_pos, 'reward', 1, 3, 4, 1)
+		.. spec.formspec_version(4)
+		.. spec.size(12,11)
+		.. spec.field(1, 1, 3, 1, 'input_name',S('Name'), self.node_meta:get_string('name'))
+		.. spec.button_exit(4.5, 1, 2, 1, 'save', S('Save'))
+		.. spec.label(1, 2.75, S('Reward'))
+		.. spec.list('nodemeta:' .. str_pos, 'reward', 1, 3, 8, 1)
+		.. spec.list('current_player', 'main', 1, 5, 8, 4)
 end
 
 --- @param fields table
