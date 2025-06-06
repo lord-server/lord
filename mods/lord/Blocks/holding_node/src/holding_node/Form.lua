78882e615f2e287ce55cff5e6915694cf7627fa5
alek13 <alek13.me@gmail.com>
2025-06-06 19:17:03 +0300

    Clans: Hold Points: Form: edit/saving `in_event_list` field. Closes #2235. Display `active` & `last_activate_at`. Relates to #2127

--- mods/lord/Blocks/holding_node/src/holding_node/Form.lua
+++ mods/lord/Blocks/holding_node/src/holding_node/Form.lua
@@ -1,37 +1,154 @@
-local S = minetest.get_mod_translator()
-local spec = minetest.formspec
+local S     = minetest.get_mod_translator()
+local spec  = forms.Spec
+local e     = spec.escape
 
+
+local SMALL = { font_size = '-1' } -- Style for formspec
 
 --- @class holding_node.node.Form: base_classes.Form.Base
 --- @field node_position Position
 --- @field node_meta NodeMetaRef
 local Form = base_classes.Form:personal():for_node():extended({
-	NAME = 'holding_node:holding_node_config',
+	--- @type string
+	NAME     = 'holding_node:holding_node_config',
+	--- @type {x:number, y:number}
+	padding  = { x = 0.5, y = 0.5 },
+	--- @type {x:number, y:number} size without paddings
+	size     = { x = 10 - 0.25, y = 11.25 }, -- `0.25` - spacing between slots of `list`, it present after last slot too.
+	--- @type number
+	fields_h = 0.65,
+	--- @type number
+	row_h    = 0.9,
+	--- @type number
+	center_x = nil,
+	--- @type boolean
+	in_event_list = nil
 })
+
+--- just pre-calculate center_x of the form
+function Form:instantiate()
+	self.center_x = self.padding.x + self.size.x / 2
+end
+
+--- @param row number form row number
+--- @return string
+function Form:get_row_center_y(row)
+	return self.padding.y + row * self.row_h + self.row_h / 2
+end
+
+--- @param row number form row number
+--- @return string
+function Form:get_row_start_y(row)
+	return self.padding.y + row * self.row_h
+end
+
+--- @private
+--- @param row         number form row number
+--- @param label       string
+--- @param description string
+--- @return string
+function Form:described_label(row, label, description)
+	local row_center_y = self:get_row_center_y(row)
+
+	return ''
+		.. spec.label(self.padding.x, row_center_y - 0.14, label)
+		.. spec.muted(self.padding.x, row_center_y + 0.14, description, SMALL)
+end
+
+--- @private
+--- @return string
+function Form:labeled_field(row, name, value, label, description)
+	return ''
+		.. self:described_label(row, label, description)
+		.. spec.field(
+			self.center_x, self.padding.y + row * self.row_h + (self.row_h - self.fields_h) / 2,
+			self.size.x / 2, self.fields_h,
+			name, '', value
+		)
+end
+
+--- @private
+--- @param row         number form row number
+--- @param name        string
+--- @param selected    boolean
+--- @param label       string
+--- @param description string
+--- @return string
+function Form:labeled_checkbox(row, name, selected, label, description)
+	local row_center_y = self:get_row_center_y(row)
+
+	return ''
+		.. self:described_label(row, label, description)
+		.. spec.checkbox(self.center_x, row_center_y, name, '', selected)
+end
+
+--- @private
+--- @param row         number
+--- @param is_yes      boolean
+--- @param label       string
+--- @param description string
+--- @return string
+function Form:labeled_boolean_ro(row, is_yes, label, description)
+	return ''
+		.. self:described_label(row, label, description)
+		.. spec.bold(self.center_x, self:get_row_center_y(row), is_yes and S('Yes') or S('No'))
+end
+
+--- @private
+--- @param row           number
+--- @param datetime    number timestamp
+--- @param label       string
+--- @param description string
+--- @return string
+function Form:labeled_datetime_ro(row, datetime, label, description)
+	local row_center = {
+		x = self.center_x,
+		y = self:get_row_center_y(row)
+	}
+
+	return ''
+		.. self:described_label(row, label, description)
+		.. spec.bold(row_center.x, row_center.y, datetime and '-' or e(os.date('%d.%m.%Y %H:%M:%S', datetime)))
+end
 
 --- @private
 --- @return string
 function Form:get_spec()
-	local str_pos = self.node_position.x .. ',' .. self.node_position.y .. ',' .. self.node_position.z
+	local name             = self.node_meta:get_string('name')
+	local in_event_list    = self.in_event_list == nil
+		and (self.node_meta:get_int('in_event_list') == 1)
+		or  self.in_event_list
+	local active           = self.node_meta:get_int('active') == 1
+	local last_activate_at = self.node_meta:get_int('last_activate_at')
 
 	return ''
 		.. spec.formspec_version(4)
-		.. spec.size(10.75, 11)
-		.. spec.field(0.5, 1, 3, 0.7, 'input_name',S('Name'), self.node_meta:get_string('name'))
-		.. spec.button_exit(4.5, 1, 2, 0.7, 'save', S('Save'))
-		.. spec.label(0.5, 3.75, S('Reward'))
-		.. spec.list('nodemeta:' .. str_pos, 'reward', 0.5, 4, 8, 1)
-		.. spec.list('current_player', 'main', 0.5, 5.5,  8, 1)
-		.. spec.list('current_player', 'main', 0.5, 6.75, 8, 3, 8)
+		.. spec.size(self.size.x + 2 * self.padding.x, self.size.y + 2 * self.padding.y)
+		.. self:labeled_field(0, 'input_name', name, S('Name'), S('This name will be shown for players'))
+		.. self:labeled_checkbox(1, 'in_event_list', in_event_list, S('Participates in future battles'), S('Description'))
+		.. self:labeled_boolean_ro(2, active, S('Active now'), S('Right now the battle is going on for this point.'))
+		.. self:labeled_datetime_ro(
+			3, last_activate_at,
+			S('Last activate at'), S('Date of the block\'s last participation in event')
+		)
+		.. spec.button_exit(self.center_x - 2/2, self:get_row_start_y(4), 2, self.fields_h, 'save', S('Save'))
+		.. spec.label(self.padding.x, self:get_row_start_y(5) - 0.25, S('Reward'))
+		.. spec.node_inventory(self.padding.x, self:get_row_start_y(5), 8, 1, self.node_position, 'reward')
+		.. spec.player_inventory(self.padding.x, self:get_row_start_y(5) + 0.2 + 1.5, 8, 1)
 end
 
 --- @param fields table
 function Form:handle(fields)
-	if fields.input_name and fields.save then
-		if self.node_meta then
-			self.node_meta:set_string('name', fields.input_name)
-		end
+	if fields.in_event_list then
+		-- just remember current value of checkbox for future saving by `Save` button
+		self.in_event_list = minetest.is_yes(fields.in_event_list)
 	end
+	if not fields.save then
+		return
+	end
+
+	self.node_meta:set_string('name', fields.input_name)
+	self.node_meta:set_int('in_event_list', self.in_event_list and 1 or 0)
 end
 
 
