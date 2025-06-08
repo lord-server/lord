f34d298e73df7ac5b44b294637440f4a548513c5
blanka kato <112077355+blanka-kato@users.noreply.github.com>
2025-06-08 16:53:24 +0500

    Game: Random facts. Closes #2221
    Вынес текст сообщений в два файла
    Добавил флаг переключения
    показа сообщений

--- mods/lord/Game/server_message/init.lua
+++ mods/lord/Game/server_message/init.lua
@@ -1,42 +1,49 @@
 minetest.mod(function(mod)
+	local resources = require('messages.resources')
+	local facts     = require('messages.facts')
+
 	local S = minetest.get_mod_translator()
 	local colorize = minetest.colorize
 
 	-- периодичность в секундах
 	local MESSAGE_PERIOD = 30 * 60
-	-- текст сообщения
-	local resources = {
-		{ title = 'Website',  url = 'https://lord-server.ru/' },
-		{ title = 'Discord',  url = 'https://www.discord.gg/YcT5FuQwUT' },
-		{ title = 'YouTube',  url = 'https://www.youtube.com/@lord-server' },
-		{ title = 'VK Group', url = 'https://vk.com/minetest_lord' },
-		{ title = 'GitHub',   url = 'https://github.com/lord-server/lord' },
-	}
 
 	local timer = 0
+	local show_links = true -- флаг для переключения между "ссылками" и "фактами"
 	minetest.register_globalstep(function(delta_time)
 		timer = timer + delta_time;
 		if (timer >= MESSAGE_PERIOD) and (minetest.get_connected_players()[1] ~= nil) then
-			local used_indices = {} --используется для хранения индексов уже выбранных ресурсов
-			local message = colorize('#0f0', '\n# Server: ' .. S('Stay in connect!'))
+			local message = ''
+			if not show_links then
+				local used_indices = {} --используется для хранения индексов уже выбранных ресурсов
+				message = message .. colorize('#0f0', '\n# Server: ' .. S('Stay in connect!'))
 				.. S(' (click on the link with Ctrl)') .. ' \n'
-			message = message .. colorize('#ff0', 'Telegram:') ..' https://t.me/lord_server_ru\n'
+				message = message .. colorize('#ff0', 'Telegram:') ..' https://t.me/lord_server_ru\n'
 
-			for i = 1, 2 do
-				local index
-				-- Цикл ниже гарантирует, что индекс будет выбран заново, пока он не окажется уникальным среди уже использованных
-				repeat
-					index = math.random(#resources)
-				until not used_indices[index]
-				used_indices[index] = true
-				message = message .. colorize('#ff0', resources[index].title) .. ': ' .. resources[index].url .. '\n'
+				for i = 1, 2 do
+					local index
+					-- Цикл ниже гарантирует, что индекс будет выбран заново,
+					-- пока он не окажется уникальным среди уже использованных
+					repeat
+						index = math.random(#resources)
+					until not used_indices[index]
+					used_indices[index] = true
+					message = message .. colorize('#ff0', resources[index].title) .. ': '
+					.. resources[index].url .. '\n'
+				end
+			else
+				local fact = facts[math.random(#facts)]
+				message = message .. colorize('#0f0', '# Server: ' .. S('Fun fact:')) .. '\n'
+				message = message .. colorize('#ff0', fact.title) .. ': ' .. '\n' .. fact.text .. '\n'
+				.. '\n' -- пустая строка чтоб отделить от сообщения с кланами
 			end
 			-- Отдельное информационное сообщение
-			message = message .. colorize(clans.COLOR, S("Clans: "))
+			message = message .. colorize(clans.COLOR, S('Clans: '))
 				.. S('Join the clan and find loyal allies! Learn more on the website:')
 				.. ' https://lord-server.ru/clans\n'
 			minetest.chat_send_all(message .. '\n')
 			timer = 0
+			show_links = not show_links -- переключаем флаг для следующего сообщения
 		end
 	end)
 
