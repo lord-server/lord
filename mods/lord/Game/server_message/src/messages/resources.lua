fe6896d43bbe3bf974de90b0addea4f35ee18bff
blanka kato <112077355+blanka-kato@users.noreply.github.com>
2025-06-08 16:53:24 +0500

    Game: Random facts. Closes #2221
    Вынес текст сообщений в два файла
    Добавил флаг переключения
    показа сообщений

---
+++ mods/lord/Game/server_message/src/messages/resources.lua
@@ -0,0 +1,7 @@
+return{
+	{ title = 'Website',  url = 'https://lord-server.ru/' },
+	{ title = 'Discord',  url = 'https://www.discord.gg/YcT5FuQwUT' },
+	{ title = 'YouTube',  url = 'https://www.youtube.com/@lord-server' },
+	{ title = 'VK Group', url = 'https://vk.com/minetest_lord' },
+	{ title = 'GitHub',   url = 'https://github.com/lord-server/lord' },
+}
