5eb2853b2902b78e3baf64aadf8d13046948c2b2
alek13 <alek13.me@gmail.com>
2025-06-10 20:26:35 +0300

    `Core/helpers`: fix debug macroses.

--- mods/lord/Core/helpers/src/lua_ext/debug.lua
+++ mods/lord/Core/helpers/src/lua_ext/debug.lua
@@ -4,17 +4,18 @@ local debug_getinfo
 --- @param depth number
 --- @return string
 function __FILE__(depth) -- luacheck: ignore unused global variable __FILE__
-	return debug_getinfo(3 + (depth or 0), 'S').source
+	return debug_getinfo(2 + (depth or 0), 'S').source
 end
 --- @param depth number
 --- @return number
 function __LINE__(depth) -- luacheck: ignore unused global variable __LINE__
-	return debug_getinfo(3 + (depth or 0), 'l').currentline
+	return debug_getinfo(2 + (depth or 0), 'l').currentline
 end
 --- @param depth number
 --- @return string
 function __FILE_LINE__(depth) -- luacheck: ignore unused global variable __FILE_LINE__
-	return __FILE__(depth) .. ':' .. __LINE__(depth)
+	depth = depth or 0
+	return __FILE__(depth + 1) .. ':' .. __LINE__(depth + 1)
 end
 --- @param depth number
 --- @return string
