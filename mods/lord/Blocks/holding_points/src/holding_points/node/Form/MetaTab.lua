6cbf0518e81b3267a509ce6566095b3be65985f9
alek13 <alek13.me@gmail.com>
2025-06-11 19:30:32 +0300

    Clans: Hold Points: form: stupid simple meta-data view in `meta` tab. Relates to #2127

--- mods/lord/Blocks/holding_points/src/holding_points/node/Form/MetaTab.lua
+++ mods/lord/Blocks/holding_points/src/holding_points/node/Form/MetaTab.lua
@@ -1,4 +1,5 @@
-local S = minetest.get_mod_translator()
+local S    = minetest.get_mod_translator()
+local spec = forms.Spec
 
 
 --- @class holding_points.node.Form.MetaTab: base_classes.Form.Element.Tab
@@ -10,9 +11,13 @@ local MetaTab = {
 MetaTab = base_classes.Form.Element.Tab:extended(MetaTab)
 
 
-
+--- @return string
 function MetaTab:get_spec()
+	local fields = self.form.node_meta:to_table().fields
+
 	return ''
+		.. spec.box(.75, 1, 9, 9, '#000')
+		.. spec.area_ro(.75, 1, 9, 9, minetest.write_json(fields, true))
 end
 
 --- @param fields table
