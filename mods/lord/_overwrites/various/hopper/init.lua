6ffb3718e418163f0919dc50a84efb11e07239a5
ssdaniel24 <107036969+ssdaniel24@users.noreply.github.com>
2025-06-19 16:24:45 +0300

    More hopper tweaks (#2279)
    * Added colored gold chests support + hoppers can take items from locked
    * Added castle:ironbound_chest support
    * Added lord_mail support
    * Added some doc :memo:

--- mods/lord/_overwrites/various/hopper/init.lua
+++ mods/lord/_overwrites/various/hopper/init.lua
@@ -26,11 +26,23 @@ minetest.clear_craft({output = "hopper:hopper_void"})
 -- 	}
 -- })
 
+-- TIP: first param of the func
+-- ┌───┐
+-- └┬─┬┘
+--  └─┘ <-------- "bottom": input
+-- ┌───┐ ┌───┐
+-- │ c │ └┬─┬┘ <- "side": input
+-- │   │  └─┘
+-- └───┘ <------- container
+-- ┌───┐
+-- └┬─┬┘ <------- "top": output
+--  └─┘
 hopper:add_container({
 	-- ### protector_lott ###
 	{"void", "protector_lott:chest", "main"},
 	{"bottom", "protector_lott:chest", "main"},
 	{"side", "protector_lott:chest", "main"},
+	-- without output
 
 	-- ### technic_chests ###
 	{"top", "technic:copper_chest",  "main"},
@@ -57,7 +69,78 @@ hopper:add_container({
 	{"void", "technic:gold_chest",    "main"},
 	{"void", "technic:mithril_chest", "main"},
 
-	-- locked: only input
+	-- colored:
+	{"top", "technic:gold_chest_black",      "main"},
+	{"top", "technic:gold_chest_blue",       "main"},
+	{"top", "technic:gold_chest_brown",      "main"},
+	{"top", "technic:gold_chest_cyan",       "main"},
+	{"top", "technic:gold_chest_dark_green", "main"},
+	{"top", "technic:gold_chest_dark_grey",  "main"},
+	{"top", "technic:gold_chest_green",      "main"},
+	{"top", "technic:gold_chest_grey",       "main"},
+	{"top", "technic:gold_chest_magenta",    "main"},
+	{"top", "technic:gold_chest_orange",     "main"},
+	{"top", "technic:gold_chest_pink",       "main"},
+	{"top", "technic:gold_chest_red",        "main"},
+	{"top", "technic:gold_chest_violet",     "main"},
+	{"top", "technic:gold_chest_white",      "main"},
+	{"top", "technic:gold_chest_yellow",     "main"},
+
+	{"side", "technic:gold_chest_black",      "main"},
+	{"side", "technic:gold_chest_blue",       "main"},
+	{"side", "technic:gold_chest_brown",      "main"},
+	{"side", "technic:gold_chest_cyan",       "main"},
+	{"side", "technic:gold_chest_dark_green", "main"},
+	{"side", "technic:gold_chest_dark_grey",  "main"},
+	{"side", "technic:gold_chest_green",      "main"},
+	{"side", "technic:gold_chest_grey",       "main"},
+	{"side", "technic:gold_chest_magenta",    "main"},
+	{"side", "technic:gold_chest_orange",     "main"},
+	{"side", "technic:gold_chest_pink",       "main"},
+	{"side", "technic:gold_chest_red",        "main"},
+	{"side", "technic:gold_chest_violet",     "main"},
+	{"side", "technic:gold_chest_white",      "main"},
+	{"side", "technic:gold_chest_yellow",     "main"},
+
+	{"bottom", "technic:gold_chest_black",      "main"},
+	{"bottom", "technic:gold_chest_blue",       "main"},
+	{"bottom", "technic:gold_chest_brown",      "main"},
+	{"bottom", "technic:gold_chest_cyan",       "main"},
+	{"bottom", "technic:gold_chest_dark_green", "main"},
+	{"bottom", "technic:gold_chest_dark_grey",  "main"},
+	{"bottom", "technic:gold_chest_green",      "main"},
+	{"bottom", "technic:gold_chest_grey",       "main"},
+	{"bottom", "technic:gold_chest_magenta",    "main"},
+	{"bottom", "technic:gold_chest_orange",     "main"},
+	{"bottom", "technic:gold_chest_pink",       "main"},
+	{"bottom", "technic:gold_chest_red",        "main"},
+	{"bottom", "technic:gold_chest_violet",     "main"},
+	{"bottom", "technic:gold_chest_white",      "main"},
+	{"bottom", "technic:gold_chest_yellow",     "main"},
+
+	{"void", "technic:gold_chest_black",      "main"},
+	{"void", "technic:gold_chest_blue",       "main"},
+	{"void", "technic:gold_chest_brown",      "main"},
+	{"void", "technic:gold_chest_cyan",       "main"},
+	{"void", "technic:gold_chest_dark_green", "main"},
+	{"void", "technic:gold_chest_dark_grey",  "main"},
+	{"void", "technic:gold_chest_green",      "main"},
+	{"void", "technic:gold_chest_grey",       "main"},
+	{"void", "technic:gold_chest_magenta",    "main"},
+	{"void", "technic:gold_chest_orange",     "main"},
+	{"void", "technic:gold_chest_pink",       "main"},
+	{"void", "technic:gold_chest_red",        "main"},
+	{"void", "technic:gold_chest_violet",     "main"},
+	{"void", "technic:gold_chest_white",      "main"},
+	{"void", "technic:gold_chest_yellow",     "main"},
+
+	-- locked:
+	{"top", "technic:copper_locked_chest",  "main"},
+	{"top", "technic:iron_locked_chest",    "main"},
+	{"top", "technic:silver_locked_chest",  "main"},
+	{"top", "technic:gold_locked_chest",    "main"},
+	{"top", "technic:mithril_locked_chest", "main"},
+
 	{"side", "technic:copper_locked_chest",  "main"},
 	{"side", "technic:iron_locked_chest",    "main"},
 	{"side", "technic:silver_locked_chest",  "main"},
@@ -75,6 +158,77 @@ hopper:add_container({
 	{"void", "technic:silver_locked_chest",  "main"},
 	{"void", "technic:gold_locked_chest",    "main"},
 	{"void", "technic:mithril_locked_chest", "main"},
+
+	-- locked colored:
+	{"top", "technic:gold_locked_chest_black",      "main"},
+	{"top", "technic:gold_locked_chest_blue",       "main"},
+	{"top", "technic:gold_locked_chest_brown",      "main"},
+	{"top", "technic:gold_locked_chest_cyan",       "main"},
+	{"top", "technic:gold_locked_chest_dark_green", "main"},
+	{"top", "technic:gold_locked_chest_dark_grey",  "main"},
+	{"top", "technic:gold_locked_chest_green",      "main"},
+	{"top", "technic:gold_locked_chest_grey",       "main"},
+	{"top", "technic:gold_locked_chest_magenta",    "main"},
+	{"top", "technic:gold_locked_chest_orange",     "main"},
+	{"top", "technic:gold_locked_chest_pink",       "main"},
+	{"top", "technic:gold_locked_chest_red",        "main"},
+	{"top", "technic:gold_locked_chest_violet",     "main"},
+	{"top", "technic:gold_locked_chest_white",      "main"},
+	{"top", "technic:gold_locked_chest_yellow",     "main"},
+
+	{"side", "technic:gold_locked_chest_black",      "main"},
+	{"side", "technic:gold_locked_chest_blue",       "main"},
+	{"side", "technic:gold_locked_chest_brown",      "main"},
+	{"side", "technic:gold_locked_chest_cyan",       "main"},
+	{"side", "technic:gold_locked_chest_dark_green", "main"},
+	{"side", "technic:gold_locked_chest_dark_grey",  "main"},
+	{"side", "technic:gold_locked_chest_green",      "main"},
+	{"side", "technic:gold_locked_chest_grey",       "main"},
+	{"side", "technic:gold_locked_chest_magenta",    "main"},
+	{"side", "technic:gold_locked_chest_orange",     "main"},
+	{"side", "technic:gold_locked_chest_pink",       "main"},
+	{"side", "technic:gold_locked_chest_red",        "main"},
+	{"side", "technic:gold_locked_chest_violet",     "main"},
+	{"side", "technic:gold_locked_chest_white",      "main"},
+	{"side", "technic:gold_locked_chest_yellow",     "main"},
+
+	{"bottom", "technic:gold_locked_chest_black",      "main"},
+	{"bottom", "technic:gold_locked_chest_blue",       "main"},
+	{"bottom", "technic:gold_locked_chest_brown",      "main"},
+	{"bottom", "technic:gold_locked_chest_cyan",       "main"},
+	{"bottom", "technic:gold_locked_chest_dark_green", "main"},
+	{"bottom", "technic:gold_locked_chest_dark_grey",  "main"},
+	{"bottom", "technic:gold_locked_chest_green",      "main"},
+	{"bottom", "technic:gold_locked_chest_grey",       "main"},
+	{"bottom", "technic:gold_locked_chest_magenta",    "main"},
+	{"bottom", "technic:gold_locked_chest_orange",     "main"},
+	{"bottom", "technic:gold_locked_chest_pink",       "main"},
+	{"bottom", "technic:gold_locked_chest_red",        "main"},
+	{"bottom", "technic:gold_locked_chest_violet",     "main"},
+	{"bottom", "technic:gold_locked_chest_white",      "main"},
+	{"bottom", "technic:gold_locked_chest_yellow",     "main"},
+
+	{"void", "technic:gold_locked_chest_black",      "main"},
+	{"void", "technic:gold_locked_chest_blue",       "main"},
+	{"void", "technic:gold_locked_chest_brown",      "main"},
+	{"void", "technic:gold_locked_chest_cyan",       "main"},
+	{"void", "technic:gold_locked_chest_dark_green", "main"},
+	{"void", "technic:gold_locked_chest_dark_grey",  "main"},
+	{"void", "technic:gold_locked_chest_green",      "main"},
+	{"void", "technic:gold_locked_chest_grey",       "main"},
+	{"void", "technic:gold_locked_chest_magenta",    "main"},
+	{"void", "technic:gold_locked_chest_orange",     "main"},
+	{"void", "technic:gold_locked_chest_pink",       "main"},
+	{"void", "technic:gold_locked_chest_red",        "main"},
+	{"void", "technic:gold_locked_chest_violet",     "main"},
+	{"void", "technic:gold_locked_chest_white",      "main"},
+	{"void", "technic:gold_locked_chest_yellow",     "main"},
+
+	-- ### castle ###
+	{"void", "castle:ironbound_chest", "main"},
+	{"bottom", "castle:ironbound_chest", "main"},
+	{"side", "castle:ironbound_chest", "main"},
+	{"top", "castle:ironbound_chest", "main"},
 
 	-- ### workbench ###
 	{"void", "workbench:workbench", "src"},
@@ -125,4 +279,10 @@ hopper:add_container({
 	{"bottom", "lord_money:shop", "stock"},
 	{"side", "lord_money:shop", "stock"},
 	{"top", "lord_money:shop", "customers_gave"},
+
+	-- ### lord_mail ###
+	{"void", "lord_mail:mail_chest", "main"},
+	{"bottom", "lord_mail:mail_chest", "main"},
+	{"side", "lord_mail:mail_chest", "main"},
+	{"top", "lord_mail:mail_chest", "main"},
 })
