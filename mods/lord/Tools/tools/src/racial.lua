d222878ae9d33f53c339db35a0b120424904ab1c
lord-papirus <iotest@onenavigation.net>
2025-06-21 22:26:22 +0300

    Racial weapons: Added extented description to racial weapon. Closes #2228

--- mods/lord/Tools/tools/src/racial.lua
+++ mods/lord/Tools/tools/src/racial.lua
@@ -1,8 +1,16 @@
 local S = minetest.get_mod_translator()
-
+local colorize = minetest.colorize
+local violet = '#87d'
+local grey = '#aaa'
 
 minetest.register_tool('tools:sword_elven', {
-	description       = S('Elven Sword'),
+	description       =
+		colorize(violet, S('"Andúril"'))..' '..colorize(grey, S('("Narsil")'))..'\n'..'\n'..
+		colorize(grey,
+			S('Reforged from the shards of Narsil.')..'\n'..
+			S('The song of its blade is heard only by the dead.')..'\n'..
+			S('Fatal to those who broke their oath and found no rest.')
+		),
 	inventory_image   = 'tools_sword_elven.png',
 	tool_capabilities = {
 		max_drop_level      = 2,
@@ -13,7 +21,14 @@ minetest.register_tool('tools:sword_elven', {
 	groups            = { bronze_item = 1, forbidden = 1 },
 })
 minetest.register_tool('tools:sword_orcish', {
-	description       = S('Morgul Blade'),
+	description       =
+		colorize(violet, S('"Morgul Blade"'))..'\n'..'\n'..
+		colorize(grey,
+			S('Forged in the terror of Morgul,')..'\n'..
+			S('its edge steeped in the ancient poison of the Black Speech.')..'\n'..
+			S('With its wound comes cold and death beyond healing.')..'\n'..
+			S('Its wounds do not close – they only kill, slowly.')
+		),
 	inventory_image   = 'tools_sword_orcish.png',
 	tool_capabilities = {
 		max_drop_level      = 2,
@@ -24,7 +39,13 @@ minetest.register_tool('tools:sword_orcish', {
 	groups            = { steel_item = 1, forbidden = 1 },
 })
 minetest.register_tool('tools:battleaxe_dwarven', {
-	description       = S('Dwarven Battleaxe'),
+	description       =
+		colorize(violet, S('"Durin’s Axe"'))..'\n'..'\n'..
+		colorize(grey,
+				S('An axe wrought in the depths of Khazad-dûm,')..'\n'..
+				S('its weight shatters even stone gates.')..'\n'..
+				S('Its strike is like a mountain’s collapse – swift and inevitable.')
+			),
 	inventory_image   = 'tools_battleaxe_dwarf.png',
 	tool_capabilities = {
 		max_drop_level      = 1,
@@ -38,7 +59,12 @@ minetest.register_tool('tools:battleaxe_dwarven', {
 	groups            = { steel_item = 1, forbidden = 1 },
 })
 minetest.register_tool('tools:sword_human', {
-	description       = S('Human Sword'),
+	description       =
+		colorize(violet, S('"Flame of Númenor"'))..'\n'..'\n'..
+		colorize(grey,
+			S('Forged in imitation of the legendary weapons of the West.')..'\n'..
+			S('Its blade burns with a heat that scars even stone.')
+		),
 	inventory_image   = 'tools_sword_human.png',
 	tool_capabilities = {
 		max_drop_level      = 2,
@@ -49,7 +75,14 @@ minetest.register_tool('tools:sword_human', {
 	groups            = { mithril_item = 1, forbidden = 1 },
 })
 minetest.register_tool('tools:dagger_hobbit', {
-	description         = S('Hobbit Dagger'),
+	description         =
+		colorize(violet, S('"Took’s Blade"'))..'\n'..'\n'..
+		colorize(grey,
+			S('Light as a feather and swift as a squirrel.')..'\n'..
+			S('For centuries, hobbits guarded it as an heirloom.')..'\n'..
+			S('In deft hands, it is as deadly as a warrior’s sword.')..'\n'..
+			S('Those who laugh at it die in surprise.')
+		),
 	inventory_image     = 'tools_dagger_hobbit.png',
 	range               = 3,
 	tool_capabilities = {
