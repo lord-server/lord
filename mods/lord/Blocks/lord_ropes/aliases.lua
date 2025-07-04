54212a6a5b0b7292e132e73b2ab4c776021670e0
blanka kato <112077355+blanka-kato@users.noreply.github.com>
2025-07-05 02:00:33 +0500

    LG-1910 Dwarven ropes moved to `lord-ropes`
    Перенёс гномьи верёвки из мода `castle` в мод `lord_ropes`

---
+++ mods/lord/Blocks/lord_ropes/aliases.lua
@@ -0,0 +1,5 @@
+-- @tags: legacy
+
+minetest.register_alias('castle:ropes', 'lord_ropes:dwarven_ropes')
+minetest.register_alias('castle:ropebox', 'lord_ropes:dwarven_ropebox')
+minetest.register_alias('castle:box_rope', 'lord_ropes:box_rope')
---
+++ mods/lord/Blocks/lord_ropes/src/ropes/dwarven.lua
@@ -0,0 +1,136 @@
+
+local S = minetest.get_mod_translator()
+
+local function register_dwarven()
+	minetest.register_node('lord_ropes:dwarven_ropes',{
+		description = S('Dwarven ropes'),
+		drawtype = 'nodebox',
+		sunlight_propagates = true,
+		tiles = {'lord_ropes_dwarven_ropes.png'},
+		use_texture_alpha = 'clip',
+		groups = {choppy=3,snappy=3,oddly_breakable_by_hand=3,flammable=1},
+		paramtype = 'light',
+		climbable = true,
+		walkable = false,
+		node_box = {
+			type = 'fixed',
+			fixed = {
+				{-1/16, -8/16, -1/16, 1/16, 8/16, 1/16},
+			},
+		},
+		selection_box = {
+			type = 'fixed',
+			fixed = {
+				{-1/16, -8/16, -1/16, 1/16, 8/16, 1/16},
+			},
+		},
+		sounds = default.node_sound_defaults(),
+	})
+
+	minetest.register_craft({
+		output = 'lord_ropes:dwarven_ropes',
+		recipe = {
+			{'farming:string'},
+			{'farming:string'},
+			{'farming:string'},
+		}
+	})
+
+	minetest.register_node('lord_ropes:dwarven_box_rope', {
+		drawtype = 'nodebox',
+		paramtype = 'light',
+		sunlight_propagates = true,
+		tiles = {'lord_ropes_dwarven_ropes.png'},
+		use_texture_alpha = 'clip',
+		groups = {not_in_creative_inventory=1},
+		climbable = true,
+		walkable = false,
+		diggable = false,
+		node_box = {
+			type = 'fixed',
+			fixed = {
+				{-1/16, -8/16, -1/16, 1/16, 8/16, 1/16},
+			},
+		},
+		selection_box = {
+			type = 'fixed',
+			fixed = {
+				{-1/16, -8/16, -1/16, 1/16, 8/16, 1/16},
+			},
+		},
+		sounds        = default.node_sound_wood_defaults(),
+		after_destruct = function(pos,oldnode)
+			local node = minetest.get_node({x=pos.x,y=pos.y-1,z=pos.z})
+			if node.name == 'lord_ropes:dwarven_box_rope' then
+				minetest.remove_node({x=pos.x,y=pos.y-1,z=pos.z})
+			end
+		end,
+	})
+
+	minetest.register_node('lord_ropes:dwarven_ropebox', {
+		description = S('Dwarven ropebox'),
+		drawtype = 'nodebox',
+		sunlight_propagates = true,
+		tiles = {'lord_ropes_dwarven_ropebox_top.png',
+				'lord_ropes_dwarven_ropebox_top.png',
+				'lord_ropes_dwarven_ropebox_side_1.png',
+				'lord_ropes_dwarven_ropebox_side_1.png',
+				'lord_ropes_dwarven_ropebox_side_2.png',
+				'lord_ropes_dwarven_ropebox_side_2.png'},
+		use_texture_alpha = 'clip',
+		paramtype = 'light',
+		paramtype2 = 'facedir',
+		groups = {choppy=3},
+		node_box = {
+			type = 'fixed',
+			fixed = {
+				{-2/16, -2/16, -4/16, 2/16, 2/16, 4/16},
+				{-2/16, -4/16, -2/16, 2/16, 4/16, 2/16},
+				{-2/16, -3/16, -3/16, 2/16, 3/16, 3/16},
+				{-3/16, -2/16, -2/16, -2/16, 8/16, 2/16},
+				{2/16, -2/16, -2/16, 3/16, 8/16, 2/16},
+				{-1/16, -8/16, -1/16, 1/16, -4/16, 1/16},
+			},
+		},
+		sounds = default.node_sound_wood_defaults(),
+		after_destruct = function(pos,oldnode)
+			local node = minetest.get_node({x=pos.x,y=pos.y-1,z=pos.z})
+			if node.name == 'lord_ropes:dwarven_box_rope' then
+				minetest.remove_node({x=pos.x,y=pos.y-1,z=pos.z})
+			end
+		end,
+	})
+
+	minetest.register_abm({
+		nodenames = {'lord_ropes:dwarven_ropebox'},
+		interval = 1,
+		chance = 1,
+		action = function(pos, node)
+		if minetest.get_node({x=pos.x,y=pos.y-1,z=pos.z}).name ~= 'air'  then return end
+				minetest.add_node({x=pos.x,y=pos.y-1,z=pos.z}, {name='lord_ropes:dwarven_box_rope'})
+		end
+	})
+
+	minetest.register_abm({
+		nodenames = {'lord_ropes:dwarven_box_rope'},
+		interval = 1,
+		chance = 1,
+		action = function(pos, node)
+		if minetest.get_node({x=pos.x,y=pos.y-1,z=pos.z}).name ~= 'air'  then return end
+				minetest.add_node({x=pos.x,y=pos.y-1,z=pos.z}, {name='lord_ropes:dwarven_box_rope'})
+		end
+	})
+
+	minetest.register_craft({
+		output = 'lord_ropes:dwarven_ropebox',
+		recipe = {
+			{'default:wood'},
+			{'lord_ropes:dwarven_ropes'},
+		}
+	})
+end
+
+
+return {
+	register = register_dwarven,
+}
