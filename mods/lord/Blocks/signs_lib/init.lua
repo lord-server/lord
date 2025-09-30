31723dca7d9991fc7bb03e0bd82a8015d74059ea
alek13 <alek13.me@gmail.com>
2025-09-30 04:54:30 +0300

    Fix place sign on blocks with `.on_rightclick()` handler. Closes #2310

--- mods/lord/Blocks/signs_lib/init.lua
+++ mods/lord/Blocks/signs_lib/init.lua
@@ -551,7 +551,7 @@ function signs_lib.determine_sign_type(itemstack, placer, pointed_thing, locked)
 	local node=minetest.get_node(pointed_thing.under)
 
 	if minetest.registered_nodes[node.name] and minetest.registered_nodes[node.name].on_rightclick then
-		return minetest.registered_nodes[node.name].on_rightclick(pointed_thing.under, node, placer, itemstack)
+		return minetest.registered_nodes[node.name].on_rightclick(pointed_thing.under, node, placer, itemstack, pointed_thing)
 	else
 		local above = pointed_thing.above
 		local under = pointed_thing.under
