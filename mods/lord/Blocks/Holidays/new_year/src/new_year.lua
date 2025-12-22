2b5f6aca82f5dfda3a011762769b069d82c29c32
blanka kato <112077355+blanka-kato@users.noreply.github.com>
2025-12-23 00:44:03 +0500

    Added event cloak for NY 2026. LG-2040

--- mods/lord/Blocks/Holidays/new_year/src/new_year.lua
+++ mods/lord/Blocks/Holidays/new_year/src/new_year.lua
@@ -1,10 +1,12 @@
+local event_cloak = require('new_year.event_cloak')
 
 
 new_year = {} -- luacheck: ignore unused global variable new_year
 
 
 return {
---- @param mod minetest.Mod
+	--- @param mod minetest.Mod
 	init = function(mod)
+		event_cloak.register()
 	end,
 }
