eedc1b292c34a1fd4c12ed127f52b199a76f89b1
alek13 <alek13.me@gmail.com>
2026-01-28 04:18:46 +0300

    Gen.Library: `Voxrame.map.Cuboid` draft as class. LG-2059

--- mods/lord/Core/map/src/map/Cuboid.lua
+++ mods/lord/Core/map/src/map/Cuboid.lua
@@ -4,5 +4,28 @@
 --- @field public to   PositionVector
 local Cuboid = {}
 
+-- TODO:
+-- also needed to create cuboids by `Cuboid:new`, and fix creation of child classes
+
+-- --- @param from Position
+-- --- @param to   Position
+-- --- @return self
+-- function Cuboid:new(from, to)
+-- 	from, to = vector.sort(from, to)
+
+-- 	local class = self
+-- 	self = {}
+-- 	self.from = from
+-- 	self.to   = to
+
+-- 	return setmetatable(self, { __index = class })
+-- end
+
+-- --- @param position Position
+-- --- @return boolean
+-- function Cuboid:contains(position)
+-- 	return vector.in_area(position, self.from, self.to)
+-- end
+
 
 return Cuboid
