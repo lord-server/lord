01e6c959be77160b00d366ba4ed6ec96d315393b
alek13 <alek13.me@gmail.com>
2026-02-14 06:27:01 +0300

    `builtin_ext`: `vector`: ability to pass `n` into "side"-methods; add `:at(side, n)`

--- mods/lord/Core/builtin_ext/src/vector.lua
+++ mods/lord/Core/builtin_ext/src/vector.lua
@@ -1,30 +1,65 @@
 
---- @return vector
-function vector:above()
-	return self:offset(0, 1, 0)
+--- @enum (key) Position.OffsetSide
+local offset_method = {
+	ceiling = 'above',
+	floor   = 'under',
+	north   = 'at_north',
+	south   = 'at_south',
+	east    = 'at_east',
+	west    = 'at_west',
+	-- additional "aliases"
+	up      = 'above',
+	down    = 'under',
+	above   = 'above',
+	under   = 'under',
+	below   = 'under',
+	['x+']  = 'at_east',
+	['x-']  = 'at_west',
+	['y+']  = 'above',
+	['y-']  = 'under',
+	['z+']  = 'at_north',
+	['z-']  = 'at_south',
+}
+
+--- @param n? integer
+--- @return self
+function vector:above(n)
+	return self:offset(0, n or 1, 0)
 end
 
---- @return vector
-function vector:under()
-	return self:offset(0, -1, 0)
+--- @param n? integer
+--- @return self
+function vector:under(n)
+	return self:offset(0, -(n or 1), 0)
 end
 
---- @return vector
-function vector:at_north()
-	return self:offset(0, 0, 1)
+--- @param n? integer
+--- @return self
+function vector:at_north(n)
+	return self:offset(0, 0, n or 1)
 end
 
---- @return vector
-function vector:at_south()
-	return self:offset(0, 0, -1)
+--- @param n? integer
+--- @return self
+function vector:at_south(n)
+	return self:offset(0, 0, -(n or 1))
 end
 
---- @return vector
-function vector:at_east()
-	return self:offset(1, 0, 0)
+--- @param n? integer
+--- @return self
+function vector:at_east(n)
+	return self:offset(n or 1, 0, 0)
 end
 
---- @return vector
-function vector:at_west()
-	return self:offset(-1, 0, 0)
+--- @param n? integer
+--- @return self
+function vector:at_west(n)
+	return self:offset(-(n or 1), 0, 0)
+end
+
+--- @param side Position.OffsetSide
+--- @param n?   integer
+--- @return self
+function vector:at(side, n)
+    return self[offset_method[side]](self, n) --- @diagnostic disable-line: param-type-mismatch
 end
