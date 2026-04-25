498627e8402c5969c07f8219260fb8c9762e1000
Petus228666 <efimovstanislav382@gmail.com>
2026-04-25 18:13:44 +0300

    feature toggle UPD

--- mods/lord/Items/lord_armor/init.lua
+++ mods/lord/Items/lord_armor/init.lua
@@ -1,6 +1,10 @@
-
-
 minetest.mod(function(mod)
-	require("armor")
-	require("shield")
+if mod.settings:get_bool('toggle_dwarf_armor', false) then
+    -- отменяем регистрацию старой брони
+		return
+     -- регистрируем новую броню
+	end
+
+    require("armor")
+    require("shield")
 end)
