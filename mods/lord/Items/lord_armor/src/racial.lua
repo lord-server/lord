38ef4bf1d87920332ee3bd5f64d5b5bc886c8fea
Petus228666 <efimovstanislav382@gmail.com>
2026-04-27 17:52:16 +0300

    LGTM Fix 3.0

--- mods/lord/Items/lord_armor/src/racial.lua
+++ mods/lord/Items/lord_armor/src/racial.lua
@@ -6,11 +6,11 @@ core.register_tool('lord_armor:dwarf_helmet', {
 	description 		= S('Dwarf Helmet'),
 	inventory_image 	= 'lord_armor_dwarf_inv_helmet.png',
 	groups 				= {
-		armor_head				=1, 
-		defense_fleshy			=15, 
-		damage_avoid_chance		=6, 
-		armor_use				=50, 
-		physics_speed			=-0.3, 
+		armor_head				=1,
+		defense_fleshy			=15,
+		damage_avoid_chance		=6,
+		armor_use				=50,
+		physics_speed			=-0.3,
 		mithril_item			=1
 		},
 	wear 				= 0,
@@ -19,39 +19,39 @@ core.register_tool('lord_armor:dwarf_chestplate', {
 	description 		= S('Dwarf Chestplate'),
 	inventory_image 	= 'lord_armor_dwarf_inv_chestplate.png',
 	groups 				= {
-		armor_torso				=1, 
-		defense_fleshy			=20, 
-		damage_avoid_chance		=10, 
-		armor_use				=50, 
-		physics_speed			=-0.07, 
+		armor_torso				=1,
+		defense_fleshy			=20,
+		damage_avoid_chance		=10,
+		armor_use				=50,
+		physics_speed			=-0.07,
 		mithril_item			=1
 		},
 	wear 				= 0,
 })
 core.register_tool('lord_armor:dwarf_leggings', {
-	description 		= S('Dwarf Leggings'),
-	inventory_image 	= 'lord_armor_dwarf_inv_leggings.png',
-	groups 				= {
-		armor_legs				=1, 
-		defense_fleshy			=10, 
-		damage_avoid_chance		=8, 
-		armor_use				=50, 
-		physics_speed			=-0.05, 
+	description 	= S('Dwarf Leggings'),
+	inventory_image = 'lord_armor_dwarf_inv_leggings.png',
+	groups 			= {
+		armor_legs				=1,
+		defense_fleshy			=10,
+		damage_avoid_chance		=8,
+		armor_use				=50,
+		physics_speed			=-0.05,
 		mithril_item			=1
 		},
-	wear 				= 0,
+	wear 			 = 0,
 })
 core.register_tool('lord_armor:dwarf_boots', {
 	description 		= S('Dwarf Boots'),
 	inventory_image 	= 'lord_armor_dwarf_inv_boots.png',
 	groups 				= {
-			armor_feet			=1,
-		 	defense_fleshy		=7,
-		  	damage_avoid_chance =6,
-		   	armor_use			=50,
-		    physics_speed		=-0.02,
-			mithril_item		=1
-			},
+		armor_feet				=1,
+		defense_fleshy			=7,
+		damage_avoid_chance 	=6,
+		armor_use				=50,
+		physics_speed			=-0.02,
+		mithril_item			=1
+		},
 	wear 				= 0,
 })
 
@@ -59,13 +59,13 @@ core.register_tool('lord_armor:dwarf_shield', {
 	description     	= S('Dwarf Shield'),
 	inventory_image 	= 'lord_armor_dwarf_shield_inv.png',
 	groups          	= {
-		armor_shield 			= 1, 
-		defense_fleshy 			= 21, 
-		damage_avoid_chance 	= 15, 
+		armor_shield 			= 1,
+		defense_fleshy 			= 21,
+		damage_avoid_chance 	= 15,
 		armor_use 				= 100,
-		physics_speed 			= -0.1, 
-		physics_sneak 			= -1, 
-		forbidden 				= 1, 
+		physics_speed 			= -0.1,
+		physics_sneak 			= -1,
+		forbidden 				= 1,
 		mithril_item 			= 1
 	},
 	wear            	= 0,
@@ -121,6 +121,6 @@ core.register_craft({
 	recipe   = {
 		{ 'lottores:mithril_ingot', 'default:mese_crystal',  'lottores:mithril_ingot' },
 		{ 'lottores:mithril_ingot', 'lottores:galvorn_ingot','lottores:mithril_ingot' },
-		{ '',                       'lottores:galvorn_ingot',          '' }
+		{ '',                       'lottores:galvorn_ingot',''					  	  }
 	    }
    })
