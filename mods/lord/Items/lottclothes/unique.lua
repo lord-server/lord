5e08e48f7a0f324f9b367ab0ff8659785528f0f0
Petus228666 <annefimova2018@gmail.com>
2026-06-03 02:03:16 +0300

    fix [200~

--- mods/lord/Items/lottclothes/unique.lua
+++ mods/lord/Items/lottclothes/unique.lua
@@ -1,7 +1,7 @@
-local S = minetest.get_mod_translator()    
+local S = minetest.get_mod_translator()
 local event_cloak_message = minetest.colorize('#FFAA00', S('Cloak "Guardian of Minas Tirith"'))
 local seasonable_collection_message = S('unique collection')
-    
+
 core.register_tool(':lord_clothes:uniq_cloak_guardian_of_minas_tirith', {
 		description     = event_cloak_message .. '\n' .. seasonable_collection_message ,
 		inventory_image = 'lord_clothes_uniq_cloak_guardian_of_minas_tirith_inv.png',
