bd4934ca2d0b1cf3a813dbcf01eb4bc1e5dfefa0
Alexander Chibrikin <alek13.me@gmail.com>
2026-06-16 23:02:23 +0300

    Unique: add describtion, change colors for Cloak Guard of MT. LG-2116.

--- mods/lord/Tools/lottclothes/unique.lua
+++ mods/lord/Tools/lottclothes/unique.lua
@@ -1,10 +1,23 @@
-local S = minetest.get_mod_translator()
-local event_cloak_message = minetest.colorize('#FFAA00', S('Cloak "Guardian of Minas Tirith"'))
-local seasonable_collection_message = S('unique collection')
+local S        = minetest.get_mod_translator()
+local colorize = minetest.colorize
+
+
+local legendry_color   = '#fa4'
+local phrase_color     = '#aaa'
+local collection_color = '#99c'
+
 
 core.register_tool(':lord_uniq_clothes_mt_guardian_cloak', {
-		description     = event_cloak_message .. '\n' .. seasonable_collection_message ,
+		description     =
+			colorize(legendry_color, S('Cloak "Guardian of Minas Tirith"')) .. '\n' ..
+			colorize(phrase_color,
+				S('When the Guardian squares his shoulders,') .. '\n' ..
+				S('    the cloak spreads like the wings of an eagle —') .. '\n' ..
+				S('    a herald of the coming victory.')
+			) .. '\n\n' ..
+			colorize(collection_color, S('Unique collection: "Guardian of Minas Tirith"'))
+		,
 		inventory_image = 'lord_uniq_clothes_mt_guardian_cloak_inv.png',
-		groups          = { clothes = 1, no_preview = 1, clothes_cloak = 1 },
+		groups          = { clothes = 1, no_preview = 1, clothes_cloak = 1, physics_speed = 0.02 },
 		wear            = 0,
 })
