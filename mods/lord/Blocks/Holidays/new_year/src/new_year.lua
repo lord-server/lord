03be533f849735e5cfa7deb4ac5bccaa867affa5
alek13 <alek13.me@gmail.com>
2026-08-24 19:40:40 +0300

    `new_year`: translations: fix russian `c`, some improvements. LG-2188.

--- mods/lord/Blocks/Holidays/new_year/src/new_year.lua
+++ mods/lord/Blocks/Holidays/new_year/src/new_year.lua
@@ -1,11 +1,8 @@
 local event_cloak = require('new_year.event_cloak')
 
 
-new_year = {} -- luacheck: ignore unused global variable new_year
-
-
 return {
-	--- @param mod minetest.Mod
+	--- @param mod core.Mod
 	init = function(mod)
 		event_cloak.register()
 	end,
--- mods/lord/Blocks/Holidays/new_year/src/new_year/event_cloak.lua
+++ mods/lord/Blocks/Holidays/new_year/src/new_year/event_cloak.lua
@@ -1,35 +1,40 @@
-local S = minetest.get_mod_translator()
+local S        = minetest.get_mod_translator()
+local spec     = core.formspec
+local colorize = minetest.colorize
 
 
 local function register_new_year_event_cloaks()
-	local event_cloak_message = minetest.colorize('#B380FF', S('Cloak of the 2026'))
-	local exchange_it_message = S('One of the server\'s mighty guardians') .. '\n' ..
-								S('Whose power to send players into oblivion') .. '\n' ..
-								S('And turn back time') .. '\n' ..
-								S('Whose hands never tire of punishing with the banhammer') .. '\n' ..
-								S('. . .') .. '\n' ..
-								S('In rare moments of peace') .. '\n' ..
-								S('He sewed this wonderful cloak for You') .. '\n' ..
-								S('In honor of the New Year 2026!')
-	local seasonable_collection_message = S('New Year сollection')
+	local event_cloak_message = colorize('#B380FF', S('Cloak of the 2026'))
+	local exchange_it_message = ''
+		.. S('One of the server\'s mighty guardians') .. '\n'
+		.. S('Whose power to send players into oblivion') .. '\n'
+		.. S('And turn back time') .. '\n'
+		.. S('Whose hands never tire of punishing with the banhammer') .. '\n'
+		.. S('. . .') .. '\n'
+		.. S('In rare moments of peace') .. '\n'
+		.. S('He sewed this wonderful cloak for You') .. '\n'
+		.. S('In honor of the New Year 2026!')
 
 	minetest.register_tool('new_year:cloak_2026', {
-		description     = event_cloak_message .. '\n' .. seasonable_collection_message,
+		description     = event_cloak_message .. '\n' .. S('New Year collection'),
 		inventory_image = 'new_year_cloak_2026_inv.png',
 		groups          = { clothes = 1, no_preview = 1, clothes_cloak = 1 },
 		wear            = 0,
 		on_use          = function(itemstack, user)
-
-			local player_name = user:get_player_name()
-
-			if not user or not player_name or player_name == '' then
+			if not user or not user:is_player() then
 				return itemstack
 			end
-			local formspec = 'formspec_version[4]' ..
-				'size[8,7]' ..
-				'label[0.5,0.6;' .. event_cloak_message .. ']' ..
-				'label[0.5,1.2;' .. exchange_it_message .. ']' ..
-				'button_exit[2.5,5.7;3,0.8;close;OK]'
+			--- @cast user Player -- we checked is_player above
+			local player_name = user:get_player_name()
+
+			if not player_name or player_name == '' then
+				return itemstack
+			end
+			local formspec = spec.formspec_version(4)
+				.. spec.size(8, 7)
+				.. spec.label(0.5, 0.6, event_cloak_message)
+				.. spec.label(0.5, 1.2, exchange_it_message)
+				.. spec.button_exit(2.5, 5.7, 3, 0.8, 'close', 'OK')
 
 			minetest.show_formspec(player_name, 'event_cloak_message', formspec)
 
