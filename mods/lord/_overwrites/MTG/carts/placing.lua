36acdc9f8cd609a8578df3636f718b551918deba
alek13 <alek13.me@gmail.com>
2026-08-24 20:28:08 +0300

    Distribution: CDB: `lord_overwrites_mtg_carts` add localization; use `core.`; use `'`. LG-2188.

--- mods/lord/_overwrites/MTG/carts/placing.lua
+++ mods/lord/_overwrites/MTG/carts/placing.lua
@@ -1,25 +1,28 @@
+local S = core.get_mod_translator()
+
+
 local function is_accelerator(p)
-	local nn = minetest.get_node(p).name
-	return minetest.get_item_group(nn, "accelerator") ~= 0
+	local nn = core.get_node(p).name
+	return core.get_item_group(nn, 'accelerator') ~= 0
 end
 
 local function validate_powerrail_place(pos, placer, itemstack, pointed_thing)
-	if minetest.is_protected(pos, placer:get_player_name()) then
+	if core.is_protected(pos, placer:get_player_name()) then
 		return true
 	end
 
 	local accel_pos = {x = pos.x, y = pos.y - 1, z = pos.z}
 	if not is_accelerator(accel_pos) then
-		minetest.chat_send_player(placer:get_player_name(), "Ускоряющий рельс можно ставить только на паровой механизм")
-		minetest.set_node(pos, { name = "air" })
+		core.chat_send_player(placer:get_player_name(), S('Powerrail can only be placed on steam mechanism'))
+		core.set_node(pos, { name = 'air' })
 		return true
 	end
 end
 
-minetest.override_item("carts:powerrail", {
+core.override_item('carts:powerrail', {
 	tiles = {
-		"carts_rail_straight_pwr.png", "carts_rail_curved_pwr.png",
-		"carts_rail_t_junction_pwr.png", "carts_rail_crossing_pwr.png"
+		'carts_rail_straight_pwr.png', 'carts_rail_curved_pwr.png',
+		'carts_rail_t_junction_pwr.png', 'carts_rail_crossing_pwr.png'
 	},
 	after_place_node = validate_powerrail_place,
 })
