1270ac2b4ff4a818395774b3f3672f0f92b763b5
alek13 <alek13.me@gmail.com>
2026-08-24 19:00:16 +0300

    `christmas:candy_cane`: translations: fix russian `c`, some improvements. LG-2188.

--- mods/lord/Blocks/christmas/candy_cane.lua
+++ mods/lord/Blocks/christmas/candy_cane.lua
@@ -1,27 +1,30 @@
-local S = minetest.get_mod_translator()
+local S        = core.get_mod_translator()
+local spec     = core.formspec
+local colorize = core.colorize
 
 
 local function register_candy_cane()
-	local candy_cane_message            = minetest.colorize('#B380FF', S('Candy Cane'))
-	local exchange_it_message           = S('Exchange it for gifts at the spawn store')
-	local seasonable_collection_message = minetest.colorize('#B380FF', S('Christmas сollection'))
+	local candy_cane_title = colorize('#B380FF', S('Candy Cane'))
 
-	minetest.register_craftitem('christmas:candy_cane', {
-		description     = candy_cane_message .. '\n' .. seasonable_collection_message,
+	core.register_craftitem('christmas:candy_cane', {
+		description     = candy_cane_title .. '\n' .. colorize('#B380FF', S('Christmas collection')),
 		inventory_image = 'christmas_candy_cane.png',
 		on_use          = function(itemstack, user)
-			local player_name = user:get_player_name()
-			if not user or not player_name or player_name == '' then
+			if not user then
+				return itemstack
+			end
+			local player_name = user--[[@as Player]]:get_player_name()
+			if not player_name or player_name == '' then
 				return itemstack
 			end
 
-			local formspec = 'formspec_version[4]' ..
-				'size[7,3]' ..
-				'label[0.5,0.6;' .. candy_cane_message .. ']' ..
-				'label[0.5,1.2;' .. exchange_it_message .. ']' ..
-				'button_exit[2,1.9;3,0.8;close;OK]'
+			local formspec = spec.formspec_version(4)
+				.. spec.size(7, 3)
+				.. spec.label(0.5, 0.6, candy_cane_title)
+				.. spec.label(0.5, 1.2, S('Exchange it for gifts at the spawn store'))
+				.. spec.button_exit(2, 1.9, 3, 0.8, 'close', 'OK')
 
-			minetest.show_formspec(player_name, 'candy_cane_message', formspec)
+			core.show_formspec(player_name, 'candy_cane_message', formspec)
 
 			return itemstack
 		end,
