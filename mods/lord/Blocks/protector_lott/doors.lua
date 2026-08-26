a0d793ed74674cbc0dc99b1244d4a8b78b9e1c08
Living Broscillograph <totservis@gmail.com>
2026-08-27 00:31:39 +0300

    Expand the list of protected doors. Closes #2404 (#2406)

--- mods/lord/Blocks/protector_lott/doors.lua
+++ mods/lord/Blocks/protector_lott/doors.lua
@@ -1,4 +1,4 @@
-local S = minetest.get_mod_translator()
+local S = core.get_mod_translator()
 
 -- Register Protected Doors
 
@@ -21,7 +21,7 @@ local function reg_prot_door(desc, name, door, mat, texture_i, texture_uv)
 			{door, "protector_lott:protect2"}
 		},
 		on_rightclick = function(pos, node, clicker)
-			if not minetest.is_protected(pos, clicker:get_player_name()) then
+			if not core.is_protected(pos, clicker:get_player_name()) then
 				doors.door_toggle(pos, node, clicker)
 			end
 		end,
@@ -91,6 +91,60 @@ reg_prot_door(
 	"lord_wooden_stuff_door_mallorn_uv.png"
 )
 
+reg_prot_door(
+	"Protected Cherry Door", "protector_lott:door_cherry", "lord_wooden_stuff:door_cherry", "wood",
+	"lord_wooden_stuff_door_cherry.png",
+	"lord_wooden_stuff_door_cherry_uv.png"
+)
+
+reg_prot_door(
+	"Protected Culumalda Door", "protector_lott:door_culumalda", "lord_wooden_stuff:door_culumalda", "wood",
+	"lord_wooden_stuff_door_culumalda.png",
+	"lord_wooden_stuff_door_culumalda_uv.png"
+)
+
+reg_prot_door(
+	"Protected Elm Door", "protector_lott:door_elm", "lord_wooden_stuff:door_elm", "wood",
+	"lord_wooden_stuff_door_elm.png",
+	"lord_wooden_stuff_door_elm_uv.png"
+)
+
+reg_prot_door(
+	"Protected Fir Door", "protector_lott:door_fir", "lord_wooden_stuff:door_fir", "wood",
+	"lord_wooden_stuff_door_fir.png",
+	"lord_wooden_stuff_door_fir_uv.png"
+)
+
+reg_prot_door(
+	"Protected Hardwood Door", "protector_lott:door_hardwood", "lord_wooden_stuff:door_hardwood", "wood",
+	"lord_wooden_stuff_door_hardwood.png",
+	"lord_wooden_stuff_door_hardwood_uv.png"
+)
+
+reg_prot_door(
+	"Protected Infected Door", "protector_lott:door_infected", "lord_wooden_stuff:door_infected", "wood",
+	"lord_wooden_stuff_door_infected.png",
+	"lord_wooden_stuff_door_infected_uv.png"
+)
+
+reg_prot_door(
+	"Protected Plum Door", "protector_lott:door_plum", "lord_wooden_stuff:door_plum", "wood",
+	"lord_wooden_stuff_door_plum.png",
+	"lord_wooden_stuff_door_plum_uv.png"
+)
+
+reg_prot_door(
+	"Protected White Door", "protector_lott:door_white", "lord_wooden_stuff:door_white", "wood",
+	"lord_wooden_stuff_door_white.png",
+	"lord_wooden_stuff_door_white_uv.png"
+)
+
+reg_prot_door(
+	"Protected Yavannamire Door", "protector_lott:door_yavannamire", "lord_wooden_stuff:door_yavannamire", "wood",
+	"lord_wooden_stuff_door_yavannamire.png",
+	"lord_wooden_stuff_door_yavannamire_uv.png"
+)
+
 -- from castle mod
 reg_prot_door(
 	"Protected Oak Door", "protector_lott:oak_door", "castle:oak_door", "wood",
@@ -102,4 +156,11 @@ reg_prot_door(
 	"Protected Jail Door", "protector_lott:jail_door", "castle:jail_door", "steel",
 	"castle_jail_door.png",
 	"castle_jail_door_uv.png"
+)
+
+-- from _minetest_game/xpanes mod
+reg_prot_door(
+	"Protected Steel Bar Door", "protector_lott:door_steel_bar", "xpanes:door_steel_bar", "steel",
+	"xpanes_item_steel_bar.png",
+	"xpanes_door_steel_bar.png"
 )
