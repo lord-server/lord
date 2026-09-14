4a76521ab603ed436b6276725736a2c6fde489a2
alek13 <alek13.me@gmail.com>
2026-09-14 19:41:40 +0300

    Smelter: actualize all recipes: remove old from furnace, add new for Smelter, change timing. LG-2205.

--- mods/lord/Blocks/ArtisanBenches/smelter/src/legacy.lua
+++ mods/lord/Blocks/ArtisanBenches/smelter/src/legacy.lua
@@ -1,24 +1,65 @@
 --удаляем рецепты с использованием печи
-local furnance_recipes = {
-	{ 'default:iron_lump', 'default:steel_ingot'},
-	{ 'group:iron_item', 'default:iron_ingot'},
-	{ 'group:bronze_item', 'default:iron_ingot'},
-	{ 'lottores:silver_lump', 'lottores:silver_ingot'},
-	{ 'group:silver_item', 'lottores:silver_ingot'},
-	{ 'default:gold_lump', 'default:gold_ingot'},
-	{ 'group:gold_item', 'default:gold_ingot'},
-	{ 'lottores:lead_lump', 'lottores:lead_ingot'},
-	{ 'group:lead_item', 'lottores:lead_ingot'},
-	{ 'lottores:mithril_lump', 'lottores:mithril_ingot'},
-	{ 'group:mithril_item', 'lottores:mithril_ingot'},
-	{ 'group:galvorn_item', 'lottores:galvorn_ingot'},
-	{ 'lottother:ringsilver_lump', 'lottother:ringsilver_ingot'},
+local furnace_recipes = {
+	-- steel ingot
+	{ 'default:iron_lump',         'default:steel_ingot'        }, -- default (MTG)
+	{ 'group:steel_item',          'default:steel_ingot'        }, -- lord_default
+	{ 'carts:rail',                'default:steel_ingot'        }, -- lord_overwrites_mtg_carts
+	{ 'carts:powerrail',           'default:steel_ingot'        }, -- lord_overwrites_mtg_carts
+	{ 'carts:brakerail',           'default:steel_ingot'        }, -- lord_overwrites_mtg_carts
+	{ 'vessels:steel_bottle',      'default:steel_ingot'        }, -- vessels (MTG)
+	-- bronze ingot
+	{ 'group:bronze_item',         'default:bronze_ingot'       }, -- lord_default
+	-- silver ingot
+	{ 'lottores:silver_lump',      'lottores:silver_ingot'      }, -- lottores
+	{ 'group:silver_item',         'lottores:silver_ingot'      }, -- lottores
+	-- gold ingot
+	{ 'default:gold_lump',         'default:gold_ingot'         }, -- default (MTG)
+	{ 'group:gold_item',           'default:gold_ingot'         }, -- lord_default
+	{ 'keys:key',                  'default:gold_ingot'         }, -- keys (MTG)
+	{ 'keys:skeleton_key',         'default:gold_ingot'         }, -- keys (MTG)
+	-- lead ingot
+	{ 'lottores:lead_lump',        'lottores:lead_ingot'        }, -- lottores
+	{ 'group:lead_item',           'lottores:lead_ingot'        }, -- lottores
+	-- galvorn ingot
+	{ 'group:galvorn_item',        'lottores:galvorn_ingot'     }, -- lottores
+	-- mithril ingot
+	{ 'lottores:mithril_lump',     'lottores:mithril_ingot'     }, -- lottores
+	{ 'group:mithril_item',        'lottores:mithril_ingot'     }, -- lottores
+	-- ringsilver ingot
+	{ 'lottother:ringsilver_lump', 'lottother:ringsilver_ingot' }, -- lottother
 }
 
-for _, row in ipairs(furnance_recipes) do
+for _, row in ipairs(furnace_recipes) do
 	core.clear_craft({
 		type   = 'cooking',
-		output = row[2],
 		recipe = row[1],
+	})
+end
+
+-- lord_overwrites_mtg_default
+core.clear_craft({
+	type   = 'shapeless',
+	recipe = { 'lottores:tin_ingot', 'default:copper_ingot' },
+})
+
+-- медь и олово в печке:
+--   переопределяем время переплавки с дефолтных 3 на 6,
+--   чтобы Плавильня (время 3) для них была быстрее и имела смысл
+local furnace_time_overrides = {
+	{ 'default:copper_lump', 'default:copper_ingot' },
+	{ 'group:copper_item',   'default:copper_ingot' },
+	{ 'lottores:tin_lump',   'lottores:tin_ingot'   },
+	{ 'group:tin_item',      'lottores:tin_ingot'   },
+}
+for _, row in ipairs(furnace_time_overrides) do
+	core.clear_craft({
+		type   = 'cooking',
+		recipe = row[1],
+	})
+	core.register_craft({
+		type     = 'cooking',
+		output   = row[2],
+		recipe   = row[1],
+		cooktime = 6,
 	})
 end
--- mods/lord/Blocks/ArtisanBenches/smelter/src/recipes.lua
+++ mods/lord/Blocks/ArtisanBenches/smelter/src/recipes.lua
@@ -1,30 +1,43 @@
 
 local smelter_recipes = {
-	-- level1
-	{ 'default:iron_lump'         , ''                          , 'default:steel_ingot'          , 6  },
-	{ 'default:iron_lump'         , 'default:iron_lump'         , 'default:steel_ingot 2'        , 12 },
-	{ 'group:steel_item'          , ''                          , 'default:steel_ingot'          , 6  },
-	{ 'lottores:tin_ingot'        , 'default:copper_ingot'      , 'default:bronze_ingot 2'       , 12 },
-	{ 'default:copper_ingot'      , 'lottores:tin_ingot'        , 'default:bronze_ingot 2'       , 12 },
-	{ 'group:bronze_item'         , ''                          , 'default:bronze_ingot'         , 6  },
-	-- level2
-	{ 'lottores:silver_lump'      , ''                          , 'lottores:silver_ingot'        , 6  },
-	{ 'lottores:silver_lump'      , 'lottores:silver_lump'      , 'lottores:silver_ingot 2'      , 12 },
-	{ 'group:silver_item'         , ''                          , 'lottores:silver_ingot'        , 6  },
-	{ 'default:gold_lump'         , ''                          , 'default:gold_ingot'           , 6  },
-	{ 'default:gold_lump'         , 'default:gold_lump'         , 'default:gold_ingot 2'         , 12 },
-	{ 'group:gold_item'           , ''                          , 'default:gold_ingot'           , 6  },
-	-- level3
-	{ 'lottores:lead_lump'        , ''                          , 'lottores:lead_ingot'          , 6  },
-	{ 'lottores:lead_lump'        , 'lottores:lead_lump'        , 'lottores:lead_ingot 2'        , 12 },
-	{ 'group:lead_item'           , ''                          , 'lottores:lead_ingot'          , 6  },
-	{ 'lottores:mithril_lump'     , ''                          , 'lottores:mithril_ingot'       , 6  },
-	{ 'lottores:mithril_lump'     , 'lottores:mithril_lump'     , 'lottores:mithril_ingot 2'     , 12 },
-	{ 'group:mithril_item'        , ''                          , 'lottores:mithril_ingot'       , 6  },
-	{ 'lottores:lead_lump'        , 'default:mese_crystal'      , 'lottores:galvorn_ingot'       , 18 },
-	{ 'group:galvorn_item'        , ''                          , 'lottores:galvorn_ingot'       , 18 },
-	{ 'lottother:ringsilver_lump' , ''                          , 'lottother:ringsilver_ingot'   , 35 },
-	{ 'lottother:ringsilver_lump' , 'lottother:ringsilver_lump' , 'lottother:ringsilver_ingot 2' , 70 },
+	{ 'default:copper_lump'      , ''                         , 'default:copper_ingot'        ,  3 },
+	{ 'default:copper_lump'      , 'default:copper_lump'      , 'default:copper_ingot 2'      ,  6 },
+	{ 'group:copper_item'        , ''                         , 'default:copper_ingot'        ,  3 },
+	{ 'lottores:tin_lump'        , ''                         , 'lottores:tin_ingot'          ,  3 },
+	{ 'lottores:tin_lump'        , 'lottores:tin_lump'        , 'lottores:tin_ingot 2'        ,  6 },
+	{ 'group:tin_item'           , ''                         , 'lottores:tin_ingot'          ,  3 },
+	-- Level: 1
+	{ 'default:iron_lump'        , ''                         , 'default:steel_ingot'         ,  3 },
+	{ 'default:iron_lump'        , 'default:iron_lump'        , 'default:steel_ingot 2'       ,  6 },
+	{ 'group:steel_item'         , ''                         , 'default:steel_ingot'         ,  3 },
+	{ 'carts:rail'               , ''                         , 'default:steel_ingot'         ,  3 },
+	{ 'carts:powerrail'          , ''                         , 'default:steel_ingot'         ,  3 },
+	{ 'carts:brakerail'          , ''                         , 'default:steel_ingot'         ,  3 },
+	{ 'vessels:steel_bottle'     , ''                         , 'default:steel_ingot'         ,  3 },
+	{ 'lottores:tin_ingot'       , 'default:copper_ingot'     , 'default:bronze_ingot 2'      ,  6 },
+	{ 'default:copper_ingot'     , 'lottores:tin_ingot'       , 'default:bronze_ingot 2'      ,  6 },
+	{ 'group:bronze_item'        , ''                         , 'default:bronze_ingot'        ,  3 },
+	-- Level: 2
+	{ 'lottores:silver_lump'     , ''                         , 'lottores:silver_ingot'       ,  3 },
+	{ 'lottores:silver_lump'     , 'lottores:silver_lump'     , 'lottores:silver_ingot 2'     ,  6 },
+	{ 'group:silver_item'        , ''                         , 'lottores:silver_ingot'       ,  3 },
+	{ 'default:gold_lump'        , ''                         , 'default:gold_ingot'          ,  3 },
+	{ 'default:gold_lump'        , 'default:gold_lump'        , 'default:gold_ingot 2'        ,  6 },
+	{ 'group:gold_item'          , ''                         , 'default:gold_ingot'          ,  3 },
+	{ 'keys:key'                 , ''                         , 'default:gold_ingot'          ,  3 },
+	{ 'keys:skeleton_key'        , ''                         , 'default:gold_ingot'          ,  3 },
+	-- Level: 3
+	{ 'lottores:lead_lump'       , 'default:mese_crystal'     , 'lottores:galvorn_ingot'      , 18 },
+	{ 'group:galvorn_item'       , ''                         , 'lottores:galvorn_ingot'      , 18 },
+	{ 'lottores:lead_lump'       , ''                         , 'lottores:lead_ingot'         ,  3 },
+	{ 'lottores:lead_lump'       , 'lottores:lead_lump'       , 'lottores:lead_ingot 2'       ,  6 },
+	{ 'group:lead_item'          , ''                         , 'lottores:lead_ingot'         ,  3 },
+	-- Level: 3.2 (4?)
+	{ 'lottores:mithril_lump'    , ''                         , 'lottores:mithril_ingot'      ,  3 },
+	{ 'lottores:mithril_lump'    , 'lottores:mithril_lump'    , 'lottores:mithril_ingot 2'    ,  6 },
+	{ 'group:mithril_item'       , ''                         , 'lottores:mithril_ingot'      ,  3 },
+	{ 'lottother:ringsilver_lump', ''                         , 'lottother:ringsilver_ingot'  , 35 },
+	{ 'lottother:ringsilver_lump', 'lottother:ringsilver_lump', 'lottother:ringsilver_ingot 2', 70 },
 }
 
 return smelter_recipes
